2af532c2802f1afe1657da3a2d3753dda9959579
driedl <dave@riedl.ca>
2019-02-23 20:27:43 -0800

    added ResourceForm component

--- src/components/DynaForm/index.js
+++ src/components/DynaForm/index.js
@@ -1,62 +1,15 @@
 import { Component } from 'react';
 import { Form } from 'integrator-ui-forms/packages/core/dist';
-import { withStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import DynaSubmit from '../../components/DynaForm/DynaSubmit';
 import getRenderer from './renderer';
 
-@withStyles(theme => ({
-  actions: {
-    textAlign: 'right',
-  },
-  actionButton: {
-    marginTop: theme.spacing.double,
-    marginLeft: theme.spacing.double,
-  },
-}))
-export default class CustomForms extends Component {
-  state = {
-    formKey: 1,
-  };
-
-  handleCancel = () => {
-    // TODO: We need to re-mount the react-forms-processor Form component...
-    // is there a better way than uing the "key" prop trick?
-    const formKey = this.state.formKey + 1;
-
-    // console.log('bumping form key to force remount: ', formKey);
-    this.setState({
-      formKey,
-    });
-  };
-
+export default class DynaForm extends Component {
   render() {
-    const { classes, children, handleSubmit, ...rest } = this.props;
-    const { formKey } = this.state;
+    const { children, ...rest } = this.props;
     const renderer = getRenderer();
 
     return (
-      <Form key={formKey} {...rest} renderer={renderer}>
+      <Form {...rest} renderer={renderer}>
         {children}
-        {true && (
-          <div className={classes.actions}>
-            <Button
-              onClick={this.handleCancel}
-              className={classes.actionButton}
-              size="small"
-              variant="contained">
-              Cancel
-            </Button>
-            <DynaSubmit
-              onClick={handleSubmit}
-              className={classes.actionButton}
-              size="small"
-              variant="contained"
-              color="secondary">
-              Save
-            </DynaSubmit>
-          </div>
-        )}
       </Form>
     );
   }
