7fd42bc44bcf635bde7f95e4a21fc849055996ed
Shiva Alluri <alluriprasad@gmail.com>
2019-05-07 16:47:47 +0530

    test cases for tiles selector

--- src/reducers/test.js
+++ src/reducers/test.js
@@ -2,6 +2,12 @@
 import { advanceBy, advanceTo, clear } from 'jest-date-mock';
 import reducer, * as selectors from './';
 import actions from '../actions';
+import {
+  ACCOUNT_IDS,
+  USER_ACCESS_LEVELS,
+  INTEGRATION_ACCESS_LEVELS,
+  TILE_STATUS,
+} from '../utils/constants';
 
 describe('global selectors', () => {
   describe(`isProfileDataReady`, () => {
@@ -260,5 +266,490 @@ describe('Comm selector to verify comms exceeding threshold', () => {
 
     expect(selectors.isAllLoadingCommsAboveThresold(state)).toBe(true);
     clear();
+  });
+});
+
+describe('tiles', () => {
+  const published = [
+    {
+      _id: 'connector1',
+      name: 'Connector 1',
+      user: { name: 'User 1', company: 'Company 1' },
+    },
+    {
+      _id: 'connector2',
+      name: 'Connector 2',
+      user: { name: 'User 2', company: 'Company 2' },
+    },
+  ];
+  const integrations = [
+    {
+      _id: 'integration1',
+    },
+    {
+      _id: 'integration2',
+    },
+    {
+      _id: 'integration3',
+    },
+    {
+      _id: 'integration4',
+    },
+    {
+      _id: 'integration5',
+    },
+    {
+      _id: 'integration6',
+      mode: 'settings',
+    },
+    {
+      _id: 'integration7',
+      mode: 'settings',
+    },
+    {
+      _id: 'integration8',
+      mode: 'install',
+    },
+  ];
+  const tilesCollection = [
+    {
+      _integrationId: 'integration1',
+      name: 'Integration One',
+      numError: 0,
+      numFlows: 1,
+    },
+    {
+      _integrationId: 'integration2',
+      name: 'Integration Two',
+      numError: 10,
+      numFlows: 2,
+    },
+    {
+      _integrationId: 'integration3',
+      name: 'Integration Three',
+      numError: 10,
+      numFlows: 3,
+    },
+    {
+      _integrationId: 'integration4',
+      name: 'Integration Four',
+      numError: 10,
+      offlineConnections: ['conn1', 'conn2'],
+      numFlows: 4,
+    },
+    {
+      _integrationId: 'integration5',
+      name: 'Integration Five',
+      numError: 0,
+      offlineConnections: ['conn1', 'conn2'],
+      numFlows: 5,
+    },
+    {
+      _integrationId: 'integration6',
+      _connectorId: 'connector1',
+      name: 'Connector 1',
+      numFlows: 6,
+    },
+    {
+      _integrationId: 'integration7',
+      _connectorId: 'connector1',
+      tag: 'tag 1',
+      name: 'Connector 1',
+      numError: 10,
+      numFlows: 7,
+    },
+    {
+      _integrationId: 'integration8',
+      _connectorId: 'connector2',
+      name: 'Connector 2',
+      numFlows: 7,
+    },
+  ];
+  const standaloneTiles = [
+    {
+      _integrationId: 'none',
+      name: 'Standalone Flows',
+      numError: 0,
+      offlineConnections: ['conn1', 'conn2'],
+      numFlows: 5,
+    },
+  ];
+
+  test('should return correct tiles info for account owner', () => {
+    const state = reducer(
+      {
+        user: {
+          profile: {},
+          preferences: { defaultAShareId: ACCOUNT_IDS.OWN },
+          org: {
+            accounts: [
+              {
+                _id: ACCOUNT_IDS.OWN,
+                accessLevel: USER_ACCESS_LEVELS.ACCOUNT_OWNER,
+              },
+            ],
+            users: [],
+          },
+        },
+        data: {
+          published,
+          integrations,
+        },
+      },
+      actions.resource.receivedCollection('tiles', [
+        ...standaloneTiles,
+        ...tilesCollection,
+      ])
+    );
+    const tiles = selectors.tiles(state);
+    const expectedIntegrationPermissions = {
+      accessLevel: INTEGRATION_ACCESS_LEVELS.OWNER,
+      connections: {
+        edit: true,
+      },
+    };
+    const expected = [
+      {
+        _integrationId: 'none',
+        name: 'Standalone Flows',
+        numError: 0,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 5,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration1',
+        name: 'Integration One',
+        numError: 0,
+        numFlows: 1,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.SUCCESS,
+      },
+      {
+        _integrationId: 'integration2',
+        name: 'Integration Two',
+        numError: 10,
+        numFlows: 2,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration3',
+        name: 'Integration Three',
+        numError: 10,
+        numFlows: 3,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration4',
+        name: 'Integration Four',
+        numError: 10,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 4,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration5',
+        name: 'Integration Five',
+        numError: 0,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 5,
+        integration: {
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration6',
+        _connectorId: 'connector1',
+        name: 'Connector 1',
+        numFlows: 6,
+        connector: {
+          owner: 'Company 1',
+        },
+        integration: {
+          mode: 'settings',
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.SUCCESS,
+      },
+      {
+        _integrationId: 'integration7',
+        _connectorId: 'connector1',
+        tag: 'tag 1',
+        name: 'Connector 1',
+        numError: 10,
+        numFlows: 7,
+        connector: {
+          owner: 'Company 1',
+        },
+        integration: {
+          mode: 'settings',
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration8',
+        _connectorId: 'connector2',
+        name: 'Connector 2',
+        numFlows: 7,
+        connector: {
+          owner: 'Company 2',
+        },
+        integration: {
+          mode: 'install',
+          permissions: expectedIntegrationPermissions,
+        },
+        status: TILE_STATUS.IS_PENDING_SETUP,
+      },
+    ];
+
+    expect(tiles).toEqual(expected);
+  });
+  test('should return correct tiles info for org users', () => {
+    const state = reducer(
+      {
+        user: {
+          profile: {},
+          preferences: { defaultAShareId: 'ashare_manage' },
+          org: {
+            accounts: [
+              {
+                _id: 'ashare_manage',
+                accessLevel: USER_ACCESS_LEVELS.ACCOUNT_MANAGE,
+              },
+              {
+                _id: 'ashare_monitor',
+                accessLevel: USER_ACCESS_LEVELS.ACCOUNT_MONITOR,
+              },
+              {
+                _id: 'ashare_tile',
+                integrationAccessLevel: [
+                  {
+                    _integrationId: 'integration1',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MANAGE,
+                  },
+                  {
+                    _integrationId: 'integration2',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MONITOR,
+                  },
+                  {
+                    _integrationId: 'integration3',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MANAGE,
+                  },
+                  {
+                    _integrationId: 'integration4',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MONITOR,
+                  },
+                  {
+                    _integrationId: 'integration5',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MANAGE,
+                  },
+                  {
+                    _integrationId: 'integration6',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MONITOR,
+                  },
+                  {
+                    _integrationId: 'integration7',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MANAGE,
+                  },
+                  {
+                    _integrationId: 'integration8',
+                    accessLevel: INTEGRATION_ACCESS_LEVELS.MONITOR,
+                  },
+                ],
+              },
+            ],
+            users: [],
+          },
+        },
+        data: {
+          published,
+          integrations,
+        },
+      },
+      actions.resource.receivedCollection('tiles', [
+        ...standaloneTiles,
+        ...tilesCollection,
+      ])
+    );
+    const expected = [
+      {
+        _integrationId: 'none',
+        name: 'Standalone Flows',
+        numError: 0,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 5,
+        integration: {},
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration1',
+        name: 'Integration One',
+        numError: 0,
+        numFlows: 1,
+        integration: {},
+        status: TILE_STATUS.SUCCESS,
+      },
+      {
+        _integrationId: 'integration2',
+        name: 'Integration Two',
+        numError: 10,
+        numFlows: 2,
+        integration: {},
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration3',
+        name: 'Integration Three',
+        numError: 10,
+        numFlows: 3,
+        integration: {},
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration4',
+        name: 'Integration Four',
+        numError: 10,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 4,
+        integration: {},
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration5',
+        name: 'Integration Five',
+        numError: 0,
+        offlineConnections: ['conn1', 'conn2'],
+        numFlows: 5,
+        integration: {},
+        status: TILE_STATUS.HAS_OFFLINE_CONNECTIONS,
+      },
+      {
+        _integrationId: 'integration6',
+        _connectorId: 'connector1',
+        name: 'Connector 1',
+        numFlows: 6,
+        connector: {
+          owner: 'Company 1',
+        },
+        integration: { mode: 'settings' },
+        status: TILE_STATUS.SUCCESS,
+      },
+      {
+        _integrationId: 'integration7',
+        _connectorId: 'connector1',
+        tag: 'tag 1',
+        name: 'Connector 1',
+        numError: 10,
+        numFlows: 7,
+        connector: {
+          owner: 'Company 1',
+        },
+        integration: { mode: 'settings' },
+        status: TILE_STATUS.HAS_ERRORS,
+      },
+      {
+        _integrationId: 'integration8',
+        _connectorId: 'connector2',
+        name: 'Connector 2',
+        numFlows: 7,
+        connector: {
+          owner: 'Company 2',
+        },
+        integration: { mode: 'install' },
+        status: TILE_STATUS.IS_PENDING_SETUP,
+      },
+    ];
+    const expectedIntegrationPermissions = {
+      manage: {
+        accessLevel: INTEGRATION_ACCESS_LEVELS.MANAGE,
+        connections: {
+          edit: true,
+        },
+      },
+      monitor: {
+        accessLevel: INTEGRATION_ACCESS_LEVELS.MONITOR,
+        connections: {
+          edit: undefined,
+        },
+      },
+    };
+
+    expected.forEach(t => {
+      // eslint-disable-next-line no-param-reassign
+      t.integration.permissions = expectedIntegrationPermissions.manage;
+    });
+
+    const tilesForManageUser = selectors.tiles(state);
+
+    expect(tilesForManageUser).toEqual(expected);
+
+    const stateForMonitor = reducer(
+      state,
+      actions.user.preferences.update({
+        defaultAShareId: 'ashare_monitor',
+      })
+    );
+
+    expected.forEach(t => {
+      // eslint-disable-next-line no-param-reassign
+      t.integration.permissions = expectedIntegrationPermissions.monitor;
+    });
+    const tilesForMonitorUser = selectors.tiles(stateForMonitor);
+
+    expect(tilesForMonitorUser).toEqual(expected);
+
+    let stateForTileAccess = reducer(
+      state,
+      actions.user.preferences.update({
+        defaultAShareId: 'ashare_tile',
+      })
+    );
+
+    stateForTileAccess = reducer(
+      stateForTileAccess,
+      actions.resource.receivedCollection('tiles', tilesCollection)
+    );
+
+    let integrationPermissions;
+
+    expected.forEach(t => {
+      if (
+        [
+          'integration1',
+          'integration3',
+          'integration5',
+          'integration7',
+        ].includes(t._integrationId)
+      ) {
+        integrationPermissions = expectedIntegrationPermissions.manage;
+      } else {
+        integrationPermissions = expectedIntegrationPermissions.monitor;
+      }
+
+      // eslint-disable-next-line no-param-reassign
+      t.integration.permissions = integrationPermissions;
+    });
+    const expectedForTileLevelAccessUser = expected.filter(
+      t => t._integrationId !== 'none'
+    );
+    const tilesForTileLevelAccessUser = selectors.tiles(stateForTileAccess);
+
+    expect(tilesForTileLevelAccessUser).toEqual(expectedForTileLevelAccessUser);
   });
 });
