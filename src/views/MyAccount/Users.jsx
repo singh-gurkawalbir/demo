cb9974e45cc25cd68c958a75ccadf49e7b3c7f86
Shiva Alluri <alluriprasad@gmail.com>
2019-05-22 19:41:43 +0530

    removing unnecessary code

--- src/views/MyAccount/Users.jsx
+++ src/views/MyAccount/Users.jsx
@@ -1,20 +1,8 @@
 import { Component } from 'react';
-import CheckPermissions from '../../components/CheckPermissions';
-import { PERMISSIONS } from '../../utils/constants';
 import UserList from '../../components/UserList';
 
 export default class Users extends Component {
-  /*
-  <UserList integrationId="5cc843dd5e1d7f53a12b1cb9" />
-  <UserList integrationId="5c6e4fa95ab5733a65bb82e3" />
-   */
   render() {
-    return (
-      <CheckPermissions permission={PERMISSIONS.agents.view}>
-        <UserList />
-        <UserList integrationId="5cc843dd5e1d7f53a12b1cb9" />
-        <UserList integrationId="5c6e4fa95ab5733a65bb82e3" />
-      </CheckPermissions>
-    );
+    return <UserList />;
   }
 }
