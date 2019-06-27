a7c28396efb37aac6ccf1d172671610c990630d9
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2019-06-27 16:57:30 +0530

    removed unncessary code

--- src/forms/definitions/connections/custom/http/outreach.js
+++ src/forms/definitions/connections/custom/http/outreach.js
@@ -13,9 +13,6 @@ export default {
     '/http/auth/oauth/refreshTokenPath': 'refresh_token',
   }),
   fields: [
-    // Fields can be referenced by their fieldDefinition key, and the
-    // framework will fetch the missing values. Any values present in
-    // this file override the references field's props.
     { fieldId: 'name' },
     {
       fieldId: 'http.auth.oauth.scope',
