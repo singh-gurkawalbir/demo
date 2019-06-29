0fca3b76df458dc2f2acdcfc6c630b21bf1e66a8
driedl <dave@riedl.ca>
2019-06-29 13:35:47 -0400

    added example global CSS overides to mui components:

--- src/theme/defaults.js
+++ src/theme/defaults.js
@@ -26,4 +26,30 @@ export default {
     quad: unit * 4,
   },
   drawerWidth: 320,
+
+  overrides: {
+    // Name of the component ⚛️ / style sheet
+    MuiButton: {
+      // Name of the rule (see https://material-ui.com/api/button/#css) for all rules.
+      root: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+      },
+      text: {
+        // Some CSS
+        borderRadius: 3,
+        border: 0,
+        color: 'white',
+        height: 48,
+        padding: '0 30px',
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+      },
+    },
+  },
+  props: {
+    // Name of the component ⚛️
+    MuiButtonBase: {
+      // The default props to change
+      variant: 'contained',
+    },
+  },
 };
