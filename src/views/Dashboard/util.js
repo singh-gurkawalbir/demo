c276bd300c3c6b28a648b636dfbe747baf66bcba
Shiva Alluri <alluriprasad@gmail.com>
2019-07-19 12:55:13 +0530

    minor fix

--- src/views/Dashboard/util.js
+++ src/views/Dashboard/util.js
@@ -1,12 +1,10 @@
 import { sortBy } from 'lodash';
 
-export default function sortTiles(tiles, tilesOrder = []) {
+export default function sortTiles(tiles = [], tilesOrder = []) {
   let maxIndex = Math.max(tiles.length, tilesOrder.length);
-  let tileId;
-  let tileIndex;
   let tilesWithOrder = tiles.map(t => {
-    tileId = t._id || t._integrationId;
-    tileIndex = tilesOrder.indexOf(tileId);
+    const tileId = t._id || t._integrationId;
+    let tileIndex = tilesOrder.indexOf(tileId);
 
     if (tileIndex === -1) {
       maxIndex += 1;
