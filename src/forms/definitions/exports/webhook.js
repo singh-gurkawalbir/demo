1ec4f879e405f2e4dbfa35fa202f445e5a7b21ac
mounika-aetukuri <mounika.245114@gmail.com>
2019-08-05 12:49:10 +0530

    webhook forms

--- src/forms/definitions/exports/webhook.js
+++ src/forms/definitions/exports/webhook.js
@@ -1,15 +1,38 @@
 export default {
   fields: [
+    { fieldId: 'security' },
     { fieldId: 'webhook.provider' },
     { fieldId: 'webhook.verify' },
-    { fieldId: 'webhook.token' },
-    { fieldId: 'webhook.path' },
     { fieldId: 'webhook.algorithm' },
     { fieldId: 'webhook.encoding' },
     { fieldId: 'webhook.key' },
     { fieldId: 'webhook.header' },
+    { fieldId: 'webhook.token' },
+    { fieldId: 'webhook.path' },
     { fieldId: 'webhook.username' },
     { fieldId: 'webhook.password' },
   ],
-  fieldSets: [],
+  fieldSets: [
+    {
+      header: 'Would you like to transform the records?',
+      collapsed: false,
+      fields: [{ fieldId: 'transform.expression.rules' }],
+    },
+    {
+      header: 'Hooks (Optional, Developers Only)',
+      collapsed: false,
+      fields: [{ formId: 'hooks' }],
+    },
+    {
+      header: 'Advanced Settings',
+      collapsed: true,
+      fields: [
+        {
+          fieldId: 'dataURITemplate',
+          type: 'relativeuri',
+          label: 'Data URITemplate',
+        },
+      ],
+    },
+  ],
 };
