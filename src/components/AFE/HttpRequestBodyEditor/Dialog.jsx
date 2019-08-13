fee431f1711bfd8e75e901314867dd0834384e93
adibjp <adibjp441@gmail.com>
2019-08-14 01:51:09 +0530

    IO-9885 - Converting HTTP Request Body editor from class based implementation to function based implementation using hooks

--- src/components/AFE/HttpRequestBodyEditor/Dialog.jsx
+++ src/components/AFE/HttpRequestBodyEditor/Dialog.jsx
@@ -1,30 +1,34 @@
-import { Component } from 'react';
 import EditorDialog from '../EditorDialog';
 import HttpRequestBodyEditor from './';
 
-export default class HttpRequestBodyDialog extends Component {
-  render() {
-    const {
-      id,
-      rule,
-      data,
-      onClose,
-      open = true,
-      title,
-      width = '85vw',
-      height = '60vh',
-    } = this.props;
+export default function HttpRequestBodyDialog(props) {
+  const {
+    id,
+    rule,
+    data,
+    onClose,
+    open = true,
+    layout = 'row',
+    title,
+    width = '85vw',
+    height = '60vh',
+  } = props;
 
-    return (
-      <EditorDialog
-        id={id}
-        open={open}
-        title={title}
-        width={width}
-        height={height}
-        onClose={onClose}>
-        <HttpRequestBodyEditor editorId={id} rule={rule} data={data} />
-      </EditorDialog>
-    );
-  }
+  return (
+    <EditorDialog
+      id={id}
+      open={open}
+      title={title}
+      layout={layout}
+      width={width}
+      height={height}
+      onClose={onClose}>
+      <HttpRequestBodyEditor
+        layout={layout}
+        editorId={id}
+        rule={rule}
+        data={data}
+      />
+    </EditorDialog>
+  );
 }
