b53ce6c07cd53fda7b3f2b28b145684051057922
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2019-08-13 21:44:34 +0530

    Dyna Script content recursive call fix

--- src/components/DynaForm/fields/DynaScriptContent.jsx
+++ src/components/DynaForm/fields/DynaScriptContent.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
@@ -35,9 +35,18 @@ export default function DynaScriptContent(props) {
     }
   }, [dispatch, resourceId, scriptContent]);
 
+  const [valueChanged, setValueChanged] = useState(false);
+
   useEffect(() => {
-    onFieldChange(id, scriptContent);
-  }, [id, onFieldChange, scriptContent]);
+    setValueChanged(true);
+  }, [scriptContent]);
+  // onFieldChange is a bound function and causing endless recursive calls
+  useEffect(() => {
+    if (valueChanged) {
+      onFieldChange(id, scriptContent);
+      setValueChanged(false);
+    }
+  }, [id, onFieldChange, scriptContent, valueChanged]);
 
   if (scriptContent === undefined) {
     return <Typography>Loading Script...</Typography>;
