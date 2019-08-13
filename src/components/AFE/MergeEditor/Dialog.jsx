6cf185c9e1a04f8f1ebdc8606274a45d03710ad8
adibjp <adibjp441@gmail.com>
2019-08-14 01:40:40 +0530

    IO-9885 - Converting Merge editor from class based implementation to function based implementation using hooks

--- src/components/AFE/MergeEditor/Dialog.jsx
+++ src/components/AFE/MergeEditor/Dialog.jsx
@@ -1,21 +1,30 @@
-import { Component } from 'react';
 import EditorDialog from '../EditorDialog';
 import MergeEditor from './';
 
-export default class MergeEditorDialog extends Component {
-  render() {
-    const defaults = {
-      layout: 'column',
-      width: '80vw',
-      height: '50vh',
-      open: true,
-    };
-    const { id, rule, data, ...rest } = this.props;
+export default function MergeEditorDialog(props) {
+  const {
+    id,
+    rule,
+    data,
+    layout = 'column',
+    title,
+    onClose,
+    width = '80vw',
+    height = '50vh',
+    open = true,
+  } = props;
 
-    return (
-      <EditorDialog id={id} {...defaults} {...rest}>
-        <MergeEditor editorId={id} rule={rule} data={data} />
-      </EditorDialog>
-    );
-  }
+  return (
+    <EditorDialog
+      id={id}
+      open={open}
+      title={title}
+      layout={layout}
+      width={width}
+      height={height}
+      onClose={onClose}
+      showFullScreen>
+      <MergeEditor layout={layout} editorId={id} rule={rule} data={data} />
+    </EditorDialog>
+  );
 }
--- src/components/AFE/MergeEditor/index.jsx
+++ src/components/AFE/MergeEditor/index.jsx
@@ -1,52 +1,50 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import actions from '../../../actions';
 import * as selectors from '../../../reducers';
 import Editor from '../GenericEditor';
 
-const mapStateToProps = (state, { editorId }) => {
-  const editor = selectors.editor(state, editorId);
-
-  return { ...editor };
-};
-
-const mapDispatchToProps = (dispatch, { editorId, rule, data }) => ({
-  handleRuleChange: rule => {
+export default function MergeEditor(props) {
+  const { editorId, layout = 'column' } = props;
+  const { data, result, rule, error, violations } = useSelector(state =>
+    selectors.editor(state, editorId)
+  );
+  // const parsedData = result && result.data && result.data[0];
+  const dispatch = useDispatch();
+  const handleRuleChange = rule => {
     dispatch(actions.editor.patch(editorId, { rule }));
-  },
-  handleDataChange: data => {
+  };
+
+  const handleDataChange = data => {
     dispatch(actions.editor.patch(editorId, { data }));
-  },
-  handleInit: () => {
+  };
+
+  const handleInit = () => {
     dispatch(
       actions.editor.init(editorId, 'merge', {
-        rule,
-        data,
+        rule: props.rule,
+        data: props.data,
       })
     );
-  },
-});
+  };
 
-class MergeEditor extends Component {
-  render() {
-    const { result, ...other } = this.props;
-    const parsedData = result && result.data && result.data[0];
-
-    return (
-      <Editor
-        result={parsedData}
-        {...other}
-        processor="merge"
-        ruleMode="json"
-        dataMode="json"
-        resultMode="json"
-        ruleTitle="Default Object"
-        dataTitle="Merge Target"
-        resultTitle="Final Object"
-      />
-    );
-  }
+  return (
+    <Editor
+      data={data}
+      rule={rule}
+      error={error}
+      violations={violations}
+      result={result && result.data ? result.data[0] : ''}
+      processor="merge"
+      ruleMode="json"
+      layout={layout}
+      dataMode="json"
+      resultMode="json"
+      handleRuleChange={handleRuleChange}
+      handleDataChange={handleDataChange}
+      handleInit={handleInit}
+      ruleTitle="Default Object"
+      dataTitle="Merge Target"
+      resultTitle="Final Object"
+    />
+  );
 }
-
-// prettier-ignore
-export default connect(mapStateToProps, mapDispatchToProps)(MergeEditor);
