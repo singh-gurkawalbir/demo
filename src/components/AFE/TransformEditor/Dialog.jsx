0cfdd322ed2cd00f4e914ddca52f389b5e958950
adibjp <adibjp441@gmail.com>
2019-08-14 12:40:23 +0530

    IO-9885 - Converting Transform editor from class based implementation to function based implementation using hooks

--- src/components/AFE/TransformEditor/Dialog.jsx
+++ src/components/AFE/TransformEditor/Dialog.jsx
@@ -1,22 +1,30 @@
-import { Component } from 'react';
 import EditorDialog from '../EditorDialog';
 import TransformEditor from './';
 
-export default class TransformEditorDialog extends Component {
-  render() {
-    const { id, rule, data, ...other } = this.props;
-    const defaults = {
-      layout: 'column',
-      width: '85vw',
-      height: '60vh',
-      open: true,
-    };
-    const dialogProps = { ...defaults, ...other };
+export default function TransformEditorDialog(props) {
+  const {
+    id,
+    rule,
+    data,
+    title,
+    onClose,
+    layout = 'column',
+    width = '85vw',
+    height = '60vh',
+    open = true,
+  } = props;
 
-    return (
-      <EditorDialog {...dialogProps} showLayoutOptions={false}>
-        <TransformEditor editorId={id} rule={rule} data={data} />
-      </EditorDialog>
-    );
-  }
+  return (
+    <EditorDialog
+      id={id}
+      open={open}
+      title={title}
+      layout={layout}
+      width={width}
+      height={height}
+      onClose={onClose}
+      showLayoutOptions={false}>
+      <TransformEditor editorId={id} rule={rule} data={data} />
+    </EditorDialog>
+  );
 }
