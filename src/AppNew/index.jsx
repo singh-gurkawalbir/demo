757f612a96d14fa4814bd36a6cae9071bd1f7b09
driedl <dave@riedl.ca>
2019-08-18 07:56:29 -0700

    more refactoring of 'NewApp' component

--- src/AppNew/index.jsx
+++ src/AppNew/index.jsx
@@ -1,80 +1,23 @@
 import React from 'react';
 import { MuiThemeProvider, makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import IconButton from '@material-ui/core/IconButton';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import MenuIcon from '@material-ui/icons/Menu';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import { Divider } from '@material-ui/core';
-import clsx from 'clsx';
 import FontStager from '../components/FontStager';
 import themeProvider from './themeProvider';
 import themeProviderOld from '../theme/themeProvider';
 import CeligoAppBar from './CeligoAppBar';
+import CeligoDrawer from './CeligoDrawer';
 import PageContent from './PageContent';
 
-const drawerWidth = 240;
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles({
   root: {
     display: 'flex',
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-    whiteSpace: 'nowrap',
-    '& > div > div': {
-      minHeight: 10,
-    },
-  },
-  drawerOpen: {
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(5) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(8) + 1,
-    },
-  },
-  drawerPaper: {
-    width: drawerWidth, // drawerWidth needs to be in our design token.
-  },
-  toolbar: theme.mixins.toolbar,
-
-  menuContainer: {
-    height: '100%',
-    display: 'grid',
-    gridTemplateRows: '40px auto 80px',
-    '& > div:first-child': {
-      margin: theme.spacing(1, 0, 0, 1),
-    },
-    '& > div:last-child': {
-      alignSelf: 'end',
-      margin: theme.spacing(0, 0, 2, 1),
-    },
-  },
-}));
+});
 const theme = themeProvider();
-const oldTheme = themeProviderOld('light');
+// const oldTheme = themeProviderOld('light');
 
 console.log('new theme', theme);
-console.log('old theme', oldTheme);
+// console.log('old theme', oldTheme);
 
 export default function AppNew() {
   const classes = useStyles();
@@ -85,77 +28,14 @@ export default function AppNew() {
   }
 
   return (
-    <MuiThemeProvider theme={oldTheme}>
+    <MuiThemeProvider theme={theme}>
       <FontStager />
       <CssBaseline />
 
       <div className={classes.root}>
         <CeligoAppBar shift={open} />
-
-        <Drawer
-          variant="permanent"
-          anchor="left"
-          className={clsx(classes.drawer, {
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          })}
-          classes={{
-            paper: clsx({
-              [classes.drawerOpen]: open,
-              [classes.drawerClose]: !open,
-            }),
-          }}
-          open={open}>
-          <div className={classes.toolbar} />
-
-          <div className={classes.menuContainer}>
-            <div>
-              <IconButton
-                // color="inherit"
-                aria-label="open drawer"
-                onClick={handleDrawerClick}
-                // edge="start"
-                // className={clsx(classes.menuButton, {
-                //   [classes.hide]: open,
-                // })}
-              >
-                <MenuIcon />
-              </IconButton>
-            </div>
-            <div>
-              <List>
-                {['Inbox', 'Starred', 'Send email', 'Drafts'].map(
-                  (text, index) => (
-                    <ListItem button key={text}>
-                      <ListItemIcon>
-                        {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                      </ListItemIcon>
-                      <ListItemText primary={text} />
-                    </ListItem>
-                  )
-                )}
-              </List>
-            </div>
-            <div>
-              <Divider />
-              <div className={classes.toolbar}>
-                <IconButton onClick={handleDrawerClick}>
-                  {open ? <ChevronLeftIcon /> : <ChevronRightIcon />}
-                </IconButton>
-              </div>
-              <IconButton
-                edge="end"
-                aria-label="account of current user"
-                aria-controls="menuId"
-                aria-haspopup="true"
-                color="inherit">
-                <AccountCircle />
-              </IconButton>
-            </div>
-          </div>
-        </Drawer>
-
-        <PageContent />
+        <CeligoDrawer onClick={handleDrawerClick} open={open} />
+        <PageContent shift={open} />
       </div>
     </MuiThemeProvider>
   );
