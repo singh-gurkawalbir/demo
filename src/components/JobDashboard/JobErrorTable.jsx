eacd0a961779c63eab14c80eb45e8c518e649b54
Shiva Alluri <alluriprasad@gmail.com>
2019-08-21 14:59:48 +0530

    minor fix

--- src/components/JobDashboard/JobErrorTable.jsx
+++ src/components/JobDashboard/JobErrorTable.jsx
@@ -20,12 +20,12 @@ import Spinner from '../Spinner';
 const styles = theme => ({
   root: {
     width: '98%',
-    marginTop: theme.spacing.unit * 3,
-    marginLeft: theme.spacing.unit,
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
     overflowX: 'auto',
   },
   title: {
-    marginBottom: theme.spacing.unit * 2,
+    marginBottom: theme.spacing(2),
     float: 'left',
   },
   table: {
