5219bdcee7ffd7bd9e10c61e7c608842e337f806
Snigdha <snigdha@localhost.io>
2019-08-26 15:04:41 +0530

    IO-9742

--- src/forms/definitions/imports/as2.js
+++ src/forms/definitions/imports/as2.js
@@ -1,9 +1,42 @@
 export default {
   fields: [
-    { fieldId: 'as2.fileNameTemplate' },
-    { fieldId: 'as2.messageIdTemplate' },
-    { fieldId: 'as2.maxRetries' },
+    { formId: 'common' },
+    {
+      id: 'importData',
+      type: 'labeltitle',
+      label: 'What would you like the data imported?',
+    },
+    { fieldId: 'as2.ediFormat' },
+    { fieldId: 'as2.fileName' },
+    { fieldId: 'as2.messageId' },
     { fieldId: 'as2.headers' },
+    {
+      id: 'dataMapped',
+      type: 'labeltitle',
+      label: 'How should the data be mapped?',
+    },
+    { fieldId: 'as2.parentOption' },
+    { fieldId: 'as2.childRecords' },
   ],
-  fieldSets: [],
+  fieldSets: [
+    {
+      header: 'Advanced',
+      collapsed: true,
+      fields: [
+        { fieldId: 'as2.compressFiles' },
+        { fieldId: 'as2.compressionFormat' },
+        { fieldId: 'as2.maxRetries' },
+      ],
+    },
+    {
+      header: 'Hooks (Optional, Developers Only)',
+      collapsed: false,
+      fields: [
+        { formId: 'hooks' },
+        { fieldId: 'hooks.postAggregate.function' },
+        { fieldId: 'hooks.postAggregate._scriptId' },
+        { fieldId: 'hooks.postAggregate._stackId' },
+      ],
+    },
+  ],
 };
--- src/forms/fieldDefinitions/resources/imports/as2.js
+++ src/forms/fieldDefinitions/resources/imports/as2.js
@@ -1,23 +1,130 @@
 export default {
-  'as2.fileNameTemplate': {
-    type: 'text',
-    label: 'As2 file Name Template',
+  'as2.ediFormat': {
+    type: 'select',
+    label: 'EDI Format',
+    options: [
+      {
+        items: [
+          { label: 'Generic 180', value: 'generic180' },
+          { label: 'Generic 850', value: 'generic850' },
+        ],
+      },
+    ],
   },
-  'as2.messageIdTemplate': {
+  'as2.fileName': {
     type: 'text',
-    label: 'As2 message Id Template',
+    label: 'File Name',
+  },
+  'as2.messageId': {
+    type: 'text',
+    label: 'Message Id',
+  },
+  'as2.headers': {
+    type: 'keyvalue',
+    label: 'Configure HTTP Headers',
+  },
+  'as2.parentOption': {
+    type: 'radiogroup',
+    label:
+      'Does each individual record being processed translate to multiple records in the import application?',
+    defaultValue: 'false',
+    options: [
+      {
+        items: [
+          { label: 'Yes(Advanced)', value: 'true' },
+          { label: 'No', value: 'false' },
+        ],
+      },
+    ],
+  },
+  'as2.childRecords': {
+    type: 'text',
+    label:
+      'if records being processed are represented by Objects then please specify the JSON path to be child records',
+    placeholder: 'Optional. Not needed for row/array formats.',
+    visibleWhen: [
+      {
+        field: 'as2.parentOption',
+        is: ['true'],
+      },
+    ],
+  },
+  'as2.compressFiles': {
+    type: 'checkbox',
+    label: 'Compress Files',
+  },
+  'as2.compressionFormat': {
+    type: 'select',
+    label: 'Compression Format',
+    options: [
+      {
+        items: [{ label: 'gzip', value: 'gzip' }],
+      },
+    ],
+    visibleWhen: [
+      {
+        field: 'as2.compressFiles',
+        is: [true],
+      },
+    ],
   },
   'as2.maxRetries': {
-    type: 'text',
-    label: 'As2 max Retries',
+    type: 'select',
+    label: 'Max Retries',
+    options: [
+      {
+        items: [
+          { label: '1', value: 1 },
+          { label: '2', value: 2 },
+          { label: '3', value: 3 },
+          { label: '4', value: 4 },
+          { label: '5', value: 5 },
+        ],
+      },
+    ],
     validWhen: [
       {
         matchesRegEx: { pattern: '^[\\d]+$', message: 'Only numbers allowed' },
       },
     ],
   },
-  'as2.headers[*].name': {
+  'hooks.postAggregate.function': {
     type: 'text',
-    label: 'As2 headers[*] name',
+    label: 'Post Aggregate',
+    placeholder: 'Function Name',
+    requiredWhen: [
+      {
+        field: 'hooks.postAggregate._scriptId',
+        isNot: [''],
+      },
+      {
+        field: 'hooks.postAggregate._stackId',
+        isNot: [''],
+      },
+    ],
+  },
+  'hooks.postAggregate._scriptId': {
+    type: 'selectresource',
+    resourceType: 'scripts',
+    placeholder: 'Please select a script',
+    label: 'Post Aggregate Script',
+    visibleWhen: [
+      {
+        field: 'hookType',
+        is: ['script'],
+      },
+    ],
+  },
+  'hooks.postAggregate._stackId': {
+    type: 'selectresource',
+    placeholder: 'Please select a stack',
+    resourceType: 'stacks',
+    label: 'Post Aggregate Stack Id',
+    visibleWhen: [
+      {
+        field: 'hookType',
+        is: ['stack'],
+      },
+    ],
   },
 };
