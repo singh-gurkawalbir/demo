84e27e82d3b33f7ca2ec00597569129183b3e26c
Snigdha <snigdha@localhost.io>
2019-08-26 15:11:20 +0530

    IO-9744

--- src/forms/definitions/imports/mongodb.js
+++ src/forms/definitions/imports/mongodb.js
@@ -1,13 +1,39 @@
 export default {
   fields: [
+    { formId: 'common' },
+    {
+      id: 'importData',
+      type: 'labeltitle',
+      label: 'What would you like the data imported?',
+    },
     { fieldId: 'mongodb.method' },
     { fieldId: 'mongodb.collection' },
+    { fieldId: 'mongodb.ignoreExisting' },
+    { fieldId: 'mongodb.identifyExistingRecords' },
+    { fieldId: 'mongodb.whichField' },
+    { fieldId: 'mongodb.ignoreLookupFilters' },
     { fieldId: 'mongodb.filter' },
-    { fieldId: 'mongodb.document' },
-    { fieldId: 'mongodb.update' },
     { fieldId: 'mongodb.upsert' },
-    { fieldId: 'mongodb.ignoreExtract' },
-    { fieldId: 'mongodb.ignoreLookupFilter' },
+    { fieldId: 'mongodb.ignoreMissing' },
+    { fieldId: 'mongodb.whichField?' },
+    {
+      id: 'dataMapped',
+      type: 'labeltitle',
+      label: 'How should the data be mapped?',
+    },
+    { fieldId: 'mongodb.parentOption' },
+    { fieldId: 'mongodb.childRecords' },
   ],
-  fieldSets: [],
+  fieldSets: [
+    {
+      header: 'Hooks (Optional, Developers Only)',
+      collapsed: false,
+      fields: [
+        { formId: 'hooks' },
+        { fieldId: 'hooks.postAggregate.function' },
+        { fieldId: 'hooks.postAggregate._scriptId' },
+        { fieldId: 'hooks.postAggregate._stackId' },
+      ],
+    },
+  ],
 };
--- src/forms/fieldDefinitions/resources/imports/mongodb.js
+++ src/forms/fieldDefinitions/resources/imports/mongodb.js
@@ -13,31 +13,170 @@ export default {
   },
   'mongodb.collection': {
     type: 'text',
-    label: 'Mongodb collection',
+    label: 'Collection',
+    visibleWhen: [
+      {
+        field: 'mongodb.method',
+        is: ['insertMany', 'updateOne'],
+      },
+    ],
+  },
+  'mongodb.ignoreExisting': {
+    type: 'checkbox',
+    label: 'Ignore Existing',
+    defaultValue: false,
+    visibleWhen: [
+      {
+        field: 'mongodb.method',
+        is: ['insertMany'],
+      },
+    ],
+  },
+  'mongodb.identifyExistingRecords': {
+    type: 'select',
+    label: 'How should we identify existing records?',
+    options: [
+      {
+        items: [
+          { label: 'Records have a specific field populated', value: 'source' },
+          { label: 'Run a dynamic search against Mongodb', value: 'lookup' },
+        ],
+      },
+    ],
+    visibleWhenAll: [
+      {
+        field: 'mongodb.ignoreExisting',
+        is: [true],
+      },
+      {
+        field: 'mongodb.method',
+        is: ['insertMany'],
+      },
+    ],
+  },
+  'mongodb.whichField': {
+    type: 'text',
+    label: 'which Field?',
+    visibleWhen: [
+      {
+        field: 'mongodb.identifyExistingRecords',
+        is: ['source'],
+      },
+    ],
+  },
+  'mongodb.ignoreLookupFilters': {
+    type: 'textarea',
+    label: 'Ignore Lookup Filters',
+    visibleWhen: [
+      {
+        field: 'mongodb.identifyExistingRecords',
+        is: ['lookup'],
+      },
+    ],
   },
   'mongodb.filter': {
-    type: 'text',
-    label: 'Mongodb filter',
-  },
-  'mongodb.document': {
-    type: 'text',
-    label: 'Mongodb document',
-  },
-  'mongodb.update': {
-    type: 'text',
-    label: 'Mongodb update',
+    type: 'textarea',
+    label: 'Filter',
+    visibleWhen: [
+      {
+        field: 'mongodb.method',
+        is: ['updateOne'],
+      },
+    ],
   },
   'mongodb.upsert': {
     type: 'checkbox',
-    label: 'Mongodb upsert',
-    defaultValue: false,
+    label: 'Upsert',
+    visibleWhen: [
+      {
+        field: 'mongodb.method',
+        is: ['updateOne'],
+      },
+    ],
   },
-  'mongodb.ignoreExtract': {
-    type: 'text',
-    label: 'Mongodb ignore Extract',
+  'mongodb.ignoreMissing': {
+    type: 'checkbox',
+    label: 'Ignore Missing',
+    visibleWhen: [
+      {
+        field: 'mongodb.method',
+        is: ['updateOne'],
+      },
+    ],
   },
-  'mongodb.ignoreLookupFilter': {
+  'mongodb.whichField?': {
     type: 'text',
-    label: 'Mongodb ignore Lookup Filter',
+    label: 'Which Field?',
+    visibleWhen: [
+      {
+        field: 'mongodb.ignoreMissing',
+        is: [true],
+      },
+    ],
+  },
+  'mongodb.parentOption': {
+    type: 'radiogroup',
+    label:
+      'Does each individual record being processed translate to multiple records in the import application?',
+    defaultValue: 'false',
+    options: [
+      {
+        items: [
+          { label: 'Yes(Advanced)', value: 'true' },
+          { label: 'No', value: 'false' },
+        ],
+      },
+    ],
+  },
+  'mongodb.childRecords': {
+    type: 'text',
+    label:
+      'if records being processed are represented by Objects then please specify the JSON path to be child records',
+    placeholder: 'Optional. Not needed for row/array formats.',
+    visibleWhen: [
+      {
+        field: 'mongodb.parentOption',
+        is: ['true'],
+      },
+    ],
+  },
+  'hooks.postAggregate.function': {
+    type: 'text',
+    label: 'Post Aggregate',
+    placeholder: 'Function Name',
+    requiredWhen: [
+      {
+        field: 'hooks.postAggregate._scriptId',
+        isNot: [''],
+      },
+      {
+        field: 'hooks.postAggregate._stackId',
+        isNot: [''],
+      },
+    ],
+  },
+  'hooks.postAggregate._scriptId': {
+    type: 'selectresource',
+    resourceType: 'scripts',
+    placeholder: 'Please select a script',
+    label: 'Post Aggregate Script',
+    visibleWhen: [
+      {
+        field: 'hookType',
+        is: ['script'],
+      },
+    ],
+  },
+  'hooks.postAggregate._stackId': {
+    type: 'selectresource',
+    placeholder: 'Please select a stack',
+    resourceType: 'stacks',
+    label: 'Post Aggregate Stack Id',
+    visibleWhen: [
+      {
+        field: 'hookType',
+        is: ['stack'],
+      },
+    ],
   },
 };
