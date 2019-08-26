7aeba776b6f19f1cdb2b34a592fe26e00369dfe2
Snigdha <snigdha@localhost.io>
2019-08-26 15:09:21 +0530

    IO-9749

--- src/forms/fieldDefinitions/resources/imports/ftp.js
+++ src/forms/fieldDefinitions/resources/imports/ftp.js
@@ -71,15 +71,16 @@ export default {
       },
     ],
   },
-  'ftp.advanceOption': {
+  'ftp.parentOption': {
     type: 'radiogroup',
     label:
       'Does each individual record being processed translate to multiple records in the import application?',
+    defaultValue: 'false',
     options: [
       {
         items: [
-          { label: 'Yes(Advanced)', value: 'Y' },
-          { label: 'No', value: 'N' },
+          { label: 'Yes(Advanced)', value: 'true' },
+          { label: 'No', value: 'false' },
         ],
       },
     ],
@@ -91,8 +92,8 @@ export default {
     placeholder: 'Optional. Not needed for row/array formats.',
     visibleWhen: [
       {
-        field: 'ftp.advanceOption',
-        is: ['Y'],
+        field: 'ftp.parentOption',
+        is: ['true'],
       },
     ],
   },
@@ -186,136 +187,6 @@ export default {
       {
         field: 'ftp.fileType',
         is: ['csv'],
-      },
-    ],
-  },
-  hookType: {
-    type: 'radiogroup',
-    label: 'Hook Type',
-    defaultValue: 'script',
-    options: [
-      {
-        items: [
-          { label: 'Script', value: 'script' },
-          { label: 'Stack', value: 'stack' },
-        ],
-      },
-    ],
-  },
-  'hooks.preMap.function': {
-    type: 'text',
-    label: 'Pre Map',
-    placeholder: 'Function Name',
-    requiredWhen: [
-      {
-        field: 'hooks.preMap._scriptId',
-        isNot: [''],
-      },
-      {
-        field: 'hooks.preMap._stackId',
-        isNot: [''],
-      },
-    ],
-  },
-  'hooks.preMap._scriptId': {
-    type: 'selectresource',
-    placeholder: 'Please select a script',
-    resourceType: 'scripts',
-    label: 'Pre Map Script',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['script'],
-      },
-    ],
-  },
-  'hooks.preMap._stackId': {
-    type: 'selectresource',
-    placeholder: 'Please select a stack',
-    resourceType: 'stacks',
-    label: 'Pre Map Stack',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['stack'],
-      },
-    ],
-  },
-  'hooks.postMap.function': {
-    type: 'text',
-    label: 'Post Map',
-    placeholder: 'Function Name',
-    requiredWhen: [
-      {
-        field: 'hooks.postMap._scriptId',
-        isNot: [''],
-      },
-      {
-        field: 'hooks.postMap._stackId',
-        isNot: [''],
-      },
-    ],
-  },
-  'hooks.postMap._scriptId': {
-    type: 'selectresource',
-    placeholder: 'Please select a script',
-    resourceType: 'scripts',
-    label: 'Post Map Script',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['script'],
-      },
-    ],
-  },
-  'hooks.postMap._stackId': {
-    type: 'selectresource',
-    placeholder: 'Please select a stack',
-    resourceType: 'stacks',
-    label: 'Post Map Stack',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['stack'],
-      },
-    ],
-  },
-  'hooks.postSubmit.function': {
-    type: 'text',
-    label: 'Post Submit',
-    placeholder: 'Function Name',
-    requiredWhen: [
-      {
-        field: 'hooks.postSubmit._scriptId',
-        isNot: [''],
-      },
-      {
-        field: 'hooks.postSubmit._stackId',
-        isNot: [''],
-      },
-    ],
-  },
-  'hooks.postSubmit._scriptId': {
-    type: 'selectresource',
-    resourceType: 'scripts',
-    placeholder: 'Please select a script',
-    label: 'Post Submit Script',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['script'],
-      },
-    ],
-  },
-  'hooks.postSubmit._stackId': {
-    type: 'selectresource',
-    placeholder: 'Please select a stack',
-    resourceType: 'stacks',
-    label: 'Post Submit Stack',
-    visibleWhen: [
-      {
-        field: 'hookType',
-        is: ['stack'],
       },
     ],
   },
