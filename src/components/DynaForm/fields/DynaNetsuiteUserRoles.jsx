4ec6010ef7d4f32cd2548dadd6316cc8085ca99c
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2019-08-27 15:57:47 +0530

    fixes...sorting options and autofiling options

--- src/components/DynaForm/fields/DynaNetsuiteUserRoles.jsx
+++ src/components/DynaForm/fields/DynaNetsuiteUserRoles.jsx
@@ -1,19 +1,27 @@
 import { useSelector } from 'react-redux';
-import { FieldWrapper } from 'react-forms-processor/dist';
-import { withStyles } from '@material-ui/core/styles';
+import { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import MaterialUiSelect from './DynaSelect';
 import * as selectors from '../../../reducers/index';
 
-const styles = () => ({
+const useStyles = makeStyles(() => ({
   root: {
     display: 'flex !important',
     flexWrap: 'nowrap',
   },
-});
+}));
 
-function DynaNetsuiteUserRolesOptions(props) {
-  const { options, resourceId, netsuiteResourceType } = props;
-  const { env, acc } = options;
+export default function DynaNetsuiteUserRolesOptions(props) {
+  const {
+    options,
+    resourceId,
+    netsuiteResourceType,
+    onFieldChange,
+    id,
+    value,
+  } = props;
+  const classes = useStyles();
+  const { env, acc } = options || {};
   const netSuiteUserRoles = useSelector(state =>
     selectors.netsuiteUserRoles(
       state,
@@ -24,20 +32,55 @@ function DynaNetsuiteUserRolesOptions(props) {
     )
   );
   const { optionsArr } = netSuiteUserRoles;
+  const matchingOption = (optionsArr || []).find(ele => ele.value === value);
+  const [valueChanged, setValueChanged] = useState(false);
+
+  useEffect(() => {
+    if (!valueChanged && !matchingOption) {
+      if (netsuiteResourceType === 'environment') {
+        if (optionsArr) {
+          if (optionsArr.length === 1) {
+            onFieldChange(id, optionsArr[0].value);
+          } else if (
+            optionsArr.length === 2 &&
+            optionsArr.find(ele => ele.value === 'beta')
+          ) {
+            const nonBetaOption = optionsArr.find(ele => ele.value !== 'beta');
+
+            onFieldChange(id, nonBetaOption.value);
+          }
+
+          setValueChanged(true);
+        }
+      }
+
+      if (
+        netsuiteResourceType === 'account' ||
+        netsuiteResourceType === 'role'
+      ) {
+        if (optionsArr) {
+          if (optionsArr.length === 1) {
+            onFieldChange(id, optionsArr[0].value);
+          }
+        }
+
+        setValueChanged(true);
+      }
+    }
+  }, [
+    id,
+    matchingOption,
+    netsuiteResourceType,
+    onFieldChange,
+    optionsArr,
+    valueChanged,
+  ]);
 
   return (
     <MaterialUiSelect
       {...props}
-      classes={props.classes}
+      classes={classes}
       options={[{ items: optionsArr || [] }]}
     />
   );
 }
-
-const DynaSelect = props => (
-  <FieldWrapper {...props}>
-    <DynaNetsuiteUserRolesOptions classes={props.classes} />
-  </FieldWrapper>
-);
-
-export default withStyles(styles)(DynaSelect);
