c1e021c974bcdec032905ff31b6e51c56973056d
Sravan Dandra <sravan.dandra@gmail.com>
2019-09-02 21:52:23 +0530

    Update test.js

--- src/reducers/session/connectors/test.js
+++ src/reducers/session/connectors/test.js
@@ -22,36 +22,91 @@ describe('Connector metadata', () => {
     expect(requestReducer).toEqual({ '1': { id: { isLoading: true } } });
   });
 
-  test('should show data for refreshed metadata', () => {
-    const data = {
-      id: 'account2',
-      name: 'Account2',
+  test('should store data received from  metadata request in the app redux store in expected format', () => {
+    const exampleResponse = {
+      id: 'account',
+      name: 'Account',
     };
+    const _integrationId = '123123';
     const requestState = reducer(
       undefined,
-      actions.metadata.request('id', 'rest.headers', '1')
+      actions.connectors.refreshMetadata('id', 'rest.headers', _integrationId)
     );
     const receivedState = reducer(
       requestState,
-      actions.connectors.receivedMetadata(data, 'rest.headers', 'id', '1')
+      actions.connectors.receivedMetadata(
+        exampleResponse,
+        'id',
+        'rest.headers',
+        _integrationId
+      )
     );
 
     expect(receivedState).toEqual({
-      '1': {
-        id: {
-          data,
+      '123123': {
+        'rest.headers': {
+          data: exampleResponse,
           isLoading: false,
         },
       },
     });
   });
 
-  test('should set isLoading to false when error occurs for refreshed metadata', () => {
+  test('should not clear the existing metadata when new metadata is added', () => {
+    const data = { dummy: 'data' };
+    const state = {};
+    const _integrationId = '123123';
+    const requestState = reducer(
+      state,
+      actions.connectors.refreshMetadata('id', 'rest.headers', _integrationId)
+    );
+    const receivedState1 = reducer(
+      requestState,
+      actions.connectors.receivedMetadata(
+        data,
+        'id',
+        'rest.headers',
+        _integrationId
+      )
+    );
+
+    expect(receivedState1).toEqual({
+      '123123': {
+        'rest.headers': { data: { dummy: 'data' }, isLoading: false },
+      },
+    });
+    const requestState2 = reducer(
+      requestState,
+      actions.connectors.refreshMetadata(
+        'id',
+        'rest.secondField',
+        _integrationId
+      )
+    );
+    const receivedState2 = reducer(
+      requestState2,
+      actions.connectors.receivedMetadata(
+        data,
+        'id',
+        'rest.secondField',
+        _integrationId
+      )
+    );
+
+    expect(receivedState2).toEqual({
+      '123123': {
+        'rest.headers': { data: { dummy: 'data' }, isLoading: false },
+        'rest.secondField': { data: { dummy: 'data' }, isLoading: false },
+      },
+    });
+  });
+
+  test('should set isLoading to false when error occurs for metadata request', () => {
     const type = 'rest.headers';
     const _integrationId = '123123';
     const requestState = reducer(
       undefined,
-      actions.metadata.request('id', 'rest.headers', '1')
+      actions.connectors.refreshMetadata('id', 'rest.headers', _integrationId)
     );
     const receivedState = reducer(
       requestState,
@@ -63,20 +118,81 @@ describe('Connector metadata', () => {
     });
   });
 
-  test('should clear the metadata when clearMetadata is called', () => {
-    const type = 'rest.headers';
-    const _integrationId = '123123';
-    const requestState = reducer(
-      undefined,
-      actions.metadata.request('id', 'rest.headers', '1')
-    );
-    const receivedState = reducer(
-      requestState,
-      actions.connectors.clearMetadata(type, _integrationId)
-    );
+  describe('Connector metadata request clear functionality', () => {
+    test('should clear the metadata when clearMetadata is called', () => {
+      const type = 'rest.headers';
+      const _integrationId = '123123';
+      const requestState = reducer(
+        undefined,
+        actions.connectors.refreshMetadata('id', 'rest.headers', _integrationId)
+      );
+      const receivedState = reducer(
+        requestState,
+        actions.connectors.clearMetadata(type, _integrationId)
+      );
 
-    expect(receivedState).toEqual({
-      '123123': { 'rest.headers': {} },
+      expect(receivedState).toEqual({
+        '123123': { 'rest.headers': {} },
+      });
+    });
+    test('should not clear other field metadata when clearMetadata is called for one field', () => {
+      const data = { dummy: 'data' };
+      const state = {};
+      const _integrationId = '123123';
+      const requestState = reducer(
+        state,
+        actions.connectors.refreshMetadata('id', 'rest.headers', _integrationId)
+      );
+      const receivedState1 = reducer(
+        requestState,
+        actions.connectors.receivedMetadata(
+          data,
+          'id',
+          'rest.headers',
+          _integrationId
+        )
+      );
+
+      expect(receivedState1).toEqual({
+        '123123': {
+          'rest.headers': { data: { dummy: 'data' }, isLoading: false },
+        },
+      });
+      const requestState2 = reducer(
+        requestState,
+        actions.connectors.refreshMetadata(
+          'id',
+          'rest.secondField',
+          _integrationId
+        )
+      );
+      const receivedState2 = reducer(
+        requestState2,
+        actions.connectors.receivedMetadata(
+          data,
+          'id',
+          'rest.secondField',
+          _integrationId
+        )
+      );
+
+      expect(receivedState2).toEqual({
+        '123123': {
+          'rest.headers': { data: { dummy: 'data' }, isLoading: false },
+          'rest.secondField': { data: { dummy: 'data' }, isLoading: false },
+        },
+      });
+      const receivedState = reducer(
+        requestState,
+        actions.connectors.clearMetadata('rest.headers', _integrationId)
+      );
+
+      expect(receivedState).toEqual({
+        '123123': {
+          'rest.headers': {},
+          'rest.secondField': { data: { dummy: 'data' }, isLoading: false },
+        },
+      });
     });
   });
 });
