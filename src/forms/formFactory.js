a60e9b4132cbcbaab91782ceed6dd895dafbdc74
mounika-aetukuri <mounika.245114@gmail.com>
2019-09-09 17:46:35 +0530

    removed console log

--- src/forms/formFactory.js
+++ src/forms/formFactory.js
@@ -67,7 +67,7 @@ const getResourceFormAssets = ({ resourceType, resource, isNew = false }) => {
       if (isNew) {
         meta = formMeta.connections.new;
       } else if (resource && resource.assistant) {
-        console.log('resource', type, resource.assistant);
+        // console.log('resource', type, resource.assistant);
         meta = formMeta.connections.custom[type];
         // console.log('type', type, resource.assistant);
 
