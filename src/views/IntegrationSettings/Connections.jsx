1bacdb78942c01b3d69efd7e61f972fe739b242d
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2019-09-10 16:03:27 +0530

    review comments

--- src/views/IntegrationSettings/Connections.jsx
+++ src/views/IntegrationSettings/Connections.jsx
@@ -6,42 +6,15 @@ import ResourceTable from '../ResourceList/ResourceTable';
 function Connections(props) {
   const { match } = props;
   const { integrationId } = match.params;
-  const connList = useSelector(state =>
-    selectors.resourceList(state, {
-      type: 'connections',
-    })
+  const list = useSelector(state =>
+    selectors.integrationConnectionList(state, integrationId)
   );
-  const integrationList = useSelector(state =>
-    selectors.resourceList(state, {
-      type: 'integrations',
-    })
-  );
-
-  if (integrationId && integrationId !== 'none') {
-    const integration =
-      integrationList &&
-      integrationList.resources &&
-      integrationList.resources.find(
-        integration => integration._id === integrationId
-      );
-    const registeredConnections =
-      integration && integration._registeredConnectionIds;
-
-    if (registeredConnections) {
-      connList.resources =
-        connList &&
-        connList.resources &&
-        connList.resources.filter(
-          conn => registeredConnections.indexOf(conn._id) >= 0
-        );
-    }
-  }
 
   return (
     <LoadResources required resources="connections">
       <ResourceTable
         resourceType="connections"
-        resources={connList && connList.resources}
+        resources={list && list.resources}
       />
     </LoadResources>
   );
