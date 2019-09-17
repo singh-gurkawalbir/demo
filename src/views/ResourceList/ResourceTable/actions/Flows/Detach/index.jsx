7c84ee59b253b2b7f04f4569880aa48f45fc25c1
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2019-09-17 12:38:26 +0530

    Datach Flow changes

--- src/views/ResourceList/ResourceTable/actions/Flows/Detach/index.jsx
+++ src/views/ResourceList/ResourceTable/actions/Flows/Detach/index.jsx
@@ -2,22 +2,47 @@ import { useDispatch } from 'react-redux';
 import { IconButton } from '@material-ui/core';
 import actions from '../../../../../../actions';
 import Icon from '../../../../../../components/icons/CloseIcon';
+import { confirmDialog } from '../../../../../../components/ConfirmDialog';
 
 export default {
   label: 'Detach Flow',
   component: function DetachFlow({ resource }) {
     const dispatch = useDispatch();
     const handleDetachFlow = () => {
-      const patchSet = [
-        {
-          op: 'replace',
-          path: '/_integrationId',
-          value: undefined,
-        },
-      ];
+      const message = [
+        'Are you sure you want to detach',
+        resource.name || resource._id,
+        'flow from this integration?',
+      ].join(' ');
 
-      dispatch(actions.resource.patchStaged(resource._id, patchSet, 'value'));
-      dispatch(actions.resource.commitStaged('flows', resource._id, 'value'));
+      confirmDialog({
+        title: 'Confirm',
+        message,
+        buttons: [
+          {
+            label: 'Cancel',
+          },
+          {
+            label: 'Yes',
+            onClick: () => {
+              const patchSet = [
+                {
+                  op: 'replace',
+                  path: '/_integrationId',
+                  value: undefined,
+                },
+              ];
+
+              dispatch(
+                actions.resource.patchStaged(resource._id, patchSet, 'value')
+              );
+              dispatch(
+                actions.resource.commitStaged('flows', resource._id, 'value')
+              );
+            },
+          },
+        ],
+      });
     };
 
     return (
