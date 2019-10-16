09d4a57722ee290999b59a81d7cd27455bb05997
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2019-10-16 13:37:03 +0530

    Enhanced description UI

--- src/views/IntegrationApps/Settings/Actions/Description/index.jsx
+++ src/views/IntegrationApps/Settings/Actions/Description/index.jsx
@@ -1,28 +1,77 @@
-import { IconButton } from '@material-ui/core';
-import CloseIcon from '../../../../../components/icons/CloseIcon';
-import { confirmDialog } from '../../../../../components/ConfirmDialog';
+import { Fragment, useState } from 'react';
+import { IconButton, Typography } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import ArrowPopper from '../../../../../components/ArrowPopper';
+import TooltipContent from '../../../../../components/TooltipContent';
+import InfoIcon from '../../../../../components/icons/InfoIcon';
+
+const useStyles = makeStyles(theme => ({
+  pageHeader: {
+    zIndex: theme.zIndex.appBar - 1,
+    padding: theme.spacing(3),
+    height: theme.pageBarHeight,
+    width: `calc(100% - ${theme.spacing(2 * 3) + 1}px)`,
+    position: 'fixed',
+  },
+  pageHeaderShift: {
+    width: `calc(100% - ${theme.drawerWidth - theme.spacing(2)}px)`,
+    transition: theme.transitions.create(['width', 'margin'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  pageBarOffset: { height: theme.pageBarHeight },
+  infoPopper: {
+    '& > div': {
+      maxWidth: 350,
+      textAlign: 'left',
+      ' & > p': {
+        textTransform: 'none',
+      },
+    },
+  },
+}));
 
 export default {
   label: 'Description',
   component: function Description({ resource }) {
-    const handleClick = () => {
-      const message = (resource && resource.description) || 'No description';
+    const classes = useStyles();
+    const theme = useTheme();
+    const infoText = resource.description || 'No description';
+    const [anchorEl, setAnchorEl] = useState(null);
 
-      confirmDialog({
-        message,
-        title: '',
-        buttons: [
-          {
-            label: 'Ok',
-          },
-        ],
-      });
-    };
+    function handleInfoOpen(event) {
+      setAnchorEl(event.currentTarget);
+    }
+
+    function handleInfoClose() {
+      setAnchorEl(null);
+    }
 
     return (
-      <IconButton size="small" onClick={handleClick}>
-        <CloseIcon />
-      </IconButton>
+      <Typography variant="h3">
+        {infoText && (
+          <Fragment>
+            <IconButton
+              size="small"
+              onClick={handleInfoOpen}
+              aria-owns={!anchorEl ? null : 'pageInfo'}
+              aria-haspopup="true">
+              <InfoIcon />
+            </IconButton>
+            <ArrowPopper
+              id="pageInfo"
+              className={classes.infoPopper}
+              zIndex={theme.zIndex.appBar + 1}
+              open={!!anchorEl}
+              anchorEl={anchorEl}
+              placement="right-start"
+              onClose={handleInfoClose}>
+              <TooltipContent>{infoText}</TooltipContent>
+            </ArrowPopper>
+          </Fragment>
+        )}
+      </Typography>
     );
   },
 };
