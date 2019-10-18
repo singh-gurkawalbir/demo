0ad405c7f1142bdd87f282c44b11b435a2565fb5
S Vamshi Krishna <vamshikrishnasaradhi@gmail.com>
2019-10-18 17:02:42 +0530

    form changes

--- src/forms/fieldDefinitions/resources/connectorLicense.js
+++ src/forms/fieldDefinitions/resources/connectorLicense.js
@@ -1,25 +1,19 @@
 import moment from 'moment';
+import { isNewId } from '../../../utils/resource';
 
 export default {
   expires: {
-    defaultValue: r => r.expires && moment(r.expires).format('MMMM Do YYYY'),
+    defaultValue: r => r.expires && moment(r.expires).format('L'),
     type: 'text',
     label: 'Expiration Date',
   },
-  created: {
-    defaultValue: r => r.created && moment(r.created).format('MMMM Do YYYY'),
-    type: 'text',
-    label: 'Created Date',
-    // visibleWhen: [{ field: 'user.email', isNot: [''] }],
-    // disabledWhen: [{ field: 'user.email', isNot: [''] }],
-  },
   opts: {
-    defaultValue: r => JSON.stringify(r.opts || {}),
-    type: 'text',
+    defaultValue: r => r.opts || {},
+    type: 'editor',
+    mode: 'json',
     label: 'Options',
   },
   sandbox: {
-    defaultValue: r => r.sandbox,
     type: 'checkbox',
     label: 'Sandbox',
   },
@@ -28,6 +22,6 @@ export default {
     type: 'text',
     label: 'Email',
     required: true,
-    // disabledWhen: [{ field: 'created', isNot: [''] }],
+    disableText: r => !isNewId(r._id),
   },
 };
