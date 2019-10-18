abd190f89515a40158e70aac82e6b26688bebf45
mounika-aetukuri <mounika.245114@gmail.com>
2019-10-18 16:32:42 +0530

    Minor changes

--- src/forms/definitions/exports/as2.js
+++ src/forms/definitions/exports/as2.js
@@ -1,4 +1,30 @@
 export default {
+  preSave: formValues => {
+    const newValues = formValues;
+
+    if (newValues['/ftp/entryParser'] === '') {
+      delete newValues['/ftp/entryParser'];
+    }
+
+    return newValues;
+  },
+  optionsHandler: (fieldId, fields) => {
+    if (fieldId === 'file.filedefinition.rules') {
+      // Fetch format specific Field Definition field to fetch id
+      // if (fileType.value === 'filedefinition')
+      const definitionFieldId = 'edix12.format';
+      const definition = fields.find(field => field.id === definitionFieldId);
+      const resourcePath = fields.find(
+        field => field.id === 'file.fileDefinition.resourcePath'
+      );
+
+      return {
+        format: definition && definition.format,
+        definitionId: definition && definition.value,
+        resourcePath: resourcePath && resourcePath.value,
+      };
+    }
+  },
   fieldMap: {
     common: { formId: 'common' },
     exportData: {
@@ -9,6 +35,12 @@ export default {
     'file.fileDefinition.resourcePath': {
       fieldId: 'file.fileDefinition.resourcePath',
     },
+    'edix12.format': {
+      fieldId: 'edix12.format',
+    },
+    'file.filedefinition.rules': {
+      fieldId: 'file.filedefinition.rules',
+    },
     rawData: { fieldId: 'rawData' },
     transform: { fieldId: 'transform' },
     hooks: { formId: 'hooks' },
@@ -18,7 +50,9 @@ export default {
     fields: [
       'common',
       'exportData',
+      'edix12.format',
       'file.fileDefinition.resourcePath',
+      'file.filedefinition.rules',
       'rawData',
     ],
     type: 'collapse',
