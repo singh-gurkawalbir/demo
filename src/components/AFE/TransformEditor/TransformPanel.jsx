3e53589f3750bafa398fa190beeb197904ba6018
adibjp <adibjp441@gmail.com>
2019-10-25 21:50:22 +0530

    fixes

--- src/components/AFE/TransformEditor/TransformPanel.jsx
+++ src/components/AFE/TransformEditor/TransformPanel.jsx
@@ -1,10 +1,8 @@
 import { useSelector, useDispatch } from 'react-redux';
-import Input from '@material-ui/core/Input';
 import { makeStyles } from '@material-ui/core/styles';
-import { IconButton } from '@material-ui/core';
 import actions from '../../../actions';
 import * as selectors from '../../../reducers';
-import CloseIcon from '../../icons/CloseIcon';
+import { KeyValueComponent } from '../../DynaForm/fields/DynaKeyValue';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -31,89 +29,16 @@ export default function TransformPanel(props) {
     dispatch(actions.editor.patch(editorId, { rule: value }));
   };
 
-  const handleDelete = row => () => {
-    const { rule } = editor;
-
-    if (rule[row]) {
-      rule.splice(row, 1);
-      patchEditor(rule);
-    }
-  };
-
-  const handleUpdate = (row, event, field) => {
-    const { value } = event.target;
-    let { rule } = editor;
-
-    if (!rule) rule = [];
-
-    event.preventDefault();
-    event.stopPropagation();
-    event.nativeEvent.stopImmediatePropagation();
-
-    if (row !== undefined) {
-      rule[row][field] = value;
-    } else {
-      rule.push({ [field]: value });
-    }
-
-    patchEditor(rule);
-  };
-
-  const rule = editor.rule
-    ? editor.rule.map((rule, index) => ({ row: index, ...rule }))
-    : [];
-  const handleExtractUpdate = row => event =>
-    handleUpdate(row, event, 'extract');
-  const handleGenerateUpdate = row => event =>
-    handleUpdate(row, event, 'generate');
-
   return (
-    <div className={classes.container}>
-      {rule.map(r => (
-        <div className={classes.rowContainer} key={r.row}>
-          <Input
-            autoFocus
-            defaultValue={r.extract}
-            placeholder="extract"
-            className={classes.input}
-            onChange={handleExtractUpdate(r.row)}
-          />
-          <Input
-            defaultValue={r.generate}
-            placeholder="generate"
-            className={classes.input}
-            onChange={handleGenerateUpdate(r.row)}
-          />
-          <IconButton
-            data-test="deleteTransformation"
-            aria-label="delete"
-            onClick={handleDelete(r.row)}
-            className={classes.margin}>
-            <CloseIcon />
-          </IconButton>
-        </div>
-      ))}
-      <div key="new" className={classes.rowContainer}>
-        <Input
-          value=""
-          placeholder="extract"
-          className={classes.input}
-          onChange={handleExtractUpdate()}
-        />
-        <Input
-          value=""
-          placeholder="generate"
-          className={classes.input}
-          onChange={handleGenerateUpdate()}
-        />
-        <IconButton
-          data-test="deleteTransformation"
-          aria-label="delete"
-          disabled
-          className={classes.margin}>
-          <CloseIcon />
-        </IconButton>
-      </div>
-    </div>
+    <KeyValueComponent
+      {...props}
+      dataTest={editorId}
+      onUpdate={patchEditor}
+      classes={classes}
+      showDelete
+      value={editor.rule}
+      keyName="extract"
+      valueName="generate"
+    />
   );
 }
