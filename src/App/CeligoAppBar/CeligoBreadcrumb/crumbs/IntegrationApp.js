b707a6a5f3583d47a0c364545053fddccb625515
Sravan Dandra <sravan.dandra@gmail.com>
2019-11-06 16:49:43 +0530

    Breadcrumbs

--- src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/IntegrationApp.js
+++ src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/IntegrationApp.js
@@ -1,19 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { isEmpty } from 'lodash';
 import { useSelector, useDispatch } from 'react-redux';
 import * as selectors from '../../../../reducers';
 import actions from '../../../../actions';
 
-export default function IntegrationAppCrumb({ integrationId }) {
+export const IntegrationAppCrumb = ({ integrationId }) => {
   const dispatch = useDispatch();
+  const [requested, setRequested] = useState(false);
   const integrationApp = useSelector(state =>
     selectors.resource(state, 'integrations', integrationId)
   );
 
   useEffect(() => {
-    if (!integrationApp) {
+    if (!integrationApp && !requested) {
       dispatch(actions.resource.requestCollection('integrations'));
+      setRequested(true);
     }
-  }, [dispatch, integrationApp]);
+  }, [dispatch, integrationApp, requested]);
 
   return integrationApp ? integrationApp.name : 'Integration App';
-}
+};
+
+export const StoreCrumb = ({ integrationId, storeId }) => {
+  const dispatch = useDispatch();
+  const [requested, setRequested] = useState(false);
+  const integrationResource = useSelector(state =>
+    selectors.integrationAppSettings(state, integrationId, storeId)
+  );
+
+  useEffect(() => {
+    if ((!integrationResource || isEmpty(integrationResource)) && !requested) {
+      dispatch(actions.resource.requestCollection('integrations'));
+      setRequested(true);
+    }
+  }, [dispatch, integrationResource, requested]);
+
+  if (
+    isEmpty(integrationResource) ||
+    (integrationResource.settings &&
+      !integrationResource.settings.supportsMultiStore)
+  ) {
+    return storeId;
+  }
+
+  const store = integrationResource.stores.find(s => s.value === storeId);
+
+  return store ? store.label : storeId;
+};
