157fe3c5575cc5d7f320241169d2f5c4f74aa0f0
Sravan Dandra <sravan.dandra@gmail.com>
2019-11-08 11:59:29 +0530

    Update DynaConnectorNColumnMap.jsx

--- src/components/DynaForm/fields/DynaTableView/DynaConnectorNColumnMap.jsx
+++ src/components/DynaForm/fields/DynaTableView/DynaConnectorNColumnMap.jsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { useCallback } from 'react';
 import * as selectors from '../../../../reducers';
 import actions from '../../../../actions';
 import DynaTableView from './DynaTable';
@@ -9,14 +10,15 @@ export default function DynaConnectorNColumnMap(props) {
   const { isLoading, shouldReset, data: metadata } = useSelector(state =>
     selectors.connectorMetadata(state, id, null, _integrationId, optionsMap)
   );
-
-  function handleRefreshClick(fieldId) {
-    dispatch(actions.connectors.refreshMetadata(fieldId, id, _integrationId));
-  }
-
-  function handleCleanup() {
+  const handleRefreshClick = useCallback(
+    fieldId => {
+      dispatch(actions.connectors.refreshMetadata(fieldId, id, _integrationId));
+    },
+    [_integrationId, dispatch, id]
+  );
+  const handleCleanup = useCallback(() => {
     dispatch(actions.connectors.clearMetadata(id, _integrationId));
-  }
+  }, [_integrationId, dispatch, id]);
 
   return (
     <DynaTableView
