9ed1d8d5eefb6302893cae8da91ffe1d48ae99ea
Sravan Dandra <sravan.dandra@gmail.com>
2019-11-10 00:17:51 +0530

    clone breadcrumbs

---
+++ src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/Clone.jsx
@@ -0,0 +1,20 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import * as selectors from '../../../../reducers';
+import actions from '../../../../actions';
+import { MODEL_PLURAL_TO_LABEL } from '../../../../utils/resource';
+
+export default function TemplateCrumb({ resourceId, resourceType }) {
+  const dispatch = useDispatch();
+  const resource = useSelector(state =>
+    selectors.resource(state, resourceType, resourceId)
+  );
+
+  useEffect(() => {
+    if (!resource) {
+      dispatch(actions.resource.request(resourceType, resourceId));
+    }
+  }, [dispatch, resource, resourceId, resourceType]);
+
+  return resource ? resource.name : `${MODEL_PLURAL_TO_LABEL[resourceType]}`;
+}
