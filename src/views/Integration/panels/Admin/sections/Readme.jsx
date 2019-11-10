02c33ce16531da68734684a06c892c4d32462da8
driedl <dave@riedl.ca>
2019-11-09 23:32:05 -0800

    updated readme view to include preview

--- src/views/Integration/panels/Admin/sections/Readme.jsx
+++ src/views/Integration/panels/Admin/sections/Readme.jsx
@@ -1,37 +1,49 @@
-import { Fragment } from 'react';
+import { Fragment, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { makeStyles } from '@material-ui/styles';
+import { Button, Typography } from '@material-ui/core';
 import * as selectors from '../../../../../reducers';
 import actions from '../../../../../actions';
-import DynaForm from '../../../../../components/DynaForm';
-import DynaSubmit from '../../../../../components/DynaForm/DynaSubmit';
 import PanelHeader from '../../PanelHeader';
+import CodeEditor from '../../../../../components/CodeEditor';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginLeft: theme.spacing(2),
+  },
+  editorContainer: {
+    border: `solid 1px ${theme.palette.secondary.lightest}`,
+    height: '25vh',
+    marginBottom: theme.spacing(3),
+  },
+  previewContainer: {
+    border: `solid 1px ${theme.palette.secondary.lightest}`,
+    borderRadius: 4,
+    backgroundColor: theme.palette.background.default,
+    height: '25vh',
+    padding: theme.spacing(1),
+    marginBottom: theme.spacing(2),
+  },
+}));
 
 export default function ReadmeSection({ integrationId }) {
+  const classes = useStyles();
   const dispatch = useDispatch();
   const integration = useSelector(state =>
     selectors.resource(state, 'integrations', integrationId)
   );
-  const fieldMeta = {
-    fieldMap: {
-      readme: {
-        id: 'readme',
-        name: 'readme',
-        type: 'editor',
-        mode: 'html',
-        label: 'ReadMe',
-        defaultValue: integration && integration.readme,
-      },
-    },
-    layout: {
-      fields: ['readme'],
-    },
-  };
-  const handleSubmit = formVal => {
+  const [value, setValue] = useState(integration && integration.readme);
+
+  function handleChange(value) {
+    setValue(value);
+  }
+
+  const handleSave = () => {
     const patchSet = [
       {
         op: 'replace',
         path: '/readme',
-        value: formVal.readme,
+        value,
       },
     ];
 
@@ -44,10 +56,28 @@ export default function ReadmeSection({ integrationId }) {
   return (
     <Fragment>
       <PanelHeader title="Readme" />
-
-      <DynaForm fieldMeta={fieldMeta} render>
-        <DynaSubmit onClick={handleSubmit}>Save</DynaSubmit>
-      </DynaForm>
+      <div className={classes.root}>
+        <div className={classes.editorContainer}>
+          <CodeEditor
+            name="readme"
+            value={value}
+            mode="html"
+            onChange={handleChange}
+          />
+        </div>
+        <Typography variant="h4">Preview</Typography>
+        <div
+          className={classes.previewContainer}
+          // TODO: We need to run the html through some type of sanitize
+          // process. I do not know if we already have something in the
+          // old UI to use here?
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{ __html: value }}
+        />
+        <Button variant="contained" color="primary" onClick={handleSave}>
+          Save
+        </Button>
+      </div>
     </Fragment>
   );
 }
