5121461e19e710d2a1afe9e10a69c7de23354f28
S Vamshi Krishna <vamshikrishnasaradhi@gmail.com>
2019-11-12 14:56:39 +0530

    Resource References Refactor

--- src/components/ResourceReferences/index.jsx
+++ src/components/ResourceReferences/index.jsx
@@ -1,5 +1,5 @@
 import { withStyles } from '@material-ui/core/styles';
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
 import { Fragment, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CeligoTable from '../../components/CeligoTable';
@@ -10,6 +10,7 @@ import { MODEL_PLURAL_TO_LABEL } from '../../utils/resource';
 import ModalDialog from '../ModalDialog';
 import metadata from './metadata';
 import { RESOURCE_TYPE_PLURAL_TO_SINGULAR } from '../../constants/resource';
+import Loader from '../Loader';
 
 const styles = theme => ({
   referenceLink: {
@@ -39,41 +40,42 @@ function ResourceReferences(props) {
   }, [dispatch, type, id]);
 
   return (
-    <ModalDialog handleClose={onClose} show>
-      <div>
-        {resourceReferences && resourceReferences.length !== 0 && (
-          <Fragment>
+    <Fragment>
+      {!resourceReferences && (
+        <Loader open>
+          <Typography variant="h4">
+            {`Retrieving ${MODEL_PLURAL_TO_LABEL[type]} References:`}
+          </Typography>
+          <Spinner className={classes.spinner} />
+        </Loader>
+      )}
+      {resourceReferences && resourceReferences.length === 0 && (
+        <Loader open>
+          <Typography>
+            This {MODEL_PLURAL_TO_LABEL[type]} is not being used anywhere
+          </Typography>
+          <Button onClick={onClose} variant="outlined" color="primary">
+            Close
+          </Button>
+        </Loader>
+      )}
+      {resourceReferences && resourceReferences.length !== 0 && (
+        <ModalDialog handleClose={onClose} show>
+          <div>
             {title
               ? `Unable to delete ${RESOURCE_TYPE_PLURAL_TO_SINGULAR[type]} as`
               : `${MODEL_PLURAL_TO_LABEL[type]} References:`}
-          </Fragment>
-        )}
-      </div>
-      <div>
-        {resourceReferences &&
-          (resourceReferences.length !== 0 ? (
-            <Fragment>
-              <Typography className={classes.message}>
-                {title &&
-                  `This ${MODEL_PLURAL_TO_LABEL[type]} is referenced by the resources below. Only resources that have no references can be deleted.`}
-              </Typography>
-              <CeligoTable data={resourceReferences} {...metadata} />
-            </Fragment>
-          ) : (
-            <Typography>
-              This {MODEL_PLURAL_TO_LABEL[type]} is not being used anywhere
+          </div>
+          <div>
+            <Typography className={classes.message}>
+              {title &&
+                `This ${MODEL_PLURAL_TO_LABEL[type]} is referenced by the resources below. Only resources that have no references can be deleted.`}
             </Typography>
-          ))}
-        {!resourceReferences && (
-          <Fragment>
-            <Typography>
-              {`Retrieving ${MODEL_PLURAL_TO_LABEL[type]} References:`}
-            </Typography>
-            <Spinner className={classes.spinner} />
-          </Fragment>
-        )}
-      </div>
-    </ModalDialog>
+            <CeligoTable data={resourceReferences} {...metadata} />
+          </div>
+        </ModalDialog>
+      )}
+    </Fragment>
   );
 }
 
