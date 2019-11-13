293223cd887d4e97507f5590d27294282458b657
Sandeep Kumar <sandeepkumar@Sandeep.local>
2019-11-13 20:32:25 +0530

    minor fix

--- src/forms/definitions/connections/custom/http/bamboohr.js
+++ src/forms/definitions/connections/custom/http/bamboohr.js
@@ -26,7 +26,6 @@ export default {
       type: 'text',
       required: true,
       label: 'Subdomain',
-      required: true,
       helpText:
         'Please enter your company name here which you configured while signing up for a new BambooHR account.',
       validWhen: {
