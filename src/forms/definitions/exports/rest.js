4b7c0aa42a12f5e1d90fab44544c5f9d801cbefc
mounika-aetukuri <mounika.245114@gmail.com>
2019-11-18 15:53:58 +0530

    IO-11511

--- src/forms/definitions/exports/rest.js
+++ src/forms/definitions/exports/rest.js
@@ -6,8 +6,40 @@ export default {
 
     if (retValues['/type'] === 'all') {
       retValues['/type'] = undefined;
+      retValues['/test'] = undefined;
+      retValues['/delta'] = undefined;
+      retValues['/once'] = undefined;
+      delete retValues['/test/limit'];
+      delete retValues['/delta/dateFormat'];
+      delete retValues['/delta/lagOffset'];
+      delete retValues['/once/booleanField'];
+      delete retValues['/rest/once/relativeURI'];
+      delete retValues['/rest/once/postBody'];
+      delete retValues['/rest/once/method'];
     } else if (retValues['/type'] === 'test') {
       retValues['/test/limit'] = 1;
+      retValues['/delta'] = undefined;
+      retValues['/once'] = undefined;
+      delete retValues['/delta/dateFormat'];
+      delete retValues['/delta/lagOffset'];
+      delete retValues['/once/booleanField'];
+      delete retValues['/rest/once/relativeURI'];
+      delete retValues['/rest/once/postBody'];
+      delete retValues['/rest/once/method'];
+    } else if (retValues['/type'] === 'delta') {
+      retValues['/once'] = undefined;
+      retValues['/test'] = undefined;
+      delete retValues['/test/limit'];
+      delete retValues['/once/booleanField'];
+      delete retValues['/rest/once/relativeURI'];
+      delete retValues['/rest/once/postBody'];
+      delete retValues['/rest/once/method'];
+    } else if (retValues['/type'] === 'once') {
+      retValues['/delta'] = undefined;
+      retValues['/test'] = undefined;
+      delete retValues['/test/limit'];
+      delete retValues['/delta/dateFormat'];
+      delete retValues['/delta/lagOffset'];
     }
 
     if (retValues['/outputMode'] === 'blob') {
