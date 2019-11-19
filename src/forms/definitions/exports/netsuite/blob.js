0a07f72e044b51689a10f6de596b3b733d5a7cde
mounika-aetukuri <mounika.245114@gmail.com>
2019-11-19 16:04:35 +0530

    Minor changes

--- src/forms/definitions/exports/netsuite/blob.js
+++ src/forms/definitions/exports/netsuite/blob.js
@@ -11,6 +11,11 @@ export default {
     common: { formId: 'common' },
     'netsuite.internalId': { fieldId: 'netsuite.internalId' },
     exportOneToMany: { formId: 'exportOneToMany' },
+    exportData: {
+      fieldId: 'exportData',
+      type: 'labeltitle',
+      label: 'What would you like to Export?',
+    },
     'netsuite.blob.purgeFileAfterExport': {
       fieldId: 'netsuite.blob.purgeFileAfterExport',
     },
@@ -19,6 +24,7 @@ export default {
     fields: [
       'common',
       'exportOneToMany',
+      'exportData',
       'netsuite.internalId',
       'netsuite.blob.purgeFileAfterExport',
     ],
