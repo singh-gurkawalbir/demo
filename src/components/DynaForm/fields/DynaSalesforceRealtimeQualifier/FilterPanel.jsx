c91a5093c1e0eafdd433e8c18bff866a44284114
Sravan Dandra <sravan.dandra@gmail.com>
2019-11-28 16:29:46 +0530

    SfQualifier - wip

--- src/components/DynaForm/fields/DynaSalesforceRealtimeQualifier/FilterPanel.jsx
+++ src/components/DynaForm/fields/DynaSalesforceRealtimeQualifier/FilterPanel.jsx
@@ -20,10 +20,7 @@ import {
   convertSalesforceQualificationCriteria,
   getFilterList,
   getFilterConfig,
-  generateRulesState,
-  getFilterRuleId,
 } from './util';
-import OperandSettingsDialog from './OperandSettingsDialog';
 import actions from '../../../../actions';
 
 const useStyles = makeStyles(theme => ({
@@ -34,7 +31,6 @@ const useStyles = makeStyles(theme => ({
     overflowY: 'auto',
   },
 }));
-const defaultData = {};
 const defaultFilters = [];
 
 export default function FilterPanel({
@@ -43,16 +39,13 @@ export default function FilterPanel({
   editorId,
   rule,
   filters = defaultFilters,
-  data = defaultData,
 }) {
   window.SQLParser = sqlParser;
   const qbuilder = useRef(null);
   const classes = useStyles();
-  const [showOperandSettingsFor, setShowOperandSettingsFor] = useState();
   const [rules, setRules] = useState();
   const [builderInitComplete, setBuilderInitComplete] = useState(false);
   const [filtersMetadata, setFiltersMetadata] = useState();
-  const [rulesState, setRulesState] = useState({});
   const dispatch = useDispatch();
   const patchEditor = useCallback(
     value => {
@@ -102,15 +95,15 @@ export default function FilterPanel({
       );
       const qbContainer = jQuery(qbuilder.current);
 
-      qbContainer.on('afterUpdateRuleOperator.queryBuilder', (e, rule) => {
-        if (
-          rule.operator &&
-          (rule.operator.type === 'is_empty' ||
-            rule.operator.type === 'is_not_empty')
-        ) {
-          rule.filter.valueGetter(rule);
-        }
-      });
+      //   qbContainer.on('afterUpdateRuleOperator.queryBuilder', (e, rule) => {
+      //     if (
+      //       rule.operator &&
+      //       (rule.operator.type === 'is_empty' ||
+      //         rule.operator.type === 'is_not_empty')
+      //     ) {
+      //       rule.filter.valueGetter(rule);
+      //     }
+      //   });
 
       qbContainer.queryBuilder({
         ...config,
@@ -145,194 +138,14 @@ export default function FilterPanel({
       }
 
       setRules(qbRules);
-      setRulesState(generateRulesState(qbRules));
     }
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [builderInitComplete]);
 
-  const updateUIForLHSRule = ({ rule = {} }) => {
-    function updateUIForExpression(rule) {
-      if (
-        rule.$el.find('.rule-filter-container textarea[name=expression]')
-          .length === 0
-      ) {
-        rule.$el
-          .find('[name$=_filter]')
-          .after(
-            '<textarea name="expression" class="io-filter-type form-control"></textarea>'
-          );
-        const ruleId = getFilterRuleId(rule);
-        const expressionField = rule.$el.find(
-          '.rule-filter-container textarea[name=expression]'
-        );
-
-        if (rulesState[ruleId].data && rulesState[ruleId].data.lhs) {
-          expressionField
-            .val(JSON.stringify(rulesState[ruleId].data.lhs.expression))
-            .trigger('change');
-        }
-
-        expressionField
-          .unbind('change')
-          .on('change', () => handleFilterRulesChange());
-      }
-    }
-
-    rule.$el.find('.rule-filter-container .io-filter-type').remove();
-
-    if (rule.filter) {
-      const ruleId = getFilterRuleId(rule);
-      let filterType = rulesState[ruleId].data.lhs.type;
-
-      if (
-        filterType === 'field' &&
-        ['formuladate', 'formulanumeric', 'formulatext'].indexOf(
-          rule.filter.id
-        ) > -1
-      ) {
-        filterType = 'expression';
-      }
-
-      if (filterType === 'field') {
-        rule.$el.find('[name$=_filter]').show();
-      } else if (filterType === 'expression') {
-        updateUIForExpression(rule);
-      }
-    }
-  };
-
-  const updateUIForRHSRule = ({ name, rule = {} }) => {
-    function updateUIForField(rule) {
-      if (
-        rule.$el.find('.rule-value-container select[name=field]').length === 0
-      ) {
-        const selectHtml = [
-          '<select name="field" class="io-filter-type form-control">',
-        ];
-
-        data.forEach(v => {
-          selectHtml.push(`<option value="${v.id}">${v.name || v.id}</option>`);
-        });
-        selectHtml.push('</select>');
-        rule.$el.find('.rule-value-container').prepend(selectHtml.join(''));
-
-        const ruleId = getFilterRuleId(rule);
-        const field = rule.$el.find(
-          '.rule-value-container  select[name=field]'
-        );
-
-        if (rulesState[ruleId].data && rulesState[ruleId].data.rhs) {
-          field.val(rulesState[ruleId].data.rhs.field);
-          setTimeout(() => {
-            rule.$el
-              .find('.rule-value-container  select[name=field]')
-              .trigger('change');
-          });
-        }
-
-        field.unbind('change').on('change', () => handleFilterRulesChange());
-      }
-    }
-
-    function updateUIForExpression(rule) {
-      if (
-        rule.$el.find('.rule-value-container textarea[name=expression]')
-          .length === 0
-      ) {
-        rule.$el
-          .find('.rule-value-container')
-          .prepend(
-            '<textarea name="expression" class="io-filter-type form-control"></textarea>'
-          );
-
-        const ruleId = getFilterRuleId(rule);
-        const expressionField = rule.$el.find(
-          '.rule-value-container  textarea[name=expression]'
-        );
-
-        if (rulesState[ruleId].data && rulesState[ruleId].data.rhs) {
-          expressionField
-            .val(JSON.stringify(rulesState[ruleId].data.rhs.expression))
-            .trigger('change');
-        }
-
-        expressionField
-          .unbind('change')
-          .on('change', () => handleFilterRulesChange());
-      }
-    }
-
-    const ruleId = getFilterRuleId(rule);
-
-    rule.$el.find('.rule-value-container .io-filter-type').remove();
-    const ruleState = rulesState[ruleId].data;
-
-    if (ruleState.rhs.type) {
-      const filterType = rulesState[ruleId].data.rhs.type;
-
-      if (filterType === 'value') {
-        if (!rule.$el.find(`[name=${name}]`).is(':visible')) {
-          rule.$el.find(`[name=${name}]`).show();
-          rule.$el.find(`[name=${name}]`).val('');
-        }
-      } else if (filterType === 'field') {
-        rule.$el.find(`[name=${name}]`).hide();
-        rule.$el.find(`[name=${name}]`).val('field'); // to bypass validation
-        updateUIForField(rule);
-      } else if (filterType === 'expression') {
-        rule.$el.find(`[name=${name}]`).hide();
-        rule.$el.find(`[name=${name}]`).val('expression'); // to bypass validation
-        updateUIForExpression(rule);
-      }
-    }
-  };
-
-  const handleCloseOperandSettings = () => {
-    setShowOperandSettingsFor();
-  };
-
-  const handleSubmitOperandSettings = operandSettings => {
-    const ruleData =
-      rulesState[getFilterRuleId(showOperandSettingsFor.rule)].data[
-        showOperandSettingsFor.rhs ? 'rhs' : 'lhs'
-      ];
-
-    rulesState[getFilterRuleId(showOperandSettingsFor.rule)].data[
-      showOperandSettingsFor.rhs ? 'rhs' : 'lhs'
-    ] = { ...ruleData, ...operandSettings };
-
-    if (showOperandSettingsFor.rhs) {
-      updateUIForRHSRule({
-        rule: showOperandSettingsFor.rule,
-        name: `${showOperandSettingsFor.rule.id}_value_0`,
-      });
-    } else {
-      updateUIForLHSRule({
-        rule: showOperandSettingsFor.rule,
-        name: `${showOperandSettingsFor.rule.id}_value_0`,
-      });
-    }
-
-    handleFilterRulesChange();
-    handleCloseOperandSettings();
-  };
-
   return (
     <div className={classes.container}>
-      <div ref={qbuilder} />
-      {showOperandSettingsFor && (
-        <OperandSettingsDialog
-          ruleData={
-            rulesState[getFilterRuleId(showOperandSettingsFor.rule)].data[
-              showOperandSettingsFor.rhs ? 'rhs' : 'lhs'
-            ]
-          }
-          // disabled={disabled}
-          onClose={handleCloseOperandSettings}
-          onSubmit={handleSubmitOperandSettings}
-        />
-      )}
+      <div className="salesforce-Qualifier" ref={qbuilder} />
     </div>
   );
 }
