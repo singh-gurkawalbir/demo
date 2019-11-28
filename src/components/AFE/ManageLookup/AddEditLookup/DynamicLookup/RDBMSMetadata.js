fe0ca04fb51480bdc166e13b0d72ed7ea05bbb37
adibjp <adibjp441@gmail.com>
2019-11-29 01:47:16 +0530

    rdbms lookup metadata

--- src/components/AFE/ManageLookup/AddEditLookup/DynamicLookup/RDBMSMetadata.js
+++ src/components/AFE/ManageLookup/AddEditLookup/DynamicLookup/RDBMSMetadata.js
@@ -2,63 +2,16 @@ export default {
   getLookupMetadata: (lookup, isEdit, showDynamicLookupOnly) => {
     const fieldMeta = {
       fieldMap: {
-        relativeURI: {
-          id: 'relativeURI',
-          name: 'relativeURI',
+        query: {
+          id: 'query',
+          name: 'query',
           type: 'text',
-          label: 'Relative URI',
-          placeholder: 'Relative URI',
-          defaultValue: lookup.relativeURI,
+          label: 'Query',
+          defaultValue: lookup.query,
           visibleWhen: [
             {
               field: 'mode',
               is: ['dynamic'],
-            },
-          ],
-        },
-        method: {
-          id: 'method',
-          name: 'method',
-          type: 'select',
-          label: 'HTTP Method',
-          defaultValue: lookup.method,
-          options: [
-            {
-              heading: 'Select Http Method',
-              items: [
-                {
-                  label: 'GET',
-                  value: 'GET',
-                },
-                {
-                  label: 'POST',
-                  value: 'POST',
-                },
-              ],
-            },
-          ],
-          visibleWhen: [
-            {
-              field: 'mode',
-              is: ['dynamic'],
-            },
-          ],
-        },
-        body: {
-          id: 'body',
-          name: 'body',
-          type: 'httprequestbody',
-          connectionId: r => r && r._connectionId,
-          label: 'Build HTTP Request Body',
-          defaultValue: lookup.body,
-          visibleWhenAll: [
-            {
-              field: 'mode',
-              is: ['dynamic'],
-            },
-            {
-              field: 'method',
-              is: ['POST'],
             },
           ],
         },
@@ -66,8 +19,7 @@ export default {
           id: 'extract',
           name: 'extract',
           type: 'text',
-          label: 'Resource Identifier Path',
-          placeholder: 'Resource Identifier Path',
+          label: 'Column',
           defaultValue: lookup.extract,
           visibleWhen: [
             {
@@ -76,49 +28,24 @@ export default {
             },
           ],
         },
-
-        mapList: {
-          id: 'mapList',
-          name: 'mapList',
-          type: 'staticMap',
-          label: '',
-          keyName: 'export',
-          keyLabel: 'Export Field',
-          valueName: 'import',
-          valueLabel: 'Import Field (HTTP)',
-          map: isEdit && lookup && lookup.map,
-          visibleWhen: [
-            {
-              field: 'mode',
-              is: ['static'],
-            },
-          ],
-        },
       },
       layout: {
-        fields: ['relativeURI', 'method', 'body', 'extract', 'mapList'],
+        fields: ['query', 'extract'],
       },
     };
 
     if (showDynamicLookupOnly) {
-      delete fieldMeta.fieldMap.mode;
-      delete fieldMeta.fieldMap.mapList;
-      fieldMeta.layout.fields = fieldMeta.layout.fields.filter(
-        el => el !== 'mode' && el !== 'mapList'
-      );
-      const { relativeURI, method, body, extract } = fieldMeta.fieldMap;
+      const { query, extract } = fieldMeta.fieldMap;
 
-      delete relativeURI.visibleWhen;
-      delete method.visibleWhen;
-      delete body.visibleWhenAll;
+      delete query.visibleWhenAll;
       delete extract.visibleWhen;
 
-      body.visibleWhen = [
-        {
-          field: 'method',
-          is: ['POST'],
-        },
-      ];
+      // body.visibleWhen = [
+      //   {
+      //     field: 'method',
+      //     is: ['POST'],
+      //   },
+      // ];
     }
 
     return fieldMeta;
