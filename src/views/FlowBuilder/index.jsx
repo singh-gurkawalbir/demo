5734b801bf24f9e3c8937b22d494a5cea5db57cc
driedl <dave@riedl.ca>
2019-12-06 14:11:11 -0800

    IO-11882: handle nameless flows in FB

--- src/views/FlowBuilder/index.jsx
+++ src/views/FlowBuilder/index.jsx
@@ -429,9 +429,12 @@ function FlowBuilder(props) {
 
       <CeligoPageBar
         title={
-          <EditableText disabled={isViewMode} onChange={handleTitleChange}>
-            {flow.name}
-          </EditableText>
+          <EditableText
+            disabled={isViewMode}
+            text={flow.name}
+            defaultText={`Unnamed (id:${flowId})`}
+            onChange={handleTitleChange}
+          />
         }
         subtitle={`Last saved: ${isNewFlow ? 'Never' : flow.lastModified}`}
         infoText={flow.description}>
