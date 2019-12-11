89f7a26a28e8db5873dad81b5171620e66ff9a77
Snigdha <snigdha@localhost.io>
2019-12-11 13:05:17 +0530

    IO-12160,IO-12128

--- src/forms/fieldDefinitions/resources/imports/http.js
+++ src/forms/fieldDefinitions/resources/imports/http.js
@@ -2,6 +2,7 @@ export default {
   'http.method': {
     type: 'radiogroup',
     label: 'Method',
+    required: true,
     visibleWhen: [
       {
         field: 'inputMode',
@@ -83,7 +84,7 @@ export default {
         ],
       },
     ],
-    defaultValue: r => (r && r.http ? r && r.http.requestMediaType : 'xml'),
+    defaultValue: r => (r && r.http ? r && r.http.requestMediaType : 'json'),
   },
   'http.compositeType': {
     type: 'select',
