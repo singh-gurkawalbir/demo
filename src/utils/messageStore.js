50b728a8577a2f51d636d222a1dd16df923d88b4
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2019-12-16 15:46:41 +0530

    IO-11899

--- src/utils/messageStore.js
+++ src/utils/messageStore.js
@@ -4,6 +4,8 @@ export const MULTIPLE_INSTALLS =
   'Looks like this Integration App is already installed in your account. If you would like to install another instance please provide a unique tag name so that we can help you differentiate this install from the others.';
 export const INTEGRATION_DELETE_VALIDATE =
   'All Flows within an integration tile must be removed before the integration tile can be deleted.';
+export const LICENSE_UPGRADE_REQUEST_RECEIVED =
+  'Your request for a license upgrade has been received. We will contact you soon via email once the upgrade is done.';
 export default {
   CONTACT_SALES_MESSAGE,
   MULTIPLE_INSTALLS,
