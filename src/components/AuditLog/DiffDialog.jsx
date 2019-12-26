6710a0b714c0c418adf670a5245f23fa35b0eb2b
Mohammed Azhar Uddin <azharweb90@gmail.com>
2019-12-26 17:13:46 +0530

    minor fixes

--- src/components/AuditLog/DiffDialog.jsx
+++ src/components/AuditLog/DiffDialog.jsx
@@ -1,12 +1,8 @@
 import { Component } from 'react';
-import Dialog from '@material-ui/core/Dialog';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import { Typography, withStyles } from '@material-ui/core';
-import IconButton from '@material-ui/core/IconButton';
-import CloseIcon from '@material-ui/icons/Close';
+import { withStyles, Typography } from '@material-ui/core';
 import ReactDiffViewer from 'react-diff-viewer';
 import { RESOURCE_TYPE_SINGULAR_TO_LABEL } from '../../constants/resource';
+import ModalDialog from '../ModalDialog';
 
 @withStyles(theme => ({
   root: {
@@ -22,33 +18,25 @@ import { RESOURCE_TYPE_SINGULAR_TO_LABEL } from '../../constants/resource';
 }))
 export default class DiffDialog extends Component {
   render() {
-    const { classes, auditLog, onCancelClick } = this.props;
+    const { auditLog, onCancelClick } = this.props;
 
     return (
-      <Dialog open maxWidth={false}>
-        <DialogTitle disableTypography className={classes.root}>
-          <Typography variant="h6">
-            {`${
-              RESOURCE_TYPE_SINGULAR_TO_LABEL[auditLog.resourceType]
-            } Audit Log`}
-          </Typography>
+      <ModalDialog show maxWidth={false} onClose={onCancelClick}>
+        <div>
+          {`${
+            RESOURCE_TYPE_SINGULAR_TO_LABEL[auditLog.resourceType]
+          } Audit Log`}
           <Typography>{`Field: ${auditLog.fieldChange.fieldPath}`}</Typography>
-          <IconButton
-            aria-label="Close"
-            data-test="closeDiffDialog"
-            className={classes.closeButton}
-            onClick={onCancelClick}>
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>
+        </div>
+
+        <div>
           <ReactDiffViewer
             hideLineNumbers
             oldValue={JSON.stringify(auditLog.fieldChange.oldValue, null, '  ')}
             newValue={JSON.stringify(auditLog.fieldChange.newValue, null, '  ')}
           />
-        </DialogContent>
-      </Dialog>
+        </div>
+      </ModalDialog>
     );
   }
 }
