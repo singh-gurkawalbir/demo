7c121eb99ee1c4c336557517c44002f34e01209e
Mohammed Azhar Uddin <azharweb90@gmail.com>
2020-01-17 15:56:30 +0530

    Issues resolved

--- src/components/AFE/JavaScriptEditor/JavaScriptPanel.jsx
+++ src/components/AFE/JavaScriptEditor/JavaScriptPanel.jsx
@@ -1,12 +1,10 @@
-import { useEffect, useCallback, Fragment } from 'react';
+import { useEffect, useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
 import Button from '@material-ui/core/Button';
 import LoadResources from '../../../components/LoadResources';
 import CodePanel from '../GenericEditor/CodePanel';
@@ -14,23 +12,43 @@ import actions from '../../../actions';
 import * as selectors from '../../../reducers';
 import Spinner from '../../Spinner';
 import { hooksLabelMap, getScriptHookStub } from '../../../utils/hooks';
+import CeligoSelect from '../../CeligoSelect';
 
 const useStyles = makeStyles(theme => ({
   container: {
     backgroundColor: theme.palette.background.default,
-    height: '100%',
-  },
-  textField: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(1),
-    // Changing this from 50% to 33% to accomodate 3 elements
-    // TODO:@Azhar Make this flexible layout to fix  multiple elements instead of having width
-    width: '33%',
-    paddingLeft: theme.spacing(1),
-    paddingRight: theme.spacing(1),
+    height: 100,
   },
   label: {
     paddingLeft: theme.spacing(1),
+  },
+  editorSettings: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+  },
+  formFields: {
+    height: 80,
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(1),
+    borderBottom: `1px solid ${theme.palette.secondary.lightest}`,
+    '& > div': {
+      minWidth: '140px',
+      marginRight: 10,
+      '&:first-child': {
+        marginLeft: 10,
+      },
+    },
+  },
+  CodeEditor: {
+    height: '100%',
+  },
+  select: {
+    height: 50,
+  },
+  loader: {
+    padding: theme.spacing(1),
   },
 }));
 
@@ -86,59 +104,63 @@ export default function JavaScriptPanel(props) {
 
   return (
     <LoadResources required resources={['scripts']}>
-      <div className={classes.container}>
-        <FormControl className={classes.textField}>
-          <InputLabel className={classes.label} htmlFor="scriptId">
-            Script
-          </InputLabel>
-          <Select
-            id="scriptId"
-            margin="dense"
-            value={scriptId}
-            disabled={disabled}
-            onChange={handleScriptChange}>
-            {allScripts.map(s => (
-              <MenuItem key={s._id} value={s._id}>
-                {s.name}
+      <div className={classes.editorSettings}>
+        <div className={classes.formFields}>
+          <FormControl>
+            <CeligoSelect
+              id="scriptId"
+              displayEmpty
+              value={scriptId || ''}
+              disabled={disabled}
+              default
+              className={classes.select}
+              onChange={handleScriptChange}>
+              <MenuItem value="" disabled>
+                Script
               </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <TextField
-          id="entryFunction"
-          disabled={disabled}
-          InputLabelProps={{ className: classes.label }}
-          className={classes.textField}
-          value={entryFunction}
-          onChange={event => patchEditor('entryFunction', event.target.value)}
-          label="Entry Function"
-          margin="dense"
-        />
-        {scriptId && insertStubKey && (
-          <Button
-            variant="contained"
-            color="primary"
-            className={classes.textField}
-            onClick={handleInsertStubClick}
+              {allScripts.map(s => (
+                <MenuItem key={s._id} value={s._id}>
+                  {s.name}
+                </MenuItem>
+              ))}
+            </CeligoSelect>
+          </FormControl>
+          <TextField
+            id="entryFunction"
             disabled={disabled}
-            data-test={insertStubKey}>
-            {`Insert ${hooksLabelMap[insertStubKey]} Stub`}
-          </Button>
-        )}
-        {scriptContent === undefined && scriptId ? (
-          <Fragment>
-            <Typography>Retrieving your script</Typography>
-            <Spinner />
-          </Fragment>
-        ) : (
-          <CodePanel
-            name="code"
-            readOnly={disabled}
-            value={code}
-            mode="javascript"
-            onChange={handleCodeChange}
+            InputLabelProps={{ className: classes.label }}
+            value={entryFunction}
+            onChange={event => patchEditor('entryFunction', event.target.value)}
+            label="Entry Function"
+            variant="filled"
           />
-        )}
+          {scriptId && insertStubKey && (
+            <Button
+              variant="outlined"
+              color="primary"
+              onClick={handleInsertStubClick}
+              disabled={disabled}
+              data-test={insertStubKey}>
+              {`Insert ${hooksLabelMap[insertStubKey]} Stub`}
+            </Button>
+          )}
+        </div>
+        <div className={classes.CodeEditor}>
+          {scriptContent === undefined && scriptId ? (
+            <div className={classes.loader}>
+              <Typography>Retrieving your script</Typography>
+              <Spinner />
+            </div>
+          ) : (
+            <CodePanel
+              name="code"
+              readOnly={disabled}
+              value={code}
+              mode="javascript"
+              onChange={handleCodeChange}
+            />
+          )}
+        </div>
       </div>
     </LoadResources>
   );
