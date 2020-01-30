e3c6c38eb871278c8fccd8df8283e27309c7fcef
adibjp <adibjp441@gmail.com>
2020-01-30 20:18:18 +0530

    Editor Save  button

---
+++ src/components/ResourceFormFactory/Actions/EditorSaveButton.jsx
@@ -0,0 +1,83 @@
+import { withStyles } from '@material-ui/core/styles';
+import { useCallback, useState, useEffect, Fragment } from 'react';
+import { Button } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import actions from '../../../actions';
+import * as selectors from '../../../reducers';
+import Spinner from '../../Spinner';
+import { useLoadingSnackbarOnSave } from '.';
+import { preSaveValidate } from '../../AFE/EditorDialog/util';
+import useEnqueueSnackbar from '../../../hooks/enqueueSnackbar';
+
+const styles = theme => ({
+  actionButton: {
+    marginTop: theme.spacing.double,
+    marginLeft: theme.spacing.double,
+  },
+});
+const EditorSaveButton = props => {
+  const {
+    id,
+    submitButtonLabel = 'Save',
+    variant = 'outlined',
+    color = 'secondary',
+    disabled = false,
+    dataTest,
+    onClose,
+  } = props;
+  const [saveTrigerred, setSaveTriggered] = useState(false);
+  const [enquesnackbar] = useEnqueueSnackbar();
+  const editor = useSelector(state => selectors.editor(state, id));
+  const { resourceType } = editor;
+  const editorViolations = useSelector(state =>
+    selectors.editorViolations(state, id)
+  );
+  const disableBtn = !editor || editorViolations || disabled;
+  const dispatch = useDispatch();
+  const { saveTerminated, saveCompleted } = useSelector(state =>
+    selectors.editorSaveProcessTerminate(state, id)
+  );
+
+  useEffect(() => {
+    if (saveTrigerred && saveCompleted && onClose) {
+      onClose();
+      setSaveTriggered(false);
+    }
+  }, [onClose, saveCompleted, saveTerminated, saveTrigerred]);
+  const onSave = useCallback(() => {
+    dispatch(actions.editor.save(id));
+    setSaveTriggered(true);
+  }, [dispatch, id]);
+  const { handleSubmitForm, disableSave } = useLoadingSnackbarOnSave({
+    saveTerminated,
+    onSave,
+    resourceType,
+  });
+  const handleButtonClick = () => {
+    if (!preSaveValidate({ editor, enquesnackbar })) {
+      return;
+    }
+
+    handleSubmitForm();
+  };
+
+  return (
+    <Button
+      data-test={dataTest}
+      variant={variant}
+      color={color}
+      disabled={disableBtn}
+      onClick={handleButtonClick}>
+      {disableSave ? (
+        <Fragment>
+          <Spinner size={16} />
+          Saving
+        </Fragment>
+      ) : (
+        <Fragment>{submitButtonLabel}</Fragment>
+      )}
+    </Button>
+  );
+};
+
+export default withStyles(styles)(EditorSaveButton);
