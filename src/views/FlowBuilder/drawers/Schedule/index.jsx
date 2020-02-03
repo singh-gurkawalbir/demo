71687b52103f7d38f99a43a8d03e9f1129ea8bac
Mohammed Azhar Uddin <azharweb90@gmail.com>
2020-02-03 18:16:18 +0530

    icon close added

--- src/views/FlowBuilder/drawers/Schedule/index.jsx
+++ src/views/FlowBuilder/drawers/Schedule/index.jsx
@@ -1,9 +1,11 @@
 import { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import { IconButton } from '@material-ui/core';
 import RightDrawerRouter from '../RightDrawer';
 import FlowSchedule from '../../../../components/FlowSchedule';
 import TitleBar from '../TitleBar';
+import Close from '../../../../components/icons/CloseIcon';
 
 const useStyle = makeStyles(theme => ({
   fbContDrawer: {
@@ -11,6 +13,11 @@ const useStyle = makeStyles(theme => ({
     overflowX: 'hidden',
     marginTop: -1,
     padding: theme.spacing(2),
+  },
+  closeButton: {
+    position: 'absolute',
+    right: theme.spacing(1),
+    top: 2,
   },
 }));
 
@@ -27,6 +34,13 @@ export default function ScheduleDrawer({
   // TODO: Connector specific things to be added for schedule drawer incase of !isViewMode && isConnector
   return (
     <RightDrawerRouter {...props} path="schedule">
+      <IconButton
+        data-test="closeFlowSchedule"
+        aria-label="Close"
+        className={classes.closeButton}
+        onClick={handleClose}>
+        <Close />
+      </IconButton>
       <TitleBar title="Flow Schedule" />
       <FlowSchedule
         disabled={isViewMode}
