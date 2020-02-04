a5cb5181972e82ef5b37d23cc4868f4949d66db5
driedl <dave@riedl.ca>
2020-02-04 14:48:53 -0800

    tweaked FB drawers + notification panel design

--- src/views/FlowBuilder/drawers/Schedule/index.jsx
+++ src/views/FlowBuilder/drawers/Schedule/index.jsx
@@ -1,39 +1,35 @@
 import { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import RightDrawerRouter from '../RightDrawer';
+import RightDrawer from '../../../../components/drawer/Right';
 import FlowSchedule from '../../../../components/FlowSchedule';
-import TitleBar from '../TitleBar';
 
 const useStyle = makeStyles(theme => ({
-  fbContDrawer: {
+  scheduleContainer: {
     width: '100%',
     overflowX: 'hidden',
     marginTop: -1,
-    padding: theme.spacing(2),
+    padding: theme.spacing(-1),
+    '& > div': {
+      padding: theme.spacing(3, 0),
+    },
   },
 }));
 
-export default function ScheduleDrawer({
-  flow,
-  isViewMode,
-  isConnector,
-  ...props
-}) {
+export default function ScheduleDrawer({ flow, isViewMode }) {
   const history = useHistory();
   const handleClose = useCallback(() => history.goBack(), [history]);
   const classes = useStyle();
 
   // TODO: Connector specific things to be added for schedule drawer incase of !isViewMode && isConnector
   return (
-    <RightDrawerRouter {...props} path="schedule">
-      <TitleBar title="Flow Schedule" />
+    <RightDrawer path="schedule" width="medium" title="Flow schedule">
       <FlowSchedule
         disabled={isViewMode}
         flow={flow}
         onClose={handleClose}
-        className={classes.fbContDrawer}
+        className={classes.scheduleContainer}
       />
-    </RightDrawerRouter>
+    </RightDrawer>
   );
 }
