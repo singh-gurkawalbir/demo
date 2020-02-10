1992847c77cf9f13633f1ff220893def8e32b01f
mounika-aetukuri <mounika.245114@gmail.com>
2020-02-10 15:35:29 +0530

    IO-13204

--- src/views/ResourceList/infoText.js
+++ src/views/ResourceList/infoText.js
@@ -1,55 +1,12 @@
 // make sure the dictionary key matches the "model plural" name.
 export default {
-  exports: `
-    Exports are used to extract data from an application. 
-    When building a data flow, defining the export is the 
-    first step in the flow wizard. You can also invoke an export 
-    standalone using the integrator.io API to pull data in real-time 
-    into your own app.!`,
-  imports: `
-    Imports are used to insert data into an application. 
-    When building a data flow, defining the import is the second 
-    step in the wizard. You can also invoke an import standalone 
-    using the integrator.io API to insert data into an application 
-    in real-time from your own app.`,
-  agents: `
-    Agents are a small piece of software that you can install on 
-    your machines to connect to data that is behind your firewall 
-    if you don't want to whitelist integrator.io's IP addresses. 
-    Each agent has an access token associated with it that should 
-    be used when installing the software on your machine. The installed 
-    agents connect to integrator.io and establish a reverse SSH tunnel 
-    allowing secure communication through your firewall without any 
-    need of whitelisting IPs in your firewall settings.`,
-  connections: `
-    Connections are used to store credentials, along with other 
-    information needed to access an application via its API. 
-    Connections can also be shared with other integrator.io users 
-    to edit and create data flows that run in your integrator.io 
-    account.`,
-  recycleBin: `
-    All deleted items will be kept in the Recycle Bin for up to 30 days 
-    before being automatically purged. All Flows in Recycle Bin 
-    are deactivated and will no longer run. You can restore any of the items 
-    listed in Recycle Bin, or purge them manually, via Actions.`,
-  scripts: `
-    Scripts are written in JavaScript and represent a powerful 
-    mechanism for customizing your integrator.io account. You can use 
-    scripts to implement hooks, filters, transforms, and more. Feel free 
-    to organize your scripts any way you like. For example, you could 
-    have just one script record and put all your code in that one record, 
-    or you could split your code across multiple different script records 
-    (i.e. that different users are responsible for). Please note that 
-    scripts can be referenced by any number of resources in your 
-    integrator.io account and changing a script should always be done 
-    with caution. Enjoy! `,
-  stacks: `
-    Stacks are simple server environments used to host code for hooks, 
-    wrappers, Integration App installers and settings pages. Stacks can be 
-    implemented in any programming language and are always invoked via HTTP. 
-    Every stack is assigned a system token that should be used to 
-    authenticate HTTP requests. You own the IP for all your stacks, 
-    and can optionally choose to share a stack with other integrator.io users.`,
+  exports: `Exports are used to extract data from an application. You can create and configure exports here or when you are building your flow. You can even invoke a stand-alone export using the integrator.io API to pull data in real-time into your own app.`,
+  imports: `Imports are used to insert data into an application. You can create and configure exports here or when you are building your flow. You can even invoke a stand-alone import using the integrator.io API to insert data in real-time into an application from your own app.`,
+  agents: `Agents are small applications that you can install on your computers to connect to data that is behind your firewall if you don't want to whitelist integrator.io's IP addresses. Each agent has an access token associated with it that should be used when installing the software on your computer. The installed agents connect to integrator.io and establish a reverse SSH tunnel allowing secure communication through your firewall without any need of whitelisting IPs in your firewall settings.`,
+  connections: `Connections store credentials and other information needed to access an application via its API. Connections can be shared with other integrator.io users to edit and create data flows that run in your integrator.io account. You can also check our Connector solutions articles for information on how to connect each supported app connector.`,
+  recycleBin: `All deleted items will be kept in the Recycle bin for up to 30 days before being automatically purged. All Flows in Recycle bin are deactivated and will no longer run. You can restore any of the items listed in Recycle bin, or purge them manually via Actions.`,
+  scripts: `You can write scripts in JavaScript to customize your integrations, enabling you to implement hooks, filters, transforms, and more. You can organize your scripts however you’d like, such as having just one script record with all of your code or splitting your code across multiple script records, even if different users are responsible for them. Scripts can be used by any number of resources in your integrator.io account, so use caution when modifying a script.`,
+  stacks: `You can use stacks to host code for hooks, wrappers, connector installers and settings pages. Stacks are simple server environments which can be implemented in any coding language and are always invoked via HTTP. Every stack is assigned a system token that should be used to authenticate HTTP requests. You own the IP for all of your stacks, and can optionally choose to share a stack with other integrator.io users.`,
   templates: `
     This is a special page enabled only for select partners. 
     Please use this page to publish templates to the integration marketplace. 
