b328b7927eeb263b1468734644d8111ec8eec178
mounika-aetukuri <mounika.245114@gmail.com>
2020-02-26 15:12:58 +0530

    IO-13122

--- src/forms/definitions/connections/salesforce.js
+++ src/forms/definitions/connections/salesforce.js
@@ -1,3 +1,5 @@
+import { isNewId } from '../../../utils/resource';
+
 export default {
   preSave: formValues => {
     const newValues = Object.assign({}, formValues);
@@ -12,30 +14,15 @@ export default {
     name: { fieldId: 'name', required: true },
     'salesforce.sandbox': { fieldId: 'salesforce.sandbox' },
     'salesforce.oauth2FlowType': { fieldId: 'salesforce.oauth2FlowType' },
-    'salesforce.flowType': {
-      id: 'salesforce.flowType',
-      type: 'text',
-      visible: false,
-      defaultValue: r => {
-        if (!(r && r.salesforce && r.salesforce.oauth2FlowType)) {
-          return 'false';
-        }
-
-        return 'true';
-      },
-    },
     'salesforce.info.email': {
       id: 'salesforce.info.email',
       type: 'text',
       label: 'Email',
       defaultDisabled: true,
       helpText: 'Your Salesforce account email.',
+      visible: r => r && !isNewId(r._id),
       defaultValue: r =>
         r && r.salesforce && r.salesforce.info && r.salesforce.info.email,
-      visibleWhenAll: [
-        { field: 'salesforce.oauth2FlowType', isNot: [''] },
-        { field: 'salesforce.flowType', is: ['true'] },
-      ],
     },
     'salesforce.info.organization_id': {
       id: 'salesforce.info.organization_id',
@@ -43,15 +30,12 @@ export default {
       label: 'Organization id',
       defaultDisabled: true,
       helpText: "Your organization's unique Salesforce ID",
+      visible: r => r && !isNewId(r._id),
       defaultValue: r =>
         r &&
         r.salesforce &&
         r.salesforce.info &&
         r.salesforce.info.organization_id,
-      visibleWhenAll: [
-        { field: 'salesforce.oauth2FlowType', isNot: [''] },
-        { field: 'salesforce.flowType', is: ['true'] },
-      ],
     },
     'salesforce.username': { fieldId: 'salesforce.username' },
     _borrowConcurrencyFromConnectionId: {
@@ -62,7 +46,6 @@ export default {
   layout: {
     fields: [
       'name',
-      'salesforce.flowType',
       'salesforce.info.email',
       'salesforce.info.organization_id',
       'salesforce.sandbox',
