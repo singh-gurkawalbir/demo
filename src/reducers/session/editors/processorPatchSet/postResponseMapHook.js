4ce330f73eda31a6142fbcc9e701627ad92eebfe
adibjp <adibjp441@gmail.com>
2020-02-27 15:27:01 +0530

    refactor

--- src/reducers/session/editors/processorPatchSet/postResponseMapHook.js
+++ src/reducers/session/editors/processorPatchSet/postResponseMapHook.js
@@ -7,8 +7,36 @@ export default {
     const { code, scriptId, entryFunction, optionalSaveParams } = editor;
     const { flowId, pageProcessorsObject, resourceIndex } =
       optionalSaveParams || {};
+    const foregroundPatchSet = [];
 
+    if (!pageProcessorsObject.hooks) {
+      foregroundPatchSet.push({
+        op: 'add',
+        path: `/pageProcessors/${resourceIndex}/hooks`,
+        value: {},
+      });
+    }
+
+    // Saves postResponseMap Hook on pageProcessor based on resourceIndex
+    // Incase of user selecting None for script, pass undefined instead of '' as BE throws error if it is ''
+    foregroundPatchSet.push({
+      op:
+        pageProcessorsObject.hooks && pageProcessorsObject.hooks.postResponseMap
+          ? 'replace'
+          : 'add',
+      path: `/pageProcessors/${resourceIndex}/hooks/postResponseMap`,
+      value: {
+        _scriptId: scriptId || undefined,
+        function: scriptId ? entryFunction : undefined,
+      },
+    });
     patches.foregroundPatch = {
+      patch: foregroundPatchSet,
+      resourceType: 'flows',
+      resourceId: flowId,
+    };
+
+    patches.backgroundPatches.push({
       patch: [
         {
           op: 'replace',
@@ -18,33 +46,6 @@ export default {
       ],
       resourceType: 'scripts',
       resourceId: scriptId,
-    };
-
-    const backgroundPatchSet = [];
-
-    if (!pageProcessorsObject.hooks) {
-      backgroundPatchSet.push({
-        op: 'add',
-        path: `/pageProcessors/${resourceIndex}/hooks`,
-        value: {},
-      });
-    }
-
-    // Saves postResponseMap Hook on pageProcessor based on resourceIndex
-    // Incase of user selecting None for script, pass undefined instead of '' as BE throws error if it is ''
-    backgroundPatchSet.push({
-      op:
-        pageProcessorsObject.hooks && pageProcessorsObject.hooks.postResponseMap
-          ? 'replace'
-          : 'add',
-      path: `/pageProcessors/${resourceIndex}/hooks/postResponseMap`,
-      value: { _scriptId: scriptId || undefined, function: entryFunction },
-    });
-
-    patches.backgroundPatches.push({
-      patch: backgroundPatchSet,
-      resourceType: 'flows',
-      resourceId: flowId,
     });
 
     return patches;
