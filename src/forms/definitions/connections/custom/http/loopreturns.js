d21c8e7abe293951f07792aabf3482db44fc5f83
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2020-03-03 15:33:07 +0530

    minor changes

--- src/forms/definitions/connections/custom/http/loopreturns.js
+++ src/forms/definitions/connections/custom/http/loopreturns.js
@@ -18,7 +18,7 @@ export default {
       fieldId: 'http.auth.token.token',
       label: 'API Key',
       helpText:
-        'Please enter your API key here. Log into your Loop Returns Account. Go to Settings > Developers, you can generate an API key or use an existing one.Please note that there are multiple layers of protection in place (including AES 256 encryption) to keep your API key safe. This can be obtained from the Settings section and Developer subsection.',
+        'Please enter your API key here. Log into your Loop Returns Account. Go to Settings > Developers, you can generate an API key or use an existing one.\n Please note that there are multiple layers of protection in place (including AES 256 encryption) to keep your API key safe.',
       required: true,
     },
     httpAdvanced: { formId: 'httpAdvanced' },
--- src/forms/fieldDefinitions/resources/template.js
+++ src/forms/fieldDefinitions/resources/template.js
@@ -1,75 +1,4 @@
-const options = [
-  { label: '3dCart', value: '3dcart' },
-  { label: '3PL Central', value: '3plcentral' },
-  { label: 'ADP', value: 'adp' },
-  { label: 'AS2', value: 'as2' },
-  { label: 'Amazon AWS', value: 'amazonaws' },
-  { label: 'Amazon MWS', value: 'amazonmws' },
-  { label: 'Ariba', value: 'ariba' },
-  { label: 'Asana', value: 'asana' },
-  { label: 'Banking', value: 'banking' },
-  { label: 'BigCommerce', value: 'bigcommerce' },
-  { label: 'Box', value: 'box' },
-  { label: 'Braintree', value: 'braintree' },
-  { label: 'Certify', value: 'certify' },
-  { label: 'Chargify', value: 'chargify' },
-  { label: 'Clover', value: 'clover' },
-  { label: 'DCL', value: 'dcl' },
-  { label: 'Desk', value: 'desk' },
-  { label: 'Docusign', value: 'docusign' },
-  { label: 'Dropbox', value: 'dropbox' },
-  { label: 'eBay', value: 'ebay' },
-  { label: 'FTP', value: 'ftp' },
-  { label: 'Facebook Ads', value: 'facebookads' },
-  { label: 'Gainsight', value: 'gainsight' },
-  { label: 'GitHub', value: 'github' },
-  { label: 'Google', value: 'google' },
-  { label: 'Jet', value: 'jet' },
-  { label: 'Jira', value: 'jira' },
-  { label: 'Jobvite', value: 'jobvite' },
-  { label: 'LiquidPlanner', value: 'liquidplanner' },
-  { label: 'Loop Returns', value: 'loopreturns' },
-  { label: 'Magento 2', value: 'magento' },
-  { label: 'NetSuite', value: 'netsuite' },
-  { label: 'Newegg', value: 'newegg' },
-  { label: 'NexTag', value: 'nextag' },
-  { label: 'OpenAir', value: 'openair' },
-  { label: 'Oracle Supplier Network', value: 'osn' },
-  { label: 'Other', value: 'other' },
-  { label: 'PayPal', value: 'paypal' },
-  { label: 'Paychex', value: 'paychex' },
-  { label: 'Salesforce', value: 'salesforce' },
-  { label: 'ServiceNow', value: 'servicenow' },
-  { label: 'ShipStation', value: 'shipstation' },
-  { label: 'Shipwire', value: 'shipwire' },
-  { label: 'Shopify', value: 'shopify' },
-  { label: 'Silicon Valley Bank', value: 'svb' },
-  { label: 'SkuVault', value: 'skuvault' },
-  { label: 'Skubana', value: 'skubana' },
-  { label: 'Slack', value: 'slack' },
-  { label: 'Square', value: 'squareup' },
-  { label: 'Stripe', value: 'stripe' },
-  { label: 'SurveyMonkey', value: 'surveymonkey' },
-  { label: 'Travis CI', value: 'travis' },
-  { label: 'Twilio', value: 'twilio' },
-  { label: 'Walmart', value: 'walmart' },
-  { label: 'Wiser', value: 'wiser' },
-  { label: 'WooCommerce', value: 'woocommerce' },
-  { label: 'X-Cart', value: 'xcart' },
-  { label: 'Zendesk', value: 'zendesk' },
-  { label: 'Zuora', value: 'zuora' },
-];
-
-options.sort((a, b) => {
-  const nameA = a.label ? a.label.toUpperCase() : '';
-  const nameB = b.label ? b.label.toUpperCase() : '';
-
-  if (nameA < nameB) return -1;
-
-  if (nameA > nameB) return 1;
-
-  return 0; // names must be equal
-});
+import { templateList } from '../../../views/TemplateList/util';
 
 export default {
   name: {
@@ -105,7 +34,7 @@ export default {
     valueDelimiter: ',',
     options: [
       {
-        items: options,
+        items: templateList,
       },
     ],
   },
