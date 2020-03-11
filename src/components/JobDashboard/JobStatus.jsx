5ae443c935f34785f89a3135a93b3298f9dfc09f
Shiva Alluri <alluriprasad@gmail.com>
2020-03-11 13:06:37 +0530

    revert IO-12446

--- src/components/JobDashboard/JobStatus.jsx
+++ src/components/JobDashboard/JobStatus.jsx
@@ -2,7 +2,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { JOB_STATUS, JOB_TYPES } from '../../utils/constants';
 import { JOB_UI_STATUS } from './util';
 import Spinner from '../Spinner';
-import StatusTag from '../../components/StatusTag';
 
 const useStyles = makeStyles({
   state: {
@@ -38,44 +37,7 @@ export default function JobStatus({ job }) {
       !job.doneExporting ||
       !job.percentComplete
     ) {
-      // return JOB_UI_STATUS[job.uiStatus];
-      let errorPercentage = 0;
-      let resolvedPercentage = 0;
-      const total =
-        (job.numSuccess || 0) + (job.numError || 0) + (job.numResolved || 0);
-
-      if (
-        [JOB_STATUS.CANCELED, JOB_STATUS.COMPLETED, JOB_STATUS.FAILED].indexOf(
-          job.status
-        ) > -1
-        // &&
-        // (job.numSuccess > 0 || job.numError > 0 || job.numResolved > 0)
-      ) {
-        if (job.numError > 0) {
-          if (job.numError < job.numResolved) {
-            errorPercentage = Math.floor((job.numError * 100) / total);
-          } else {
-            errorPercentage = Math.ceil((job.numError * 100) / total);
-          }
-        }
-
-        if (job.numResolved > 0) {
-          if (job.numResolved < job.numError) {
-            resolvedPercentage = Math.floor((job.numResolved * 100) / total);
-          } else {
-            resolvedPercentage = Math.ceil((job.numResolved * 100) / total);
-          }
-        }
-
-        return (
-          <StatusTag
-            variant="success"
-            label={JOB_UI_STATUS[job.uiStatus]}
-            errorValue={errorPercentage}
-            resolvedValue={resolvedPercentage}
-          />
-        );
-      }
+      return JOB_UI_STATUS[job.uiStatus];
     }
 
     if (job.percentComplete === 100) {
@@ -86,89 +48,12 @@ export default function JobStatus({ job }) {
   }
 
   if (job.type === JOB_TYPES.EXPORT) {
-    if (job.uiStatus !== JOB_STATUS.RUNNING) {
-      let errorPercentage = 0;
-      let resolvedPercentage = 0;
-      const total =
-        (job.numSuccess || 0) + (job.numError || 0) + (job.numResolved || 0);
-
-      if (
-        [JOB_STATUS.CANCELED, JOB_STATUS.COMPLETED, JOB_STATUS.FAILED].includes(
-          job.status
-        )
-        // &&
-        // (job.numSuccess > 0 || job.numError > 0 || job.numResolved > 0)
-      ) {
-        if (job.numError > 0) {
-          if (job.numError < job.numResolved) {
-            errorPercentage = Math.floor((job.numError * 100) / total);
-          } else {
-            errorPercentage = Math.ceil((job.numError * 100) / total);
-          }
-        }
-
-        if (job.numResolved > 0) {
-          if (job.numResolved < job.numError) {
-            resolvedPercentage = Math.floor((job.numResolved * 100) / total);
-          } else {
-            resolvedPercentage = Math.ceil((job.numResolved * 100) / total);
-          }
-        }
-
-        return (
-          <StatusTag
-            variant="success"
-            label={JOB_UI_STATUS[job.uiStatus]}
-            errorValue={errorPercentage}
-            resolvedValue={resolvedPercentage}
-          />
-        );
-      }
-    }
-
     return JOB_UI_STATUS[job.uiStatus];
   }
 
   if (job.type === JOB_TYPES.IMPORT) {
     if (job.uiStatus !== JOB_STATUS.RUNNING || !job.percentComplete) {
-      // return JOB_UI_STATUS[job.uiStatus];
-      let errorPercentage = 0;
-      let resolvedPercentage = 0;
-      const total =
-        (job.numSuccess || 0) + (job.numError || 0) + (job.numResolved || 0);
-
-      if (
-        [JOB_STATUS.CANCELED, JOB_STATUS.COMPLETED, JOB_STATUS.FAILED].indexOf(
-          job.status
-        ) > -1
-        // &&
-        // (job.numSuccess > 0 || job.numError > 0 || job.numResolved > 0)
-      ) {
-        if (job.numError > 0) {
-          if (job.numError < job.numResolved) {
-            errorPercentage = Math.floor((job.numError * 100) / total);
-          } else {
-            errorPercentage = Math.ceil((job.numError * 100) / total);
-          }
-        }
-
-        if (job.numResolved > 0) {
-          if (job.numResolved < job.numError) {
-            resolvedPercentage = Math.floor((job.numResolved * 100) / total);
-          } else {
-            resolvedPercentage = Math.ceil((job.numResolved * 100) / total);
-          }
-        }
-
-        return (
-          <StatusTag
-            variant="success"
-            label={JOB_UI_STATUS[job.uiStatus]}
-            errorValue={errorPercentage}
-            resolvedValue={resolvedPercentage}
-          />
-        );
-      }
+      return JOB_UI_STATUS[job.uiStatus];
     }
 
     if (job.percentComplete === 100) {
