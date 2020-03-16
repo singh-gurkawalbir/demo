0fb6f404f831e6f164e48998b3f0a4bc0876bf71
mounika-aetukuri <mounika.245114@gmail.com>
2020-03-16 18:40:39 +0530

    Ping changes

--- src/forms/definitions/connections/custom/http/saplitmos.js
+++ src/forms/definitions/connections/custom/http/saplitmos.js
@@ -1,18 +1,31 @@
+import { isProduction } from '../../../../utils';
+
 export default {
-  preSave: formValues => ({
-    ...formValues,
-    '/type': 'http',
-    '/assistant': 'saplitmos',
-    '/http/auth/type': 'token',
-    '/http/mediaType': 'json',
-    '/http/ping/method': 'GET',
-    '/http/baseURI': 'https://api.litmos.com/v1.svc',
-    '/http/auth/token/location': 'header',
-    '/http/auth/token/headerName': 'apikey',
-    '/http/auth/token/scheme': ' ',
-    '/http/ping/relativeURI': '/users?source=integrator.io',
-    '/http/concurrencyLevel': `${formValues['/http/concurrencyLevel']}` || 1,
-  }),
+  preSave: formValues => {
+    const retValues = { ...formValues };
+    let pingURI = '';
+
+    if (isProduction()) {
+      pingURI = '/users?source=integrator.io';
+    } else {
+      pingURI = '/users?source=staging.integrator.io';
+    }
+
+    return {
+      ...retValues,
+      '/type': 'http',
+      '/assistant': 'saplitmos',
+      '/http/auth/type': 'token',
+      '/http/mediaType': 'json',
+      '/http/ping/method': 'GET',
+      '/http/baseURI': 'https://api.litmos.com/v1.svc',
+      '/http/auth/token/location': 'header',
+      '/http/auth/token/headerName': 'apikey',
+      '/http/auth/token/scheme': ' ',
+      '/http/ping/relativeURI': pingURI,
+      '/http/concurrencyLevel': `${formValues['/http/concurrencyLevel']}` || 1,
+    };
+  },
   fieldMap: {
     name: { fieldId: 'name' },
     'http.auth.token.token': {
