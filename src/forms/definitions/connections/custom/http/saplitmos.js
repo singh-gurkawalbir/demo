c7169617c608dc439a20ebafde35ad07a7a5d78f
mounika-aetukuri <mounika.245114@gmail.com>
2020-03-16 15:13:57 +0530

    Minor changes

--- src/forms/definitions/connections/custom/http/saplitmos.js
+++ src/forms/definitions/connections/custom/http/saplitmos.js
@@ -10,7 +10,7 @@ export default {
     '/http/auth/token/location': 'header',
     '/http/auth/token/headerName': 'apikey',
     '/http/auth/token/scheme': ' ',
-    '/http/ping/relativeURI': '/users?source=staging.integrator.io',
+    '/http/ping/relativeURI': '/users?source=integrator.io',
     '/http/concurrencyLevel': `${formValues['/http/concurrencyLevel']}` || 1,
   }),
   fieldMap: {
@@ -20,7 +20,7 @@ export default {
       label: 'API Key',
       required: true,
       helpText:
-        'Please enter API Key of your SAP Litmos Account.Steps to generate API Key: Login to SAP Litmos Account -- > Click "My Profile & Settings" from the drop-down menu at the top right corner of the screen -- > View the bottom of your profile.',
+        'Please enter API Key of your SAP Litmos Account.Please note that there are multiple layers of protection in place (including AES 256 encryption) to keep your user secret safe.Steps to generate API Key: Login to SAP Litmos Account -- > Click "My Profile & Settings" from the drop-down menu at the top right corner of the screen -- > View the bottom of your profile.',
       description:
         'Note: for security reasons this field must always be re-entered.',
     },
