0e59931fa1ebee0cc821cc1fff784695455ae260
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-03-18 01:14:41 +0530

    WIP

--- src/utils/test.js
+++ src/utils/test.js
@@ -6,6 +6,7 @@ import getExistingResourcePagePath from './resource';
 import { RESOURCE_TYPE_SINGULAR_TO_PLURAL } from '../constants/resource';
 import getRequestOptions from './requestOptions';
 import actionTypes from '../actions/types';
+import resourceConflictResolution from './resourceConflictResolution';
 
 const uiRoutePathPrefix = '/pg';
 
@@ -167,4 +168,147 @@ describe('getRequestOptions util method', () => {
       expect(getRequestOptions(action, options)).toEqual(expected);
     }
   );
+});
+
+describe('resourceConflictResolution', () => {
+  /*
+  Some background to these test cases:
+    
+  master: this refers to the local unaltered of a record
+  origin: this refers to the latest copy of that record from the server
+  merged: this refers to local changes on top of master
+
+
+  */
+
+  const master = {
+    a: '1',
+    b: '2',
+    lastModified: 12,
+  };
+  const alteredMerged = {
+    a: '1',
+    b: '3',
+    lastModified: 12,
+  };
+  const unalteredOrigin = {
+    a: '1',
+    b: '2',
+    lastModified: 14,
+  };
+  const alteredOrigin = {
+    a: '0',
+    b: '2',
+    lastModified: 14,
+  };
+
+  test('should return no conflict with merged unaltered when master and origin hasn`t changed ', () => {
+    const result = resourceConflictResolution({
+      merged: alteredMerged,
+      master,
+      origin: master,
+    });
+
+    expect(result).toEqual({
+      conflict: null,
+
+      merged: alteredMerged,
+    });
+  });
+
+  test('should return no conflict with merged unaltered when master and origin has changed but values remain the same', () => {
+    const result = resourceConflictResolution({
+      merged: alteredMerged,
+      master,
+      origin: unalteredOrigin,
+    });
+
+    expect(result).toEqual({
+      conflict: null,
+
+      merged: alteredMerged,
+    });
+  });
+
+  test('should return no conflict with merged incorporating origin changes when master and merged remain the same but origin values differ from master', () => {
+    const result = resourceConflictResolution({
+      master,
+      merged: master,
+      origin: alteredOrigin,
+    });
+    const resolvedMerged = {
+      a: '0',
+      b: '2',
+      lastModified: 12,
+    };
+
+    expect(result).toEqual({
+      conflict: null,
+
+      merged: resolvedMerged,
+    });
+  });
+
+  describe('mutual properties of merged and origin has changed', () => {
+    const master = {
+      a: '1',
+      b: '2',
+      lastModified: 12,
+    };
+    const alteredMerged = {
+      a: '1',
+      b: '3',
+      lastModified: 12,
+    };
+    const alteredOrigin = {
+      a: '2',
+      b: '2',
+      lastModified: 14,
+    };
+    const unresolvableMerged = {
+      a: '4',
+      b: '3',
+      lastModified: 12,
+    };
+
+    test('should return no conflict with merged incorporating resolvable origin changes when master and origin has changed', () => {
+      const result = resourceConflictResolution({
+        master,
+        merged: alteredMerged,
+        origin: alteredOrigin,
+      });
+      const resolvedMerged = {
+        a: '2',
+        b: '3',
+        lastModified: 12,
+      };
+
+      // i apply automatic resolution when staged and master hasn't changed..then i incorporate origin changes
+
+      expect(result).toEqual({
+        conflict: null,
+
+        merged: resolvedMerged,
+      });
+    });
+    test('should return a conflict with merged incorporating resolvable origin changes when master and origin has changes are unresolvable', () => {
+      const result = resourceConflictResolution({
+        master,
+        merged: unresolvableMerged,
+        origin: alteredOrigin,
+      });
+      // in this case origin has changes....and i cannot apply any automatic resolution changes ..since i have stagged some changes which are completely
+      const resolvedMerged = {
+        a: '4',
+        b: '3',
+        lastModified: 12,
+      };
+      const conflictPatches = [{ op: 'replace', path: '/a', value: '2' }];
+
+      expect(result).toEqual({
+        conflict: conflictPatches,
+        merged: resolvedMerged,
+      });
+    });
+  });
 });
--- src/views/Resources/ConflictAlertDialog.jsx
+++ src/views/Resources/ConflictAlertDialog.jsx
@@ -1,18 +1,17 @@
-import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogActions from '@material-ui/core/DialogActions';
-import Dialog from '@material-ui/core/Dialog';
 import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
-import { useDispatch, useSelector } from 'react-redux';
-import { useCallback, useMemo } from 'react';
-import actions from '../../actions';
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
 import * as selectors from '../../reducers';
 
 const useStyles = makeStyles(theme => ({
@@ -21,35 +20,45 @@ const useStyles = makeStyles(theme => ({
     // minWidth: 700,
   },
 }));
+const SingleResourceConflictsTable = ({ resourceId, conflict }) => {
+  const classes = useStyles();
+
+  return (
+    <Table className={classes.table}>
+      <caption>{`Resource conflict for resourceId: ${resourceId}`} </caption>
+      <TableHead>
+        <TableRow>
+          <TableCell>Operation</TableCell>
+          <TableCell>Field</TableCell>
+          <TableCell>Value</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {conflict &&
+          conflict.map(p => (
+            <TableRow key={p.path}>
+              <TableCell component="th" scope="row">
+                {p.op}
+              </TableCell>
+              <TableCell>{p.path}</TableCell>
+              <TableCell>
+                {typeof p.value === 'string'
+                  ? p.value
+                  : JSON.stringify(p.value)}
+              </TableCell>
+            </TableRow>
+          ))}
+      </TableBody>
+    </Table>
+  );
+};
 
 export default function ConflictAlertDialog() {
   const allResourceConflicts = useSelector(state =>
     selectors.getAllResourceConflicts(state)
   );
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const handleClearConflict = useCallback(
-    id => {
-      dispatch(actions.resource.clearConflict(id));
-    },
-    [dispatch]
-  );
-  const handleConflict = useCallback(
-    id => {
-      dispatch(actions.resource.commitStaged(null, id));
-      dispatch(actions.resource.clearConflict(id));
-    },
-    [dispatch]
-  );
-  const firstResourceConflict = useMemo(
-    () =>
-      allResourceConflicts &&
-      allResourceConflicts.length &&
-      allResourceConflicts[0],
-    [allResourceConflicts]
-  );
 
-  if (!firstResourceConflict) return null;
+  if (!allResourceConflicts || !allResourceConflicts.length) return null;
 
   return (
     <Dialog
@@ -63,54 +72,27 @@ export default function ConflictAlertDialog() {
       </DialogTitle>
       <DialogContent>
         <Typography variant="body1">
-          The following changes have been made to this resource. They have
-          automatically been merged with your changes.
+          The following changes have been made to this resource. We have
+          attempted to automatically merge these changes, but this resolution
+          was not successful. Please the click the refresh button to refresh
+          your browser.
         </Typography>
 
-        <Table className={classes.table}>
-          <TableHead>
-            <TableRow>
-              <TableCell>Operation</TableCell>
-              <TableCell>Field</TableCell>
-              <TableCell>Value</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {firstResourceConflict &&
-              firstResourceConflict.conflict &&
-              firstResourceConflict.conflict.map(p => (
-                <TableRow key={p.path}>
-                  <TableCell component="th" scope="row">
-                    {p.op}
-                  </TableCell>
-                  <TableCell>{p.path}</TableCell>
-                  <TableCell>
-                    {typeof p.value === 'string'
-                      ? p.value
-                      : JSON.stringify(p.value)}
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
+        {allResourceConflicts.map((r, index) => (
+          <Fragment key={r.resourceId}>
+            <SingleResourceConflictsTable {...r} />
+            {index !== allResourceConflicts.length - 1 && <hr />}
+          </Fragment>
+        ))}
       </DialogContent>
       <DialogActions>
         <Button
           data-test="cancelAndReview"
           variant="contained"
           onClick={() => {
-            handleClearConflict(firstResourceConflict.resourceId);
+            window.location.reload();
           }}>
-          Cancel and Review
-        </Button>
-        <Button
-          data-test="completeCommit"
-          variant="contained"
-          onClick={() => {
-            handleConflict(firstResourceConflict.resourceId);
-          }}
-          color="secondary">
-          Complete Commit
+          Refresh
         </Button>
       </DialogActions>
     </Dialog>
