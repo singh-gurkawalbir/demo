825cdfb0a32652bed31ffa7452b6ef768f913778
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-04-02 18:47:19 +0530

    script changes WIP

--- scriptHelp.js
+++ scriptHelp.js
@@ -1,37 +1,25 @@
-import { join } from 'path';
-import { existsSync, readdirSync, lstatSync, readFileSync } from 'fs';
+import definitions from './src/forms/definitions';
 
-function fromDir(startPath, filter, callback) {
-  // console.log('Starting from dir '+startPath+'/');
+const checkForHelpText = fieldMap => {
+  Object.keys(fieldMap).forEach(key => {
+    const { helpText } = fieldMap[key];
 
-  if (!existsSync(startPath)) {
-    console.log('no dir ', startPath);
-
-    return;
-  }
-
-  const files = readdirSync(startPath);
-
-  for (let i = 0; i < files.length; i++) {
-    const filename = join(startPath, files[i]);
-    const stat = lstatSync(filename);
-
-    if (stat.isDirectory()) {
-      fromDir(filename, filter, callback); // recurse
-    } else if (filter.test(filename)) callback(filename);
-  }
-}
-
-const executeRegex = fileName => {
-  const str = readFileSync(fileName, 'utf-8');
-  const matches = /(.+):.*?{.*/.exec(str);
-  const fieldMap = matches && matches[0];
-
-  if (fieldMap) {
-    console.log('check ', fieldMap);
-  }
+    if (helpText) {
+      console.log(`${key} : ${helpText}`);
+    }
+  });
 };
 
-fromDir('./src/forms/definitions', /\.js$/, filename => {
-  executeRegex(filename);
-});
+const recurs = obj => {
+  if (!obj) return;
+
+  if (obj.fieldMap) {
+    checkForHelpText(obj.fieldMap);
+  }
+
+  Object.keys(obj).forEach(key => {
+    recurs(definitions[key]);
+  });
+};
+
+recurs(definitions);
