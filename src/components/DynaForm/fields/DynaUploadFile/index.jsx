cd22f114b74bc6390a8f532512afb9befcad3bb8
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-04-02 22:28:06 +0530

    Refactored Upload file component separating View layer and Business logic on upload of a file

---
+++ src/components/DynaForm/fields/DynaUploadFile/index.jsx
@@ -0,0 +1,140 @@
+import { FormContext } from 'react-forms-processor/dist';
+import { useEffect, useState, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import FileUploader from './FileUploader';
+import actions from '../../../../actions';
+import {
+  getFileReaderOptions,
+  getCsvFromXlsx,
+  getJSONContent,
+  getUploadedFileStatus,
+} from '../../../../utils/file';
+
+function DynaUploadFile(props) {
+  const {
+    options = '',
+    id,
+    resourceId,
+    resourceType,
+    formContext,
+    onFieldChange,
+  } = props;
+  const DEFAULT_PLACEHOLDER = 'No file chosen';
+  const dispatch = useDispatch();
+  const [fileName, setFileName] = useState(DEFAULT_PLACEHOLDER);
+  const [uploadError, setUploadError] = useState();
+  let selectedFile;
+  /*
+   * File types supported for upload are CSV, XML, XLSX and JSON
+   * For xlsx file , content gets converted to 'csv' before parsing to verify valid xlsx file
+   * For JSON file, content should be parsed from String to JSON
+   */
+  const handleFileRead = useCallback(
+    event => {
+      const { result } = event.target;
+      let fileContent = result;
+
+      if (['xlsx', 'json'].includes(options)) {
+        const { error, data } =
+          options === 'xlsx'
+            ? getCsvFromXlsx(fileContent)
+            : getJSONContent(fileContent);
+
+        if (error) {
+          return setUploadError(error);
+        }
+
+        if (options === 'json') {
+          fileContent = data;
+        }
+      }
+
+      setUploadError();
+      setFileName(selectedFile.name);
+      onFieldChange(id, fileContent);
+
+      // Dispatches an action to process uploaded file data
+      dispatch(
+        actions.sampleData.request(
+          resourceId,
+          resourceType,
+          {
+            type: options,
+            file: fileContent,
+            formValues: formContext.value,
+          },
+          'file'
+        )
+      );
+    },
+    [
+      dispatch,
+      formContext.value,
+      id,
+      onFieldChange,
+      options,
+      resourceId,
+      resourceType,
+      selectedFile,
+    ]
+  );
+
+  useEffect(() => {
+    // resets sample data on change of file type
+    if (options) {
+      dispatch(actions.sampleData.reset(resourceId));
+      onFieldChange(id, '', true);
+      setFileName(DEFAULT_PLACEHOLDER);
+      setUploadError();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch, id, options, resourceId]);
+
+  /*
+   * Gets the uploaded file and reads it based on the options provided
+   */
+  const handleFileChosen = useCallback(event => {
+    const file = event.target.files[0];
+
+    if (!file) return;
+    // Checks for file size and file types
+    const { error } = getUploadedFileStatus(file, options);
+
+    if (error) {
+      onFieldChange(id, '');
+
+      return setUploadError(error);
+    }
+
+    // TODO @Raghu come up with a better approach
+    selectedFile = file;
+    const fileReaderOptions = getFileReaderOptions(options);
+    const fileReader = new FileReader();
+
+    fileReader.onload = handleFileRead;
+
+    if (fileReaderOptions.readAsArrayBuffer) {
+      // Incase of XLSX file
+      fileReader.readAsArrayBuffer(file);
+    } else {
+      fileReader.readAsText(file);
+    }
+  }, []);
+
+  return (
+    <FileUploader
+      {...props}
+      fileName={fileName}
+      uploadError={uploadError}
+      handleFileChosen={handleFileChosen}
+    />
+  );
+}
+
+const DynaUploadFileWithFormContext = props => (
+  <FormContext.Consumer {...props}>
+    {form => <DynaUploadFile {...props} formContext={form} />}
+  </FormContext.Consumer>
+);
+
+export default DynaUploadFileWithFormContext;
