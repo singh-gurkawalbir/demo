210b601dc2a3d8c96d20390c73c0b1ee258d9d89
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-04-13 11:12:22 +0530

    performance enhancement

--- src/App/AppRouting/index.jsx
+++ src/App/AppRouting/index.jsx
@@ -60,130 +60,126 @@ const ConnectorLicenses = loadable(() =>
   import(/* webpackChunkName: 'Licenses' */ '../../views/Connector/Licenses')
 );
 
-@hot(module)
-export default class AppRouting extends Component {
-  render() {
-    // console.log('render: <AppRouting>');
+function AppRouting() {
+  // console.log('render: <AppRouting>');
 
-    return (
-      <Switch>
-        <Route
-          path="/pg"
-          exact
-          render={({ history }) => history.replace('/pg/dashboard')}
-        />
-        <Route
-          path="/pg/clone/:resourceType/:resourceId/preview"
-          component={ClonePreview}
-        />
-        <Route
-          path="/pg/clone/:resourceType/:resourceId/setup"
-          component={CloneSetup}
-        />
-        <Route
-          path="/pg/templates/:templateName([\w-]{5,})/:integrationId"
-          exact
-          render={({ history, match }) =>
-            history.replace(
-              `/pg/templates/${match.params.templateName}/${match.params.integrationId}/flows`
-            )
-          }
-        />
-        <Route
-          path="/pg/integrations/:integrationId"
-          exact
-          render={({ history, match }) =>
-            history.replace(
-              `/pg/integrations/${match.params.integrationId}/flows`
-            )
-          }
-        />
+  return (
+    <Switch>
+      <Route
+        path="/pg"
+        exact
+        render={({ history }) => history.replace('/pg/dashboard')}
+      />
+      <Route
+        path="/pg/clone/:resourceType/:resourceId/preview"
+        component={ClonePreview}
+      />
+      <Route
+        path="/pg/clone/:resourceType/:resourceId/setup"
+        component={CloneSetup}
+      />
+      <Route
+        path="/pg/templates/:templateName([\w-]{5,})/:integrationId"
+        exact
+        render={({ history, match }) =>
+          history.replace(
+            `/pg/templates/${match.params.templateName}/${match.params.integrationId}/flows`
+          )
+        }
+      />
+      <Route
+        path="/pg/integrations/:integrationId"
+        exact
+        render={({ history, match }) =>
+          history.replace(
+            `/pg/integrations/${match.params.integrationId}/flows`
+          )
+        }
+      />
 
-        <Route
-          path="/pg/marketplace/templates/:templateId"
-          exact
-          render={({ history, match }) =>
-            history.replace(
-              `/pg/marketplace/templates/${match.params.templateId}/preview`
-            )
-          }
-        />
-        <Route
-          path={[
-            '/pg/integrationapps/:integrationAppName/:integrationId/flowBuilder/:flowId',
-            '/pg/integrations/:integrationId/flowBuilder/:flowId',
-            '/pg/templates/:templateName([\\w-]{5,})/:integrationId/flowBuilder/:flowId',
-            '/pg/integrationapps/:integrationAppName/:integrationId/dataLoader/:flowId',
-            '/pg/templates/:templateName([\\w-]{5,})/:integrationId/dataLoader/:flowId',
-            '/pg/integrations/:integrationId/dataLoader/:flowId',
-          ]}>
-          <FlowBuilder />
-        </Route>
+      <Route
+        path="/pg/marketplace/templates/:templateId"
+        exact
+        render={({ history, match }) =>
+          history.replace(
+            `/pg/marketplace/templates/${match.params.templateId}/preview`
+          )
+        }
+      />
+      <Route
+        path={[
+          '/pg/integrationapps/:integrationAppName/:integrationId/flowBuilder/:flowId',
+          '/pg/integrations/:integrationId/flowBuilder/:flowId',
+          '/pg/templates/:templateName([\\w-]{5,})/:integrationId/flowBuilder/:flowId',
+          '/pg/integrationapps/:integrationAppName/:integrationId/dataLoader/:flowId',
+          '/pg/templates/:templateName([\\w-]{5,})/:integrationId/dataLoader/:flowId',
+          '/pg/integrations/:integrationId/dataLoader/:flowId',
+        ]}>
+        <FlowBuilder />
+      </Route>
 
-        <Route
-          path="/pg/integrations/:integrationId/:tab"
-          component={Integration}
-        />
-        <Route
-          path="/pg/integrationapps/:integrationAppName/:integrationId/setup"
-          component={IntegrationAppInstallation}
-        />
-        <Route
-          path="/pg/integrationapps/:integrationAppName/:integrationId/install/addNewStore"
-          component={IntegrationAppAddNewStore}
-        />
-        <Route
-          path={[
-            `/pg/integrationapps/:integrationAppName/:integrationId/uninstall/:storeId`,
-            `/pg/integrationapps/:integrationAppName/:integrationId/uninstall`,
-          ]}
-          component={IntegrationAppUninstallation}
-        />
-        <Route
-          path={[
-            '/pg/integrationapps/:integrationAppName/:integrationId/child/:storeId/:tab',
-            '/pg/integrationapps/:integrationAppName/:integrationId/:tab',
-            '/pg/integrationapps/:integrationAppName/:integrationId',
-          ]}
-          component={IntegrationApp}
-        />
-        <Route
-          // Slight hack here, Included a minimum word length of 4 for templateName to exclude add, edit to match template Name
-          // templateName has structure of application2-application2 will contain atleast 5 characters
-          path="/pg/templates/:templateName([\w-]{5,})/:integrationId/:tab"
-          component={Integration}
-        />
+      <Route
+        path="/pg/integrations/:integrationId/:tab"
+        component={Integration}
+      />
+      <Route
+        path="/pg/integrationapps/:integrationAppName/:integrationId/setup"
+        component={IntegrationAppInstallation}
+      />
+      <Route
+        path="/pg/integrationapps/:integrationAppName/:integrationId/install/addNewStore"
+        component={IntegrationAppAddNewStore}
+      />
+      <Route
+        path={[
+          `/pg/integrationapps/:integrationAppName/:integrationId/uninstall/:storeId`,
+          `/pg/integrationapps/:integrationAppName/:integrationId/uninstall`,
+        ]}
+        component={IntegrationAppUninstallation}
+      />
+      <Route
+        path={[
+          '/pg/integrationapps/:integrationAppName/:integrationId/child/:storeId/:tab',
+          '/pg/integrationapps/:integrationAppName/:integrationId/:tab',
+          '/pg/integrationapps/:integrationAppName/:integrationId',
+        ]}
+        component={IntegrationApp}
+      />
+      <Route
+        // Slight hack here, Included a minimum word length of 4 for templateName to exclude add, edit to match template Name
+        // templateName has structure of application2-application2 will contain atleast 5 characters
+        path="/pg/templates/:templateName([\w-]{5,})/:integrationId/:tab"
+        component={Integration}
+      />
 
-        <Route
-          path="/pg/connectors/:connectorId/connectorLicenses"
-          component={ConnectorLicenses}
-        />
-        <Route
-          path="/pg/connectors/:connectorId/installBase"
-          component={ConnectorInstallBase}
-        />
-        <Route
-          path="/pg/marketplace/:application"
-          component={MarketplaceList}
-        />
-        <Route exact path="/pg/marketplace" component={Marketplace} />
+      <Route
+        path="/pg/connectors/:connectorId/connectorLicenses"
+        component={ConnectorLicenses}
+      />
+      <Route
+        path="/pg/connectors/:connectorId/installBase"
+        component={ConnectorInstallBase}
+      />
+      <Route path="/pg/marketplace/:application" component={MarketplaceList} />
+      <Route exact path="/pg/marketplace" component={Marketplace} />
 
-        <Route path="/pg/dashboard" component={Dashboard} />
-        <Route path="/pg/recycleBin" component={RecycleBin} />
-        <Route path="/pg/resources" component={Resources} />
-        <Route path="/pg/editors" component={Editors} />
-        <Route path="/pg/permissions" component={Permissions} />
-        <Route path="/pg/myAccount/:tab" component={MyAccount} />
-        <Route path="/pg/templates" component={TemplateList} />
-        <Route path="/pg/accesstokens" component={AccessTokenList} />
-        <Route
-          path="/pg/tokens"
-          exact
-          render={({ history }) => history.replace('/pg/accesstokens')}
-        />
-        <Route path="/pg/:resourceType" component={ResourceList} />
-        <Route component={NotFound} />
-      </Switch>
-    );
-  }
+      <Route path="/pg/dashboard" component={Dashboard} />
+      <Route path="/pg/recycleBin" component={RecycleBin} />
+      <Route path="/pg/resources" component={Resources} />
+      <Route path="/pg/editors" component={Editors} />
+      <Route path="/pg/permissions" component={Permissions} />
+      <Route path="/pg/myAccount/:tab" component={MyAccount} />
+      <Route path="/pg/templates" component={TemplateList} />
+      <Route path="/pg/accesstokens" component={AccessTokenList} />
+      <Route
+        path="/pg/tokens"
+        exact
+        render={({ history }) => history.replace('/pg/accesstokens')}
+      />
+      <Route path="/pg/:resourceType" component={ResourceList} />
+      <Route component={NotFound} />
+    </Switch>
+  );
 }
+
+export default AppRouting;
