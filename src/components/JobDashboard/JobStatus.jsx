eca9bfd151e30b79bfec1bdcc3fe6021efa5c84d
Mohammed Azhar Uddin <azharweb90@gmail.com>
2020-04-21 17:05:43 +0530

    p1 is fixed

--- src/components/JobDashboard/JobStatus.jsx
+++ src/components/JobDashboard/JobStatus.jsx
@@ -1,13 +1,12 @@
-import { useCallback } from 'react';
+import { useCallback, Fragment } from 'react';
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { IconButton } from '@material-ui/core';
+import { Button } from '@material-ui/core';
 import StatusTag from '../../components/StatusTag';
-import HelpIcon from '../icons/HelpIcon';
 import Spinner from '../Spinner';
 import { getJobStatusDetails } from './util';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   state: {
     display: 'flex',
     alignItems: 'center',
@@ -16,7 +15,14 @@ const useStyles = makeStyles({
   spinnerWrapper: {
     marginRight: 10,
   },
-});
+  link: {
+    fontFamily: 'Roboto400',
+    color: theme.palette.primary.main,
+    '&:hover': {
+      color: theme.palette.primary.light,
+    },
+  },
+}));
 
 export default function JobStatus({ job }) {
   const classes = useStyles();
@@ -41,17 +47,30 @@ export default function JobStatus({ job }) {
 
   if (jobStatusDetails.showSpinner) {
     return (
-      <div className={classes.state}>
-        <div className={classes.spinnerWrapper}>
-          <Spinner size={24} color="primary" />
-        </div>
-        {jobStatusDetails.status}
-        {isJobInQueuedStatus && (
-          <IconButton color="secondary" onClick={handleQueuedJobsClick}>
-            <HelpIcon />
-          </IconButton>
+      <Fragment>
+        {!isJobInQueuedStatus && (
+          <div className={classes.state}>
+            <div className={classes.spinnerWrapper}>
+              <Spinner size={24} color="primary" />
+            </div>
+            {jobStatusDetails.status}
+          </div>
         )}
-      </div>
+        {isJobInQueuedStatus && (
+          <div className={classes.state}>
+            <div className={classes.spinnerWrapper}>
+              <Spinner size={24} color="primary" />
+            </div>
+            <Button
+              variant="text"
+              color="secondary"
+              className={classes.link}
+              onClick={handleQueuedJobsClick}>
+              {jobStatusDetails.status}
+            </Button>
+          </div>
+        )}
+      </Fragment>
     );
   }
 
