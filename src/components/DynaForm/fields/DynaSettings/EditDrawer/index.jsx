17711e66604858f341acc2c06be81352281f68dd
driedl <dave@riedl.ca>
2020-04-23 13:10:34 -0700

    added setting values to edit drawer, using CSS grid-layout

--- src/components/DynaForm/fields/DynaSettings/EditDrawer/index.jsx
+++ src/components/DynaForm/fields/DynaSettings/EditDrawer/index.jsx
@@ -3,24 +3,34 @@ import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import { makeStyles, Button, Typography } from '@material-ui/core';
 import actions from '../../../../../actions';
-import { isJsonString } from '../../../../../utils/string';
+// import { isJsonString } from '../../../../../utils/string';
 import CodeEditor from '../../../../CodeEditor';
 import RightDrawer from '../../../../drawer/Right';
 import DynaForm from '../../../../DynaForm';
 
-const useStyles = makeStyles({
-  content: {
-    display: 'flex',
+const useStyles = makeStyles(theme => ({
+  gridContainer: {
+    display: 'grid',
+    gridGap: theme.spacing(2),
     alignItems: 'stretch',
-    '& > div': {
-      flexGrow: 1,
-      flexBasis: '50%',
-    },
+    height: 'calc(100vh - 170px)', // full height - drawer header/footer.
+    gridTemplateColumns: '3fr 2fr',
+    gridTemplateRows: '3fr 2fr 0fr',
+    gridTemplateAreas: '"code form" "code settings" "error error"',
   },
-  editor: {
-    height: 500,
+  codeArea: {
+    gridArea: 'code',
   },
-});
+  formArea: {
+    gridArea: 'form',
+  },
+  settingsArea: {
+    gridArea: 'settings',
+  },
+  errorArea: {
+    gridArea: 'error',
+  },
+}));
 
 // TODO: add some more schema validation to this function... possibly we could
 // create a "json schema" for the forms metadata and then use a 3rd party schema
@@ -51,45 +61,46 @@ export default function EditDrawer({ resourceId, settingsForm }) {
       : '{}';
   const [formKey, setFormKey] = useState(1);
   const [alteredMeta, setAlteredMeta] = useState(defaultFormMeta);
+  const [previewValues, setPreviewValues] = useState({});
   const [validMeta, setValidMeta] = useState(
     (settingsForm && settingsForm.form) || undefined
   );
-  const [metaError, setMetaError] = useState();
+  const [errorMessage, setErrorMessage] = useState();
   const dispatch = useDispatch();
   const handleFormMetaChange = useCallback(
     value => {
       try {
         const parsedMeta = JSON.parse(value);
 
-        setMetaError();
+        setErrorMessage();
 
         if (isValidFormMetadata(parsedMeta)) {
           setValidMeta(parsedMeta);
           setFormKey(formKey + 1); // force re-render.
         } else {
-          setMetaError(
+          setErrorMessage(
             'Your form metadata does not match the required schema.'
           );
         }
       } catch (e) {
-        setMetaError('Form metadata must be valid JSON.');
+        setErrorMessage('Form metadata must be valid JSON.');
       }
 
       setAlteredMeta(value);
     },
     [formKey]
   );
-  const handleSaveFormMeta = useCallback(() => {
+  const handleFormPreviewChange = useCallback((values, isValid) => {
+    setPreviewValues(values);
+    setErrorMessage(isValid ? '' : 'Some settings are currently invalid');
+  }, []);
+  const handleCancel = useCallback(() => {
+    history.goBack();
+  }, [history]);
+  const handleSave = useCallback(() => {
     // console.log('save form meta!', alteredMeta);
-    let meta;
 
-    if (!isJsonString(alteredMeta)) {
-      setMetaError('Form metadata must be valid JSON.');
-
-      return;
-    }
-
-    dispatch(actions.customSettings.patchForm(resourceId, meta));
+    dispatch(actions.customSettings.patchForm(resourceId, alteredMeta));
     history.goBack();
   }, [alteredMeta, dispatch, history, resourceId]);
 
@@ -98,10 +109,11 @@ export default function EditDrawer({ resourceId, settingsForm }) {
       path="editSettings"
       height="tall"
       width="large"
+      type="paper"
       title="Edit settings form"
       variant="temporary">
-      <div className={classes.content}>
-        <div className={classes.editor}>
+      <div className={classes.gridContainer}>
+        <div className={classes.codeArea}>
           <CodeEditor
             name="settings-form"
             value={alteredMeta}
@@ -109,9 +121,13 @@ export default function EditDrawer({ resourceId, settingsForm }) {
             onChange={handleFormMetaChange}
           />
         </div>
-        <div>
+        <div className={classes.formArea}>
           {validMeta ? (
-            <DynaForm key={formKey} fieldMeta={validMeta} />
+            <DynaForm
+              key={formKey}
+              fieldMeta={validMeta}
+              onChange={handleFormPreviewChange}
+            />
           ) : (
             <Typography>
               A preview of your settings form will appear once you add some
@@ -119,11 +135,27 @@ export default function EditDrawer({ resourceId, settingsForm }) {
             </Typography>
           )}
         </div>
+        <div className={classes.settingsArea}>
+          <CodeEditor
+            key={previewValues}
+            name="settings"
+            value={previewValues}
+            mode="json"
+            readOnly
+          />
+        </div>
+        {errorMessage && (
+          <div className={classes.errorArea}>
+            <Typography color="error">{errorMessage}</Typography>
+          </div>
+        )}
       </div>
-      <Button disabled={!!metaError} onClick={handleSaveFormMeta}>
+      <Button disabled={!!errorMessage} onClick={handleSave}>
         Save
       </Button>
-      {metaError && <Typography color="error">{metaError}</Typography>}
+      <Button disabled={!!errorMessage} onClick={handleCancel}>
+        Cancel
+      </Button>
     </RightDrawer>
   );
 }
