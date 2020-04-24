4db94d9457fd9842f0523d9c2b0948c01c32dd5a
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-04-24 19:48:05 +0530

    Added Pagination replacing infiniteScroll

--- src/components/ErrorList/index.js
+++ src/components/ErrorList/index.js
@@ -1,17 +1,26 @@
-import React, { useCallback, useState, useEffect } from 'react';
+import { useCallback, useState, useEffect, Fragment } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TablePagination from '@material-ui/core/TablePagination';
 import list from './list';
 import actions from '../../actions';
 import { getSampleDataWrapper } from '../../reducers';
 import CeligoTable from '../CeligoTable';
 import metadata from './metadata';
 
+const useStyles = makeStyles(() => ({
+  tablePaginationRoot: { float: 'right' },
+}));
+
 export default function ErrorList() {
   const dispatch = useDispatch();
-  const size = 15;
+  const classes = useStyles();
+  const size = 100;
   const [errorList, setErrorList] = useState(list.errors.slice(0, size));
-  const [count, setCount] = useState(size);
+  const rowsPerPage = 20;
+  const [page, setPage] = useState(0);
+  const [errorsInCurrentPage, setErrorsInCurrentPage] = useState([]);
   const exportId = '5cb5b1f108eac23dd4ebe62c';
   const flowId = '5e0995566672d67d7a952eb1';
   const { status: sampleDataStatus } = useSelector(state =>
@@ -25,16 +34,21 @@ export default function ErrorList() {
 
   useEffect(() => {
     if (sampleDataStatus === 'received') {
-      const newErrorList = [
-        ...errorList,
-        ...list.errors.slice(count, count + size),
-      ];
+      const newErrorList = [...errorList, ...list.errors.slice(size, size * 2)];
 
       setErrorList(newErrorList);
-      setCount(count + size);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dispatch, sampleDataStatus]);
+
+  useEffect(() => {
+    const currentErrorList = errorList.slice(
+      page * rowsPerPage,
+      (page + 1) * rowsPerPage
+    );
+
+    setErrorsInCurrentPage(currentErrorList);
+  }, [errorList, page]);
 
   const fetchMoreData = useCallback(() => {
     dispatch(
@@ -46,15 +60,36 @@ export default function ErrorList() {
       )
     );
   }, [dispatch]);
+  const handleChangePage = useCallback(
+    (event, newPage) => setPage(newPage),
+    []
+  );
 
   return (
-    <InfiniteScroll
-      dataLength={errorList.length}
-      next={fetchMoreData}
-      hasMore
-      height={660}
-      loader={<h4>Loading...</h4>}>
-      <CeligoTable data={errorList} filterKey="errorList" {...metadata} />
-    </InfiniteScroll>
+    <Fragment>
+      <Button class="primary" onClick={fetchMoreData}>
+        Load more
+      </Button>
+      <TablePagination
+        className={classes.tablePaginationRoot}
+        component="div"
+        count={errorList.length}
+        rowsPerPageOptions={[rowsPerPage]}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        backIconButtonProps={{
+          'aria-label': 'Previous Page',
+        }}
+        nextIconButtonProps={{
+          'aria-label': 'Next Page',
+        }}
+        onChangePage={handleChangePage}
+      />
+      <CeligoTable
+        data={errorsInCurrentPage}
+        filterKey="errorList"
+        {...metadata}
+      />
+    </Fragment>
   );
 }
