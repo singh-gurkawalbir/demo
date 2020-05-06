379e85deb5ec79814c11bf70e5d27dff37ef3215
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-05-06 12:56:59 +0530

    small fix

--- src/views/MyAccount/Profile.jsx
+++ src/views/MyAccount/Profile.jsx
@@ -1,15 +1,17 @@
-import { useMemo, Fragment } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
 import { Button, InputLabel } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Fragment, useCallback, useEffect, useMemo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import useTraceUpdate from 'use-trace-update';
 import actions from '../../actions';
-import * as selectors from '../../reducers';
 import DynaForm from '../../components/DynaForm';
 import DynaSubmit from '../../components/DynaForm/DynaSubmit';
 import PanelHeader from '../../components/PanelHeader';
+import useFormInitWithPermissions from '../../hooks/useFormInitWithPermissions';
+import * as selectors from '../../reducers';
 import dateTimezones from '../../utils/dateTimezones';
-import { getDomain } from '../../utils/resource';
 import getImageUrl from '../../utils/image';
+import { getDomain } from '../../utils/resource';
 import getRoutePath from '../../utils/routePaths';
 
 const useStyles = makeStyles(theme => ({
@@ -36,25 +38,43 @@ const useStyles = makeStyles(theme => ({
     lineHeight: 0,
   },
 }));
+// const preferences = { name: 'suRYA' };
+const dateFormats = [
+  { value: 'MM/DD/YYYY', label: '12/31/1900' },
+  { value: 'DD/MM/YYYY', label: '31/12/1900' },
+  { value: 'DD-MMM-YYYY', label: '31-Dec-1900' },
+  { value: 'DD.MM.YYYY', label: '31.12.1900' },
+  { value: 'DD-MMMM-YYYY', label: '31-December-1900' },
+  { value: 'DD MMMM, YYYY', label: '31 December, 1900' },
+  { value: 'YYYY/MM/DD', label: '1900/12/31' },
+  { value: 'YYYY-MM-DD', label: '1900-12-31' },
+];
 
 export default function ProfileComponent() {
   const classes = useStyles();
-  const dateFormats = useMemo(
-    () => [
-      { value: 'MM/DD/YYYY', label: '12/31/1900' },
-      { value: 'DD/MM/YYYY', label: '31/12/1900' },
-      { value: 'DD-MMM-YYYY', label: '31-Dec-1900' },
-      { value: 'DD.MM.YYYY', label: '31.12.1900' },
-      { value: 'DD-MMMM-YYYY', label: '31-December-1900' },
-      { value: 'DD MMMM, YYYY', label: '31 December, 1900' },
-      { value: 'YYYY/MM/DD', label: '1900/12/31' },
-      { value: 'YYYY-MM-DD', label: '1900-12-31' },
-    ],
-    []
-  );
+
+  useEffect(() => {
+    console.log('component mounted');
+  }, []);
+
   const preferences = useSelector(state =>
     selectors.userProfilePreferencesProps(state)
   );
+
+  // const preferences = useSelector(
+  //   state => {
+  //     console.log('state ,');
+
+  //     return { name: 'surya' };
+  //   },
+  //   (left, right) => {
+  //     console.log('shallow equal ,', shallowEqual(left.name, right.name));
+
+  //     return left.name === right.name;
+  //   }
+  // );
+
+  console.log('check preferences ', preferences);
   const dateTimeZonesList = useMemo(
     () => [
       {
@@ -81,7 +101,7 @@ export default function ProfileComponent() {
           [],
       },
     ],
-    [dateFormats]
+    []
   );
   const timeFormatList = useMemo(
     () => [
@@ -101,126 +121,146 @@ export default function ProfileComponent() {
     []
   );
   const dispatch = useDispatch();
-  const handleSubmit = formVal => {
-    const completePayloadCopy = { ...formVal };
-    const { timeFormat, dateFormat } = completePayloadCopy;
-    const preferencesPayload = { timeFormat, dateFormat };
+  const handleSubmit = useCallback(
+    formVal => {
+      const completePayloadCopy = { ...formVal };
+      const { timeFormat, dateFormat } = completePayloadCopy;
+      const preferencesPayload = { timeFormat, dateFormat };
 
-    dispatch(actions.user.preferences.update(preferencesPayload));
-    // deleting preferenecs from completePayloadCopy
-    delete completePayloadCopy.timeFormat;
-    delete completePayloadCopy.dateFormat;
-    dispatch(actions.user.profile.update(completePayloadCopy));
-  };
-
-  const handleLinkWithGoogle = () => {
+      dispatch(actions.user.preferences.update(preferencesPayload));
+      // deleting preferenecs from completePayloadCopy
+      delete completePayloadCopy.timeFormat;
+      delete completePayloadCopy.dateFormat;
+      dispatch(actions.user.profile.update(completePayloadCopy));
+    },
+    [dispatch]
+  );
+  const handleLinkWithGoogle = useCallback(() => {
     dispatch(actions.auth.linkWithGoogle(getRoutePath('/myAccount/profile')));
-  };
-
-  const handleUnLinkWithGoogle = () => {
+  }, [dispatch]);
+  const handleUnLinkWithGoogle = useCallback(() => {
     dispatch(actions.user.profile.unlinkWithGoogle());
-  };
+  }, [dispatch]);
+  const fieldMeta = useMemo(
+    () => ({
+      fieldMap: {
+        name: {
+          id: 'name',
+          name: 'name',
+          type: 'text',
+          label: 'Name',
+          required: true,
+        },
+        email: {
+          id: 'email',
+          name: 'email',
+          type: 'useremail',
+          label: 'Email',
+        },
+        password: {
+          id: 'password',
+          name: 'password',
+          type: 'userpassword',
+        },
+        company: {
+          id: 'company',
+          name: 'company',
+          type: 'text',
+          label: 'Company',
+        },
+        phone: {
+          id: 'phone',
+          name: 'phone',
+          type: 'text',
+          label: 'Phone',
+        },
+        role: {
+          id: 'role',
+          name: 'role',
+          type: 'text',
+          label: 'Role',
+        },
+        timezone: {
+          id: 'timezone',
+          name: 'timezone',
+          type: 'select',
+          label: 'Time Zone',
+          required: true,
 
-  const fieldMeta = {
-    fieldMap: {
-      name: {
-        id: 'name',
-        name: 'name',
-        type: 'text',
-        label: 'Name',
-        required: true,
-        defaultValue: preferences && preferences.name,
+          options: dateTimeZonesList,
+        },
+        dateFormat: {
+          id: 'dateFormat',
+          name: 'dateFormat',
+          type: 'select',
+          required: true,
+          label: 'Date format',
+
+          options: dateFormatList,
+        },
+        timeFormat: {
+          id: 'timeFormat',
+          name: 'timeFormat',
+          type: 'select',
+          required: true,
+          label: 'Time Format',
+
+          options: timeFormatList,
+        },
+        developer: {
+          id: 'developer',
+          name: 'developer',
+          type: 'checkbox',
+          label: 'Developer Mode',
+        },
       },
-      email: {
-        id: 'email',
-        name: 'email',
-        type: 'useremail',
-        label: 'Email',
-        value: preferences && preferences.email,
+      layout: {
+        fields: [
+          'name',
+          'email',
+          'password',
+          'company',
+          'role',
+          'phone',
+          'timezone',
+          'dateFormat',
+          'timeFormat',
+          'developer',
+        ],
       },
-      password: {
-        id: 'password',
-        name: 'password',
-        type: 'userpassword',
-      },
-      company: {
-        id: 'company',
-        name: 'company',
-        type: 'text',
-        label: 'Company',
-        defaultValue: preferences && preferences.company,
-      },
-      phone: {
-        id: 'phone',
-        name: 'phone',
-        type: 'text',
-        label: 'Phone',
-        defaultValue: preferences && preferences.phone,
-      },
-      role: {
-        id: 'role',
-        name: 'role',
-        type: 'text',
-        label: 'Role',
-        defaultValue: preferences && preferences.role,
-      },
-      timezone: {
-        id: 'timezone',
-        name: 'timezone',
-        type: 'select',
-        label: 'Time Zone',
-        required: true,
-        defaultValue: preferences && preferences.timezone,
-        options: dateTimeZonesList,
-      },
-      dateFormat: {
-        id: 'dateFormat',
-        name: 'dateFormat',
-        type: 'select',
-        required: true,
-        label: 'Date format',
-        defaultValue: preferences && preferences.dateFormat,
-        options: dateFormatList,
-      },
-      timeFormat: {
-        id: 'timeFormat',
-        name: 'timeFormat',
-        type: 'select',
-        required: true,
-        label: 'Time Format',
-        defaultValue: preferences && preferences.timeFormat,
-        options: timeFormatList,
-      },
-      developer: {
-        id: 'developer',
-        name: 'developer',
-        type: 'checkbox',
-        label: 'Developer Mode',
-        defaultValue: preferences && preferences.developer,
-      },
-    },
-    layout: {
-      fields: [
-        'name',
-        'email',
-        'password',
-        'company',
-        'role',
-        'phone',
-        'timezone',
-        'dateFormat',
-        'timeFormat',
-        'developer',
-      ],
-    },
-  };
+    }),
+    [dateFormatList, dateTimeZonesList, timeFormatList]
+  );
+  const metaValue = useMemo(() => {
+    console.log('re executing');
+
+    return {
+      name: preferences && preferences.name,
+      email: preferences && preferences.email,
+      company: preferences && preferences.company,
+      phone: preferences && preferences.phone,
+      role: preferences && preferences.role,
+      timezone: preferences && preferences.timezone,
+      dateFormat: preferences && preferences.dateFormat,
+      timeFormat: preferences && preferences.timeFormat,
+      developer: preferences && preferences.developer,
+    };
+  }, [preferences]);
+  const formKey = useFormInitWithPermissions({
+    fieldsMeta: fieldMeta,
+    metaValue,
+  });
+
+  useTraceUpdate(preferences);
 
   return (
     <Fragment>
       <PanelHeader title="Profile" />
-      <DynaForm fieldMeta={fieldMeta} render>
-        <DynaSubmit onClick={handleSubmit}>Save</DynaSubmit>
-      </DynaForm>
+      <DynaForm formKey={formKey} fieldMeta={fieldMeta} />
+      <DynaSubmit formKey={formKey} onClick={handleSubmit}>
+        Save
+      </DynaSubmit>
+
       {getDomain() !== 'eu.integrator.io' && (
         <div>
           <PanelHeader
