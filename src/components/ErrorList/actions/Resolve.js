ef0bc2f528047872973b4c07438292f9a96c508e
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-05-06 19:44:07 +0530

    Added retry/resolve actions row level

--- src/components/ErrorList/actions/Resolve.js
+++ src/components/ErrorList/actions/Resolve.js
@@ -1,9 +1,20 @@
 import { Fragment, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import actions from '../../../actions';
 
 export default {
   label: 'Resolve',
-  component: function Resolve() {
-    const handleClick = useCallback(() => {}, []);
+  component: function Resolve({ flowId, resourceId, resource }) {
+    const dispatch = useDispatch();
+    const handleClick = useCallback(() => {
+      dispatch(
+        actions.errorManager.flowErrorDetails.retry({
+          flowId,
+          resourceId,
+          errorIds: [resource.errorId],
+        })
+      );
+    }, [dispatch, flowId, resource.errorId, resourceId]);
 
     return (
       <Fragment>
--- src/components/ErrorList/actions/Retry.js
+++ src/components/ErrorList/actions/Retry.js
@@ -1,9 +1,20 @@
 import { Fragment, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import actions from '../../../actions';
 
 export default {
   label: 'Retry',
-  component: function Retry() {
-    const handleClick = useCallback(() => {}, []);
+  component: function Retry({ flowId, resourceId, resource }) {
+    const dispatch = useDispatch();
+    const handleClick = useCallback(() => {
+      dispatch(
+        actions.errorManager.flowErrorDetails.retry({
+          flowId,
+          resourceId,
+          errorIds: [resource.errorId],
+        })
+      );
+    }, [dispatch, flowId, resource.errorId, resourceId]);
 
     return (
       <Fragment>
