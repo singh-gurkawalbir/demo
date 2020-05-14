1f4a42f2a47d5c4322dce3da9293e2f38aa691cf
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-05-14 12:50:56 +0530

    Refactor

--- src/components/ErrorList/OpenErrors/index.jsx
+++ src/components/ErrorList/OpenErrors/index.jsx
@@ -1,11 +1,5 @@
 import { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  useRouteMatch,
-  useHistory,
-  matchPath,
-  useLocation,
-} from 'react-router-dom';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import actions from '../../../actions';
@@ -20,8 +14,7 @@ import ErrorTable from '../ErrorTable';
 import RefreshCard from '../components/RefreshCard';
 import ErrorActions from '../components/ErrorActions';
 import Spinner from '../../Spinner';
-import RightDrawer from '../../drawer/Right';
-import ErrorDetails from '../ErrorDetails';
+import ErrorDetailsDrawer from './ErrorDetailsDrawer';
 
 const useStyles = makeStyles(theme => ({
   tablePaginationRoot: {
@@ -49,18 +42,9 @@ const defaultFilter = {
 export default function OpenErrors({ flowId, resourceId, show }) {
   const dispatch = useDispatch();
   const classes = useStyles();
-  const match = useRouteMatch();
-  const { pathname } = useLocation();
-  const history = useHistory();
   const filterKey = `openErrors-${flowId}-${resourceId}`;
   const errorFilter = useSelector(
     state => filter(state, filterKey) || defaultFilter
-  );
-  const { errors: allErrors = [] } = useSelector(state =>
-    resourceErrors(state, {
-      flowId,
-      resourceId,
-    })
   );
   const {
     status,
@@ -154,33 +138,6 @@ export default function OpenErrors({ flowId, resourceId, show }) {
     [dispatch, flowId, resourceId]
   );
 
-  const handleClose = useCallback(() => {
-    history.goBack();
-  }, [history]);
-  // Controls the nested drawer to open error details only when it is a valid errorId
-  // TODO : @Raghu check for a better way to control
-  const showDrawer = useMemo(() => {
-    if (!match.isExact) {
-      const matchErrorPath = matchPath(pathname, {
-        path: `${match.url}/details/:errorId/:mode`,
-      });
-
-      if (!matchErrorPath || !matchErrorPath.params) return false;
-      const { errorId } = matchErrorPath.params;
-
-      if (!errorId) return false;
-
-      return allErrors.some(error => error.errorId === errorId);
-    }
-  }, [allErrors, match.isExact, match.url, pathname]);
-
-  useEffect(() => {
-    if (!showDrawer && allErrors.length && !match.isExact) {
-      // Incase of an invalid nested url, redirects to current url
-      history.replace(match.url);
-    }
-  }, [allErrors.length, history, match.isExact, match.url, showDrawer]);
-
   return (
     <div className={clsx({ [classes.hide]: !show })}>
       {!isFreshDataLoad ? <RefreshCard onRefresh={requestOpenErrors} /> : null}
@@ -203,21 +160,7 @@ export default function OpenErrors({ flowId, resourceId, show }) {
           emptyRowsLabel="No Open errors"
         />
       )}
-
-      {showDrawer ? (
-        <RightDrawer
-          path="details/:errorId/:mode"
-          width="full"
-          title="Error Record"
-          variant="temporary"
-          hideBackButton>
-          <ErrorDetails
-            flowId={flowId}
-            resourceId={resourceId}
-            onClose={handleClose}
-          />
-        </RightDrawer>
-      ) : null}
+      <ErrorDetailsDrawer flowId={flowId} resourceId={resourceId} />
     </div>
   );
 }
