3d75c9bf32e24905e637aac0ba89417d276a0cb3
adibjp <adibjp441@gmail.com>
2020-05-24 14:55:20 +0530

    IO-15340

--- src/views/MyAccount/ChangeEmail.jsx
+++ src/views/MyAccount/ChangeEmail.jsx
@@ -1,97 +1,106 @@
-import { Component, Fragment } from 'react';
+import { Fragment, useCallback } from 'react';
 import TextField from '@material-ui/core/TextField';
-import { connect } from 'react-redux';
-import { Button, Typography } from '@material-ui/core';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  Button,
+  Typography,
+  makeStyles,
+  FormControl,
+  FormLabel,
+} from '@material-ui/core';
+import * as selectors from '../../reducers';
 import actions from '../../actions';
 import ModalDialog from '../../components/ModalDialog';
-import {
-  changeEmailFailure,
-  changeEmailSuccess,
-  changeEmailMsg,
-} from '../../reducers';
+import FieldHelp from '../../components/DynaForm/FieldHelp';
 
-const mapDispatchToProps = dispatch => ({
-  changeEmail: message => {
-    dispatch(actions.auth.changeEmail(message));
+const useStyles = makeStyles(() => ({
+  textFormControl: {
+    width: '100%',
   },
-});
-const mapStateToProps = state => ({
-  error: changeEmailFailure(state),
-  success: changeEmailSuccess(state),
-  message: changeEmailMsg(state),
-});
+}));
 
-class ChangeEmail extends Component {
-  handleOnSubmit = e => {
-    e.preventDefault();
-    const payload = {
-      newEmail: e.target.newEmail.value,
-      password: e.target.password.value,
-    };
+export default function ChangeEmail(props) {
+  const { show, onhandleClose } = props;
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const error = useSelector(state => selectors.changeEmailFailure(state));
+  const success = useSelector(state => selectors.changeEmailSuccess(state));
+  const message = useSelector(state => selectors.changeEmailMsg(state));
+  const handleOnSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const payload = {
+        newEmail: e.target.newEmail.value,
+        password: e.target.password.value,
+      };
 
-    this.props.changeEmail(payload);
-  };
+      dispatch(actions.auth.changeEmail(payload));
+    },
+    [dispatch]
+  );
 
-  render() {
-    const { show, onhandleClose, error, success, message } = this.props;
-
-    return (
-      <ModalDialog show={show} onClose={onhandleClose}>
-        Change Email
-        {success ? (
-          <Typography variant="body2">{message}</Typography>
-        ) : (
-          <div>
-            <form id="changeEmailForm" onSubmit={this.handleOnSubmit}>
-              <TextField
-                id="newEmail"
-                label="New Email"
-                margin="normal"
-                fullWidth
-                variant="filled"
-              />
+  return (
+    <ModalDialog show={show} onClose={onhandleClose}>
+      Change email
+      {success ? (
+        <Typography variant="body2">{message}</Typography>
+      ) : (
+        <div>
+          <form id="changeEmailForm" onSubmit={handleOnSubmit}>
+            <FormControl className={classes.textFormControl}>
+              <div className={classes.dynaTextLabelWrapper}>
+                <FormLabel htmlFor="newEmail" required>
+                  New email
+                </FormLabel>
+                {/* <FieldHelp /> */}
+              </div>
+              <TextField id="newEmail" fullWidth required variant="filled" />
               <br />
+            </FormControl>
 
+            <FormControl className={classes.textFormControl}>
+              <div className={classes.dynaTextLabelWrapper}>
+                <FormLabel htmlFor="newEmail" required>
+                  Password
+                </FormLabel>
+                {/* <FieldHelp /> */}
+              </div>
               <TextField
                 id="password"
-                label="Password"
-                margin="normal"
                 type="password"
                 variant="filled"
+                required
                 fullWidth
               />
-            </form>
-            <Typography variant="body2">
-              Note: we require your current password again to help safeguard
-              your integrator.io account.
+            </FormControl>
+          </form>
+          <Typography variant="body2">
+            Note: we require your current password again to help safeguard your
+            integrator.io account.
+          </Typography>
+        </div>
+      )}
+      {success ? (
+        <span />
+      ) : (
+        <Fragment>
+          {error && (
+            <Typography variant="body2" component="span" color="error">
+              {message}
             </Typography>
-          </div>
-        )}
-        {success ? (
-          <span />
-        ) : (
-          <Fragment>
-            {error && (
-              <Typography variant="body2" component="span" color="error">
-                {message}
-              </Typography>
-            )}
+          )}
 
-            <Button
-              data-test="changeEmail"
-              variant="outlined"
-              color="primary"
-              type="submit"
-              form="changeEmailForm"
-              value="Submit">
-              Change email
-            </Button>
-          </Fragment>
-        )}
-      </ModalDialog>
-    );
-  }
+          <Button
+            data-test="changeEmail"
+            variant="outlined"
+            color="primary"
+            type="submit"
+            form="changeEmailForm"
+            value="Submit">
+            Change email
+          </Button>
+        </Fragment>
+      )}
+    </ModalDialog>
+  );
 }
-
-// prettier-ignore
-export default connect(mapStateToProps,mapDispatchToProps)(ChangeEmail);
