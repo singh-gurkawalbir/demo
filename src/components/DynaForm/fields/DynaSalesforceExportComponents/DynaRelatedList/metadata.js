7e65c2694c94226ac3883ae76cdec5a5e5e004e8
adibjp <adibjp441@gmail.com>
2020-06-04 21:53:25 +0530

    IO-15572

--- src/components/DynaForm/fields/DynaSalesforceExportComponents/DynaRelatedList/metadata.js
+++ src/components/DynaForm/fields/DynaSalesforceExportComponents/DynaRelatedList/metadata.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import DeleteIcon from '../../../../icons/TrashIcon';
 import EditIcon from '../../../../icons/EditIcon';
 
