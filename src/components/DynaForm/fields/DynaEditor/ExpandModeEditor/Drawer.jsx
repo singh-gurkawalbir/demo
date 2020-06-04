271d5f1c642cb0c27abf42c59ec3c2ca83d02b73
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-06-04 11:02:47 +0530

    lint fixes

--- src/components/DynaForm/fields/DynaEditor/ExpandModeEditor/Drawer.jsx
+++ src/components/DynaForm/fields/DynaEditor/ExpandModeEditor/Drawer.jsx
@@ -1,10 +1,10 @@
-import { useState, useCallback } from 'react';
+import React, { useState, useCallback } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Drawer } from '@material-ui/core';
 import clsx from 'clsx';
 import { useSelector } from 'react-redux';
 import Button from '@material-ui/core/Button';
-import CodeEditor from '../../../../../components/CodeEditor';
+import CodeEditor from '../../../../CodeEditor';
 import DrawerTitleBar from '../../../../drawer/TitleBar';
 import * as selectors from '../../../../../reducers';
 
--- src/components/DynaForm/fields/DynaEditor/ExpandModeEditor/index.jsx
+++ src/components/DynaForm/fields/DynaEditor/ExpandModeEditor/index.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import EditorDrawer from './Drawer';
 import EditorModal from './Modal';
 
