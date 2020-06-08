5dfd7f32f76d395933805a7769d7f6bf776944b3
adibjp <adibjp441@gmail.com>
2020-06-08 19:42:27 +0530

    todo comment

--- src/components/DynaForm/fields/DynaCsvHasHeaderRow.jsx
+++ src/components/DynaForm/fields/DynaCsvHasHeaderRow.jsx
@@ -11,6 +11,9 @@ export default function DynaCsvHasHeaderRow({
   const handleFieldChange = useCallback(
     (_id, newVal) => {
       onFieldChange(id, newVal);
+      // TODO: look for better way. I guess, once form refactor is done, this could be removed
+      // keyColumn field is dependent on hasHeader Row, and also, it should reset on clearing hasHeader row.
+      // The field could be reset inside inside DynaFileKeyColumn as well where it looks for change in useEffect and resets the value.
       setTimeout(() => {
         if (fieldToReset) onFieldChange(fieldToReset, fieldResetValue);
       }, 500)
