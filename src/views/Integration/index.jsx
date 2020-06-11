c323b220c8d6e742b3329f79e68005668a70d1af
Sravan Dandra <sravan.dandra@gmail.com>
2020-06-11 17:15:30 +0530

    Minor fixes

--- src/views/Integration/index.jsx
+++ src/views/Integration/index.jsx
@@ -8,8 +8,13 @@ import IntegrationDIY from './DIY';
 export default function Integration() {
   const match = useRouteMatch()
   const { integrationId, storeId } = match.params;
+  const isIntegrationApp = useSelector(state => {
+    const integration = selectors.resource(state, 'integrations', integrationId);
+
+    return !!(integration && integration._connectorId)
+  })
   const isFrameWork2 = useSelector(state => selectors.isIntegrationAppVersion2(state, integrationId, true));
-  return isFrameWork2 ?
-    <IntegrationDIY match={match} {...match.params} childId={storeId} /> :
-    <IntegrationApp match={match} {...match.params} />
+  return isIntegrationApp && !isFrameWork2 ?
+    <IntegrationApp match={match} {...match.params} /> :
+    <IntegrationDIY match={match} {...match.params} childId={storeId} />
 }
