5ae0812430c178694b0d4302379dfa76c81e18a4
mounika-aetukuri <mounika.245114@gmail.com>
2020-06-12 19:47:52 +0530

    IO-15672

--- src/views/Integration/App/panels/Admin/sections/Subscription/index.jsx
+++ src/views/Integration/App/panels/Admin/sections/Subscription/index.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { Fragment } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { useRouteMatch, Link } from 'react-router-dom';
