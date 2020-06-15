02a7ed7aa452568c0dd6b9324b2813b464dc9aa0
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2020-06-15 14:49:00 +0530

    dummy commit

--- src/components/DynaForm/fields/DynaFormView.jsx
+++ src/components/DynaForm/fields/DynaFormView.jsx
@@ -17,7 +17,7 @@ import { emptyObject } from '../../../utils/constants';
 
 const emptyObj = {};
 const isParent = true;
-
+export const a = () => {};
 export function FormView(props) {
   const { resourceType, flowId, resourceId, formContext, value } = props;
   const dispatch = useDispatch();
