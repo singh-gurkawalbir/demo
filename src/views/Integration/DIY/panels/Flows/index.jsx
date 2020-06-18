6d9d2fe8b9fe363d63cd1a1af88f5eb6afb0cee5
Yang <yang.jiang@celigo.com>
2020-06-17 23:51:00 -0700

    IO-15696 close attach flow modal dialog

--- src/views/Integration/DIY/panels/Flows/index.jsx
+++ src/views/Integration/DIY/panels/Flows/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useEffect } from 'react';
+import React, { useState, useMemo, useEffect, useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core';
@@ -78,6 +78,9 @@ export default function FlowsPanel({ integrationId, childId }) {
   const isUserInErrMgtTwoDotZero = useSelector(state =>
     selectors.isUserInErrMgtTwoDotZero(state)
   );
+  const handleClose = useCallback(() => {
+    setShowDialog();
+  }, [setShowDialog]);
 
   useEffect(() => {
     if (!status && isUserInErrMgtTwoDotZero) {
@@ -112,7 +115,7 @@ export default function FlowsPanel({ integrationId, childId }) {
       {showDialog && (
         <AttachFlowsDialog
           integrationId={integrationId}
-          onClose={setShowDialog}
+          onClose={handleClose}
         />
       )}
       <MappingDrawer integrationId={integrationId} />
