19878efe9816dfaff9b4571a1b04aac99cd8c3bf
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-06-18 16:15:57 +0530

    Added changes to Invoke field

--- src/components/DynaForm/fields/DynaApiIdentifier.jsx
+++ src/components/DynaForm/fields/DynaApiIdentifier.jsx
@@ -1,32 +1,51 @@
 import React from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { makeStyles } from '@material-ui/styles';
-import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import DynaText from './DynaText';
 import { getApiUrl } from '../../../utils/resource';
-import FieldHelp from '../FieldHelp';
 
 const useStyles = makeStyles(theme => ({
-  text: {
-    fontSize: theme.spacing(2),
-    marginRight: theme.spacing(0.5),
-  },
   dynaAPIWrapper: {
     flexDirection: 'row !important',
-    alignItems: 'center',
+  },
+  children: {
+    flex: 1,
+  },
+  copyInvokeUrlBtn: {
+    marginTop: 26,
+    marginLeft: theme.spacing(1),
   },
 }));
 
 export default function DynaApiIdentifier(props) {
-  const { value, id } = props;
+  const { value } = props;
   const classes = useStyles();
   const apiUrl = getApiUrl();
-
+  const invokeUrl = `${apiUrl}/${value}`
   return (
     <div className={classes.dynaAPIWrapper}>
-      <Typography data-test={id} className={classes.text}>
-        Invoke :{` ${apiUrl}/${value}`}
-      </Typography>
-      <FieldHelp {...props} />
+      <DynaText
+        {...props}
+        disabled
+        className={classes.children}
+        value={invokeUrl}
+        />
+      <div className={classes.copyInvokeUrlBtn}>
+        <CopyToClipboard
+          text={invokeUrl}>
+          <Button
+            data-test="copyToClipboard"
+            title="Copy to clipboard"
+            variant="outlined"
+            color="secondary">
+            Copy URL
+          </Button>
+        </CopyToClipboard>
+      </div>
+
+
     </div>
   );
 }
