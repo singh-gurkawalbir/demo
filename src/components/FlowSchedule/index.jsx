233a6b3d70c7ef390a196e03798f0b701ec938d4
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-06-24 10:04:20 +0530

    Fix for schedule drawer

--- src/components/FlowSchedule/index.jsx
+++ src/components/FlowSchedule/index.jsx
@@ -15,6 +15,7 @@ import {
   getScheduleVal,
 } from './util';
 import useSelectorMemo from '../../hooks/selectors/useSelectorMemo';
+import useSaveStatusIndicator from '../../hooks/useSaveStatusIndicator';
 
 const exportFilterConfig = { type: 'exports' };
 const flowsFilterConfig = { type: 'flows' };
@@ -49,8 +50,61 @@ export default function FlowSchedule({
   let resource = pg || flow;
   const schedule = (pg && pg.schedule) || flow.schedule;
   const scheduleStartMinute = getScheduleStartMinute(exp || flow, preferences);
-  const handleSubmit = useCallback(
-    formVal => {
+
+  const onSave = useCallback((formVal) => {
+    const scheduleVal = getScheduleVal(formVal, scheduleStartMinute);
+    const patchSet = [
+      {
+        op: 'replace',
+        path:
+          pg && pg._exportId
+            ? `/pageGenerators/${index}/schedule`
+            : '/schedule',
+        value: scheduleVal,
+      },
+      {
+        op: 'replace',
+        path:
+          pg && pg._exportId
+            ? `/pageGenerators/${index}/_keepDeltaBehindFlowId`
+            : '/_keepDeltaBehindFlowId',
+        value:
+          formVal && formVal._keepDeltaBehindFlowId
+            ? formVal._keepDeltaBehindFlowId
+            : undefined,
+      },
+      {
+        op: 'replace',
+        path:
+          pg && pg._exportId
+            ? `/pageGenerators/${index}/_keepDeltaBehindExportId`
+            : '/_keepDeltaBehindExportId',
+        value:
+          pg && pg._exportId && formVal && formVal._keepDeltaBehindFlowId
+            ? formVal._keepDeltaBehindExportId
+            : undefined,
+      },
+    ];
+    const sanitized = sanitizePatchSet({
+      patchSet,
+      flow,
+      skipRemovePatches: true,
+    });
+
+    dispatch(actions.resource.patchStaged(flow._id, sanitized, 'value'));
+    dispatch(actions.resource.commitStaged('flows', flow._id, 'value'));
+  }, [dispatch, flow, pg, index, scheduleStartMinute]);
+
+  const { submitHandler, disableSave, defaultLabels} = useSaveStatusIndicator(
+    {
+      path: `/flows/${flow._id}`,
+      onSave,
+      onClose,
+    }
+  );
+
+  const handleValidateAndSubmit = useCallback(
+    closeOnSave => formVal => {
       if (
         formVal.startTime &&
         formVal.endTime &&
@@ -61,51 +115,10 @@ export default function FlowSchedule({
           variant: 'error',
         });
       }
-
-      const scheduleVal = getScheduleVal(formVal, scheduleStartMinute);
-      const patchSet = [
-        {
-          op: 'replace',
-          path:
-            pg && pg._exportId
-              ? `/pageGenerators/${index}/schedule`
-              : '/schedule',
-          value: scheduleVal,
-        },
-        {
-          op: 'replace',
-          path:
-            pg && pg._exportId
-              ? `/pageGenerators/${index}/_keepDeltaBehindFlowId`
-              : '/_keepDeltaBehindFlowId',
-          value:
-            formVal && formVal._keepDeltaBehindFlowId
-              ? formVal._keepDeltaBehindFlowId
-              : undefined,
-        },
-        {
-          op: 'replace',
-          path:
-            pg && pg._exportId
-              ? `/pageGenerators/${index}/_keepDeltaBehindExportId`
-              : '/_keepDeltaBehindExportId',
-          value:
-            pg && pg._exportId && formVal && formVal._keepDeltaBehindFlowId
-              ? formVal._keepDeltaBehindExportId
-              : undefined,
-        },
-      ];
-      const sanitized = sanitizePatchSet({
-        patchSet,
-        flow,
-        skipRemovePatches: true,
-      });
-
-      dispatch(actions.resource.patchStaged(flow._id, sanitized, 'value'));
-      dispatch(actions.resource.commitStaged('flows', flow._id, 'value'));
-      onClose();
+      // If valid form values
+      submitHandler(closeOnSave)(formVal);
     },
-    [dispatch, enqueueSnackbar, flow, index, onClose, pg, scheduleStartMinute]
+    [enqueueSnackbar, submitHandler]
   );
 
   resource = setValues(resource, schedule, scheduleStartMinute);
@@ -136,8 +149,11 @@ export default function FlowSchedule({
         disabled={disabled}
         fieldMeta={fieldMeta}
         optionsHandler={fieldMeta.optionsHandler}>
-        <DynaSubmit onClick={handleSubmit} color="primary">
-          Save
+        <DynaSubmit onClick={handleValidateAndSubmit()} color="primary" disabled={disableSave}>
+          {defaultLabels.saveLabel}
+        </DynaSubmit>
+        <DynaSubmit onClick={handleValidateAndSubmit(true)} color="primary" disabled={disableSave}>
+          {defaultLabels.saveAndCloseLabel}
         </DynaSubmit>
         <Button onClick={onClose} variant="text" color="primary">
           Cancel
