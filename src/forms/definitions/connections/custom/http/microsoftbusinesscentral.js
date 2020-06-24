a85fafcf41fdc8574cbde129e24e08a1e3dd29d2
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2020-06-24 14:40:28 +0530

    IO-13172

--- src/forms/definitions/connections/custom/http/microsoftbusinesscentral.js
+++ src/forms/definitions/connections/custom/http/microsoftbusinesscentral.js
@@ -5,7 +5,9 @@ export default {
     '/assistant': 'microsoftbusinesscentral',
     '/http/auth/type': 'oauth',
     '/http/mediaType': 'json',
-    '/http/baseURI': 'https://api.businesscentral.dynamics.com',
+    '/http/baseURI': `https://api.businesscentral.dynamics.com/v2.0/${
+      formValues['/http/unencrypted/environmentName']
+    }/api/v1.0`,
     '/http/auth/oauth/authURI':
       'https://login.microsoftonline.com/common/oauth2/authorize',
     '/http/auth/oauth/tokenURI':
@@ -20,6 +22,14 @@ export default {
   }),
   fieldMap: {
     name: { fieldId: 'name' },
+    'http.unencrypted.environmentName': {
+      type: 'text',
+      id: 'http.unencrypted.environmentName',
+      helpText:
+        "Enter your Microsoft Dynamics 365 Business Central's enviornment.",
+      label: 'Environment Name',
+      required: true,
+    },
     application: {
       fieldId: 'application',
     },
@@ -28,7 +38,7 @@ export default {
   layout: {
     type: 'collapse',
     containers: [
-      { collapsed: true, label: 'General', fields: ['name', 'application'] },
+      { collapsed: true, label: 'General', fields: ['name', 'application', 'http.unencrypted.environmentName'] },
       { collapsed: true, label: 'Advanced', fields: ['httpAdvanced'] },
     ],
   },
