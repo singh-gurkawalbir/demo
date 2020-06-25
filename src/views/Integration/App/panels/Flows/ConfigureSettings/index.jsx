5793c4f929e409fad3f554a5ca93217673e303ee
ashu-gupta <gupta.ashu1602@gmail.com>
2020-06-25 15:42:54 +0530

    IO-15711 (IA1.0 settings revamp)

--- src/views/Integration/App/panels/Flows/ConfigureSettings/index.jsx
+++ src/views/Integration/App/panels/Flows/ConfigureSettings/index.jsx
@@ -1,39 +1,28 @@
 import React, { useMemo } from 'react';
 import clsx from 'clsx';
 import { useSelector, shallowEqual } from 'react-redux';
-import { Route, useRouteMatch } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
 import * as selectors from '../../../../../../reducers';
 import { integrationSettingsToDynaFormMetadata } from '../../../../../../forms/utils';
-import DrawerTitleBar from '../../../../../../components/drawer/TitleBar';
 import LoadResources from '../../../../../../components/LoadResources';
 import { IAFormStateManager } from '..';
 import useIASettingsStateWithHandleClose from '../../../../../../hooks/useIASettingsStateWithHandleClose';
 
 const useStyles = makeStyles(theme => ({
-  drawerPaper: {
-    marginTop: theme.appBarHeight,
-    paddingBottom: theme.appBarHeight,
-    width: 1300,
-    border: 'solid 1px',
-    borderColor: theme.palette.secondary.lightest,
-    boxShadow: '-4px 4px 8px rgba(0,0,0,0.15)',
-    zIndex: theme.zIndex.drawer + 1,
-  },
-  configureDrawerform: {
+  configureform: {
     padding: theme.spacing(2, 3),
     '& + div': {
       padding: theme.spacing(2, 0),
       margin: theme.spacing(0, 3),
     },
-    '& > * div.MuiTabs-vertical': {
+    '& > * div.MuiTabs-horizontal': {
       marginTop: theme.spacing(-2),
       marginLeft: theme.spacing(-3),
     },
     '& > div[class*= "fieldsContainer"]': {
       height: '100%',
       '& > div[class*= "makeStyles-root"]': {
+        paddingTop: theme.spacing(5),
         height: '100%',
         '& > div[class*= "panelContainer"]': {
           paddingBottom: theme.spacing(5),
@@ -41,14 +30,13 @@ const useStyles = makeStyles(theme => ({
       },
     },
   },
-  configureDrawerCamForm: {
+  configureCamForm: {
     minHeight: '100%',
   },
 }));
 
-function ConfigureDrawer({ integrationId, storeId, sectionId, parentUrl }) {
+export default function Configure({ integrationId, storeId, sectionId, parentUrl }) {
   const classes = useStyles();
-  const match = useRouteMatch();
   const section = useSelector(state => {
     const flowSections = selectors.integrationAppFlowSections(
       state,
@@ -57,7 +45,7 @@ function ConfigureDrawer({ integrationId, storeId, sectionId, parentUrl }) {
     );
 
     return flowSections.find(s => s.titleId === sectionId);
-  });
+  }, shallowEqual);
   const flowSettingsMeta = useSelector(
     state =>
       selectors.integrationAppSectionMetadata(
@@ -77,7 +65,7 @@ function ConfigureDrawer({ integrationId, storeId, sectionId, parentUrl }) {
       ),
     [flowSettingsMeta, integrationId]
   );
-  const { formState, handleClose } = useIASettingsStateWithHandleClose(
+  const { formState } = useIASettingsStateWithHandleClose(
     integrationId,
     null,
     sectionId,
@@ -88,38 +76,19 @@ function ConfigureDrawer({ integrationId, storeId, sectionId, parentUrl }) {
     <LoadResources
       required
       resources={['flows', 'exports', 'imports', 'connections']}>
-      <Drawer
-        // variant="persistent"
-        anchor="right"
-        open={!!match}
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        onClose={handleClose}>
-        <DrawerTitleBar title={`Configure all ${section.title} flows`} />
-
-        <IAFormStateManager
-          onSubmitComplete={handleClose}
-          formState={formState}
-          className={clsx(classes.configureDrawerform, {
-            [classes.configureDrawerCamForm]: section.sections,
-          })}
-          integrationId={integrationId}
-          storeId={storeId}
-          sectionId={sectionId}
-          fieldMeta={translatedMeta}
+      <IAFormStateManager
+        key={storeId}
+        formState={formState}
+        className={clsx(classes.configureform, {
+          [classes.configureCamForm]: section.sections,
+        })}
+        isIAForm
+        integrationId={integrationId}
+        storeId={storeId}
+        sectionId={sectionId}
+        fieldMeta={translatedMeta}
+        orientation="horizontal"
         />
-      </Drawer>
     </LoadResources>
-  );
-}
-
-export default function ConfigureDrawerRoute(props) {
-  const match = useRouteMatch();
-
-  return (
-    <Route exact path={`${match.url}/configure`}>
-      <ConfigureDrawer {...props} parentUrl={match.url} />
-    </Route>
   );
 }
