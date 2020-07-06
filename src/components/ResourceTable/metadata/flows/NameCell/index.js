d7442b3121f479080f81123cf0d036e0b6f577cb
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2020-07-06 22:46:52 +0530

    Minor change

--- src/components/ResourceTable/metadata/flows/NameCell/index.js
+++ src/components/ResourceTable/metadata/flows/NameCell/index.js
@@ -34,7 +34,6 @@ export default function NameCell({
   isIntegrationApp,
   integrationId,
   onClick,
-  isSubscriptionPage = false,
 }) {
   const classes = useStyles();
   const isDataLoader = useSelector(state =>
