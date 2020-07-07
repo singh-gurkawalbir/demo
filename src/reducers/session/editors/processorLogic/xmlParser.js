72ee8ec90908178daa44cb1d848ecdb2943ede1a
driedl <dave@riedl.ca>
2020-07-07 15:00:25 -0700

    fixed overposting to xmlParser processor API

--- src/reducers/session/editors/processorLogic/xmlParser.js
+++ src/reducers/session/editors/processorLogic/xmlParser.js
@@ -1,6 +1,24 @@
 const parseNodes = nodesAsText => nodesAsText?.split('\n');
 
 const requestBody = editor => {
+  let options;
+
+  if (editor.V0_json) {
+    options = { V0_json: true };
+  } else {
+    options = {
+      V0_json: false,
+      trimSpaces: editor.trimSpaces,
+      stripNewLineChars: editor.stripNewLineChars,
+    };
+
+    if (editor.attributePrefix) options.attributePrefix = editor.attributePrefix;
+    if (editor.textNodeName) options.textNodeName = editor.textNodeName;
+    if (editor.listNodes) options.listNodes = parseNodes(editor.listNodes);
+    if (editor.includeNodes) options.includeNodes = parseNodes(editor.includeNodes);
+    if (editor.excludeNodes) options.excludeNodes = parseNodes(editor.excludeNodes);
+  }
+
   const rules = {
     resourcePath: editor.resourcePath,
     doc: {
@@ -8,16 +26,7 @@ const requestBody = editor => {
         {
           type: 'xml',
           version: 1,
-          rules: {
-            V0_json: editor.V0_json,
-            trimSpaces: editor.trimSpaces,
-            stripNewLineChars: editor.stripNewLineChars,
-            attributePrefix: editor.attributePrefix,
-            textNodeName: editor.textNodeName,
-            listNodes: parseNodes(editor.listNodes),
-            includeNodes: parseNodes(editor.includeNodes),
-            excludeNodes: parseNodes(editor.excludeNodes),
-          },
+          rules: options,
         },
       ],
     },
