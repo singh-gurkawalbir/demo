d54ede7f05f3e6ce0d69e506558a2a73024c7e02
Sravan Dandra <sravan.dandra@gmail.com>
2020-07-07 18:55:15 +0530

    Update Connection.jsx

--- src/views/FlowBuilder/drawers/BottomDrawer/panels/Connection.jsx
+++ src/views/FlowBuilder/drawers/BottomDrawer/panels/Connection.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { makeStyles } from '@material-ui/styles';
 import LoadResources from '../../../../../components/LoadResources';
 import CeligoTable from '../../../../../components/CeligoTable';
 import metadata from '../../../../../components/ResourceTable/metadata/connections';
 import * as selectors from '../../../../../reducers';
+import actions from '../../../../../actions';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,9 +17,22 @@ const useStyles = makeStyles(theme => ({
 export default function ConnectionPanel({ flow }) {
   const { _integrationId: integrationId } = flow;
   const classes = useStyles();
+  const dispatch = useDispatch();
   const flowConnections = useSelector(state =>
     selectors.flowConnectionList(state, flow)
   );
+
+  useEffect(() => {
+    dispatch(actions.resource.connections.refreshStatus(integrationId));
+    // For connections resource table, we need to poll the connection status and queueSize
+    const interval = setInterval(() => {
+      dispatch(actions.resource.connections.refreshStatus(integrationId));
+    }, 10 * 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [dispatch, integrationId]);
 
   return (
     <div className={classes.root}>
