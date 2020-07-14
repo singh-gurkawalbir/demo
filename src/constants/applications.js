d71af8d37d42e1159b12cd7171dad8da230660e2
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2020-07-14 17:51:41 +0530

    IO-13575

--- src/constants/applications.js
+++ src/constants/applications.js
@@ -816,6 +816,8 @@ export const groupApplications = (
           'yammer',
           'hybris',
           'etsy',
+          'concur',
+          'concurall',
           'concurv4',
           'constantcontact',
         ].includes(asst.id)
