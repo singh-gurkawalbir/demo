f307f7776d60f47dd9032d636aa91dc0ae38863a
ashu-gupta <gupta.ashu1602@gmail.com>
2020-07-17 14:02:45 +0530

    IO-15954

--- src/components/DynaForm/fields/DynaNetsuiteUserRoles.jsx
+++ src/components/DynaForm/fields/DynaNetsuiteUserRoles.jsx
@@ -31,9 +31,8 @@ export default function DynaNetsuiteUserRolesOptions(props) {
       acc
     )
   );
-  const { optionsArr } = netSuiteUserRoles;
-  const matchingOption =
-    optionsArr && optionsArr.find(ele => ele.value === value);
+  const { optionsArr, userRoles } = netSuiteUserRoles;
+  const matchingOption = optionsArr?.find(ele => ele.value === value);
   const [autofill, setAutofill] = useState(false);
 
   useEffect(() => {
@@ -81,6 +80,11 @@ export default function DynaNetsuiteUserRolesOptions(props) {
       );
     }
   };
+
+  // we dont want to show the fields unless the connection is validated
+  if (!userRoles) {
+    return null;
+  }
 
   return (
     <MaterialUiSelect
