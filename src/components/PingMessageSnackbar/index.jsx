a63a7b57f40cf99b9da7d4f97de942abf75201ee
driedl <dave@riedl.ca>
2020-07-20 19:13:38 -0700

    simplified ping error notification + updated style. still needs improvement

--- src/components/PingMessageSnackbar/index.jsx
+++ src/components/PingMessageSnackbar/index.jsx
@@ -1,9 +1,16 @@
-import React, { Fragment } from 'react';
-import {Snackbar, Typography} from '@material-ui/core';
+import React from 'react';
+import { makeStyles, Snackbar } from '@material-ui/core';
 import { PING_STATES } from '../../reducers/comms/ping';
-import ClosableSnackbarContent from './ClosableSnackbarContent';
-import TestConnectionSnackbar from './TestConnectionSnackbar';
 import RawHtml from '../RawHtml';
+import NotificationToaster from '../NotificationToaster';
+import TestConnectionSnackbar from './TestConnectionSnackbar';
+
+const useStyles = makeStyles(() => ({
+  errorText: {
+    maxHeight: 400,
+    overflowY: 'auto',
+  },
+}));
 
 const commStateToVariantType = {
   success: 'success',
@@ -11,49 +18,38 @@ const commStateToVariantType = {
   error: 'error',
   aborted: 'success',
 };
-
-const ErroredMessageList = ({ messages }) =>
-  messages?.length
-    ? messages.filter(msg => !!msg).map((msg, index) => (
-      <Fragment key={msg}>
-        { /* If the message contains html elements, render it as html */ }
-        {/<\/?[a-z][\s\S]*>/i.test(msg) ? (
-          <RawHtml html={msg} />
-        ) : (
-          <Typography color="error">{msg}</Typography>
-        )}
-        {index > 0 && <br />}
-      </Fragment>
-    ))
-    : null;
+const isHTML = s => /<\/?[a-z][\s\S]*>/i.test(s);
 
 export default function PingMessageSnackbar({ commStatus, onClose, onCancelTask }) {
   const { commState, message } = commStatus;
   const variant = commStateToVariantType[commState];
+  const classes = useStyles();
 
   if (commState === PING_STATES.LOADING) {
     return <TestConnectionSnackbar onCancel={onCancelTask} />;
   }
 
-  if (commState !== PING_STATES.ERROR || !message) {
+  if (commState !== PING_STATES.ERROR || !message || !message.length) {
     return null;
   }
 
+  const msg = 'Test message that is not too long'; // message[0];
   return (
-    <div>
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'center',
-        }}
-        open
-        autoHideDuration={6000}>
-        <ClosableSnackbarContent
-          onClose={onClose}
-          variant={variant || 'info'}
-          message={<ErroredMessageList messages={message} />}
-        />
-      </Snackbar>
-    </div>
-  );
+    <Snackbar
+      open
+      autoHideDuration={6000}
+      anchorOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}>
+      <NotificationToaster
+        variant={variant || 'info'}
+        size="medium"
+        fullWidth
+        onClose={onClose}>
+        <div className={classes.errorText}>
+          {isHTML(msg) ? <RawHtml html={msg} /> : msg}
+        </div>
+      </NotificationToaster>
+    </Snackbar>);
 }
