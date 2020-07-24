d9e7a8d2b6dc97e6ed4271906f3a8fec09ec5e49
adibjp <adibjp441@gmail.com>
2020-07-24 22:19:48 +0530

    Mapping netsuite settings metadata

--- src/components/AFE/ImportMappingSettings/application/index.js
+++ src/components/AFE/ImportMappingSettings/application/index.js
@@ -48,15 +48,11 @@ const getFormattedLookup = (lookup, formVal) => {
       case 'useDefaultOnMultipleMatches':
         lookupTmp.useDefaultOnMultipleMatches = true;
         lookupTmp.default =
-          formVal.lookupDefault ||
-          formVal.lookupSelect ||
-          formVal.lookupCheckbox;
+          formVal.lookupDefault;
         break;
       case 'default':
         lookupTmp.default =
-          formVal.lookupDefault ||
-          formVal.lookupSelect ||
-          formVal.lookupCheckbox;
+          formVal.lookupDefault;
         break;
       default:
     }
@@ -217,17 +213,17 @@ export default {
             settings.hardCodedValue = null;
             break;
           case 'default':
-            settings.hardCodedValue =
-              formVal.hardcodedDefault;
+            settings.hardCodedValue = Array.isArray(formVal.hardcodedDefault)
+              ? formVal.hardcodedDefault.join(',')
+              : formVal.hardcodedDefault;
             break;
           default:
         }
-      } else if (formVal.hardcodedSelect) {
-        settings.hardCodedValue = Array.isArray(formVal.hardcodedSelect)
-          ? formVal.hardcodedSelect.join(',')
-          : formVal.hardcodedSelect;
-      } else if (formVal.hardcodedCheckbox) {
-        settings.hardCodedValue = formVal.hardcodedCheckbox;
+      } else if (formVal.hardcodedDefault) {
+        // in some cases hardcodedDefault is shown without hardcodedAction
+        settings.hardCodedValue = Array.isArray(formVal.hardcodedDefault)
+          ? formVal.hardcodedDefault.join(',')
+          : formVal.hardcodedDefault;
       }
     } else if (
       formVal.fieldMappingType === 'standard' ||
