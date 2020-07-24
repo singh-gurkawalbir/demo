959319e768baff9a9280044a99251dc4d8cac7bc
Sravan Dandra <sravan.dandra@gmail.com>
2020-07-24 18:39:07 +0530

    IO-16605

--- src/utils/resource.js
+++ src/utils/resource.js
@@ -155,10 +155,16 @@ export function getResourceSubType(resource) {
   // if not, we cant proceed.
   if (!adaptorType && !type) return {};
 
-  // Note that "simple" (data-loader) exports dont have an adaptorType,
+  // Note that "simple" (data-loader) exports may or may not have an adaptorType,
   // but DO have a type prop with value "simple".
+  let resourceType;
+  if (type === 'simple') {
+    resourceType = type;
+  } else {
+    resourceType = adaptorTypeMap[adaptorType] || type;
+  }
   return {
-    type: adaptorTypeMap[adaptorType] || type,
+    type: resourceType,
     assistant,
     resourceType: inferResourceType(adaptorType),
   };
