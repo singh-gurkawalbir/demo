4692b0f1eed6b55793dcb1716e7f999360d21eaf
santhu111193 <santhu111193@gmail.com>
2020-08-11 16:19:27 +0530

    removed appName logic from component

--- src/components/InstallationWizard/index.jsx
+++ src/components/InstallationWizard/index.jsx
@@ -27,7 +27,7 @@ import { INSTALL_STEP_TYPES } from '../../utils/constants';
 import { SCOPES } from '../../sagas/resourceForm';
 import Loader from '../Loader';
 import Spinner from '../Spinner';
-import {applicationsList} from '../../constants/applications';
+import { getApplicationName } from '../../utils/template';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -158,14 +158,7 @@ export default function InstallationWizard(props) {
       }
 
       delete connObj._id;
-      const applications = applicationsList();
-      const applicationObj = applications.find(app => {
-        if (connObj.rdbms) {
-          return connObj.rdbms.type === app.id;
-        }
-        return connObj?.assistant ? connObj?.assistant === app.id : connObj?.type === app.id;
-      });
-      connObj.application = applicationObj?.name;
+      connObj.application = getApplicationName(connObj);
       dispatch(
         actions.resource.patchStaged(
           newId,
