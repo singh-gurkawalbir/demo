a81fe870bee7ba1a449c0697fbfe6f44b672926f
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-08-12 13:59:31 +0530

    Added ellipsis action menu for run dashboard em 2.0

---
+++ src/components/EllipsisActionMenu/index.jsx
@@ -0,0 +1,67 @@
+import React, { useCallback, useState} from 'react';
+import { IconButton, Menu, MenuItem } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import EllipsisIcon from '../icons/EllipsisHorizontalIcon';
+
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    '& > .MuiMenu-paper': {
+      marginLeft: theme.spacing(-2),
+    },
+  },
+}));
+
+export default function EllipsisActionMenu({ actionsMenu, onAction }) {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const handleMenuClick = useCallback(event => {
+    setAnchorEl(event.currentTarget);
+  }, []);
+  const handleMenuClose = useCallback(() => setAnchorEl(null), []);
+  const open = Boolean(anchorEl);
+  const actionsPopoverId = open ? 'more-row-actions' : undefined;
+  const renderActionLabel = useCallback(({ label, Icon }) => {
+    if (Icon) {
+      return (<><Icon /> {label}</>);
+    }
+
+    return label;
+  }, []);
+  const handleAction = useCallback(action => () => {
+    handleMenuClose();
+    onAction(action);
+  }, [onAction, handleMenuClose]);
+
+  return (
+    <>
+      <IconButton
+        data-test="openActionsMenu"
+        aria-label="more"
+        aria-controls={actionsPopoverId}
+        aria-haspopup="true"
+        size="small"
+        onClick={handleMenuClick}>
+        <EllipsisIcon />
+      </IconButton>
+
+      <Menu
+        elevation={2}
+        variant="menu"
+        id={actionsPopoverId}
+        anchorEl={anchorEl}
+        className={classes.wrapper}
+        open={open}
+        onClose={handleMenuClose}>
+        {actionsMenu.map(({ action, label, Icon, disabled }) => (
+          <MenuItem
+            key={label}
+            data-test={`${action}`}
+            disabled={disabled}
+            onClick={handleAction(action)}>
+            {renderActionLabel({ label, Icon })}
+          </MenuItem>
+        ))}
+      </Menu>
+    </>
+  );
+}
--- src/views/FlowBuilder/drawers/BottomDrawer/panels/RunDashboardActions.jsx
+++ src/views/FlowBuilder/drawers/BottomDrawer/panels/RunDashboardActions.jsx
@@ -3,12 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core';
 import RefreshIcon from '../../../../../components/icons/RefreshIcon';
 import CancelIcon from '../../../../../components/icons/CancelIcon';
+import DownloadIntegrationIcon from '../../../../../components/icons/DownloadIntegrationIcon';
 import RunFlowButton from '../../../../../components/RunFlowButton';
 import IconTextButton from '../../../../../components/IconTextButton';
 import actions from '../../../../../actions';
 import { selectors } from '../../../../../reducers';
 import useConfirmDialog from '../../../../../components/ConfirmDialog';
 import { JOB_STATUS } from '../../../../../utils/constants';
+import EllipsisActionMenu from '../../../../../components/EllipsisActionMenu';
 
 const useStyles = makeStyles(theme => ({
   rightActionContainer: {
@@ -36,6 +38,34 @@ export default function RunDashboardActions({ flowId }) {
 
     return jobIdsToCancel;
   }, [latestJobs]);
+
+  const validDashboardActions = useMemo(() => {
+    // return set of actions [cancel, downloadDiagnostics]
+    const actions = [];
+    const jobsInProgress = latestJobs
+      .filter(job => [JOB_STATUS.RUNNING, JOB_STATUS.QUEUED].includes(job.status));
+
+    if (jobsInProgress.length) actions.push('cancel');
+    if (!jobsInProgress.length) actions.push('downloadDiagnostics');
+
+    return actions;
+  }, [latestJobs]);
+
+  const dashboardActionsMenu = useMemo(() => [
+    {
+      Icon: CancelIcon,
+      action: 'cancel',
+      label: 'Cancel',
+      disabled: !validDashboardActions.includes('cancel'),
+    },
+    {
+      Icon: DownloadIntegrationIcon,
+      action: 'downloadDiagnostics',
+      label: 'Download diagnostics',
+      disabled: !validDashboardActions.includes('downloadDiagnostics'),
+    },
+  ], [validDashboardActions]);
+
   const handleRefresh = useCallback(() => {
     dispatch(actions.job.clear());
   }, [dispatch]);
@@ -62,15 +92,31 @@ export default function RunDashboardActions({ flowId }) {
     });
   }, [dispatch, confirmDialog, cancellableJobIds]);
 
+  const handleDownloadDiagnostics = useCallback(() => {
+    latestJobs
+      .filter(job => ![JOB_STATUS.RUNNING, JOB_STATUS.QUEUED].includes(job.status))
+      .map(job => dispatch(actions.job.downloadFiles({ jobId: job._id, fileType: 'diagnostics' })));
+  }, [latestJobs, dispatch]);
+
+  const handleAction = useCallback(action => {
+    switch (action) {
+      case 'cancel': handleCancel();
+        break;
+      case 'downloadDiagnostics': {
+        handleDownloadDiagnostics();
+        break;
+      }
+      default:
+    }
+  }, [handleCancel, handleDownloadDiagnostics]);
+
   return (
     <div className={classes.rightActionContainer}>
       <RunFlowButton variant="iconText" flowId={flowId} label="Run" />
       <IconTextButton onClick={handleRefresh} disabled={areFlowJobsLoading}>
         <RefreshIcon /> Refresh
       </IconTextButton>
-      <IconTextButton onClick={handleCancel} disabled={cancellableJobIds.length === 0}>
-        <CancelIcon /> Cancel
-      </IconTextButton>
+      <EllipsisActionMenu actionsMenu={dashboardActionsMenu} onAction={handleAction} />
     </div>
   );
 }
