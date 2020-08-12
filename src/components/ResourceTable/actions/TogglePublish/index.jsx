4fb41b4374ad523c5661517a46cc9601e400dcc3
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2020-08-12 15:35:12 +0530

    IO-17038

--- src/components/ResourceTable/actions/TogglePublish/index.jsx
+++ src/components/ResourceTable/actions/TogglePublish/index.jsx
@@ -1,14 +1,18 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import UnpublishIcon from '../../../icons/UnpublishedIcon';
+// import UnpublishIcon from '../../../icons/UnpublishedIcon';
 import PublishIcon from '../../../icons/PublishIcon';
 import actions from '../../../../actions';
+import useConfirmDialog from '../../../ConfirmDialog';
 
 export default {
   label: r => (r.published ? 'Unpublish app' : 'Publish app'),
-  icon: r => (r.published ? UnpublishIcon : PublishIcon),
+  // icon: r => (r.published ? UnpublishIcon : PublishIcon),
+  // ToDo: Need to do changes here to render icon dynamically.
+  icon: PublishIcon,
   component: function TogglePublish({ resourceType, rowData = {} }) {
     const { _id: resourceId, published: isPublished } = rowData;
+    const { confirmDialog } = useConfirmDialog();
     const dispatch = useDispatch();
     const togglePublish = useCallback(() => {
       const patchSet = [
@@ -22,10 +26,26 @@ export default {
       dispatch(actions.resource.patchStaged(resourceId, patchSet, 'value'));
       dispatch(actions.resource.commitStaged(resourceType, resourceId));
     }, [dispatch, isPublished, resourceId, resourceType]);
+    const togglePublishConfirm = useCallback(() => {
+      confirmDialog({
+        title: `Confirm ${isPublished ? 'unpublish' : 'publish'}`,
+        message: `Are you sure you want to ${isPublished ? 'unpublish' : 'publish'} this app?`,
+        buttons: [
+          {
+            label: isPublished ? 'Unpublish' : 'Publish',
+            onClick: togglePublish,
+          },
+          {
+            label: 'Cancel',
+            color: 'secondary',
+          },
+        ],
+      });
+    }, [confirmDialog, togglePublish, isPublished]);
 
     useEffect(() => {
-      togglePublish();
-    }, [togglePublish]);
+      togglePublishConfirm();
+    }, [togglePublishConfirm]);
 
     return null;
   },
