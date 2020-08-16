fadef24283cd337d161e9c27140dad4bdddda555
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-08-16 19:19:57 +0530

    Fixed resetting actions issue

--- src/views/FlowBuilder/drawers/ErrorsDetails/index.jsx
+++ src/views/FlowBuilder/drawers/ErrorsDetails/index.jsx
@@ -1,10 +1,17 @@
-import React, { useCallback, useState, useMemo } from 'react';
+import React, { useCallback, useState, useMemo, useEffect } from 'react';
+import { useRouteMatch, matchPath, useLocation } from 'react-router-dom';
 import RightDrawer from '../../../../components/drawer/Right';
 import ErrorList from '../../../../components/ErrorList';
 import ErrorDetailsTitle from './ErrorDetailsTitle';
 import TextToggle from '../../../../components/TextToggle';
 
 export default function ErrorDetailsDrawer({ flowId }) {
+  const match = useRouteMatch();
+  const location = useLocation();
+  const errorDrawerPath = matchPath(location.pathname, {
+    path: `${match.path}/errors/:resourceId`,
+  });
+  const resourceId = errorDrawerPath?.params?.resourceId;
   const [errorType, setErrorType] = useState('open');
   const errorTypes = [
     { label: 'View open errors', value: 'open' },
@@ -21,6 +28,13 @@ export default function ErrorDetailsDrawer({ flowId }) {
       options={errorTypes}
     />
   ), [errorType, errorTypes, handleErrorTypeChange]);
+
+  useEffect(() => {
+    // Used to reset error type when drawer is closed
+    if (!resourceId && errorType === 'resolved') {
+      setErrorType('open');
+    }
+  }, [resourceId, errorType]);
 
   return (
     <RightDrawer
