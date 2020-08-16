62c7ad5eace5b7cc42df5ca9df9b9e98027ffdcf
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-08-16 09:36:52 +0530

    Added confirm dialog for retry resolved errors

--- src/components/ErrorList/actions/Retry.js
+++ src/components/ErrorList/actions/Retry.js
@@ -1,12 +1,14 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import actions from '../../../actions';
+import useConfirmDialog from '../../ConfirmDialog';
 
 export default {
   label: 'Retry',
   component: function Retry({ flowId, resourceId, rowData, isResolved }) {
     const dispatch = useDispatch();
-    const handleClick = useCallback(() => {
+    const { confirmDialog } = useConfirmDialog();
+    const handleRetry = useCallback(() => {
       dispatch(
         actions.errorManager.flowErrorDetails.retry({
           flowId,
@@ -16,6 +18,28 @@ export default {
         })
       );
     }, [dispatch, flowId, isResolved, rowData.retryDataKey, resourceId]);
+    const handleClick = useCallback(() => {
+      if (!isResolved) {
+        return handleRetry();
+      }
+      // show confirmation dialog for resolved errors trying to be retried
+      confirmDialog({
+        title: 'Confirm retry',
+        message: 'You are requesting to retry one or more errors that have been resolved. The retry data associated with these errors represents the data at the time of the original error, and could be older and/or out of date. Please confirm you would like to proceed.',
+        buttons: [
+          {
+            label: 'Retry',
+            onClick: () => {
+              handleRetry();
+            },
+          },
+          {
+            label: 'Cancel',
+            color: 'secondary',
+          },
+        ],
+      });
+    }, [isResolved, handleRetry, confirmDialog]);
 
     useEffect(() => {
       handleClick();
