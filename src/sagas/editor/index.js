d3555e0df9e7ba29004de34d88454c4248870095
santhu111193 <santhu111193@gmail.com>
2020-08-20 11:50:31 +0530

    added comment for the bug fix

--- src/sagas/editor/index.js
+++ src/sagas/editor/index.js
@@ -50,7 +50,8 @@ export function* evaluateProcessor({ id }) {
       // Error with status code between 400 and 500 are json, hence we can parse them
       if (e.status >= 400 && e.status < 500) {
         const errJSON = JSON.parse(e.message);
-
+        // Receiving errors in different formats from BE, for now added below check
+        // Can remove this once backend bug gets fixed (Id: IO-17172)
         const errorMessage = [`Message: ${errJSON.message || errJSON.errors?.[0]?.message}`];
 
         if (errJSON.location) {
