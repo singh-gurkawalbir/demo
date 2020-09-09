2233983bb23de19920481b95346e0c8bb6fdf277
Mohammed Azhar Uddin <azharweb90@gmail.com>
2020-09-09 15:18:39 +0530

    small fix

--- src/views/FlowBuilder/LineGraphButton/index.jsx
+++ src/views/FlowBuilder/LineGraphButton/index.jsx
@@ -9,10 +9,15 @@ const useStyles = makeStyles(theme => ({
   chartsIcon: {
     padding: 0,
     marginRight: 12,
+    color: theme.palette.secondary.main,
     '&:hover': {
-      color: theme.palette.primary.main,
       background: 'none',
+      color: theme.palette.primary.main,
     },
+  },
+  iconGraph: {
+
+    width: 28,
   },
 }));
 
@@ -37,7 +42,7 @@ export default function LineGraphButton({flowId, onClickHandler}) {
       className={classes.chartsIcon}
       data-test="charts"
       onClick={onClickHandler('charts')}>
-      <GraphIcon />
+      <GraphIcon className={classes.iconGraph} />
     </IconButton>
   );
 }
