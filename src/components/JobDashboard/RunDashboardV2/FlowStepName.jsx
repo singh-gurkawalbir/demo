6dae188b946065c702cc1a0e20a7cf463c18fb06
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-09-14 12:24:07 +0530

    Bug fix 17760

--- src/components/JobDashboard/RunDashboardV2/FlowStepName.jsx
+++ src/components/JobDashboard/RunDashboardV2/FlowStepName.jsx
@@ -3,15 +3,18 @@ import { JOB_STATUS } from '../../../utils/constants';
 import { selectors } from '../../../reducers';
 
 export default function FlowStepName({ job }) {
+  console.log(job);
   const exportName = useSelector(state => {
     const exportObj = selectors.resource(state, 'exports', job._exportId);
 
-    return exportObj?.name;
+    return exportObj?.name || 'Export';
   });
 
   if (job?.status === JOB_STATUS.QUEUED) {
     return exportName;
   }
 
-  return job.name;
+  // Incase of Old flows , we show Export/Import instead of names as they don't exist for old resources
+  // Referred to EM 1.0 for this behaviour
+  return job.name || (job._exportId ? 'Export' : 'Import');
 }
