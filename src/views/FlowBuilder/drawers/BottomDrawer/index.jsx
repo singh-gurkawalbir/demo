e6bc1cdb586817eee15e54e5891fa367362bb8a3
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2020-09-18 15:20:59 +0530

    Reverted last commit

--- src/views/FlowBuilder/drawers/BottomDrawer/index.jsx
+++ src/views/FlowBuilder/drawers/BottomDrawer/index.jsx
@@ -22,7 +22,6 @@ import IconTextButton from '../../../../components/IconTextButton';
 import useSelectorMemo from '../../../../hooks/selectors/useSelectorMemo';
 import RunDashboardActions from './panels/Dashboard/RunDashboardActions';
 import useBottomDrawer from './useBottomDrawer';
-import { isNewId } from '../../../../utils/resource';
 
 const useStyles = makeStyles(theme => ({
   drawerPaper: {
@@ -331,13 +330,11 @@ export default function BottomDrawer({
         </div>
       </div>
       <>
-        {flowId && !isNewId(flowId) && (
         <TabPanel value={tabValue} index={0} classes={classes}>
           { isUserInErrMgtTwoDotZero
             ? <RunDashboardV2Panel flowId={flowId} />
             : <RunDashboardPanel flowId={flowId} />}
         </TabPanel>
-        )}
         <TabPanel value={tabValue} index={1} classes={classes}>
           <ConnectionPanel flowId={flowId} />
         </TabPanel>
