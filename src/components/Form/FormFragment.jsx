1610171321bd406741678b1d119bb8afdb167f60
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-09-24 00:27:20 +0530

    IO-16169

--- src/components/Form/FormFragment.jsx
+++ src/components/Form/FormFragment.jsx
@@ -1,27 +1,65 @@
 import React, { useCallback } from 'react';
+import { makeStyles} from '@material-ui/core';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import actions from '../../actions';
 import { selectors } from '../../reducers';
-import getRenderer from '../DynaForm/renderer';
+import fields from '../DynaForm/fields';
 
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    marginBottom: 16,
+  },
+  fieldStyle: {
+    flexGrow: '1',
+    textAlign: 'left',
+    width: '100%',
+  },
+});
+
+const dummyFn = () => false;
 export const FieldComponent = props => {
-  const { formKey, id, renderer } = props;
+  const { formKey, id, fieldId, type} = props;
+  const classes = useStyles();
   const fieldState = useSelector(
     state => selectors.fieldState(state, formKey, id),
     shallowEqual
   );
-
-  if (!fieldState || !fieldState.visible) return null;
-
-  return renderer({ ...props, fieldState });
-};
-
-export default function FormFragment({ defaultFields, formKey }) {
-  const dispatch = useDispatch();
   const formParentContext = useSelector(
     state => selectors.formParentContext(state, formKey),
     shallowEqual
   );
+  const fid = id || fieldId;
+
+  const { resourceId, resourceType } = formParentContext || {};
+  const context = { resourceId, resourceType };
+  const DynaField = fields[type] || dummyFn;
+  // get the element early on and check if its returning null
+  // we had to host this earlier or else fieldState visibility is impacting it by changing the order of hooks
+  const ele = DynaField({...props, ...fieldState, resourceContext: context});
+
+  if (!fieldState || !fieldState.visible) return null;
+
+  if (!fields[type]) {
+    return <div>No mapped field for type: [{type}]</div>;
+  }
+
+  return (
+  // if its returning null wrap it within divs else return null
+    ele ? (
+      <div key={fid} className={classes.wrapper}>
+        <div id={id} className={classes.fieldStyle} >
+          {ele}
+        </div>
+      </div>
+    ) : null
+  );
+};
+
+export default function FormFragment({ defaultFields, formKey }) {
+  const dispatch = useDispatch();
+
   const onFieldChange = useCallback(
     (fieldId, value, skipFieldTouched) =>
       dispatch(
@@ -40,17 +78,6 @@ export default function FormFragment({ defaultFields, formKey }) {
   const registerField = useCallback(
     field => dispatch(actions.form.registerField(formKey)(field)),
     [dispatch, formKey]
-  );
-  const renderer = useCallback(
-    field => {
-      // rest could mostly be form context such as edit mode what type of resource
-      // we change this interface for getRenderer
-      const { resourceId, resourceType } = formParentContext;
-
-      // i really may not need this considering metadata is generating this props
-      return getRenderer(formKey, resourceId, resourceType)(field);
-    },
-    [formKey, formParentContext]
   );
 
   // both useForm hook and the FormFragment were getting executed simultaneously
@@ -75,7 +102,6 @@ export default function FormFragment({ defaultFields, formKey }) {
           {...field}
           key={field.id}
           id={field.id}
-          renderer={renderer}
           onFieldChange={onFieldChange}
           onFieldBlur={onFieldBlur}
           onFieldFocus={onFieldFocus}
