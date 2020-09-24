d854a78fdde6dbb1d910db4e7c6173fca96786eb
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-09-24 13:00:39 +0530

    Fix

--- src/components/ErrorList/ErrorTable/ErrorActions/DownloadErrorsModal/index.jsx
+++ src/components/ErrorList/ErrorTable/ErrorActions/DownloadErrorsModal/index.jsx
@@ -1,14 +1,14 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Button } from '@material-ui/core';
 import {makeStyles} from '@material-ui/core/styles';
-import moment from 'moment';
 import ModalDialog from '../../../../ModalDialog';
 import actions from '../../../../../actions';
 import useFormInitWithPermissions from '../../../../../hooks/useFormInitWithPermissions';
 import DynaForm from '../../../../DynaForm';
 import ButtonGroup from '../../../../ButtonGroup';
 import useFormContext from '../../../../Form/FormContext';
+import useEnqueueSnackbar from '../../../../../hooks/enqueueSnackbar';
 
 const fieldMeta = {
   fieldMap: {
@@ -18,7 +18,6 @@ const fieldMeta = {
       type: 'date',
       label: 'From',
       closeOnSelect: true,
-      validWhen: { isNot: { values: ['Invalid date'], message: '' } },
     },
     toDate: {
       id: 'toDate',
@@ -26,7 +25,6 @@ const fieldMeta = {
       type: 'date',
       label: 'To',
       closeOnSelect: true,
-      validWhen: { isNot: { values: ['Invalid date'] } },
     },
   },
   layout: {
@@ -39,23 +37,43 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(1),
   },
 }));
+const INVALID_DATE = 'Invalid date';
 
 export default function DownloadErrorsModal({flowId, resourceId, onClose, isResolved}) {
   const dispatch = useDispatch();
   const classes = useStyles();
+  const [enqueueSnackbar] = useEnqueueSnackbar();
   const formKey = useFormInitWithPermissions({ fieldMeta });
   const formContext = useFormContext(formKey);
+  const [isValidForm, setIsValidForm] = useState(true);
+
+  useEffect(() => {
+    const values = formContext.value || {};
+    const { fromDate, toDate } = values;
+
+    if (fromDate === INVALID_DATE || toDate === INVALID_DATE) {
+      setIsValidForm(false);
+    } else {
+      setIsValidForm(true);
+    }
+  }, [formContext.value]);
 
   const handleDownload = useCallback(
     () => {
       const { value: formValues = {} } = formContext || {};
-      const fromDate = formValues.fromDate && moment(formValues.fromDate).toISOString();
-      const toDate = formValues.toDate && moment(formValues.toDate).toISOString();
+      const fromDate = formValues.fromDate && new Date(formValues.fromDate).toISOString();
+      const toDate = formValues.toDate && new Date(formValues.toDate).toISOString();
 
+      if (fromDate && toDate && fromDate > toDate) {
+        return enqueueSnackbar({
+          message: 'Invalid date range',
+          variant: 'error',
+        });
+      }
       dispatch(actions.errorManager.flowErrorDetails.download({flowId, resourceId, isResolved, filters: { fromDate, toDate } }));
       onClose();
     },
-    [dispatch, flowId, resourceId, isResolved, onClose, formContext],
+    [dispatch, flowId, resourceId, isResolved, onClose, formContext, enqueueSnackbar],
   );
 
   return (
@@ -69,7 +87,7 @@ export default function DownloadErrorsModal({flowId, resourceId, onClose, isReso
               variant="outlined"
               color="primary"
               data-test="downloadErrors"
-              disabled={!formContext.isValid}
+              disabled={!isValidForm}
               onClick={handleDownload}>
               Download
             </Button>
