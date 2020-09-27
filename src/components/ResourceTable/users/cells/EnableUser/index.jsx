4629c06ab30cb622ddc6158c7bd55d549307f719
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-09-27 19:26:43 +0530

    added enable user action

--- src/components/ResourceTable/users/cells/EnableUser/index.jsx
+++ src/components/ResourceTable/users/cells/EnableUser/index.jsx
@@ -1,11 +1,34 @@
 import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import CeligoSwitch from '../../../../CeligoSwitch';
 import { ACCOUNT_IDS } from '../../../../../utils/constants';
+import useConfirmDialog from '../../../../ConfirmDialog';
+import actions from '../../../../../actions';
 
 export default function EnableUser({ user, integrationId}) {
+  const { confirmDialog } = useConfirmDialog();
+  const dispatch = useDispatch();
+
   const handleSwitch = useCallback(() => {
-    // console.log('switch click');
-  }, []);
+    confirmDialog({
+      title: `Confirm ${user.disabled ? 'enable' : 'disable'}`,
+      message: `Are you sure you want to ${
+        user.disabled ? 'enable' : 'disable'
+      } this user?`,
+      buttons: [
+        {
+          label: user.disabled ? 'Enable' : 'Disable',
+          onClick: () => {
+            dispatch(actions.user.org.users.disable(user._id, user.disabled));
+          },
+        },
+        {
+          label: 'Cancel',
+          color: 'secondary',
+        },
+      ],
+    });
+  }, [confirmDialog, user._id, user.disabled, dispatch]);
 
   return (
     <CeligoSwitch
