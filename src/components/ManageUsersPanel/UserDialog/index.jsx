2ac51cf4a9df728d88940ca6cd1f6ca772eebb4c
ashu-gupta <gupta.ashu1602@gmail.com>
2020-10-06 12:56:41 +0530

    refactored UserDialog component

--- src/components/ManageUsersPanel/UserDialog/index.jsx
+++ src/components/ManageUsersPanel/UserDialog/index.jsx
@@ -1,29 +1,19 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import { useDispatch } from 'react-redux';
-import { Typography } from '@material-ui/core';
 import {
   USER_ACCESS_LEVELS,
   INTEGRATION_ACCESS_LEVELS,
 } from '../../../utils/constants';
 import actions from '../../../actions';
 import actionTypes from '../../../actions/types';
-import { COMM_STATES } from '../../../reducers/comms/networkComms';
-import CommStatus from '../../CommStatus';
 import ModalDialog from '../../ModalDialog';
 import UserForm from './UserForm';
-import inferErrorMessage from '../../../utils/inferErrorMessage';
+import getRequestOptions from '../../../utils/requestOptions';
+import useSaveStatusIndicator from '../../../hooks/useSaveStatusIndicator';
 
-export default function UserDialog({ open, userId, onClose, onSuccess }) {
-  const [errorMessage, setErrorMessage] = useState();
-  const [disableSave, setDisableSave] = useState(false);
-  const [actionsToClear, setActionsToClear] = useState();
-  const [callSuccessCleanup, setCallSuccessCleanup] = useState(false);
+export default function UserDialog({ open, userId, onClose }) {
   const dispatch = useDispatch();
-  const handleClose = useCallback(() => {
-    setErrorMessage();
-
-    if (onClose) onClose();
-  }, [onClose]);
+  const { path, opts } = useMemo(() => getRequestOptions(userId ? actionTypes.USER_UPDATE : actionTypes.USER_CREATE, {resourceId: userId }), [userId]);
   const handleSaveClick = useCallback(
     ({ email, accessLevel, integrationsToMonitor, integrationsToManage }) => {
       const aShareData = {
@@ -32,8 +22,6 @@ export default function UserDialog({ open, userId, onClose, onSuccess }) {
         accessLevel,
         integrationAccessLevel: [],
       };
-
-      setDisableSave(true);
 
       if (accessLevel === USER_ACCESS_LEVELS.ACCOUNT_MONITOR) {
         integrationsToManage.forEach(_integrationId =>
@@ -68,69 +56,26 @@ export default function UserDialog({ open, userId, onClose, onSuccess }) {
     },
     [userId, dispatch]
   );
-  const commStatusHandler = useCallback(
-    objStatus => {
-      if (
-        objStatus &&
-        objStatus.createOrUpdate &&
-        [COMM_STATES.SUCCESS, COMM_STATES.ERROR].includes(
-          objStatus.createOrUpdate.status
-        )
-      ) {
-        if (objStatus.createOrUpdate.status === COMM_STATES.SUCCESS) {
-          if (onSuccess) {
-            setCallSuccessCleanup(true);
-          }
 
-          setErrorMessage();
-        } else if (objStatus.createOrUpdate.status === COMM_STATES.ERROR) {
-          setErrorMessage(
-            inferErrorMessage(objStatus.createOrUpdate.message)[0]
-          );
-        }
-
-        setActionsToClear(['createOrUpdate']);
-        setDisableSave(false);
-      }
-    },
-    [onSuccess]
+  const { submitHandler, disableSave} = useSaveStatusIndicator(
+    {
+      path,
+      method: opts.method,
+      onSave: handleSaveClick,
+      onClose,
+    }
   );
-
-  useEffect(() => {
-    // this is a hack to overcome React Batching mechanism.
-    // If we add onSuccess() directly in commStatusHandler, then local state like setActionsToClear was not getting set(and was removed directly
-    // because onSuccess handler unmounts this component) and hence the comm status was never clearing up
-    // TODO: any other better way to do this? (eg using asynchronous call, setTimeout(() => onSuccess());)
-
-    if (callSuccessCleanup) { onSuccess(); }
-  }, [callSuccessCleanup, onSuccess]);
 
   return (
     <>
-      <CommStatus
-        actionsToMonitor={{
-          createOrUpdate: {
-            action: userId ? actionTypes.USER_UPDATE : actionTypes.USER_CREATE,
-            resourceId: userId,
-          },
-        }}
-        actionsToClear={actionsToClear}
-        commStatusHandler={commStatusHandler}
-      />
-
-      <ModalDialog show={open} onClose={handleClose}>
+      <ModalDialog show={open} onClose={onClose}>
         <div>{userId ? 'Change user permissions' : 'Invite user'}</div>
         <div>
-          {errorMessage && (
-            <Typography color="error" variant="body2">
-              {errorMessage}
-            </Typography>
-          )}
           <UserForm
             id={userId}
             disableSave={disableSave}
-            onSaveClick={handleSaveClick}
-            onCancelClick={handleClose}
+            onSaveClick={submitHandler(true)}
+            onCancelClick={onClose}
           />
         </div>
       </ModalDialog>
