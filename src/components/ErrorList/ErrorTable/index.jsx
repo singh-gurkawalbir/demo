12a452fca7a45db535dbda87dd2a7f68120a36be
Mohammed Azhar Uddin <azharweb90@gmail.com>
2020-10-07 11:05:25 +0530

    fix

--- src/components/ErrorList/ErrorTable/index.jsx
+++ src/components/ErrorList/ErrorTable/index.jsx
@@ -24,9 +24,9 @@ const useStyles = makeStyles(theme => ({
       background: theme.palette.common.white,
       '& > div[class*="MuiInputBase-root"]': {
         width: '100%',
-        height: '100%',
         '& > input': {
           width: '100%',
+          height: '100%',
         },
       },
     },
