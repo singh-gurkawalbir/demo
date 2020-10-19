b7db1ce29ba4eabfa5777584d24fdeff76425f64
Sravan Dandra <sravan.dandra@gmail.com>
2020-10-19 16:27:57 +0530

    IO-18408

--- src/components/ResourceTable/accesstokens/actions/AutoPurgeAt/index.jsx
+++ src/components/ResourceTable/accesstokens/actions/AutoPurgeAt/index.jsx
@@ -1,24 +1,10 @@
-import { useState, useEffect } from 'react';
-import moment from 'moment';
-import { getAutoPurgeAtAsString } from './util';
+import React from 'react';
+import TimeAgo from 'react-timeago';
 
 export default function AutoPurgeAt({ accessToken }) {
-  const [autoPurge, setAutoPurge] = useState(null);
+  if (accessToken && accessToken.autoPurgeAt) {
+    return <TimeAgo date={accessToken.autoPurgeAt} />;
+  }
 
-  useEffect(() => {
-    setAutoPurge(getAutoPurgeAtAsString(accessToken));
-
-    /** Check and update purge status every minute until the token is purged */
-    if (accessToken.autoPurgeAt && moment().diff(accessToken.autoPurgeAt) < 0) {
-      const updateAutoPurgeTimer = setInterval(() => {
-        setAutoPurge(getAutoPurgeAtAsString(accessToken));
-      }, 60 * 1000);
-
-      return () => {
-        clearTimeout(updateAutoPurgeTimer);
-      };
-    }
-  }, [accessToken]);
-
-  return autoPurge;
+  return 'Never';
 }
