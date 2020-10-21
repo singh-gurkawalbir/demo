0294c947c5fb2cc82c730cbf5786a9e8051ce003
Yang <yang.jiang@celigo.com>
2020-10-21 16:58:58 -0700

    logrocket bug fix

--- release.sh
+++ release.sh
@@ -5,6 +5,7 @@ set -e
 [[ -n $2 ]] || (echo "missing logrocket env file!\nUsage: release.sh <release version> <logrocket env file name>" && exit 1)
 [[ -f $2 ]] || (echo "logrocket env file invalid $2" && exit 1)
 echo "building version $1 and uploading source maps to logrocket..."
+export RELEASE_VERSION="$1"
 . $2
 NODE_ENV=production webpack --mode=production
 # bundle is build, we no longer need the version number left in
