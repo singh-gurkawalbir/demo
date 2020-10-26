077ad639afde0024cc3a412932ac3abb02adf0fc
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2020-10-26 09:28:09 +0530

    Fix

--- src/components/ManageUsersPanel/Drawers/ViewNotifications/metadata.js
+++ src/components/ManageUsersPanel/Drawers/ViewNotifications/metadata.js
@@ -10,15 +10,6 @@ export default function viewNotificationsMetadata(props) {
   return (
     {
       fieldMap: {
-        connections: {
-          id: 'connections',
-          name: 'connections',
-          type: 'multiselect',
-          valueDelimiter: ',',
-          label: 'Subscribed connections',
-          defaultValue: connectionValues,
-          options: [{ items: connectionOps }],
-        },
         flows: {
           id: 'flows',
           name: 'flows',
@@ -28,6 +19,15 @@ export default function viewNotificationsMetadata(props) {
           defaultValue: flowValues,
           options: [{ items: flowOps }],
           selectAllIdentifier: integrationId,
+        },
+        connections: {
+          id: 'connections',
+          name: 'connections',
+          type: 'multiselect',
+          valueDelimiter: ',',
+          label: 'Subscribed connections',
+          defaultValue: connectionValues,
+          options: [{ items: connectionOps }],
         },
       },
     }
