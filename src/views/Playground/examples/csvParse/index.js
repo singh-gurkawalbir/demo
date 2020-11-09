1a35fa3072d6b65be46f0bb48a943fcfc883e9d4
driedl <dave@riedl.ca>
2020-11-08 16:49:29 -0800

    re-worked example metadata

---
+++ src/views/Playground/examples/csvParse/index.js
@@ -0,0 +1,4 @@
+import simple from './simple';
+import multiline from './multiline';
+
+export default [simple, multiline];
---
+++ src/views/Playground/examples/index.js
@@ -0,0 +1,11 @@
+import csvParse from './csvParse';
+import ediParse from './ediParse';
+import xmlParse from './xmlParse';
+import settingsForm from './settingsForm';
+
+export default {
+  csvParse,
+  ediParse,
+  xmlParse,
+  settingsForm,
+};
