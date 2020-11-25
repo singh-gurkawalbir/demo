bd12ba33d2ae03135bf45f9bc4c6adaba4ecc0c2
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2020-11-25 16:08:23 +0530

    IO-14703

--- src/reducers/session/resource/test.js
+++ src/reducers/session/resource/test.js
@@ -68,6 +68,82 @@ describe('session.resource reducers', () => {
       expect(state.new123).toEqual(dbId);
     });
   });
+  describe('LICENSE_TRIAL_ISSUED action', () => {
+    test('should store the license trial issued message', () => {
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.trialLicenseIssued()
+      );
+      const expected = {platformLicenseActionMessage: 'Congratulations! Your 30 days of unlimited flows starts now - what will you integrate next?'};
+
+      expect(state).toEqual(expected);
+    });
+  });
+  describe('LICENSE_UPGRADE_REQUEST_SUBMITTED action', () => {
+    test('should store the license upgrade request submitted message', () => {
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.licenseUpgradeRequestSubmitted()
+      );
+      const expected = {platformLicenseActionMessage: 'Your request has been received. We will contact you soon.'};
+
+      expect(state).toEqual(expected);
+    });
+  });
+  describe('LICENSE_NUM_ENABLED_FLOWS_RECEIVED action', () => {
+    test('should store the number of enabled flows info', () => {
+      const response = {numEnabledPaidFlows: 2, numEnabledSandboxFlows: 2, numEnabledFreeFlows: 1 };
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.receivedNumEnabledFlows(response)
+      );
+
+      expect(state.numEnabledFlows).toEqual(response);
+    });
+  });
+  describe('LICENSE_ENTITLEMENT_USAGE_RECEIVED action', () => {
+    test('should store the license entitlement usage info', () => {
+      const response = {production: {agentUsage: {numActive: 2}, flowUsage: {numEnabled: 2}, endpointUsage: {numConsumed: 11}, tradingPartnerUsage: {numConsumed: 4} }, sandbox: {agentUsage: {numActive: 2}, flowUsage: {numEnabled: 2}, endpointUsage: {numConsumed: 11}, tradingPartnerUsage: {numConsumed: 4} }};
+
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.receivedLicenseEntitlementUsage(response)
+      );
+
+      expect(state.licenseEntitlementUsage).toEqual(response);
+    });
+  });
+  describe('UPDATE_CHILD_INTEGRATION action', () => {
+    test('should store the parent child integration map info', () => {
+      const parentId = 'parentId';
+      const childId = 'childId';
+      const expected = {parentChildMap: {[parentId]: childId }};
+
+      const state = reducer(
+        undefined,
+        actions.resource.updateChildIntegration(parentId, childId)
+      );
+
+      expect(state).toEqual(expected);
+    });
+  });
+  describe('CLEAR_CHILD_INTEGRATION action', () => {
+    test('should be able to clear parent child integration map if any', () => {
+      const parentId = 'parentId';
+      const childId = 'childId';
+
+      const prevState = reducer(
+        undefined,
+        actions.resource.updateChildIntegration(parentId, childId)
+      );
+      const state = reducer(
+        prevState,
+        actions.resource.clearChildIntegration()
+      );
+
+      expect(state).toEqual({});
+    });
+  });
 
   describe('session.resource selectors', () => {
     describe('createdResourceId', () => {
@@ -117,6 +193,133 @@ describe('session.resource reducers', () => {
 
         expect(selectors.resourceReferences(state)).toEqual(referencesArray);
       });
+    });
+  });
+  describe('platformLicenseActionMessage', () => {
+    test('should return undefined when no match found.', () => {
+      expect(selectors.platformLicenseActionMessage(undefined)).toEqual(
+        undefined
+      );
+      expect(selectors.platformLicenseActionMessage({})).toEqual(undefined);
+    });
+
+    test('should return correct platform license action message', () => {
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.trialLicenseIssued()
+      );
+      const expected = {platformLicenseActionMessage: 'Congratulations! Your 30 days of unlimited flows starts now - what will you integrate next?'};
+
+      expect(selectors.platformLicenseActionMessage(state)).toEqual(expected.platformLicenseActionMessage);
+    });
+    test('should return correct license upgrade action message', () => {
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.licenseUpgradeRequestSubmitted()
+      );
+      const expected = {platformLicenseActionMessage: 'Your request has been received. We will contact you soon.'};
+
+      expect(selectors.platformLicenseActionMessage(state)).toEqual(expected.platformLicenseActionMessage);
+    });
+  });
+  describe('getChildIntegrationId', () => {
+    test('should return undefined when no match found.', () => {
+      expect(selectors.getChildIntegrationId(undefined)).toEqual(
+        undefined
+      );
+      expect(selectors.getChildIntegrationId({})).toEqual(undefined);
+    });
+
+    test('should return correct child integration id', () => {
+      const parentId = 'parentId';
+      const childId = 'childId';
+
+      const state = reducer(
+        undefined,
+        actions.resource.updateChildIntegration(parentId, childId)
+      );
+
+      expect(selectors.getChildIntegrationId(state, parentId)).toEqual(childId);
+    });
+    test('should return undefined if no parent child integration map exists for corresponding parent id', () => {
+      const parentId = 'parentId';
+      const childId = 'childId';
+
+      const state = reducer(
+        undefined,
+        actions.resource.updateChildIntegration(parentId, childId)
+      );
+
+      expect(selectors.getChildIntegrationId(state, 'dummy')).toEqual(undefined);
+    });
+  });
+  describe('getNumEnabledFlows', () => {
+    test('should return defaultObject when no match found.', () => {
+      const defaultObject = { numEnabledPaidFlows: 0, numEnabledSandboxFlows: 0 };
+
+      expect(selectors.getNumEnabledFlows(undefined)).toEqual(
+        defaultObject
+      );
+      expect(selectors.getNumEnabledFlows({})).toEqual(defaultObject);
+    });
+
+    test('should return correct number of enabled flows', () => {
+      const response = {numEnabledPaidFlows: 2, numEnabledSandboxFlows: 2, numEnabledFreeFlows: 1 };
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.receivedNumEnabledFlows(response)
+      );
+      const expected = {
+        numEnabledPaidFlows: 2,
+        numEnabledSandboxFlows: 2,
+        numEnabledFreeFlows: 1,
+      };
+
+      expect(selectors.getNumEnabledFlows(state)).toEqual(expected);
+    });
+  });
+  describe('getNumEnabledFlows', () => {
+    test('should return defaultObject when no match found.', () => {
+      const defaultObject = { numEnabledPaidFlows: 0, numEnabledSandboxFlows: 0 };
+
+      expect(selectors.getNumEnabledFlows(undefined)).toEqual(
+        defaultObject
+      );
+      expect(selectors.getNumEnabledFlows({})).toEqual(defaultObject);
+    });
+
+    test('should return correct number of enabled flows', () => {
+      const response = {numEnabledPaidFlows: 2, numEnabledSandboxFlows: 2, numEnabledFreeFlows: 1 };
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.receivedNumEnabledFlows(response)
+      );
+      const expected = {
+        numEnabledPaidFlows: 2,
+        numEnabledSandboxFlows: 2,
+        numEnabledFreeFlows: 1,
+      };
+
+      expect(selectors.getNumEnabledFlows(state)).toEqual(expected);
+    });
+  });
+  describe('getLicenseEntitlementUsage', () => {
+    test('should return null when no match found.', () => {
+      expect(selectors.getLicenseEntitlementUsage(undefined)).toEqual(
+        null
+      );
+      expect(selectors.getLicenseEntitlementUsage({})).toEqual(null);
+    });
+
+    test('should return correct license entitlement usage info', () => {
+      const response = {production: {agentUsage: {numActive: 2}, flowUsage: {numEnabled: 2}, endpointUsage: {numConsumed: 11}, tradingPartnerUsage: {numConsumed: 4} }, sandbox: {agentUsage: {numActive: 2}, flowUsage: {numEnabled: 2}, endpointUsage: {numConsumed: 11}, tradingPartnerUsage: {numConsumed: 4} }};
+
+      const state = reducer(
+        undefined,
+        actions.user.org.accounts.receivedLicenseEntitlementUsage(response)
+      );
+
+      expect(selectors.getLicenseEntitlementUsage(state)).toEqual(response);
     });
   });
 });
