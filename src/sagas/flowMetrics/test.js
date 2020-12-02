12d2affd929805c825ee7461ec563408c3d72ba4
Kaushik-Nandan <kaushiknandan32@gmail.com>
2020-12-02 10:56:54 +0530

    added required validations to test cases

--- src/sagas/flowMetrics/test.js
+++ src/sagas/flowMetrics/test.js
@@ -1,13 +1,13 @@
 /* global describe, test */
 
 import { expectSaga } from 'redux-saga-test-plan';
-import * as matchers from 'redux-saga-test-plan/matchers';
 import { throwError } from 'redux-saga-test-plan/providers';
-import { select } from 'redux-saga/effects';
+import { call, select } from 'redux-saga/effects';
 import actions from '../../actions';
 import { apiCallWithRetry } from '../index';
 import { selectors } from '../../reducers';
 import { requestMetric, requestFlowMetrics } from '.';
+import { getFlowMetricsQuery } from '../../utils/flowMetrics';
 
 describe('flowMetrics saga', () => {
   describe('requestMetrics generator', () => {
@@ -17,23 +17,37 @@ describe('flowMetrics saga', () => {
       { Length: 2.34, Make: 'Ford', Model: 'E350', Year: 1997 },
       { Length: 2.38, Make: 'Mercury', Model: 'Cougar', Year: 2000 },
     ];
+    const args = {
+      path: '/stats/tsdb',
+      opts: {
+        body: {query},
+        method: 'POST',
+      },
+      message: 'Loading',
+    };
 
     test('should return parsed response on successful api call', () =>
       expectSaga(requestMetric, { query })
-        .provide([[matchers.call.fn(apiCallWithRetry), csvResponse]])
-        .call.fn(apiCallWithRetry)
+        .provide([[call(apiCallWithRetry, args), csvResponse]])
+        .call(apiCallWithRetry, args)
         .returns(data)
         .run());
 
-    test('should return empty array, if api call fails', () => expectSaga(requestMetric, { query })
-      .provide([[matchers.call.fn(apiCallWithRetry), throwError([])]])
-      .call.fn(apiCallWithRetry)
-      .returns([])
-      .run());
+    test('should return empty array, if api call fails', () =>
+      expectSaga(requestMetric, { query })
+        .provide([[call(apiCallWithRetry, args), throwError([])]])
+        .call(apiCallWithRetry, args)
+        .returns([])
+        .run());
   });
   describe('requestFlowMetrics saga', () => {
     const resourceId = 'dummyResourceId';
     const filters = {
+      range: {
+        preset: 'yesterday',
+        startDate: new Date('05 October 2011 14:48 UTC'),
+        endDate: new Date('06 October 2011 14:48 UTC'),
+      },
       selectedResources: [],
     };
     const userId = '123';
@@ -41,7 +55,12 @@ describe('flowMetrics saga', () => {
     test('should dispatch received action with corresponding data, if resource is an integration with some flowIds and api call is successful', () => {
       const resourceType = 'integrations';
       const flowIds = ['dummyId'];
-      const data = 'dummyData';
+
+      filters.selectedResources = flowIds;
+      const data = [{ dummy: 'dummy' }];
+      const args = {
+        query: getFlowMetricsQuery(resourceType, resourceId, userId, filters),
+      };
 
       return expectSaga(requestFlowMetrics, {
         resourceType,
@@ -51,15 +70,18 @@ describe('flowMetrics saga', () => {
         .provide([
           [select(selectors.ownerUserId), userId],
           [select(selectors.integrationEnabledFlowIds, resourceId), flowIds],
-          [matchers.call.fn(requestMetric), data],
+          [call(requestMetric, args), data],
         ])
-        .call.fn(requestMetric)
+        .call(requestMetric, args)
         .put(actions.flowMetrics.received(resourceType, resourceId, data))
         .run();
     });
     test('should dispatch received action with corresponding data, if resource is not an integration and api call is successful', () => {
       const resourceType = 'notIntegrations';
-      const data = 'dummyData';
+      const data = [{ dummy: 'dummy' }];
+      const args = {
+        query: getFlowMetricsQuery(resourceType, resourceId, userId, filters),
+      };
 
       return expectSaga(requestFlowMetrics, {
         resourceType,
@@ -68,9 +90,9 @@ describe('flowMetrics saga', () => {
       })
         .provide([
           [select(selectors.ownerUserId), userId],
-          [matchers.call.fn(requestMetric), data],
+          [call(requestMetric, args), data],
         ])
-        .call.fn(requestMetric)
+        .call(requestMetric, args)
         .put(actions.flowMetrics.received(resourceType, resourceId, data))
         .run();
     });
@@ -94,6 +116,11 @@ describe('flowMetrics saga', () => {
     test('should dispatch failed action, if the api call failed', () => {
       const resourceType = 'integrations';
       const flowIds = ['dummyId'];
+
+      filters.selectedResources = flowIds;
+      const args = {
+        query: getFlowMetricsQuery(resourceType, resourceId, userId, filters),
+      };
       const e = new Error();
 
       return expectSaga(requestFlowMetrics, {
@@ -104,9 +131,9 @@ describe('flowMetrics saga', () => {
         .provide([
           [select(selectors.ownerUserId), userId],
           [select(selectors.integrationEnabledFlowIds, resourceId), flowIds],
-          [matchers.call.fn(requestMetric), throwError(e)],
+          [call(requestMetric, args), throwError(e)],
         ])
-        .call.fn(requestMetric)
+        .call(requestMetric, args)
         .put(actions.flowMetrics.failed(e))
         .run();
     });
