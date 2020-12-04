a422d565934c68b0985068343990fd454ac2994a
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-12-04 20:30:31 +0530

    removed some more ghost code files

--- src/forms/definitions/exports/index.js
+++ src/forms/definitions/exports/index.js
@@ -1,11 +1,7 @@
-import inputFilter from './inputFilter';
-import test from './testLimit';
 import delta from './delta';
 import once from './once';
-import valueDelta from './valueDelta';
 import webhook from './webhook';
 import distributed from './distributed';
-import filter from './filter';
 import rest from './rest';
 import ftp from './ftp';
 import subForms from './subForms';
@@ -17,7 +13,6 @@ import salesforce from './salesforce';
 import wrapper from './wrapper';
 import mongodb from './mongodb';
 import as2 from './as2';
-// import common from './common';
 import newExport from './new';
 import custom from './custom';
 import simple from './simple';
@@ -25,15 +20,10 @@ import dynamodb from './dynamodb';
 
 export default {
   new: newExport,
-  // common, //ghost code
-  inputFilter, // ghost code
-  test,
   delta,
   once,
-  valueDelta, // ghost code
   webhook,
   distributed,
-  filter, // ghost code
   ftp,
   rest,
   subForms,
