e7b9a2e1c9d36194bb3c375a27c03b0ee1fce58c
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2020-12-04 20:27:25 +0530

    delete some ghost files

--- src/forms/definitions/exports/testLimit.js
+++
@@ -1,5 +0,0 @@
-export default {
-// TODO: ghost code
-  fieldMap: { 'test.limit': { fieldId: 'test.limit' } },
-  layout: { fields: ['test.limit'] },
-};
--- src/forms/definitions/exports/valueDelta.js
+++
@@ -1,8 +0,0 @@
-export default {
-  // TODO:ghost code
-  fieldMap: {
-    'valueDelta.exportedField': { fieldId: 'valueDelta.exportedField' },
-    'valueDelta.pendingField': { fieldId: 'valueDelta.pendingField' },
-  },
-  layout: { fields: ['valueDelta.exportedField', 'valueDelta.pendingField'] },
-};
