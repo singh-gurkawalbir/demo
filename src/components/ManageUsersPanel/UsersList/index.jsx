24b43171c4abd23a37e60280129b04ef0bf04d3b
Sravan Dandra <sravan.dandra@gmail.com>
2020-12-09 11:27:45 +0530

    Admin role changes

--- src/components/ManageUsersPanel/UsersList/index.jsx
+++ src/components/ManageUsersPanel/UsersList/index.jsx
@@ -13,7 +13,7 @@ import LoadResources from '../../LoadResources';
 export default function UsersList({ integrationId, storeId, className }) {
   const dispatch = useDispatch();
   const isAccountOwner = useSelector(state =>
-    [USER_ACCESS_LEVELS.ACCOUNT_OWNER].includes(selectors.resourcePermissions(state).accessLevel)
+    [USER_ACCESS_LEVELS.ACCOUNT_OWNER, USER_ACCESS_LEVELS.ACCOUNT_ADMIN].includes(selectors.resourcePermissions(state).accessLevel)
   );
 
   const isUserInErrMgtTwoDotZero = useSelector(state =>
