88bc16d750173eead8a8a91b2a749f6854770ed1
ashu-gupta <gupta.ashu1602@gmail.com>
2020-12-28 23:16:52 +0530

    added PP transforms (response and lookup)

--- src/reducers/session/_editors/processorLogic/responseTransform.js
+++ src/reducers/session/_editors/processorLogic/responseTransform.js
@@ -1,45 +1,73 @@
-/*
- * Creates transform rules as per required format to be saved
- */
-const constructTransformData = values => {
-  const { editorType, rule, scriptId, entryFunction } = values;
-  const type = editorType === 'transform' ? 'expression' : 'script';
-
-  return {
-    type,
-    expression: {
-      version: 1,
-      rules: rule ? [rule] : [[]],
-    },
-    script: {
-      _scriptId: scriptId,
-      function: entryFunction,
-    },
-  };
-};
+import { hooksToFunctionNamesMap } from '../../../../utils/hooks';
+import flowTransform from './flowTransform';
 
 export default {
+  processor: ({activeProcessor}) => activeProcessor,
+  init: ({resource, options}) => {
+    let activeProcessor = 'transform';
+    const transformObj = resource?.responseTransform;
+    const { script = {}, expression = {} } = transformObj || {};
+    const rule = {
+      javascript: {
+        fetchScriptContent: true,
+      },
+    };
+
+    // set values only if undefined (to pass dirty check correctly)
+    if (expression.rules?.[0]) {
+      rule.transform = expression.rules?.[0];
+    }
+    if (script._scriptId) {
+      rule.javascript.scriptId = script._scriptId;
+      activeProcessor = 'javascript';
+    }
+    rule.javascript.entryFunction = script.function || hooksToFunctionNamesMap.transform;
+
+    return {
+      ...options,
+      rule,
+      activeProcessor,
+    };
+  },
+  buildData: sampleData => flowTransform.buildData(sampleData),
+  requestBody: editor => flowTransform.requestBody(editor),
+  validate: editor => flowTransform.validate(editor),
+  dirty: editor => flowTransform.dirty(editor),
+  processResult: (editor, result) => flowTransform.processResult(editor, result),
   patchSet: editor => {
     const patches = {
       foregroundPatches: undefined,
       backgroundPatches: [],
     };
-    const responseTransform = constructTransformData(editor);
     const {
-      code,
-      scriptId,
-      type,
-      data: sampleResponseData,
-      optionalSaveParams = {},
+      rule,
+      data,
+      resourceId,
+      resourceType,
+      activeProcessor,
     } = editor;
-    const { resourceId, resourceType } = optionalSaveParams;
+    const {transform: transformRule, javascript} = rule || {};
+    const {scriptId, code, entryFunction } = javascript || {};
+
+    const type = activeProcessor === 'transform' ? 'expression' : 'script';
+    const responseTransform = {
+      type,
+      expression: {
+        version: 1,
+        rules: transformRule ? [transformRule] : [[]],
+      },
+      script: {
+        _scriptId: scriptId,
+        function: entryFunction,
+      },
+    };
 
     patches.foregroundPatches = {
       patch: [
         {
           op: 'replace',
           path: '/sampleResponseData',
-          value: sampleResponseData,
+          value: data?.transform,
         },
         {
           op: 'replace',
