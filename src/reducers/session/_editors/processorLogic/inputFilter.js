24e81bd10c3ab3700a5db2f1b31e112d9a75a548
ashu-gupta <gupta.ashu1602@gmail.com>
2020-12-28 23:12:11 +0530

    hooked up input and output filters (FB actions)

--- src/reducers/session/_editors/processorLogic/inputFilter.js
+++ src/reducers/session/_editors/processorLogic/inputFilter.js
@@ -1,22 +1,104 @@
+import isEqual from 'lodash/isEqual';
 import actions from '../../../../actions';
 import { RESOURCE_TYPE_PLURAL_TO_SINGULAR } from '../../../../constants/resource';
+import { hooksToFunctionNamesMap } from '../../../../utils/hooks';
+import javascript from './javascript';
+import filter from './filter';
 
 export default {
+  processor: ({activeProcessor}) => activeProcessor,
+  init: ({resource, options}) => {
+    let activeProcessor = 'filter';
+
+    const filterObj = options.resourceType === 'imports' ? resource?.filter : resource?.inputFilter;
+    const { script = {}, expression = {} } = filterObj || {};
+    const rule = {
+      filter: expression.rules || [],
+      javascript: {
+        fetchScriptContent: true,
+      },
+    };
+
+    // set values only if undefined (to pass dirty check correctly)
+    if (script._scriptId) {
+      rule.javascript.scriptId = script._scriptId;
+      activeProcessor = 'javascript';
+    }
+    rule.javascript.entryFunction = script.function || hooksToFunctionNamesMap.filter;
+
+    return {
+      ...options,
+      rule,
+      activeProcessor,
+    };
+  },
+  buildData: sampleData => ({
+    filter: sampleData,
+    javascript: sampleData,
+  }),
+  requestBody: editor => {
+    if (editor.activeProcessor === 'filter') {
+      return filter.requestBody({
+        data: editor.data?.filter,
+        rule: editor.rule?.filter,
+      });
+    }
+
+    return javascript.requestBody({
+      data: editor.data?.javascript,
+      rule: editor.rule?.javascript,
+      context: editor.context,
+    });
+  },
+  validate: editor => {
+    if (editor.activeProcessor === 'filter') {
+      return filter.validate({
+        data: editor.data?.filter,
+        rule: editor.rule?.filter,
+      });
+    }
+
+    return javascript.validate({
+      data: editor.data?.javascript,
+    });
+  },
+  dirty: editor => {
+    if (editor.activeProcessor === 'javascript') {
+      return javascript.dirty({
+        rule: editor.rule?.javascript,
+        originalRule: editor.originalRule?.javascript,
+      });
+    }
+    if (!isEqual(editor.originalRule?.filter, editor.rule?.filter)) {
+      return true;
+    }
+
+    return false;
+  },
+  processResult: (editor, result) => {
+    if (editor.activeProcessor === 'filter') {
+      return filter.processResult(editor, result);
+    }
+
+    return javascript.processResult(editor, result);
+  },
   patchSet: editor => {
     const patches = {
       foregroundPatches: undefined,
       backgroundPatches: [],
     };
     const {
-      editorType,
-      rule: filterRules = [],
-      scriptId,
-      code,
-      entryFunction,
-      optionalSaveParams = {},
+      rule,
+      originalRule,
+      resourceId,
+      resourceType,
+      activeProcessor,
     } = editor;
-    const { resourceId, resourceType, rules } = optionalSaveParams || {};
-    const type = editorType === 'filter' ? 'expression' : 'script';
+    const {filter: filterRules, javascript} = rule || {};
+    const {filter: originalRules} = originalRule || {};
+    const {scriptId, code, entryFunction } = javascript || {};
+
+    const type = activeProcessor === 'filter' ? 'expression' : 'script';
     const path = resourceType === 'imports' ? '/filter' : '/inputFilter';
     const value = {
       type,
@@ -50,7 +132,7 @@ export default {
       });
     }
 
-    if ((type === 'expression' && !rules.length) || !scriptId) {
+    if ((type === 'expression' && !originalRules.length) || !scriptId) {
       // If user configures filters first time
       if ((type === 'expression' && filterRules.length) || scriptId) {
         patches.backgroundPatches.push({
