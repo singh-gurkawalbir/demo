d1f573ea024e241fbfe3ab8caf891b86495e69b1
driedl <dave@riedl.ca>
2021-01-08 10:04:31 -0800

    Completed handlebar guide drawer action

--- src/components/AFE2/Drawer/actions/HandlebarGuide.jsx
+++ src/components/AFE2/Drawer/actions/HandlebarGuide.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import { useTheme, useMediaQuery, IconButton } from '@material-ui/core';
 import CeligoDivider from '../../../CeligoDivider';
 import IconTextButton from '../../../IconTextButton';
+import Icon from '../../../icons/InstallationGuideIcon';
 
-// TODO: We need a new icon for this guide.
-import Icon from '../../../icons/HelpCenterIcon';
+const helpUrl = 'https://docs.celigo.com/hc/en-us/articles/360039326071-Handlebars-helper-reference';
 
 export default function HandlebarGuide() {
+  const theme = useTheme();
+  const hasWideScreen = useMediaQuery(theme.breakpoints.up('md'));
+
   return (
     <>
-      <IconTextButton variant="text"><Icon />Handlebar guide</IconTextButton>
+      {hasWideScreen ? (
+        <IconTextButton
+          component="a"
+          href={helpUrl}
+          target="_blank"
+          variant="text"><Icon />
+          Handlebar guide
+        </IconTextButton>
+      ) : (
+        <IconButton
+          component="a"
+          href={helpUrl}
+          target="_blank">
+          <Icon />
+        </IconButton>
+      )}
 
       <CeligoDivider position="right" />
     </>
