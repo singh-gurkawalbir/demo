3d648a9240edfa96b895a03b6886151d35536c51
driedl <dave@riedl.ca>
2021-01-08 11:30:59 -0800

    fixed whitespace issue with button placement.

--- src/components/AFE2/Drawer/actions/HandlebarGuide.jsx
+++ src/components/AFE2/Drawer/actions/HandlebarGuide.jsx
@@ -1,30 +1,34 @@
 import React from 'react';
-import { useTheme, useMediaQuery, IconButton } from '@material-ui/core';
+import { makeStyles, useTheme, useMediaQuery, IconButton } from '@material-ui/core';
 import CeligoDivider from '../../../CeligoDivider';
 import IconTextButton from '../../../IconTextButton';
 import Icon from '../../../icons/InstallationGuideIcon';
 
-const helpUrl = 'https://docs.celigo.com/hc/en-us/articles/360039326071-Handlebars-helper-reference';
+const useStyles = makeStyles({
+  button: {
+    marginRight: '0 !important',
+  },
+});
+
+const anchorProps = {
+  component: 'a',
+  target: '_blank',
+  href: 'https://docs.celigo.com/hc/en-us/articles/360039326071-Handlebars-helper-reference',
+};
 
 export default function HandlebarGuide() {
   const theme = useTheme();
+  const classes = useStyles();
   const hasWideScreen = useMediaQuery(theme.breakpoints.up('md'));
 
   return (
     <>
       {hasWideScreen ? (
-        <IconTextButton
-          component="a"
-          href={helpUrl}
-          target="_blank"
-          variant="text"><Icon />
-          Handlebar guide
+        <IconTextButton {...anchorProps} className={classes.button} variant="text">
+          <Icon /> Handlebar guide
         </IconTextButton>
       ) : (
-        <IconButton
-          component="a"
-          href={helpUrl}
-          target="_blank">
+        <IconButton {...anchorProps} className={classes.button} >
           <Icon />
         </IconButton>
       )}
