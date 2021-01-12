6e7e37be2ada4a484b7fb1e324dd6de0c13e2bc4
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-01-12 14:04:13 +0530

    fix 19729

--- src/components/ResourceTable/errorManagement/cells/SelectDate.jsx
+++ src/components/ResourceTable/errorManagement/cells/SelectDate.jsx
@@ -60,6 +60,7 @@ export default function SelectDate({
   const rangeFilters = [
     {id: 'today', label: 'Today'},
     {id: 'yesterday', label: 'Yesterday'},
+    {id: 'last24hours', label: 'Last 24 hours'},
     {id: 'last7days', label: 'Last 7 Days'},
     {id: 'last15days', label: 'Last 15 Days'},
     {id: 'last30days', label: 'Last 30 Days'},
