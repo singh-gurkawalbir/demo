65e5df4fe3ae01cc26d51eae87312fc422c2df13
Sravan Dandra <sravan.dandra@gmail.com>
2021-01-13 01:00:40 +0530

    test cases

--- src/utils/date.test.js
+++ src/utils/date.test.js
@@ -4,6 +4,18 @@ import momenttz from 'moment-timezone';
 const { convertUtcToTimezone} = require('./date');
 
 describe('date util function test', () => {
+  let momentSpy;
+
+  beforeAll(() => {
+    // Lock timezone
+    momentSpy = jest.spyOn(momenttz.tz, 'guess').mockImplementation(() => 'GMT');
+  });
+
+  afterAll(() => {
+    // Unlock Timezone
+    momentSpy.mockRestore();
+  });
+
   describe('convertUtcToTimezone function test', () => {
     const testDate = new Date('2018-12-24T10:33:30.000+05:30');
 
@@ -13,21 +25,21 @@ describe('date util function test', () => {
       expect(convertUtcToTimezone()).toEqual('Invalid date');
       expect(convertUtcToTimezone(null)).toEqual('Invalid date');
       expect(convertUtcToTimezone(null, null)).toEqual('Invalid date');
-      expect(convertUtcToTimezone(testDate, '')).toEqual(' 10:33:30 am');
+      expect(convertUtcToTimezone(testDate, '')).toEqual(' 5:03:30 am');
       expect(convertUtcToTimezone(testDate, '', '')).toEqual(' ');
     });
 
     test('should return correct time for convertUtctoTimezone in expected date and time formats', () => {
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm')).toEqual('12/24/18 10:33');
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YYYY', 'hh:mm')).toEqual('12/24/2018 10:33');
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'HH:mm')).toEqual('12/24/18 10:33');
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss')).toEqual('12/24/18 10:33:30');
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss a')).toEqual('12/24/18 10:33:30 am');
-      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss A')).toEqual('12/24/18 10:33:30 AM');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm')).toEqual('12/24/18 05:03');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YYYY', 'hh:mm')).toEqual('12/24/2018 05:03');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'HH:mm')).toEqual('12/24/18 05:03');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss')).toEqual('12/24/18 05:03:30');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss a')).toEqual('12/24/18 05:03:30 am');
+      expect(convertUtcToTimezone(testDate, 'MM/DD/YY', 'hh:mm:ss A')).toEqual('12/24/18 05:03:30 AM');
     });
 
     test('should return in default time and date formats when formats not passed in params', () => {
-      expect(convertUtcToTimezone(testDate)).toEqual('12/24/2018 10:33:30 am');
+      expect(convertUtcToTimezone(testDate)).toEqual('12/24/2018 5:03:30 am');
     });
 
     test('should return in correct timezone when timezone is passed in params', () => {
@@ -45,17 +57,6 @@ describe('date util function test', () => {
     });
 
     describe('', () => {
-      let momentSpy;
-
-      beforeAll(() => {
-        // Lock timezone
-        momentSpy = jest.spyOn(momenttz.tz, 'guess').mockImplementation(() => 'GMT');
-      });
-
-      afterAll(() => {
-        // Unlock Timezone
-        momentSpy.mockRestore();
-      });
       test('should return in system timezone when timezone not passed in params', () => {
         expect(convertUtcToTimezone(testDate, undefined, undefined)).toEqual('12/24/2018 5:03:30 am');
       });
