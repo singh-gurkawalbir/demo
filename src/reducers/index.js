78dffa55077002f255dd8fcc4ecbd1cbb6079891
Sravan Dandra <sravan.dandra@gmail.com>
2021-01-21 18:01:00 +0530

    IO-19858

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2194,9 +2194,11 @@ selectors.isProfileLoading = state => {
 
 selectors.availableUsersList = (state, integrationId) => {
   const isAccountOwnerOrAdmin = selectors.isAccountOwnerOrAdmin(state);
+  const userPermissions = selectors.userPermissions(state) || emptyObject;
+
   let _users = [];
 
-  if (isAccountOwnerOrAdmin) {
+  if (userPermissions.accessLevel === USER_ACCESS_LEVELS.ACCOUNT_OWNER) {
     if (integrationId) {
       _users = selectors.integrationUsersForOwner(state, integrationId);
     } else {
