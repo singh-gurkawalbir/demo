54b05a329a35e4af914dfb54f10567f004778ed1
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2021-02-03 13:16:23 +0530

    IO-20088

--- src/components/DynaForm/fields/DynaTableView/DynaRefreshableStaticMap.jsx
+++ src/components/DynaForm/fields/DynaTableView/DynaRefreshableStaticMap.jsx
@@ -150,7 +150,7 @@ export default function DynaRefreshableStaticMap(props) {
       {...props}
       isLoading={isLoadingMap}
       metadata={metadata}
-      shouldReset={optionsMap}
+      shouldReset={metadata}
       optionsMap={optionsMap}
       value={computedValue}
       onFieldChange={handleFieldChange}
