4eb8633258d4a3dac1c1b01fdd64030f61707c31
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2021-02-03 13:11:05 +0530

    IO-20088

--- src/reducers/data/resources/index.js
+++ src/reducers/data/resources/index.js
@@ -1,5 +1,5 @@
 import produce from 'immer';
-import { get } from 'lodash';
+import { get, isEqual } from 'lodash';
 import { createSelector } from 'reselect';
 import sift from 'sift';
 import actionTypes from '../../../actions/types';
@@ -53,6 +53,9 @@ function replaceOrInsertResource(state, resourceType, resourceValue) {
     return { ...state, [type]: [...collection, resource] };
   }
 
+  if (isEqual(resource, collection[index])) {
+    return state;
+  }
   const newCollection = [
     ...collection.slice(0, index),
     resource,
