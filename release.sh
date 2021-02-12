31760878229d1d4eb704f332b26fa6ce8c029bf1
SandeepKumarG <sandeepkumarcse0509@gmail.com>
2021-02-12 20:16:13 +0530

    respecting different files for EU and NA

--- release.sh
+++ release.sh
@@ -1,8 +1,9 @@
 #!/bin/zsh
 set -e
 
-LR_vars=$2
-[[ -n $2 ]] || LR_vars="./.lr_staging" #defaulting to staging.
+LR_vars=$1
+echo $LR_vars
+[[ -n $1 ]] || LR_vars="./.lr_staging" #defaulting to staging.
 [[ -f $LR_vars ]] || (echo "logrocket env file invalid $LR_vars" && exit 1)
 version=`cat package.json | python -c 'import json,sys,datetime; from datetime import time; d = datetime.datetime.now(); obj=json.load(sys.stdin); version = obj["gitRevision"]; dateString = d.strftime("%d-%m-%H-%M"); print({version.find("release") == 0: version[8:-2] + "." + dateString}.get(True, version + "." + dateString));'`
 echo "building version $version ..."
@@ -10,16 +11,23 @@ echo $LR_vars
 export RELEASE_VERSION="$version"
 . $LR_vars
 NODE_ENV=production webpack --mode=production
-echo "creating logrocket release and uploading source maps for $LOGROCKET_IDENTIFIER ..."
 URL_PREFIX="~/react/$version/"
 echo $URL_PREFIX
-logrocket release $version --apikey=$LOGROCKET_API_KEY
-logrocket upload build/ --release=$version --apikey=$LOGROCKET_API_KEY --url-prefix=$URL_PREFIX
-if [[ $LOGROCKET_IDENTIFIER_EU != $LOGROCKET_IDENTIFIER ]]; then
-    echo "creating logrocket release and uploading source maps for $LOGROCKET_IDENTIFIER_EU ..."
+
+if [ ! -z "$LOGROCKET_IDENTIFIER" ] 
+then
+    echo "creating logrocket release and uploading source maps for $LOGROCKET_IDENTIFIER in NA ..."
+    logrocket release $version --apikey=$LOGROCKET_API_KEY
+    logrocket upload build/ --release=$version --apikey=$LOGROCKET_API_KEY --url-prefix=$URL_PREFIX
+elif [ ! -z "$LOGROCKET_IDENTIFIER_EU" ] 
+then
+    echo "creating logrocket release and uploading source maps for $LOGROCKET_IDENTIFIER_EU in EU ..."
     logrocket release $version --apikey=$LOGROCKET_API_KEY_EU
     logrocket upload build/ --release=$version --apikey=$LOGROCKET_API_KEY_EU --url-prefix=$URL_PREFIX
+else
+    (echo "Neither of LOGROCKET_API_KEY nor LOGROCKET_API_KEY_EU set so exiting" && exit 1)
 fi
+
 # move the source map files into a separate folder
 mkdir -p build/sourcemaps && mv build/*.js.map build/sourcemaps/
 aws s3 cp build/ s3://integrator-staging-ui-resources/react/$version/ --recursive --acl public-read
