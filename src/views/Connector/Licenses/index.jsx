e54ab639597a8efab007d1c33efb88cffdd2cd89
Kaushik-Nandan <kaushiknandan32@gmail.com>
2021-02-17 15:50:52 +0530

    --added required condition to show childlicenses only when its integrationSchema has initChild.function

--- src/views/Connector/Licenses/index.jsx
+++ src/views/Connector/Licenses/index.jsx
@@ -127,22 +127,24 @@ export default function Licenses(props) {
         </div>
       </CeligoPageBar>
       <div className={classes.resultContainer}>
-        {list.count === 0 ? (
-          <Typography>
-            {list.total === 0
-              ? 'You don\'t have any licenses.'
-              : 'Your search didn’t return any matching results. Try expanding your search criteria.'}
-          </Typography>
-        ) : (
-          <CeligoTable
-            data={list.resources}
-            {...metadata}
-            filterKey={sortFilterKey}
-            actionProps={{
-              resourceType: `connectors/${connectorId}/licenses`,
-            }}
+        <LoadResources required resources="integrations" >
+          {list.count === 0 ? (
+            <Typography>
+              {list.total === 0
+                ? 'You don\'t have any licenses.'
+                : 'Your search didn’t return any matching results. Try expanding your search criteria.'}
+            </Typography>
+          ) : (
+            <CeligoTable
+              data={list.resources}
+              {...metadata}
+              filterKey={sortFilterKey}
+              actionProps={{
+                resourceType: `connectors/${connectorId}/licenses`,
+              }}
           />
-        )}
+          )}
+        </LoadResources>
       </div>
       <ShowMoreDrawer
         filterKey="connectorLicenses"
