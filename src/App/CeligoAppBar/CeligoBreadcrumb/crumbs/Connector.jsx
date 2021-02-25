307feeb128264210156c8cc7bff5e3b1a06db2ad
Kaushik-Nandan <kaushiknandan32@gmail.com>
2021-02-25 20:07:25 +0530

    --added button to connector breadcrumb

--- src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/Connector.jsx
+++ src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/Connector.jsx
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { selectors } from '../../../../reducers';
 import LoadResources from '../../../../components/LoadResources';
 
 export default function ConnectorCrumb({ connectorId }) {
+  const history = useHistory();
   const connector = useSelector(state =>
     selectors.resource(state, 'connectors', connectorId)
   );
 
+  const handleClick = useCallback(e => {
+    e.preventDefault();
+    history.push('/connectors');
+  }, [history]);
+
   return (
     <LoadResources resources="connectors">
-      {connector?.name || 'Integration app'}
+      <div onClick={handleClick} >
+        {connector?.name || 'Integration app'}
+      </div>
     </LoadResources>
   );
 }
