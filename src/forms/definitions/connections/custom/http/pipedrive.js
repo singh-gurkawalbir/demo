cee2444acbbd08b6e148228fd1589f080943649f
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2021-02-26 09:44:27 +0530

    IO-20455

--- src/forms/definitions/connections/custom/http/pipedrive.js
+++ src/forms/definitions/connections/custom/http/pipedrive.js
@@ -100,6 +100,11 @@ export default {
       helpKey: 'pipedrive.connection.http.auth.token.token',
       visibleWhen: [{ field: 'http.auth.type', is: ['token'] }],
     },
+    'http.auth.oauth.callbackURL': {
+      fieldId: 'http.auth.oauth.callbackURL',
+      copyToClipboard: true,
+      visibleWhen: [{ field: 'http.auth.type', is: ['oauth'] }],
+    },
     application: {
       fieldId: 'application',
     },
@@ -120,6 +125,7 @@ export default {
       fields: ['http.auth.type',
         'http.subdomain',
         'http._iClientId',
+        'http.auth.oauth.callbackURL',
         'http.auth.token.token',
       ],
     },
