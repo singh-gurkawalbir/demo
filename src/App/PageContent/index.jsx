cd8e70ab2ce8bbe09058b9a8976c8a72fb47d71e
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-03-04 18:54:26 +0530

    fix

--- src/App/PageContent/index.jsx
+++ src/App/PageContent/index.jsx
@@ -7,7 +7,7 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     overflowX: 'auto',
-    minHeight: '100%',
+    height: '100vh',
   },
   toolbar: {
     height: theme.appBarHeight,
