5c7d20071396bd9d566b21ce372074a8d23abeb5
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-03-04 21:47:36 +0530

    added minor enhancement to show a confirmation dialog

--- src/components/ErrorList/ErrorTable/ErrorActions/index.jsx
+++ src/components/ErrorList/ErrorTable/ErrorActions/index.jsx
@@ -59,6 +59,8 @@ function getAllErrorsLabelToRetry(count, hasSearchFilter) {
 
 const RetryAction = ({ onClick, flowId, resourceId, isResolved, disable, isSearchFilterApplied }) => {
   const classes = useStyles();
+  const { confirmDialog } = useConfirmDialog();
+
   const allRetriableErrorCount = useSelector(state => {
     const {errors = []} = selectors.resourceFilteredErrorDetails(state, {
       flowId,
@@ -81,11 +83,36 @@ const RetryAction = ({ onClick, flowId, resourceId, isResolved, disable, isSearc
 
   const disableRetry = disable || (!selectedRetriableErrorCount && !allRetriableErrorCount);
 
+  const allErrorsLabel = getAllErrorsLabelToRetry(allRetriableErrorCount, isSearchFilterApplied);
+
+  const handleRetry = useCallback(e => {
+    if (isResolved || e.target.value === 'selected') {
+      return onClick(e);
+    }
+
+    confirmDialog({
+      title: 'Confirm retry',
+      message: `Are you sure you want to retry ${allErrorsLabel.toLowerCase()}?`,
+      buttons: [
+        {
+          label: 'Retry',
+          onClick: () => {
+            onClick(e);
+          },
+        },
+        {
+          label: 'Cancel',
+          color: 'secondary',
+        },
+      ],
+    });
+  }, [confirmDialog, isResolved, onClick, allErrorsLabel]);
+
   return (
     <CeligoSelect
       className={clsx(classes.actionBtn, classes.retryBtn, { [classes.noPadding]: disableRetry && !isRetryInProgress })}
       data-test="retryJobs"
-      onChange={onClick}
+      onChange={handleRetry}
       disabled={disableRetry}
       displayEmpty
       value="">
@@ -93,10 +120,10 @@ const RetryAction = ({ onClick, flowId, resourceId, isResolved, disable, isSearc
         Retry { isRetryInProgress && <Spinner size={16} className={classes.loading} />}
       </MenuItem>
       <MenuItem value="selected" disabled={!selectedRetriableErrorCount}>
-        {selectedRetriableErrorCount} retriable errors
+        {selectedRetriableErrorCount} retriable {selectedRetriableErrorCount === 1 ? 'error' : 'errors'}
       </MenuItem>
       <MenuItem value="all" disabled={!allRetriableErrorCount}>
-        {getAllErrorsLabelToRetry(allRetriableErrorCount, isSearchFilterApplied)}
+        {allErrorsLabel}
       </MenuItem>
     </CeligoSelect>
   );
@@ -104,6 +131,8 @@ const RetryAction = ({ onClick, flowId, resourceId, isResolved, disable, isSearc
 
 const ResolveAction = ({ onClick, flowId, resourceId, disable, isSearchFilterApplied }) => {
   const classes = useStyles();
+  const { confirmDialog } = useConfirmDialog();
+
   const allErrorCount = useSelector(state => {
     const {errors = []} = selectors.resourceFilteredErrorDetails(state, { flowId, resourceId });
 
@@ -120,11 +149,36 @@ const ResolveAction = ({ onClick, flowId, resourceId, disable, isSearchFilterApp
 
   const disableResolve = disable || (!selectedErrorCount && !allErrorCount);
 
+  const allErrorsLabel = getAllErrorsLabelToResolve(allErrorCount, isSearchFilterApplied);
+
+  const handleResolve = useCallback(e => {
+    if (e.target.value === 'selected') {
+      return onClick(e);
+    }
+
+    confirmDialog({
+      title: 'Confirm resolve',
+      message: `Are you sure you want to resolve ${allErrorsLabel.toLowerCase()}?`,
+      buttons: [
+        {
+          label: 'Resolve',
+          onClick: () => {
+            onClick(e);
+          },
+        },
+        {
+          label: 'Cancel',
+          color: 'secondary',
+        },
+      ],
+    });
+  }, [confirmDialog, onClick, allErrorsLabel]);
+
   return (
     <CeligoSelect
       className={clsx(classes.actionBtn, classes.resolveBtn, { [classes.noPadding]: disableResolve && !isResolveInProgress })}
       data-test="retryJobs"
-      onChange={onClick}
+      onChange={handleResolve}
       disabled={disableResolve}
       displayEmpty
       value="">
@@ -132,10 +186,10 @@ const ResolveAction = ({ onClick, flowId, resourceId, disable, isSearchFilterApp
         Resolve  { isResolveInProgress && <Spinner size={16} className={classes.loading} />}
       </MenuItem>
       <MenuItem value="selected" disabled={!selectedErrorCount}>
-        {selectedErrorCount} selected errors
+        {selectedErrorCount} selected {selectedErrorCount === 1 ? 'error' : 'errors'}
       </MenuItem>
       <MenuItem value="all" disabled={!allErrorCount}>
-        {getAllErrorsLabelToResolve(allErrorCount, isSearchFilterApplied)}
+        {allErrorsLabel}
       </MenuItem>
     </CeligoSelect>
   );
