7bc69ebab6727f3614f78c6969ab74b41a75e4bd
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-03-09 11:32:28 +0530

    fix

--- src/components/CeligoTabLayout/CustomPanels/DefaultPanel/index.jsx
+++ src/components/CeligoTabLayout/CustomPanels/DefaultPanel/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import ClipboardPanel from '../ClipboardPanel';
-import CodeEditor from '../../../CodeEditor';
+import CodeEditor from '../../../CodeEditor2';
 
 const useStyles = makeStyles(theme => ({
   defaultPanelContainer: {
