926698e799bd9ac46021f0ab627b5389dc5bf3fd
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2021-03-16 17:47:19 +0530

    Minor change

--- src/components/DynaForm/fields/DynaLicenseEdition.jsx
+++ src/components/DynaForm/fields/DynaLicenseEdition.jsx
@@ -6,7 +6,7 @@ import { selectors } from '../../../reducers';
 const emptyArr = [];
 export default function DynaLicenseEdition(props) {
   const { connectorId } = props;
-  const editions = useSelector(state => selectors.resource(state, 'connectors', connectorId)?.twoDotZero?.editions) || emptyArr;
+  const editions = useSelector(state => selectors.resource(state, 'connectors', connectorId))?.twoDotZero?.editions || emptyArr;
 
   const options = useMemo(() => editions.map(edition => ({
     label: edition.displayName || edition._id,
