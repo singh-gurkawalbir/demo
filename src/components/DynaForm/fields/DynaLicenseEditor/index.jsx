9f4e9e379b144fb968ba511a2964004bd756039d
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2021-04-06 16:02:00 +0530

    --required changes

--- src/components/DynaForm/fields/DynaLicenseEditor/index.jsx
+++ src/components/DynaForm/fields/DynaLicenseEditor/index.jsx
@@ -5,16 +5,22 @@ import { emptyObject } from '../../../../utils/constants';
 import DynaEditor from '../DynaEditor';
 
 export default function DynaLicenseEditor(props) {
-  const { formKey, id, value } = props;
+  const { formKey, id, value, onFieldChange } = props;
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if (!value || typeof value === 'object') {
-      dispatch(actions.form.fieldChange(formKey)(id, emptyObject));
+    if (!value) {
+      onFieldChange(id, emptyObject);
       dispatch(actions.form.forceFieldState(formKey)(id, {isValid: true}));
 
       return;
     }
+    if (typeof value === 'object') {
+      dispatch(actions.form.forceFieldState(formKey)(id, {isValid: true}));
+
+      return;
+    }
+    // try block is for all the string cases
     try {
       JSON.parse(value);
 
@@ -22,7 +28,7 @@ export default function DynaLicenseEditor(props) {
     } catch (e) {
       dispatch(actions.form.forceFieldState(formKey)(id, {isValid: false, errorMessages: 'Invalid Json format'}));
     }
-  }, [id, dispatch, formKey, value]);
+  }, [id, dispatch, formKey, value, onFieldChange]);
 
   // suspend force field state compuation once the component turns invisible
   useEffect(() => () => {
