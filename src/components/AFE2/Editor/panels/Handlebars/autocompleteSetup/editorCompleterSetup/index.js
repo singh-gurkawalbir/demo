c1b17011ad3d48d887fd422359fab74acfca22c6
ashu-gupta <gupta.ashu1602@gmail.com>
2021-04-09 17:17:28 +0530

    moved handlebars autocomplete logic to AFE2

---
+++ src/components/AFE2/Editor/panels/Handlebars/autocompleteSetup/editorCompleterSetup/index.js
@@ -0,0 +1,15 @@
+import * as helpers from '../completers';
+import shouldAutocompleteOnBackSpace from '../editorBehaviorTweaks/index';
+
+export default function handleBarsAutocompleteSetup(editor) {
+  shouldAutocompleteOnBackSpace(editor);
+  const editorInst = editor;
+
+  editorInst.completers = [];
+  const { handleBarsCompleters } = helpers;
+  const completers = handleBarsCompleters.getCompleters();
+
+  Object.keys(completers).forEach(key =>
+    editorInst.completers.push(completers[key])
+  );
+}
