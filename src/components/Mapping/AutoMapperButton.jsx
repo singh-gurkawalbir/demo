186dd7b8810d6f14681f92c93bdc3c3c47fd702e
driedl <dave@riedl.ca>
2021-04-13 18:06:57 -0700

    removed auto-map confirm dialog

--- src/components/Mapping/AutoMapperButton.jsx
+++ src/components/Mapping/AutoMapperButton.jsx
@@ -1,8 +1,6 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { Button, makeStyles } from '@material-ui/core';
 import { useSelector, useDispatch, shallowEqual } from 'react-redux';
-import { useRouteMatch } from 'react-router-dom';
-import useConfirmDialog from '../ConfirmDialog';
 import useEnqueueSnackbar from '../../hooks/enqueueSnackbar';
 import actions from '../../actions';
 import {selectors} from '../../reducers';
@@ -22,10 +20,7 @@ const useStyles = makeStyles(theme => ({
 export default function AutoMapperButton({disabled}) {
   const classes = useStyles();
   const dispatch = useDispatch();
-  const match = useRouteMatch();
-  const [saveTriggered, setSaveTriggered] = useState(false);
   const [enqueueSnackbar] = useEnqueueSnackbar();
-  const { confirmDialog } = useConfirmDialog();
 
   const flowHasExport = useSelector(state => {
     const {flowId} = selectors.mapping(state);
@@ -33,8 +28,6 @@ export default function AutoMapperButton({disabled}) {
 
     return !!adaptorType;
   });
-  const { saveInProgress, saveCompleted, saveTerminated } = useSelector(state => selectors.mappingSaveStatus(state), shallowEqual);
-  const mappingsChanged = useSelector(state => selectors.mappingChanged(state));
 
   const {failMsg, failSeverity, isFetchingAutoSuggestions} = useSelector(state => {
     const {status, failMsg, failSeverity} = selectors.autoMapper(state);
@@ -42,41 +35,8 @@ export default function AutoMapperButton({disabled}) {
     return { isFetchingAutoSuggestions: status === 'requested', failMsg, failSeverity};
   }, shallowEqual);
 
-  const handleAutoMapperRequest = useCallback(() =>
+  const handleButtonClick = useCallback(() =>
     dispatch(actions.mapping.autoMapper.request()), [dispatch]);
-
-  const onSaveAndMap = useCallback(() => {
-    setSaveTriggered(true);
-    dispatch(actions.mapping.save({ match }));
-  }, [dispatch, match]);
-
-  const handleButtonClick = useCallback(() => {
-    if (!mappingsChanged) {
-      return handleAutoMapperRequest();
-    }
-
-    confirmDialog({
-      title: 'Save changes and auto-map fields?',
-      message: `Auto-mapping automatically adds export fields mapped to suggested import fields, 
-                but will not change any fields that you have already mapped. You can manually 
-                delete or modify fields after they have been auto-mapped.`,
-      buttons: [
-        {
-          label: 'Save changes & auto-map fields',
-          variant: 'primary',
-          onClick: onSaveAndMap,
-        },
-        {
-          label: 'Auto-map fields',
-          variant: 'secondary',
-          onClick: handleAutoMapperRequest,
-        },
-        { label: 'Cancel', variant: 'tertiary' },
-      ],
-    });
-  },
-  [confirmDialog, handleAutoMapperRequest, mappingsChanged, onSaveAndMap]
-  );
 
   useEffect(() => {
     if (failMsg) {
@@ -84,43 +44,28 @@ export default function AutoMapperButton({disabled}) {
     }
   }, [enqueueSnackbar, failMsg, failSeverity]);
 
-  useEffect(() => {
-    if (saveTriggered && saveTerminated) {
-      // only dispatch the auto-mapper request if the pending
-      // save mapping operation is successful.
-      if (saveCompleted) handleAutoMapperRequest();
-
-      setSaveTriggered(false);
-    }
-  }, [dispatch, handleAutoMapperRequest, saveCompleted, saveTerminated, saveTriggered]);
-
   if (!flowHasExport) {
     return null;
   }
-
-  // Save may be in progress, but unless it is triggered by this
-  // button, then we don't want to incorrectly flag as in progress.
-  const inProgress = (saveTriggered && saveInProgress) || isFetchingAutoSuggestions;
 
   return (
     <>
       <Button
         color="primary"
         variant="outlined"
-        dataTest="autoMapper"
-        disabled={disabled || inProgress}
+        data-test="autoMapper"
+        disabled={disabled || isFetchingAutoSuggestions}
         onClick={handleButtonClick}
       >
-        {(isFetchingAutoSuggestions || inProgress) ? (
+        {(isFetchingAutoSuggestions) ? (
           <>
             <Spinner size="small" className={classes.spinner} />
             Auto-mapping fields
           </>
         ) : 'Auto-map fields'}
       </Button>
-      <Help
-        title="Auto-map fields" helpKey="autoMapFields" className={classes.helpTextButton}
- />
+
+      <Help title="Auto-map fields" helpKey="autoMapFields" className={classes.helpTextButton} />
     </>
   );
 }
