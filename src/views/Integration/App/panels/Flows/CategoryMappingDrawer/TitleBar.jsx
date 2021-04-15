936950ec84499bd9a7be8b2e76b767ddfcf7824b
Sravan Dandra <sravan.dandra@gmail.com>
2021-04-15 22:43:59 +0530

    Performance fix

--- src/views/Integration/App/panels/Flows/CategoryMappingDrawer/TitleBar.jsx
+++ src/views/Integration/App/panels/Flows/CategoryMappingDrawer/TitleBar.jsx
@@ -7,7 +7,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import { selectors } from '../../../../../../reducers';
 import CloseIcon from '../../../../../../components/icons/CloseIcon';
 import AddIcon from '../../../../../../components/icons/AddIcon';
-import LoadResources from '../../../../../../components/LoadResources';
 import IconTextButton from '../../../../../../components/IconTextButton';
 import Help from '../../../../../../components/Help';
 import BackArrowIcon from '../../../../../../components/icons/BackArrowIcon';
@@ -83,9 +82,9 @@ export default function DrawerTitleBar({
 
   return (
     <div className={clsx(classes.titleBar, className)}>
-      <LoadResources required resources="flows">
-        <Typography variant="h3" className={classes.title}>
-          {backToParent && (
+
+      <Typography variant="h3" className={classes.title}>
+        {backToParent && (
           <IconButton
             data-test="backToCateogryMapping"
             aria-label="back"
@@ -93,42 +92,41 @@ export default function DrawerTitleBar({
             className={classes.arrowLeft}>
             <BackArrowIcon />
           </IconButton>
-          )}
-          {title ||
+        )}
+        {title ||
             `${addCategory ? 'Add category: ' : 'Edit mappings:'} ${
               flowName.length > 40
                 ? `${flowName.substring(0, 40 - 3)}...`
                 : flowName
             }`}
-        </Typography>
-        {!addCategory && (
-          <IconTextButton
-            variant="text"
-            data-test="addCategory"
-            onClick={handleAddCategoryClick}
-            color="primary"
-            className={classes.button}>
-            <AddIcon /> Add Category
-          </IconTextButton>
-        )}
-        {/* TODO:Sravan we need to add the help for add category and variation mapping */}
-        {help && (
-          <Help
-            title="Help"
-            className={classes.helpTextButton}
-            helpKey="test"
-            helpText="Dummy content here now, just to give styling to the element"
+      </Typography>
+      {!addCategory && (
+      <IconTextButton
+        variant="text"
+        data-test="addCategory"
+        onClick={handleAddCategoryClick}
+        color="primary"
+        className={classes.button}>
+        <AddIcon /> Add Category
+      </IconTextButton>
+      )}
+      {/* TODO:Sravan we need to add the help for add category and variation mapping */}
+      {help && (
+      <Help
+        title="Help"
+        className={classes.helpTextButton}
+        helpKey="test"
+        helpText="Dummy content here now, just to give styling to the element"
           />
-        )}
-        <Divider orientation="veritical" className={classes.divider} />
-        <IconButton
-          data-test="closeCategoryMapping"
-          aria-label="Close"
-          onClick={handleClose}
-          className={classes.closeIcon}>
-          <CloseIcon />
-        </IconButton>
-      </LoadResources>
+      )}
+      <Divider orientation="veritical" className={classes.divider} />
+      <IconButton
+        data-test="closeCategoryMapping"
+        aria-label="Close"
+        onClick={handleClose}
+        className={classes.closeIcon}>
+        <CloseIcon />
+      </IconButton>
     </div>
   );
 }
