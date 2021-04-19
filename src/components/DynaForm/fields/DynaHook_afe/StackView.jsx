8b6271579259aa2a20abf6ed29d6ca28b1fac9f3
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2021-04-19 15:54:49 +0530

    IO-20273

---
+++ src/components/DynaForm/fields/DynaHook_afe/StackView.jsx
@@ -0,0 +1,65 @@
+/* eslint-disable camelcase */
+import React from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import DynaSelect from '../DynaSelect';
+import { selectors } from '../../../../reducers';
+import useSelectorMemo from '../../../../hooks/selectors/useSelectorMemo';
+
+const useStyles = makeStyles(theme => ({
+  field: {
+    width: '50%',
+    paddingRight: theme.spacing(1),
+    overflow: 'hidden',
+    '& >.MuiFormControl-root': {
+      width: '100%',
+    },
+    '&:last-child': {
+      paddingRight: 0,
+    },
+  },
+}));
+
+const stacksFilterConfig = { type: 'stacks' };
+
+export default function StackView({
+  disabled,
+  required,
+  value = {},
+  isValidHookField,
+  handleFieldChange,
+}) {
+  const classes = useStyles();
+
+  const allStacks = useSelectorMemo(
+    selectors.makeResourceListSelector,
+    stacksFilterConfig
+  ).resources;
+
+  const allStacksOptions = allStacks.map(stack => ({
+    label: stack.name,
+    value: stack._id,
+  }));
+
+  return (
+
+    <div className={classes.field}>
+      <FormControl className={classes.select}>
+        <InputLabel htmlFor="stackId">Stack</InputLabel>
+        <DynaSelect
+          id="stackId"
+          label="Stacks"
+          value={value._stackId}
+          placeholder="None"
+          disabled={disabled}
+          required={required}
+          isValid={isValidHookField('_stackId')}
+          onFieldChange={handleFieldChange('_stackId')}
+          options={[{ items: allStacksOptions || [] }]}
+                />
+      </FormControl>
+    </div>
+  );
+}
