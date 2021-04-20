91a404240a5a45f3dbf4dc55ca47e550cd422485
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2021-04-20 21:32:03 +0530

    small fix

--- src/components/DynaForm/fields/DynaTableView/DynaRefreshableStaticMap.jsx
+++ src/components/DynaForm/fields/DynaTableView/DynaRefreshableStaticMap.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { isNaN } from 'lodash';
 import actions from '../../../../actions';
