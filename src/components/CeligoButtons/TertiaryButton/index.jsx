1fd014f60f95e9950c6a9e422c36c2046fa3eedd
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-04-26 21:36:31 +0530

    fix

--- src/components/CeligoButtons/TertiaryButton/index.jsx
+++ src/components/CeligoButtons/TertiaryButton/index.jsx
@@ -4,9 +4,12 @@ import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
 
 const useStyles = makeStyles(theme => ({
-  root: {
+  wrapper: {
     color: props => props.color === 'primary' ? theme.palette.primary.main : theme.palette.secondary.main,
     fontFamily: props => props.bold ? 'source sans pro semibold' : 'source sans pro',
+    '&:focus': {
+      color: props => props.color === 'primary' ? theme.palette.primary.main : theme.palette.text.secondary,
+    },
   },
   /* Note 1: we haven't added any sizes to the buttons into our theme,
     ideally, we should set the button sizes for every variant, because on many places we are overriding the font sizes with line-height. This will help us to showcase the buttons stories into the storybook */
@@ -22,7 +25,7 @@ export default function TertiaryButton(props) {
       color={color}
       size={size}
       bold={bold}
-      className={classes.root}
+      className={classes.wrapper}
       startIcon={startIcon}
       endIcon={endIcon}
       {...rest}>
