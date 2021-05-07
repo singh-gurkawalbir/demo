3739c6664085c1623aff2ba14bda686ef90c0ad6
adibjp <adibjp441@gmail.com>
2021-05-08 03:44:29 +0530

    IO-20715

--- src/components/ResourceTable/connections/actions/OpenDebugger/index.jsx
+++ src/components/ResourceTable/connections/actions/OpenDebugger/index.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import moment from 'moment';
 import { selectors } from '../../../../../reducers';
 import actions from '../../../../../actions';
 import DebugIcon from '../../../../icons/DebugIcon';
@@ -20,10 +21,18 @@ export default {
   component: function OpenDebugger({ rowData = {} }) {
     const { _id: connectionId } = rowData;
     const dispatch = useDispatch();
+    const isDebugActive = useSelector(state => {
+      const {debugDate} = selectors.resource(state, 'connections', connectionId);
+
+      return !!(debugDate && moment().isBefore(moment(debugDate)));
+    });
     const openDebugger = useCallback(() => {
       dispatch(actions.logs.connections.request(connectionId));
       dispatch(actions.bottomDrawer.addTab({tabType: 'connectionLogs', resourceId: connectionId}));
-    }, [connectionId, dispatch]);
+      if (!isDebugActive) {
+        dispatch(actions.logs.connections.startDebug(connectionId, 15));
+      }
+    }, [connectionId, dispatch, isDebugActive]);
 
     useEffect(() => {
       openDebugger();
