3c0988191afb53a3394e934d8cc89d477e5f1735
SiddharthMMore <SiddharthMMore@outlook.com>
2021-05-10 16:21:46 +0530

    IO-13980 Fix

--- src/components/Help/helpTextMap.js
+++ src/components/Help/helpTextMap.js
@@ -2265,8 +2265,6 @@ export default {
     'The CSV parser helper can be used to visualize and experiment with how <a href="http://integrator.io" target="_blank" data-saferedirecturl="https://www.google.com/url?q=http://integrator.io&amp;source=gmail&amp;ust=1590834533735000&amp;usg=AFQjCNFu7ZCLXUvr9xFWTLIWM0LeXPlPwg">integrator.io</a> parses CSV files (or any other delimited text files) into the JSON records/rows that then get processed by your flow.',
   apiIdentifier:
     'This resource can be invoked via an HTTP request to this unique URL.',
-  oneToMany:
-    'Select this option if the records being processed contain child records that should instead be treated as the main records (i.e. for this step only in the flow).  For example, if you are processing order records, but for this step in the flow you want to process the line items within the orders as the main records.',
   pathToMany:
     'If the records being processed are JSON objects, then use this field to specify the JSON path to the child records. This field does not need to be set for array/row based data.',
   'connection.debug':
