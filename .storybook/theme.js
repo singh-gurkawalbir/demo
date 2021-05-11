5cd63f6fb54bd4a077a75495cbb6f378c0d92967
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-05-11 11:30:37 +0530

    removed the other theme variabels for now

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -2,31 +2,7 @@ import { create } from '@storybook/theming';
 import theme from '../src/theme/light';
 
 export default create({
-  base: 'light',
-  colorPrimary: theme.palette.warning.main,
-  colorSecondary: theme.palette.primary.main,
-
-  // UI
-  appBg: theme.palette.secondary.main,
-  appContentBg: theme.palette.background.paper,
-  appBorderColor: theme.palette.secondary.lightest,
-  appBorderRadius: 4,
-
-  // Text colors
-  textColor: 'white',
-  textInverseColor: 'rgba(0,0,0,0.9)',
-
-  // Toolbar default and active colors
-  barTextColor: theme.palette.secondary.main,
-  barSelectedColor: theme.palette.primary.main,
-  barBg: theme.palette.background.paper2,
-
-  // Form colors
-  inputBg: 'white',
-  inputBorder: 'silver',
-  inputTextColor: 'black',
-  inputBorderRadius: 4,
-
+  /*Limited theme variables provided by Storybook, so reverting all the styles */
   brandTitle: 'Celigo Styleguide',
   brandImage: 'https://www.celigo.com/wp-content/uploads/celigo-logo.svg',
 });
