04838e4c78aa783d607782272b95635cdc2d0177
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-05-16 14:54:17 +0530

    fix the theme

--- .storybook/theme.js
+++ .storybook/theme.js
@@ -1,5 +1,4 @@
 import { create } from '@storybook/theming';
-import theme from '../src/theme/light';
 
 export default create({
   /*Limited theme variables provided by Storybook, so reverting all the styles */
