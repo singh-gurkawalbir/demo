9d044dad95911e58b006a8a9f59b828e58064d85
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-05-19 07:30:47 +0530

    moving this file to a separate PR.

--- .storybook/theme.js
+++
@@ -1,7 +0,0 @@
-import { create } from '@storybook/theming';
-
-export default create({
-  /*Limited theme variables provided by Storybook, so reverting all the styles */
-  brandTitle: 'Celigo Styleguide',
-  brandImage: 'https://www.celigo.com/wp-content/uploads/celigo-logo.svg',
-});
