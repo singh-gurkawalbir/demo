b9f147d0910d51ce43cd75e7fb097421b8640418
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-05-27 06:51:35 +0530

    Removed the TertiaryButton

--- src/components/CeligoButtons/TextButton/index.jsx
+++ src/components/CeligoButtons/TextButton/index.jsx
@@ -3,11 +3,14 @@ import Button from '@material-ui/core/Button';
 import {makeStyles} from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles(theme => ({
   root: {
     fontFamily: props => props.bold ? 'Source sans pro semibold' : 'Source sans pro',
+    '&:focus': {
+      color: props => props.color === 'secondary' ? theme.palette.text.secondary : theme.palette.primary.main,
+    },
   },
-});
+}));
 export default function TextButton(props) {
   const classes = useStyles(props);
   const {children, error, ...rest} = props;
--- src/components/FetchProgressIndicator/index.jsx
+++ src/components/FetchProgressIndicator/index.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography} from '@material-ui/core';
 import Spinner from '../Spinner';
-import TertiaryButton from '../CeligoButtons/TertiaryButton';
+import TextButton from '../CeligoButtons/TextButton';
 
 const useStyles = makeStyles(theme => ({
   fetchLogTextWrapper: {
@@ -52,21 +52,21 @@ export default function FetchProgressIndicator({
           : <>Fetching paused... {percentDone}% completed</>}
       </Typography>
       {fetchInProgress ? (
-        <TertiaryButton
+        <TextButton
           data-test="pauseFetch"
           bold
           onClick={pauseHandler}
           >
           Pause
-        </TertiaryButton>
+        </TextButton>
       ) : (
-        <TertiaryButton
+        <TextButton
           data-test="resumeFetch"
           bold
           onClick={resumeHandler}
          >
           Resume
-        </TertiaryButton>
+        </TextButton>
       )}
     </div>
   );
