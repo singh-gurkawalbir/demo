e9d3291dfab67ce2b2343f9ba3259d2fe9953b00
ashu-gupta <gupta.ashu1602@gmail.com>
2021-06-24 11:39:28 +0530

    IO-22023 added validations component

---
+++ src/components/DynaForm/fields/select/DynaSelectWithValidations.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { makeStyles } from '@material-ui/core/styles';
+import { selectors } from '../../../../reducers';
+import DynaSelect from '../DynaSelect';
+import FieldHelp from '../../FieldHelp';
+import FieldMessage from '../FieldMessage';
+
+const useStyles = makeStyles({
+  dynaTextLabelWrapper: {
+    display: 'flex',
+  },
+});
+const emptyObj = {};
+const emptyArr = [];
+
+export default function DynaSelectWithValidations(props) {
+  const {formKey, value, options = emptyArr} = props;
+  const classes = useStyles();
+  const [description, setDescription] = useState('');
+  const formFields = useSelector(state => selectors.formState(state, formKey)?.fields || emptyObj);
+
+  const {fieldsToValidate, regex, description: itemDescription, helpKey} = useMemo(() => options[0]?.items?.find(item => item.value === value) || emptyObj, [options, value]);
+
+  useEffect(() => {
+    setDescription(itemDescription);
+
+    // if either of the fields contain the regex, remove the description
+    const validated = fieldsToValidate?.some(field => regex.test(formFields[field]?.value));
+
+    if (validated) {
+      setDescription('');
+    }
+  }, [fieldsToValidate, formFields, regex, itemDescription]);
+
+  return (
+    <>
+      <DynaSelect
+        {...props} />
+      <div className={classes.dynaTextLabelWrapper}>
+        <FieldMessage
+          isValid
+          description={description} />
+        {description && <FieldHelp helpKey={helpKey} />}
+      </div>
+    </>
+  );
+}
+
