b9c2b607013723cf8fea315724c4bbe01c8b1f27
Sravan Dandra <sravan.dandra@gmail.com>
2021-07-08 22:20:07 +0530

    Update index.js

--- src/components/ResourceTable/commonCells/ConnectorName/index.js
+++ src/components/ResourceTable/commonCells/ConnectorName/index.js
@@ -7,6 +7,7 @@ import { selectors } from '../../../../reducers';
 export default function ConnectorName({resource}) {
   const { type, assistant, resourceType } = useMemo(() => getResourceSubType(resource), [resource]);
   const _connectionId = resource?._connectionId;
+  const useTechAdaptorForm = !!resource?.useTechAdaptorForm;
   const rdbmsType = resource?.rdbms?.type;
   const rdbmsConnType = useSelector(state =>
     selectors.resource(state, 'connections', _connectionId)?.rdbms?.type
@@ -16,7 +17,13 @@ export default function ConnectorName({resource}) {
     if (type === 'simple') return 'Data Loader';
 
     if (type !== 'rdbms') {
-      return getApp(type, assistant).name || null;
+      const appType = getApp(type, assistant).name || null;
+
+      if (appType?.toLowerCase() === 'http' && useTechAdaptorForm) {
+        return 'REST API';
+      }
+
+      return appType;
     }
 
     if (resourceType === 'exports' || resourceType === 'imports') {
@@ -29,7 +36,7 @@ export default function ConnectorName({resource}) {
     }
 
     return 'RDBMS';
-  }, [type, resourceType, rdbmsType, assistant, rdbmsConnType]);
+  }, [type, resourceType, rdbmsType, assistant, rdbmsConnType, useTechAdaptorForm]);
 
   return out;
 }
