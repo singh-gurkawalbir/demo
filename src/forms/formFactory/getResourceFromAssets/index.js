1b26f86e1c4f182e5b970847a1952d03be646681
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2021-07-13 10:51:32 +0530

    --required change

--- src/forms/formFactory/getResourceFromAssets/index.js
+++ src/forms/formFactory/getResourceFromAssets/index.js
@@ -2,7 +2,7 @@ import produce from 'immer';
 
 import formMeta from '../../definitions';
 import { isJsonString } from '../../../utils/string';
-import { RDBMS_TYPES, REST_ASSISTANTS } from '../../../utils/constants';
+import { FILE_PROVIDER_ASSISTANTS, RDBMS_TYPES, REST_ASSISTANTS } from '../../../utils/constants';
 import { getResourceSubType, isNewId } from '../../../utils/resource';
 
 const getAllOptionsHandlerSubForms = (
@@ -296,7 +296,7 @@ const getFormMeta = ({resourceType, isNew, resource, connection, assistantData})
         ) {
           // Financial Force assistant is same as Salesforce. For more deatils refer https://celigo.atlassian.net/browse/IO-14279.
           meta = meta.salesforce;
-        } else if (resource.assistant === 'googledrive' || resource.assistant === 'azurestorageaccount') {
+        } else if (FILE_PROVIDER_ASSISTANTS.includes(resource.assistant)) {
           // Common metadata for both the file providers googledrive and azurestorageaccount
           meta = meta.commonfileprovider;
         } else if (
