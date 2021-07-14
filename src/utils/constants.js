ab6a9d2b35b72bb851d4077fce62618594486c43
SiddharthMMore <SiddharthMMore@outlook.com>
2021-07-14 22:12:49 +0530

    AFE Save and close

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,6 +5,10 @@ export const emptyObject = Object.freeze({});
 export const ACCOUNT_IDS = Object.freeze({
   OWN: 'own',
 });
+export const AFE_SAVE_STATUS = Object.freeze({
+  SUCCESS: 'success',
+  REQUESTED: 'requested',
+});
 export const FORM_SAVE_STATUS = Object.freeze({
   COMPLETE: 'complete',
   FAILED: 'failed',
