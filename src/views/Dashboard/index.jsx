f866cf4275f795ff5aaadcf25f3bd8bd87f89225
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2021-07-20 20:33:08 +0530

    Minor changes

--- src/views/Dashboard/index.jsx
+++ src/views/Dashboard/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { selectors } from '../../reducers';
 import Tabs from './Tabs';
 import LoadResources from '../../components/LoadResources';
 import CeligoPageBar from '../../components/CeligoPageBar';
+import getRoutePath from '../../utils/routePaths';
+import {HOME_PAGE_PATH} from '../../utils/constants';
 
 export default function Dashboard() {
+  const history = useHistory();
   const isUserInErrMgtTwoDotZero = useSelector(state =>
     selectors.isOwnerUserInErrMgtTwoDotZero(state)
   );
 
   if (!isUserInErrMgtTwoDotZero) {
+    history.push(getRoutePath(HOME_PAGE_PATH));
+
     return null;
   }
 
