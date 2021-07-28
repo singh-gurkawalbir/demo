3c933877f2e4d33ef32c37d3daa8a3538f785102
Dantham Ashok  Kumar <ashok10m006@gmail.com>
2021-07-28 12:53:14 +0530

    Minor changes

--- src/reducers/accountdashboard.test.js
+++ src/reducers/accountdashboard.test.js
@@ -4,6 +4,24 @@ import { STANDALONE_INTEGRATION } from '../utils/constants';
 import {FILTER_KEYS_AD} from '../utils/accountDashboard';
 
 const state = {
+  session: {
+    filters: {
+      completedFlows: {
+        paging: {
+          rowsPerPage: 10,
+          currPage: 0,
+        },
+        integrationIds: ['int1', 'int2'],
+      },
+      runningFlows: {
+        paging: {
+          rowsPerPage: 10,
+          currPage: 0,
+        },
+        integrationIds: ['int1', 'int2'],
+      },
+    },
+  },
   data: {
     completedJobs: {
       completedJobs: [
@@ -138,6 +156,97 @@ describe('selectors.accountDashboardJobs', () => {
   test('should return all completed jobs', () => {
     const res = selectors.accountDashboardJobs(state, FILTER_KEYS_AD.COMPLETED);
     const expected = {jobs: [{_flowId: 'flow1', numPages: 3, numRuns: 2}, {_flowId: 'flow2', numPages: 0, numRuns: 210}, {_flowId: 'flow3', numPages: 0, numRuns: 2}, {_flowId: 'flow4', numPages: 1, numRuns: 1}], nextPageURL: undefined, status: undefined};
+
+    expect(res).toEqual(expected);
+  });
+});
+
+describe('selectors.accountDashboardRunningJobs', () => {
+  test('should return empty array when state is null or undefined', () => {
+    const res = selectors.accountDashboardRunningJobs();
+
+    expect(res).toEqual([]);
+  });
+  test('should return all running jobs', () => {
+    const res = selectors.accountDashboardRunningJobs(state);
+    const expected = [{_exportId: 'exp1', _flowId: 'flow1', _id: 'job1', name: 'flow1', percentComplete: 0, doneExporting: false, duration: undefined, numPagesProcessed: 0, type: 'flow', uiStatus: undefined}];
+
+    expect(res).toEqual(expected);
+  });
+});
+
+describe('selectors.accountDashboardCompletedJobs', () => {
+  test('should return empty array when state is null or undefined', () => {
+    const res = selectors.accountDashboardCompletedJobs();
+
+    expect(res).toEqual([]);
+  });
+  test('should return all completed jobs', () => {
+    const res = selectors.accountDashboardCompletedJobs(state);
+    const expected = [
+      {
+        _flowId: 'flow1',
+        numPages: 3,
+        numRuns: 2,
+      },
+      {
+        _flowId: 'flow2',
+        numPages: 0,
+        numRuns: 210,
+      },
+      {
+        _flowId: 'flow3',
+        numPages: 0,
+        numRuns: 2,
+      },
+      {
+        _flowId: 'flow4',
+        numPages: 1,
+        numRuns: 1,
+      },
+    ];
+
+    expect(res).toEqual(expected);
+  });
+});
+
+describe('selectors.requestOptionsOfDashboardJobs', () => {
+  test('should return default request options when state is null or undefined', () => {
+    const res = selectors.requestOptionsOfDashboardJobs('', {});
+
+    expect(res).toEqual({opts: {body: {sandbox: false}, method: 'POST'}, path: '/flows/runs/stats'});
+  });
+  test('should return correct request options when integration ids set in filter for running jobs', () => {
+    const res = selectors.requestOptionsOfDashboardJobs(state, {filterKey: FILTER_KEYS_AD.RUNNING});
+    const expected = {opts: {body: { _integrationIds: ['int1', 'int2'], sandbox: false}, method: 'POST'}, path: '/jobs/current'};
+
+    expect(res).toEqual(expected);
+  });
+  test('should return correct request options when flow ids set in filter for running jobs', () => {
+    state.session.filters.runningFlows.flowIds = ['flow1', 'flow2'];
+    const res = selectors.requestOptionsOfDashboardJobs(state, {filterKey: FILTER_KEYS_AD.RUNNING});
+    const expected = {opts: {body: {_flowIds: ['flow1', 'flow2'], sandbox: false}, method: 'POST'}, path: '/jobs/current'};
+
+    expect(res).toEqual(expected);
+  });
+  test('should return correct request options when status set in filter for running jobs', () => {
+    state.session.filters.runningFlows.status = ['running'];
+
+    const res = selectors.requestOptionsOfDashboardJobs(state, {filterKey: FILTER_KEYS_AD.RUNNING});
+    const expected = {opts: {body: {_flowIds: ['flow1', 'flow2'], sandbox: false, status: ['running']}, method: 'POST'}, path: '/jobs/current'};
+
+    expect(res).toEqual(expected);
+  });
+  test('should return correct request options when integration ids set in filter for completed jobs', () => {
+    const res = selectors.requestOptionsOfDashboardJobs(state, {filterKey: FILTER_KEYS_AD.COMPLETED});
+    const expected = {opts: {body: {_integrationIds: ['int1', 'int2'], sandbox: false}, method: 'POST'}, path: '/flows/runs/stats'};
+
+    expect(res).toEqual(expected);
+  });
+  test('should return correct request options when flow ids set in filter for completed jobs', () => {
+    state.session.filters.completedFlows.flowIds = ['flow1', 'flow2'];
+    const res = selectors.requestOptionsOfDashboardJobs(state, {filterKey: FILTER_KEYS_AD.COMPLETED});
+    const expected = {opts: {body: {_flowIds: ['flow1', 'flow2'], sandbox: false}, method: 'POST'}, path: '/flows/runs/stats'};
 
     expect(res).toEqual(expected);
   });
