82ad9500a47082492b9f6035b5f1dbbad422c6e2
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-08-04 16:33:54 +0530

    test cases

--- src/sagas/sampleData/resourceForm/test.js
+++ src/sagas/sampleData/resourceForm/test.js
@@ -16,13 +16,19 @@ import {
   _requestFileSampleData,
   _requestLookupSampleData,
   _requestImportSampleData,
-  // _requestImportFileSampleData,
+  _requestImportFileSampleData,
   _parseFileData,
   _handlePreviewError,
   _fetchResourceInfoFromFormKey,
   _hasSampleDataOnResource,
 } from '.';
 import requestRealTimeMetadata from '../sampleDataGenerator/realTimeSampleData';
+import { pageProcessorPreview } from '../utils/previewCalls';
+
+const formKey = 'form-123';
+const resourceId = 'export-123';
+const flowId = 'flow-123';
+const integrationId = 'int-123';
 
 describe('resourceFormSampleData sagas', () => {
   describe('requestResourceFormSampleData saga', () => {
@@ -31,20 +37,15 @@ describe('resourceFormSampleData sagas', () => {
       .not.call.fn(_requestExportSampleData)
       .not.call.fn(_requestImportSampleData)
       .run());
-    test('should do nothing if the formKey passed does not associate with any resourceId or resourceType is not a valid one for sample data', () => {
-      const formKey = 'form-123';
-
-      return expectSaga(requestResourceFormSampleData, { formKey })
-        .provide([
-          [call(_fetchResourceInfoFromFormKey, { formKey }), {}],
-        ])
-        .not.put(actions.resourceFormSampleData.setStatus(undefined, 'requested'))
-        .not.call.fn(_requestExportSampleData)
-        .not.call.fn(_requestImportSampleData)
-        .run();
-    });
+    test('should do nothing if the formKey passed does not associate with any resourceId or resourceType is not a valid one for sample data', () => expectSaga(requestResourceFormSampleData, { formKey })
+      .provide([
+        [call(_fetchResourceInfoFromFormKey, { formKey }), {}],
+      ])
+      .not.put(actions.resourceFormSampleData.setStatus(undefined, 'requested'))
+      .not.call.fn(_requestExportSampleData)
+      .not.call.fn(_requestImportSampleData)
+      .run());
     test('should dispatch requested status and call _requestExportSampleData with refreshCache option incase of exports resourceType ', () => {
-      const formKey = 'form-123';
       const resourceId = 'import-123';
 
       return expectSaga(requestResourceFormSampleData, { formKey })
@@ -58,8 +59,6 @@ describe('resourceFormSampleData sagas', () => {
         .run();
     });
     test('should dispatch requested status and call _requestImportSampleData incase of imports resourceType ', () => {
-      const formKey = 'form-123';
-      const resourceId = 'export-123';
       const refreshCache = true;
 
       return expectSaga(requestResourceFormSampleData, { formKey, options: {refreshCache} })
@@ -74,9 +73,6 @@ describe('resourceFormSampleData sagas', () => {
     });
   });
   describe('_requestExportSampleData saga', () => {
-    const formKey = 'form-123';
-    const resourceId = 'export-123';
-    const flowId = 'flow-123';
     const refreshCache = true;
 
     test('should call _requestPGExportSampleData if the resource is PG export or stand alone export', () => {
@@ -122,8 +118,6 @@ describe('resourceFormSampleData sagas', () => {
       .run());
   });
   describe('_requestPGExportSampleData saga', () => {
-    const formKey = 'form-123';
-    const resourceId = 'export-123';
     const refreshCache = true;
 
     test('should call _requestFileSampleData incase of file adaptor export', () => {
@@ -185,10 +179,6 @@ describe('resourceFormSampleData sagas', () => {
     });
   });
   describe('_requestExportPreviewData saga', () => {
-    const formKey = 'form-123';
-    const resourceId = 'export-123';
-    const flowId = 'flow-123';
-    const integrationId = 'int-123';
     const sampleDataRecordSize = 5;
 
     test('should construct body with needed props for making preview call and dispatch receivedPreviewStages on success', () => {
@@ -256,8 +246,6 @@ describe('resourceFormSampleData sagas', () => {
     });
   });
   describe('_requestRealTimeSampleData saga', () => {
-    const formKey = 'form-123';
-    const resourceId = 'export-123';
     const refreshCache = true;
     const nsResource = {
       _id: '123',
@@ -294,9 +282,6 @@ describe('resourceFormSampleData sagas', () => {
       .run());
   });
   describe('_requestFileSampleData saga', () => {
-    const formKey = 'form-123';
-    const resourceId = 'export-123';
-
     test('should dispatch clear stages action and do nothing if the file type is not valid or does not exist', () => expectSaga(_requestFileSampleData, { formKey })
       .provide([
         [call(_fetchResourceInfoFromFormKey, { formKey }), {resourceObj: {}, resourceId}],
@@ -305,35 +290,249 @@ describe('resourceFormSampleData sagas', () => {
       .not.call.fn(_hasSampleDataOnResource)
       .not.call.fn(_parseFileData)
       .run());
-    // test('should call parseFileData for File definitions by fetching sampleData and rules from fileDefinitionSampleData selector', () => {
+    test('should call parseFileData for File definitions by fetching sampleData and rules from fileDefinitionSampleData selector', () => {
+      const ftpResource = {
+        _id: 'export-123',
+        name: 'FTP export',
+        file: {
+          type: 'filedefinition',
+        },
+        adaptorType: 'FTPExport',
+        sampleData: { test: 5 },
+      };
+      const fieldState = {
+        userDefinitionId: 'id-123',
+        options: {format: 'edi'},
+      };
+      const fileDefinitionSampleData = 'UNB+UNOC:3+<Sender GLN>:14+<Receiver GLN>:14+140407:1000+100+ + + + +EANCOM';
+      const rule = {
+        name: '84 Lumber 810',
+        description: 'Invoice',
+        sampleData: 'ISA*02*SW810 *00* *01*84EXAMPLE *12',
+        rules: [{
+          maxOccurrence: 1,
+          skipRowSuffix: true,
+        }],
+      };
 
-    // });
-    // test('should check for uploaded file content from getUploadedFile and call parseFileData saga with that content for other valid file types ', () => {
+      return expectSaga(_requestFileSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, resourceObj: ftpResource}],
+          [select(selectors.fieldState, formKey, 'file.filedefinition.rules'), fieldState],
+          [select(selectors.fileDefinitionSampleData, {
+            userDefinitionId: fieldState.userDefinitionId,
+            resourceType: 'exports',
+            options: { format: fieldState.options.format, definitionId: undefined, resourcePath: undefined },
+          }), { rule, sampleData: fileDefinitionSampleData }],
+          [call(_parseFileData, {
+            resourceId,
+            fileContent: fileDefinitionSampleData,
+            parserOptions: rule,
+            fileType: 'fileDefinitionParser',
+          }), {}],
+        ])
+        .call(_parseFileData, {
+          resourceId,
+          fileContent: fileDefinitionSampleData,
+          parserOptions: rule,
+          fileType: 'fileDefinitionParser',
+        })
+        .not.put(actions.resourceFormSampleData.clearStages(resourceId))
+        .run();
+    });
+    test('should check for uploaded file content from getUploadedFile and call parseFileData saga with that content for other valid file types ', () => {
+      const fileProps = {
+        columnDelimiter: '|',
+        hasHeaderRow: true,
+        rowsToSkip: 1,
+      };
+      const parserOptions = {
+        rowsToSkip: 1,
+        trimSpaces: undefined,
+        columnDelimiter: '|',
+        hasHeaderRow: true,
+        rowDelimiter: undefined,
+        multipleRowsPerRecord: undefined,
+        keyColumns: undefined,
+      };
+      const ftpResource = {
+        _id: 'export-123',
+        name: 'FTP export',
+        file: {
+          type: 'csv',
+          csv: fileProps,
+        },
+        adaptorType: 'FTPExport',
+      };
+      const fileId = `${resourceId}-uploadFile`;
+      const uploadedFile = {
+        file: "CUSTOMER_NUMBER|VENDOR_NAME|VENDOR_PART_NUM|DISTRIBUTOR_PART_NUM|LIST_PRICE|DESCRIPTION|CONTRACT_PRICE|QUANTITY_AVAILABLE↵C1000010839|Sato|12S000357CS|12S000357CS|99.12|wax rib 3.00\"X84',T113L,CSO,1\"core,24/cs|60.53|0",
+      };
 
-    // });
-    // test('should call _hasSampleDataOnResource saga and pass resourceObj\'s sampleData as fileContent for parseFileData saga', () => {
+      return expectSaga(_requestFileSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, resourceObj: ftpResource}],
+          [select(selectors.getUploadedFile, fileId), uploadedFile],
+          [call(_parseFileData, {
+            resourceId,
+            fileContent: uploadedFile.file,
+            fileType: 'csv',
+            parserOptions,
+            fileProps,
+            isNewSampleData: true}), {}],
+        ])
+        .call(_parseFileData, {
+          resourceId,
+          fileContent: uploadedFile.file,
+          fileType: 'csv',
+          parserOptions,
+          fileProps,
+          isNewSampleData: true})
+        .not.put(actions.resourceFormSampleData.clearStages(resourceId))
+        .run();
+    });
+    test('should call _hasSampleDataOnResource saga and pass resourceObj\'s sampleData as fileContent for parseFileData saga', () => {
+      const fileProps = {
+        columnDelimiter: '|',
+        hasHeaderRow: true,
+        rowsToSkip: 1,
+      };
+      const parserOptions = {
+        rowsToSkip: 1,
+        trimSpaces: undefined,
+        columnDelimiter: '|',
+        hasHeaderRow: true,
+        rowDelimiter: undefined,
+        multipleRowsPerRecord: undefined,
+        keyColumns: undefined,
+      };
+      const ftpResource = {
+        _id: 'export-123',
+        name: 'FTP export',
+        file: {
+          type: 'csv',
+          csv: fileProps,
+        },
+        adaptorType: 'FTPExport',
+        sampleData: "CUSTOMER_NUMBER|VENDOR_NAME|VENDOR_PART_NUM|DISTRIBUTOR_PART_NUM|LIST_PRICE|DESCRIPTION|CONTRACT_PRICE|QUANTITY_AVAILABLE↵C1000010839|Sato|12S000357CS|12S000357CS|99.12|wax rib 3.00\"X84',T113L,CSO,1\"core,24/cs|60.53|0",
+      };
+      const fileId = `${resourceId}-uploadFile`;
 
-    // });
+      return expectSaga(_requestFileSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, resourceObj: ftpResource}],
+          [select(selectors.getUploadedFile, fileId), undefined],
+          [call(_hasSampleDataOnResource, { formKey }), true],
+          [call(_parseFileData, { resourceId, fileContent: ftpResource.sampleData, fileProps, fileType: 'csv', parserOptions }), {}],
+        ])
+        .call(_parseFileData, { resourceId, fileContent: ftpResource.sampleData, fileProps, fileType: 'csv', parserOptions })
+        .not.put(actions.resourceFormSampleData.clearStages(resourceId))
+        .run();
+    });
   });
-  // describe('_requestLookupSampleData saga', () => {
-  //   test('should call _requestFileSampleData incase of file adaptor lookup export', () => {
+  describe('_requestLookupSampleData saga', () => {
+    test('should call _requestFileSampleData incase of file adaptor lookup export', () => {
+      const ftpResource = {
+        _id: 'export-123',
+        name: 'FTP export',
+        file: {
+          type: 'csv',
+        },
+        isLookup: true,
+        adaptorType: 'FTPExport',
+        sampleData: "CUSTOMER_NUMBER|VENDOR_NAME|VENDOR_PART_NUM|DISTRIBUTOR_PART_NUM|LIST_PRICE|DESCRIPTION|CONTRACT_PRICE|QUANTITY_AVAILABLE↵C1000010839|Sato|12S000357CS|12S000357CS|99.12|wax rib 3.00\"X84',T113L,CSO,1\"core,24/cs|60.53|0",
+      };
 
-  //   });
-  //   test('should call pageProcessorPreview saga to fetch preview data for lookup and dispatch receivedPreviewStages on success', () => {
+      return expectSaga(_requestLookupSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, flowId, resourceObj: ftpResource}],
+          [call(_requestFileSampleData, { formKey }), {}],
+        ])
+        .call(_requestFileSampleData, { formKey })
+        .not.call.fn(pageProcessorPreview)
+        .not.put(actions.resourceFormSampleData.receivedPreviewStages(resourceId, undefined))
+        .run();
+    });
+    test('should call pageProcessorPreview saga to fetch preview data for lookup and dispatch receivedPreviewStages on success', () => {
+      const restResource = {
+        _id: resourceId,
+        adaptorType: 'RESTExport',
+      };
+      const previewData = {
+        stages: [{
+          name: 'parse',
+          data: {
+            test: 5,
+          },
+        }],
+      };
 
-  //   });
-  //   test('should call pageProcessorPreview saga to fetch preview data for lookup and call _handlePreviewError saga on failure', () => {
+      return expectSaga(_requestLookupSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, flowId, resourceObj: restResource}],
+          [matchers.call.fn(pageProcessorPreview), previewData],
+        ])
+        .not.call.fn(_requestFileSampleData)
+        .call.fn(pageProcessorPreview)
+        .put(actions.resourceFormSampleData.receivedPreviewStages(resourceId, previewData))
+        .run();
+    });
+    test('should call pageProcessorPreview saga to fetch preview data for lookup and call _handlePreviewError saga on failure', () => {
+      const restResource = {
+        _id: resourceId,
+        adaptorType: 'RESTExport',
+      };
+      const error = { status: 401, message: '{"code":"error code"}' };
 
-  //   });
-  // });
-  // describe('_requestImportSampleData saga', () => {
-  //   test('should call _requestImportFileSampleData incase of file import', () => {
+      return expectSaga(_requestLookupSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, flowId, resourceObj: restResource}],
+          [matchers.call.fn(pageProcessorPreview), throwError(error)],
+        ])
+        .not.call.fn(_requestFileSampleData)
+        .call.fn(pageProcessorPreview)
+        .not.put(actions.resourceFormSampleData.receivedPreviewStages(resourceId, undefined))
+        .call(_handlePreviewError, { e: error, resourceId })
+        .run();
+    });
+  });
+  describe('_requestImportSampleData saga', () => {
+    test('should call _requestImportFileSampleData incase of file import', () => {
+      const ftpResource = {
+        _id: 'import-123',
+        name: 'FTP import',
+        file: {
+          type: 'csv',
+        },
+        adaptorType: 'FTPImport',
+        sampleData: "CUSTOMER_NUMBER|VENDOR_NAME|VENDOR_PART_NUM|DISTRIBUTOR_PART_NUM|LIST_PRICE|DESCRIPTION|CONTRACT_PRICE|QUANTITY_AVAILABLE↵C1000010839|Sato|12S000357CS|12S000357CS|99.12|wax rib 3.00\"X84',T113L,CSO,1\"core,24/cs|60.53|0",
+      };
+      const resourceId = 'import-123';
 
-  //   });
-  //   test('should do nothing if the import is not a file import', () => {
+      return expectSaga(_requestImportSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, resourceObj: ftpResource }],
+          [call(_requestImportFileSampleData, { formKey }), {}],
+        ])
+        .call(_requestImportFileSampleData, { formKey })
+        .run();
+    });
+    test('should do nothing if the import is not a file import', () => {
+      const restResource = {
+        _id: 'import-123',
+        name: 'test',
+        adaptorType: 'RESTImport',
+      };
+      const resourceId = 'import-123';
 
-  //   });
-  // });
+      return expectSaga(_requestImportSampleData, { formKey })
+        .provide([
+          [call(_fetchResourceInfoFromFormKey, { formKey }), { resourceId, resourceObj: restResource }],
+        ])
+        .not.call(_requestImportFileSampleData, { formKey })
+        .run();
+    });
+  });
   // describe('_requestImportFileSampleData saga', () => {
   //   test('should dispatch clear stages action and do nothing if the file type does not exist', () => {
 
