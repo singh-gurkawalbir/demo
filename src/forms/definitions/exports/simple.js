bd8ac1969b182d90c37693ab73ccb02aa14a9e70
ashok10m006@gmail.com <ashok10m006@gmail.com>
2021-08-23 11:33:12 +0530

    IO-22997

--- src/forms/definitions/exports/simple.js
+++ src/forms/definitions/exports/simple.js
@@ -118,7 +118,7 @@ export default {
     'file.type': {
       id: 'file.type',
       name: '/file/type',
-      type: 'select',
+      type: 'filetypeselect',
       label: 'File type',
       required: true,
       defaultValue: r => (r && r.file && r.file.type) || '',
