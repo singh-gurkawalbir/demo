2a14e633c114e6336d5c53037a6ce3bd75a4fe1c
driedl <dave@riedl.ca>
2021-08-24 13:54:44 -0700

    added zoom/pan control stub

--- src/stories/lab/flowBuilderCanvas/Prototype/index.jsx
+++ src/stories/lab/flowBuilderCanvas/Prototype/index.jsx
@@ -3,6 +3,7 @@ import * as PIXI from 'pixi.js';
 import { Viewport } from 'pixi-viewport';
 import flowSchema from './metadata/flowSchema';
 
+let app;
 const pageProcessors = {};
 const pageGenerators = {};
 
@@ -48,89 +49,126 @@ function getAppBlock(resource, x, y) {
   return container;
 }
 
-// PIXI APP INIT
-const app = new PIXI.Application({
-  resolution: window.devicePixelRatio,
-  autoDensity: true,
-  backgroundColor: 0xFFFFFF,
-});
+function buildViewportControls() {
+  const gr = new PIXI.Graphics();
 
-// create viewport
-const viewport = new Viewport({
-  // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
-  interaction: app.renderer.plugins.interaction,
-});
+  gr.beginFill(0x1D76C7);
+  gr.lineStyle(1);
+  gr.drawCircle(0, 0, 20);
+  gr.endFill();
 
-app.stage.addChild(viewport);
+  const texture = app.renderer.generateTexture(gr);
 
-// activate plugins
-viewport
-  .drag()
-  .pinch()
-  .wheel()
-  .decelerate();
+  const controls = new PIXI.Sprite(texture);
 
-// PAGE GENERATORS
-for (let i = 0; i < flowSchema.pg.length; i += 1) {
-  const pg = flowSchema.pg[i];
-  const appBlock = getAppBlock(pg, 25, 25 + i * 200);
+  controls.interactive = true;
+  controls.buttonMode = true;
+  controls.on('mouseover', () => { controls.alpha = 0.5; });
+  controls.on('mouseout', () => { controls.alpha = 1; });
 
-  // Add the appBlock to the scene we are building.
-  viewport.addChild(appBlock);
-  pageGenerators[pg.id] = appBlock;
+  controls.position.set(app.screen.width, 25);
+  // controls.anchor.x = 1;
+
+  return controls;
 }
 
-// PAGE PROCESSORS
-for (let i = 0; i < flowSchema.pp.length; i += 1) {
-  const pp = flowSchema.pp[i];
-  const appBlock = getAppBlock(pp, 425 + i * 350, 25);
+function buildFlow(viewport, flowSchema) {
+  // PAGE GENERATORS
+  for (let i = 0; i < flowSchema.pg.length; i += 1) {
+    const pg = flowSchema.pg[i];
+    const appBlock = getAppBlock(pg, 25, 25 + i * 200);
 
-  // Add the appBlock to the scene we are building.
-  viewport.addChild(appBlock);
-  pageProcessors[pp.id] = appBlock;
+    // Add the appBlock to the scene we are building.
+    viewport.addChild(appBlock);
+    pageGenerators[pg.id] = appBlock;
+  }
+
+  // PAGE PROCESSORS
+  for (let i = 0; i < flowSchema.pp.length; i += 1) {
+    const pp = flowSchema.pp[i];
+    const appBlock = getAppBlock(pp, 425 + i * 350, 25);
+
+    // Add the appBlock to the scene we are building.
+    viewport.addChild(appBlock);
+    pageProcessors[pp.id] = appBlock;
+  }
 }
-
-// Listen for frame updates
-app.ticker.maxFPS = 30;
-// app.ticker.add(handleTick);
 
 const handleWheelEvent = e => { e.preventDefault(); };
 
+function initializeFbCanvas(canvasNode) {
+  // PIXI APP INIT
+  app = new PIXI.Application({
+    resolution: window.devicePixelRatio,
+    autoDensity: true,
+    backgroundColor: 0xFFFFFF,
+  });
+
+  // create viewport
+  const viewport = new Viewport({
+  // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+    interaction: app.renderer.plugins.interaction,
+  });
+
+  // activate plugins
+  viewport
+    .drag()
+    .pinch()
+    .wheel()
+    .decelerate();
+
+  app.stage.addChild(viewport);
+  // add all DisplayObjects that should not be part of the viewport.
+  // any of these objects should NOT zoom and pan with the flow.
+  app.stage.addChild(buildViewportControls());
+
+  // This function adds all DisplayObjects that make-up a flow's current state.
+  buildFlow(viewport, flowSchema);
+
+  // Listen for frame updates
+  app.ticker.maxFPS = 30;
+  // app.ticker.add(handleTick);
+
+  // if we do not disable the 'wheel' event, then the browser will
+  // scale on track pad (and touchscreen) pinch.
+  window.addEventListener('wheel', handleWheelEvent, {passive: false});
+
+  const {height: worldHeight, width: worldWidth} = viewport.parent;
+
+  // The bounce feature of the viewport will prevent panning of the canvas
+  // past the "world" boundaries...
+  const bouncePadding = 50;
+
+  viewport.screenWidth = canvasNode.offsetWidth;
+  viewport.screenHeight = canvasNode.offsetHeight;
+  viewport.worldWidth = Math.ceil(worldWidth) + bouncePadding;
+  viewport.worldHeight = Math.ceil(worldHeight) + bouncePadding;
+  viewport.bounce();
+
+  canvasNode.appendChild(app.view);
+  // the resizeTo helper prop lets us tell Pixi to track the size of a DOM node and
+  // set its canvas sie to match. This facilitates the use-case where a user resizes
+  // the browser, or expands/collapses the Celigo Menu.
+  app.resizeTo = canvasNode;
+  app.start();
+}
+
+function dismantleFbCanvas() {
+  window.removeEventListener('wheel', handleWheelEvent, { passive: false });
+
+  // On unload completely destroy the application and all of it's children
+  app.stop();
+  app.destroy(true, true);
+}
+
+// React component wrapper over native pixi.js controlled HTML5 canvas.
 export default function Proto() {
   const canvasRef = useRef();
 
   useEffect(() => {
-    // if we do not disable the 'wheel' event, then the browser will
-    // scale on track pad (and touchscreen) pinch.
-    window.addEventListener('wheel', handleWheelEvent, {passive: false});
+    initializeFbCanvas(canvasRef.current);
 
-    const canvasNode = canvasRef.current;
-    const {height: worldHeight, width: worldWidth} = viewport.parent;
-
-    // The bounce feature of the viewport will prevent panning of the canvas
-    // past the "world" boundaries...
-    const bouncePadding = 50;
-
-    viewport.screenWidth = canvasNode.offsetWidth;
-    viewport.screenHeight = canvasNode.offsetHeight;
-    viewport.worldWidth = Math.ceil(worldWidth) + bouncePadding;
-    viewport.worldHeight = Math.ceil(worldHeight) + bouncePadding;
-    viewport.bounce();
-
-    canvasNode.appendChild(app.view);
-    // the resizeTo helper prop lets us tell Pixi to track the size of a DOM node and
-    // set its canvas sie to match. This facilitates the use-case where a user resizes
-    // the browser, or expands/collapses the Celigo Menu.
-    app.resizeTo = canvasNode;
-    app.start();
-
-    return () => {
-      window.removeEventListener('wheel', handleWheelEvent, { passive: false });
-
-      // On unload completely destroy the application and all of it's children
-      app.stop();
-      // app.destroy(false, true);
-    };
+    return () => dismantleFbCanvas();
   }, []);
 
   return (
