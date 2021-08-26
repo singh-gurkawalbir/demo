56dd5fc7643955bc8d354d3a560a1c76f0ef3b80
Sravan Dandra <sravan.dandra@gmail.com>
2021-08-27 00:46:53 +0530

    Update resource.js

--- src/utils/resource.js
+++ src/utils/resource.js
@@ -114,8 +114,6 @@ export function getResourceSubType(resource) {
 
   if (type === 'simple') {
     resourceType = type;
-  } else if (adaptorType?.startsWith('HTTP') || type === 'http') {
-    resourceType = resource.http?.formType === 'rest' ? 'rest' : 'http';
   } else {
     resourceType = adaptorTypeMap[adaptorType] || type;
   }
