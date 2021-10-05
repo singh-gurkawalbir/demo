2c3228fbb8bb85434e03b15d51b853306336eb2e
Sravan Dandra <sravan.dandra@gmail.com>
2021-10-05 12:09:24 +0530

    IO-23151

--- src/components/InstallationWizard/index.jsx
+++ src/components/InstallationWizard/index.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
+import jsonPatch from 'fast-json-patch';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import {
@@ -6,6 +7,7 @@ import {
   IconButton,
   Breadcrumbs,
 } from '@material-ui/core';
+import useTraceUpdate from 'use-trace-update';
 import ArrowBackIcon from '../icons/ArrowLeftIcon';
 import { selectors } from '../../reducers';
 import actions from '../../actions';
@@ -21,11 +23,12 @@ import {
   generateNewId,
 } from '../../utils/resource';
 import jsonUtil from '../../utils/json';
-import { INSTALL_STEP_TYPES } from '../../utils/constants';
+import { emptyObject, INSTALL_STEP_TYPES } from '../../utils/constants';
 import { SCOPES } from '../../sagas/resourceForm';
 import Loader from '../Loader';
 import Spinner from '../Spinner';
 import { getApplication } from '../../utils/template';
+import { defaultPatchSetConverter, sanitizePatchSet } from '../../forms/formFactory/utils';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -49,6 +52,15 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
+const oAuthApplications = [
+  ...resourceConstants.OAUTH_APPLICATIONS,
+  'netsuite-oauth',
+  'shopify-oauth',
+  'acumatica-oauth',
+  'hubspot-oauth',
+  'amazonmws-oauth',
+];
+
 export default function InstallationWizard(props) {
   const classes = useStyles();
   const {
@@ -62,17 +74,10 @@ export default function InstallationWizard(props) {
     handleSetupComplete,
     variant,
   } = props;
-  const oAuthApplications = useMemo(
-    () => [
-      ...resourceConstants.OAUTH_APPLICATIONS,
-      'netsuite-oauth',
-      'shopify-oauth',
-      'acumatica-oauth',
-      'hubspot-oauth',
-      'amazonmws-oauth',
-    ],
-    []
-  );
+
+  console.log('InstallationWizard init');
+  useTraceUpdate({...props});
+
   const [installInProgress, setInstallInProgress] = useState(false);
   const [connection, setSelectedConnectionId] = useState(null);
   const [stackId, setShowStackDialog] = useState(null);
@@ -88,14 +93,16 @@ export default function InstallationWizard(props) {
         resourceType,
         resourceId,
       })
-    ) || {};
-  const {redirectTo, isInstallFailed, environment: destinationEnvironment } = useSelector(state =>
+    ) || emptyObject;
+  const { redirectTo, isInstallFailed, environment: destinationEnvironment } = useSelector(state =>
     selectors.redirectToOnInstallationComplete(state, {
       resourceType,
       resourceId,
       templateId,
     })
   );
+
+  useTraceUpdate({...props, redirectTo, isInstallFailed, destinationEnvironment, connectionMap, data, isSetupComplete});
 
   useEffect(() => {
     if (isSetupComplete) {
@@ -222,12 +229,14 @@ export default function InstallationWizard(props) {
   };
 
   const handleSubmitComplete = (connectionId, isAuthorized, createdConnectionDoc) => {
-    if (
-      oAuthApplications.includes(
-        getConnectionType(createdConnectionDoc)
-      ) &&
-      !isAuthorized
-    ) {
+    const patchSet = sanitizePatchSet({
+      patchSet: defaultPatchSetConverter(createdConnectionDoc || {}),
+      resource: {},
+    });
+    const connectionJson = jsonPatch.applyPatch({}, patchSet)?.newDocument;
+
+    if (oAuthApplications.includes(getConnectionType(connectionJson)) && !isAuthorized) {
+      // Step should not be marked as completed until Oauth application authorization is completed on other window.
       return;
     }
 
