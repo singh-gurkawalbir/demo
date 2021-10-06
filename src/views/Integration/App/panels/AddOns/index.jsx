37927e4611961a11ca6b17a07f61c71de5699563
ashok10m006@gmail.com <ashok10m006@gmail.com>
2021-10-06 16:16:51 +0530

    Moved changes to data layer

--- src/views/Integration/App/panels/AddOns/index.jsx
+++ src/views/Integration/App/panels/AddOns/index.jsx
@@ -88,24 +88,9 @@ export default function AddOnsPanel({ integrationId, childId }) {
   const addOnState = useSelector(state =>
     selectors.integrationAppAddOnState(state, integrationId)
   );
-  const subscribedAddOns = addOnState?.addOns?.addOnLicenses?.filter(model => supportsMultiStore && childId ? model.storeId === childId : true);
-
-  if (subscribedAddOns) {
-    subscribedAddOns.forEach((f, i) => {
-      const addon = addOnState?.addOns?.addOnMetaData?.find(addOn => addOn.id === f.id);
-
-      subscribedAddOns[i]._id = i;
-      subscribedAddOns[i].integrationId = integrationId;
-      subscribedAddOns[i].name = addon ? addon.name : f.id;
-      subscribedAddOns[i].description = addon ? addon.description : '';
-      subscribedAddOns[i].uninstallerFunction = addon
-        ? addon.uninstallerFunction
-        : '';
-      subscribedAddOns[i].installerFunction = addon
-        ? addon.installerFunction
-        : '';
-    });
-  }
+  const subscribedAddOns = useSelector(state =>
+    selectors.subscribedAddOns(state, integrationId, supportsMultiStore, childId)
+  );
 
   const subscribedAddOn = metadata => subscribedAddOns?.find(sa => sa.id === metadata.id);
   const addOnMetadata = addOnState?.addOns?.addOnMetaData;
