fdc81c86ec2a780b6ddc329a304ee98f5a230bd0
driedl <dave@riedl.ca>
2021-10-07 17:10:41 -0700

    added comments to stories

--- src/stories/lab/monacoEditor/index.stories.js
+++ src/stories/lab/monacoEditor/index.stories.js
@@ -14,3 +14,9 @@ export default {
 export const DiffEditor = diffEditor;
 export const ScriptEditor = scriptEditor;
 export const GraphQlEditor = graphQlEditor;
+
+// TODOs: Add stories for:
+// 1. Altering theme colors. Not a deal breaker, but surely we would want some
+//    control over the color palette for syntax highlighting.
+// 2. Dynamically edit Monaco's internal model to support a user editing their sample data.
+//    This is needed so the auto suggest matches the shape of the sample data.
--- src/stories/lab/monacoEditor/script.jsx
+++ src/stories/lab/monacoEditor/script.jsx
@@ -10,12 +10,26 @@ const useStyles = makeStyles({
   },
 });
 
-// extra libraries
-const libSource = `
+// This is an example of how to use Celigo-created virtual files that the Monaco editor
+// would parse and use for the Monaco auto-suggest feature. We can use this for 2 use-cases:
+// 1) we should create a json file that represents the "sample data" json. This would then
+//    instruct the Monaco auto-suggest to present the sample data object for a user to scan through.
+// 2) Our users who write scripts have access to our JS runtime which supports access to
+//    an IO API. (https://docs.google.com/document/d/1jJv0B0olZY7udyED_u__7_3hu6F5kA9qVGRgIlEA4B0/edit#)
+//    we could create a static file added to all editor models so that auto-suggest could let our
+//    users self-discover our API without needing to study the above help doc.
+//    Here is an example of how Monaco's own language libraries are defined for built-in auto suggest.
+//    https://github.com/microsoft/TypeScript/blob/main/lib/lib.es2015.core.d.ts
+const celigoAPIsource = `
   // simple JS suggestions
-  const FindMe = ()=>{ return 0; };',
+  /**',
+   * Comments here should appear in Monaco's auto suggest',
+   */',
+   function autoSuggestMe(a)  { 
+     return Number(a) + 10; 
+    };',
 
-  // TS suggestions include types.
+  // TS suggestions would allow us to include types.
   declare class Facts {',
       /**',
        * Returns the next fact',
@@ -32,22 +46,22 @@ const Template = args => {
   useEffect(() => {
     if (monaco) {
       monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
-        // ...opts,
         target: monaco.languages.typescript.ScriptTarget.ES2015,
         allowJs: true,
         allowNonTsExtensions: true,
         lib: ['es2015'],
       });
 
-      // console.log(monaco, opts);
+      // console.log(monaco);
 
-      monaco.languages.typescript.javascriptDefaults.addExtraLib(libSource, libUri);
+      monaco.languages.typescript.javascriptDefaults.addExtraLib(celigoAPIsource, libUri);
       // When resolving definitions and references, the editor will try to use created models.
       // Creating a model for the library allows "peek definition/references" commands to work with the library.
-      const model = monaco.editor.createModel(libSource, 'typescript', monaco.Uri.parse(libUri));
+      const model = monaco.editor.createModel(celigoAPIsource, 'typescript', monaco.Uri.parse(libUri));
 
       // an editor can only have 1 model. we need to dispose of it so that the next
-      // init will be able to create a new one.
+      // init will be able to create a new one, since we only have a single global Monaco
+      // instance.
       return () => {
         // console.log('disposing editor model', model);
         model.dispose();
