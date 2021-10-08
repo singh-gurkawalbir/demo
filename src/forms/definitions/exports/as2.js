878d17b53f65447c0edd06a099054bb76b766cdf
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-10-08 11:54:57 +0530

    IO-22960

--- src/forms/definitions/exports/as2.js
+++ src/forms/definitions/exports/as2.js
@@ -1,5 +1,8 @@
-import { alterFileDefinitionRulesVisibility } from '../../formFactory/utils';
-import { updateFileProviderFormValues } from '../../metaDataUtils/fileUtil';
+import {
+  updateFileProviderFormValues,
+  EXPORT_FILE_FIELD_MAP,
+  getFileProviderExportsOptionsHandler,
+} from '../../metaDataUtils/fileUtil';
 
 export default {
   preSave: formValues => {
@@ -21,82 +24,11 @@ export default {
       ...newValues,
     };
   },
-  init: fieldMeta => {
-    const fileDefinitionRulesField =
-      fieldMeta.fieldMap['file.filedefinition.rules'];
-
-    if (fileDefinitionRulesField.userDefinitionId) {
-      // make visibility of format fields false incase of edit mode of file adaptors
-      const formatField = fieldMeta.fieldMap['edix12.format'];
-
-      delete formatField.visibleWhenAll;
-      formatField.visible = false;
-    }
-
-    return fieldMeta;
-  },
-  optionsHandler: (fieldId, fields) => {
-    const fileType = fields.find(field => field.id === 'file.type');
-
-    if (fieldId === 'file.filedefinition.rules') {
-      let definitionFieldId;
-
-      // Fetch format specific Field Definition field to fetch id
-      if (fileType.value === 'filedefinition') definitionFieldId = 'edix12.format';
-      else if (fileType.value === 'fixed') definitionFieldId = 'fixed.format';
-      else definitionFieldId = 'edifact.format';
-      const definition = fields.find(field => field.id === definitionFieldId);
-      const resourcePath = fields.find(
-        field => field.id === 'file.fileDefinition.resourcePath'
-      );
-
-      alterFileDefinitionRulesVisibility(fields);
-
-      return {
-        format: definition && definition.format,
-        definitionId: definition && definition.value,
-        resourcePath: resourcePath && resourcePath.value,
-      };
-    }
-  },
+  optionsHandler: getFileProviderExportsOptionsHandler,
   fieldMap: {
-    common: { formId: 'common' },
-    'edix12.format': {
-      fieldId: 'edix12.format',
-    },
-    outputMode: {
-      id: 'outputMode',
-      type: 'text',
-      label: 'Parse files being transferred',
-      defaultValue: 'records',
-      visible: false,
-    },
-    'file.type': { fieldId: 'file.type' },
-    'file.csv': { fieldId: 'file.csv' },
-    'file.xlsx.hasHeaderRow': { fieldId: 'file.xlsx.hasHeaderRow' },
-    'file.xlsx.rowsPerRecord': { fieldId: 'file.xlsx.rowsPerRecord' },
-    'file.xlsx.keyColumns': { fieldId: 'file.xlsx.keyColumns' },
-    'file.xml.resourcePath': { fieldId: 'file.xml.resourcePath' },
-    'file.json.resourcePath': {
-      fieldId: 'file.json.resourcePath',
-    },
-    'fixed.format': { fieldId: 'fixed.format' },
-    'edifact.format': { fieldId: 'edifact.format' },
-    'file.filedefinition.rules': {
-      fieldId: 'file.filedefinition.rules',
-      refreshOptionsOnChangesTo: [
-        'edix12.format',
-        'fixed.format',
-        'edifact.format',
-        'file.fileDefinition.resourcePath',
-        'file.type',
-      ],
-      required: true,
-    },
+    ...EXPORT_FILE_FIELD_MAP,
     advancedSettings: { formId: 'advancedSettings' },
-    exportOneToMany: { formId: 'exportOneToMany' },
-    'file.sortByFields': { fieldId: 'file.sortByFields' },
-    'file.groupByFields': { fieldId: 'file.groupByFields' },
+
   },
   layout: {
     type: 'collapse',
@@ -109,7 +41,7 @@ export default {
         fields: [
           'outputMode',
           'file.type',
-          'file.xml.resourcePath',
+          'uploadFile',
           'file.json.resourcePath',
           'file.xlsx.hasHeaderRow',
           'file.xlsx.rowsPerRecord',
@@ -120,6 +52,7 @@ export default {
           'file.filedefinition.rules',
         ],
         containers: [{fields: [
+          'parsers',
           'file.csv',
         ]}],
       },
