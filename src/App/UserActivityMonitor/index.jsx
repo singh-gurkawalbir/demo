0baef6288aec866effac02bd47e1b1b7d855e31d
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2021-10-09 16:11:37 +0530

    added support for environment variables

--- src/App/UserActivityMonitor/index.jsx
+++ src/App/UserActivityMonitor/index.jsx
@@ -5,27 +5,20 @@ import actions from '../../actions';
 import {selectors} from '../../reducers';
 import { POLLING_STATUS } from '../../reducers/app';
 
-const USER_INACTIVE_PERIOD = 1000 * 60 * 1;
-
-const SLOW_DOWN_PERIOD = 1000 * 60 * 0.5;
 export default function UserActivityMonitor() {
   const dispatch = useDispatch();
 
   const handleOnIdle = useCallback(() => {
-    console.error('idle ');
-
     dispatch(
       actions.app.polling.stop()
     );
   }, [dispatch]);
 
   const handleOnActive = useCallback(() => {
-    console.error('active ');
-
     dispatch(actions.app.polling.resume());
   }, [dispatch]);
   const { getRemainingTime } = useIdleTimer({
-    timeout: USER_INACTIVE_PERIOD,
+    timeout: Number(process.env.USER_INACTIVE_PERIOD),
     onIdle: handleOnIdle,
     onActive: handleOnActive,
     debounce: 500,
@@ -38,7 +31,7 @@ export default function UserActivityMonitor() {
 
     if (!pollingStatus || pollingStatus === POLLING_STATUS.RESUME) {
       intervalID = setInterval(() => {
-        const shouldUpdateSlowPollingState = getRemainingTime() < SLOW_DOWN_PERIOD;
+        const shouldUpdateSlowPollingState = getRemainingTime() < Number(process.env.SLOW_DOWN_PERIOD);
 
         if (shouldUpdateSlowPollingState) {
           dispatch(actions.app.polling.slowDown());
