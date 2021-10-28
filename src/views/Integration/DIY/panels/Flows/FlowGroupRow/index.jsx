faea68a5f5433224ec10a6ec562a524ba3c62ae1
Karthik Pillai <karthikpillaigithub@gmail.com>
2021-10-28 22:54:19 +0530

    IO-23746 fixed

--- src/views/Integration/DIY/panels/Flows/FlowGroupRow/index.jsx
+++ src/views/Integration/DIY/panels/Flows/FlowGroupRow/index.jsx
@@ -1,14 +1,54 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import SortableHandle from '../../../../../../components/Sortable/SortableHandle';
 import { selectors } from '../../../../../../reducers';
 import FlowSectionTitle from '../../../../common/FlowSectionTitle';
 
+const useStyles = makeStyles(theme => ({
+  rowContainer: {
+    display: 'flex',
+    minHeight: 42,
+    alignItems: 'center',
+    position: 'relative',
+    '&>div': {
+      paddingTop: 0,
+      minWidth: theme.spacing(3),
+    },
+    '&>a': {
+      color: theme.palette.text.primary,
+      padding: theme.spacing(1, 0),
+      '&>span': {
+        gridTemplateColumns: theme.spacing(12, 7),
+      },
+    },
+    '&:before': {
+      content: '""',
+      width: '3px',
+      top: 0,
+      height: '100%',
+      position: 'absolute',
+      background: 'transparent',
+      left: '0px',
+    },
+    '&:hover': {
+      '&:before': {
+        background: theme.palette.primary.main,
+      },
+    },
+  },
+  active: {
+    '&>a': {
+      color: theme.palette.primary.main,
+    },
+  },
+}));
 export default function FlowGroupRow({
   rowData,
   integrationId,
-  classes,
+  className,
   isDragInProgress = false,
   isRowDragged = false,
 }) {
@@ -27,6 +67,7 @@ export default function FlowGroupRow({
   const handleOnMouseLeave = useCallback(() => {
     setShowGripper(false);
   }, []);
+  const classes = useStyles();
 
   useEffect(() => {
     if (isRowDragged) {
@@ -43,7 +84,7 @@ export default function FlowGroupRow({
       <div
         onMouseEnter={handleOnMouseEnter}
         onMouseLeave={handleOnMouseLeave}
-        className={classes.rowContainer}>
+        className={clsx(className, classes.rowContainer, classes.active)}>
         <SortableHandle isVisible={showGripper} />
         <NavLink
           data-public
