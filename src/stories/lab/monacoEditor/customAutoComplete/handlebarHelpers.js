7de952a59983712b6f302a4f470b4c0f49260f54
driedl <dave@riedl.ca>
2021-10-29 14:57:37 -0700

    added comments to Monaco story

--- src/stories/lab/monacoEditor/customAutoComplete/handlebarHelpers.js
+++ src/stories/lab/monacoEditor/customAutoComplete/handlebarHelpers.js
@@ -396,6 +396,9 @@ export function createProposals(monaco, range) {
     },
   ];
 
+  // The auto-suggest algorithm matches the string starting with '{{' prefix.
+  // Here we use the label as the base text, and use the filterText property
+  // match on the full matching text. (See field spec above)
   proposals.forEach((p, i) => { proposals[i].filterText = `{{${p.label}`; });
 
   return proposals;
