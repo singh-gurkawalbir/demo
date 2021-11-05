82d685b04e40c4bfff0eeef122c22f8014288a14
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-11-05 15:32:43 +0530

    added image

--- src/components/ResourceTable/errorManagement/cells/Classification.jsx
+++ src/components/ResourceTable/errorManagement/cells/Classification.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
 import { Tooltip } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
 import TimeAgo from 'react-timeago';
 import TextOverflowCell from '../../../TextOverflowCell';
 import { CLASSIFICATION_LABELS_MAP } from '../../../../utils/errorManagement';
-import FilterIcon from '../../../icons/FilterIcon';
+import AutoRetryIcon from '../../../icons/AutoRetryIcon';
 
 const formatter = (value, unit) => `${value} ${unit}${value !== 1 ? 's' : ''}`;
 
+const useStyles = makeStyles({
+  wrapper: {
+    display: 'flex',
+    alignItems: 'flex-start',
+    whiteSpace: 'nowrap',
+  },
+});
+
 export default function Classification({ error }) {
+  const classes = useStyles();
   const { classification, retryAt } = error;
 
   if (classification === CLASSIFICATION_LABELS_MAP.intermittent && !!retryAt) {
     return (
-      <span>
+      <span className={classes.wrapper}>
         {classification}
         <Tooltip data-public title={<><span>Next Auto-retry: </span><TimeAgo date={retryAt} formatter={formatter} /></>} >
-          <span><FilterIcon /></span>
+          <span><AutoRetryIcon /></span>
         </Tooltip>
       </span>
     );
