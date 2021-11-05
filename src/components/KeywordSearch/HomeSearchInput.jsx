10d20e29248a3830d5035b673b5b6395c07e9c2d
Mohammed Azhar Uddin <azharweb90@gmail.com>
2021-11-05 11:29:58 +0530

    FIX

--- src/components/KeywordSearch/HomeSearchInput.jsx
+++ src/components/KeywordSearch/HomeSearchInput.jsx
@@ -1,4 +1,4 @@
-import React, { useState} from 'react';
+import React, { useState, useRef} from 'react';
 import clsx from 'clsx';
 import {InputBase, IconButton} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -87,41 +87,50 @@ export default function HomeSearchInput({value, onChange}) {
   const [searchFocused, setSearchFocused] = useState(false);
   const [isSearchIconHidden, setSearchIconHidden] = useState(false);
   const [isCloseIconHidden, setCloseIconHidden] = useState(true);
-
+  const inputRef = useRef();
   const classes = useStyles();
   const onChangeHandler = e => {
-    if (e.target.value !== '') {
-      setCloseIconHidden(false);
+    if (e.target.value === '') {
+      inputRef.current.firstChild.focus();
+      inputRef.current.firstChild.placeholder = '';
       setSearchIconHidden(true);
-    } else {
       setCloseIconHidden(true);
+    } else {
+      setSearchIconHidden(true);
+      setCloseIconHidden(false);
     }
+
+    // if (e.target.value !== '') {
+    //   setCloseIconHidden(false);
+    //   setSearchIconHidden(true);
+    // } else {
+    //   setCloseIconHidden(true);
+    //   setSearchFocused(true);
+    // }
     onChange(e);
-  };
-  const searchClickHandler = () => {
-    setSearchFocused(true);
-    setSearchIconHidden(true);
-  };
-  const onClearInput = e => {
-    // eslint-disable-next-line no-param-reassign
-    e.target.value = '';
-    setSearchFocused(false);
-    setSearchIconHidden(false);
-    onChangeHandler(e);
   };
   const handleBlur = e => {
     if (e.target.value !== '') {
       setSearchIconHidden(true);
       setCloseIconHidden(false);
     } else {
+      inputRef.current.firstChild.placeholder = 'Search integrations & flows';
       setSearchIconHidden(false);
     }
     setSearchFocused(false);
   };
+  const onClearInput = e => {
+    // eslint-disable-next-line no-param-reassign
+    e.target.value = '';
+    onChangeHandler(e);
+  };
+  const focusHandler = () => {
+    setSearchFocused(true);
+  };
 
   return (
 
-    <div className={clsx(classes.search, {[classes.searchActive]: searchFocused})} onClick={searchClickHandler} onBlur={handleBlur}>
+    <div className={clsx(classes.search, {[classes.searchActive]: searchFocused})} onBlur={handleBlur}>
       {!isSearchIconHidden && (
         <div className={clsx(classes.searchIcon, {[classes.hideSearchIcon]: isSearchIconHidden})}>
           <SearchIcon />
@@ -129,6 +138,8 @@ export default function HomeSearchInput({value, onChange}) {
       )}
       <InputBase
         value={value}
+        ref={inputRef}
+        onFocus={focusHandler}
         onChange={onChangeHandler}
         placeholder="Search integrations & flows"
         classes={{
