d6bdbabb9e686e827e3a8fbcec73c17db9efd53d
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2021-11-05 11:18:36 +0530

    fix

--- src/views/MappingDrawer/DatabaseMapping_afe/index.jsx
+++ src/views/MappingDrawer/DatabaseMapping_afe/index.jsx
@@ -33,7 +33,7 @@ export default function DatabaseMapping_afe() {
       fieldId,
       resourceId: importId,
       resourceType: 'imports',
-      stage: 'inputFilter', // TODO: verify
+      stage: 'importMappingExtract',
       arrayIndex: index && parseInt(index, 10),
     }));
   // eslint-disable-next-line react-hooks/exhaustive-deps
