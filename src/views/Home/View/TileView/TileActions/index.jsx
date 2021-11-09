cb23808152411e008fcf8ab68cdb93663fb1b244
SiddharthMMore <SiddharthMMore@outlook.com>
2021-11-09 12:46:20 +0530

    Added actions handler

--- src/views/Home/View/TileView/TileActions/index.jsx
+++ src/views/Home/View/TileView/TileActions/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import EllipsisActionMenu from '../../../../components/EllipsisActionMenu';
-import useTileActions from '../../../../hooks/useTileActions';
+import EllipsisActionMenu from '../../../../../components/EllipsisActionMenu';
+import useTileActions from './useTileActions';
 
 // todo: siddharth
 export default function TileActions({tile}) {
   const {tileActions, handleAction} = useTileActions(tile);
-
-  console.log('tileActions', tileActions);
 
   return (
     <EllipsisActionMenu
