9deb2d9df31820d6416136c71878fd2404af3800
driedl <dave@riedl.ca>
2021-11-10 11:57:22 -0800

    CDN_BASE_URI fix

---
+++ src/stories/tools/settings/index.stories.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default {
+  title: 'Tools / Settings',
+};
+
+const Template = () => {
+  const settings = process.env;
+
+  console.log(settings);
+
+  return (
+    <pre>
+      {JSON.stringify(settings, null, 2)}
+    </pre>
+  );
+};
+
+export const browseAllFieldDefinitions = Template.bind({});
+
