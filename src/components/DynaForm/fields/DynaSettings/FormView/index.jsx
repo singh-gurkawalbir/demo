b7c90b7de618ff2b2b3e7d72d55d6595ac6d10a4
Surya Vamsi Vemparala <suryavamsi.code@gmail.com>
2021-11-17 11:35:43 +0530

    IO-23029

--- src/components/DynaForm/fields/DynaSettings/FormView/index.jsx
+++ src/components/DynaForm/fields/DynaSettings/FormView/index.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useMemo } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import {isEqual} from 'lodash';
 import { selectors } from '../../../../../reducers';
 import actions from '../../../../../actions';
 import DynaForm from '../../..';
@@ -15,6 +16,39 @@ const useStyles = makeStyles({
   },
 });
 
+const isPropsDiff = (prev, curr) => Object.keys(curr).filter(key => !!curr[key]).some(key => {
+  // this util function evaluates the content of properties and excludes lastModified prop...
+  // it can so happen that lastModified prop could be different and the content be the same
+  // and so in this case the props is the same.
+  const currPropCopy = {...curr[key] };
+  const prevPropCopy = {...prev[key]};
+
+  delete currPropCopy.lastModified;
+  delete prevPropCopy.lastModified;
+
+  return !isEqual(currPropCopy, prevPropCopy);
+});
+
+function usePrevious(value) {
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current = value;
+  });
+
+  return ref.current;
+}
+const useIsCustomSettingsFormPropsDiff = props => {
+  const {settingsForm, settings, script} = props;
+  const curr = {settingsForm, settings, script};
+  const prev = usePrevious(curr);
+
+  if (!prev || !curr) {
+    return true;
+  }
+
+  return isPropsDiff(prev, curr);
+};
 export default function FormView({
   resourceId,
   resourceType,
@@ -28,24 +62,21 @@ export default function FormView({
     selectors.customSettingsForm(state, resourceId)
   );
 
-  const settingsForm = useSelectorMemo(selectors.mkGetCustomFormPerSectionId, resourceType, resourceId, sectionId) ?.settingsForm;
-  const settings = useSelectorMemo(selectors.mkGetCustomFormPerSectionId, resourceType, resourceId, sectionId) ?.settings;
+  const settingsForm = useSelectorMemo(selectors.mkGetCustomFormPerSectionId, resourceType, resourceId, sectionId || 'general') ?.settingsForm;
+  const settings = useSelectorMemo(selectors.mkGetCustomFormPerSectionId, resourceType, resourceId, sectionId || 'general') ?.settings;
+  const script = useSelectorMemo(selectors.makeResourceSelector, 'scripts', settingsForm?.init?._scriptId);
+
+  // this hook evaluates if any of the settings props are different thereby if an initialization call is necessary
+  const isCustomsettingsPropsDiff = useIsCustomSettingsFormPropsDiff({settingsForm, settings, script});
 
   useEffect(() => {
-    // use effect will fire any time formState changes but...
-    // Only if the formState is missing do we need to perform an init.
-    if (!settingsFormState) {
+    // only if settingsForm, settings, script is different do you reinitialize
+    if (isCustomsettingsPropsDiff) {
       dispatch(actions.customSettings.formRequest(resourceType, resourceId, sectionId));
     }
-  }, [dispatch, settingsFormState, resourceId, resourceType, sectionId]);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isCustomsettingsPropsDiff]);
 
-  useEffect(
-    () => () => {
-      // reload settings form when the settingsForm or settings changes
-      dispatch(actions.customSettings.formClear(resourceId));
-    },
-    [dispatch, resourceId, settingsForm, settings]
-  );
   const updatedMeta = useMemo(() => {
     // sanitize all a tag elements within help texts
 
