f5298fd8e8d0b20fb7efc3c60016ea152eab21c9
SiddharthMMore <SiddharthMMore@outlook.com>
2021-11-22 13:23:20 +0530

    Added test cases for home search input reducer

---
+++ src/components/SearchInput/HomeSearchInput/stateReducer/test.js
@@ -0,0 +1,33 @@
+/* global describe,test,expect */
+
+import reducer from '.';
+
+const initialState = {
+  isSearchFocused: false,
+  isSearchIconHidden: false,
+  isCloseIconHidden: true,
+};
+
+describe('home search input reducer', () => {
+  test('should return correct state when input field changes', () => {
+    const state = reducer(initialState, {type: 'onInputChange'});
+
+    expect(state).toEqual(
+      {isCloseIconHidden: false, isSearchFocused: false, isSearchIconHidden: true}
+    );
+  });
+  test('should return correct state when search input is blurred', () => {
+    const state = reducer(initialState, {type: 'onBlur'});
+
+    expect(state).toEqual(
+      {isCloseIconHidden: false, isSearchFocused: false, isSearchIconHidden: true}
+    );
+  });
+  test('should return correct state when search is focused', () => {
+    const state = reducer(initialState, {type: 'onFocus'});
+
+    expect(state).toEqual(
+      {isCloseIconHidden: true, isSearchFocused: true, isSearchIconHidden: true}
+    );
+  });
+});
