dc7faecd133bf3b6a8042e16d12fdfb9bf4a696f
driedl <dave@riedl.ca>
2021-12-10 18:01:34 -0800

    added remaining layouts to panel resize story

--- src/stories/lab/afePanelResize/Prototype/index.jsx
+++ src/stories/lab/afePanelResize/Prototype/index.jsx
@@ -228,30 +228,3 @@ export default function ResizeProto({panels, layout}) {
   );
 }
 
-/**
-  compact2: {
-    gridTemplateColumns: '1fr 1fr',
-    gridTemplateRows: '1fr 1fr 0fr',
-    gridTemplateAreas: '"rule data" "result data" "error error"',
-  },
-  row: {
-    gridTemplateColumns: '1fr',
-    gridTemplateRows: '1fr 1ft 1fr 0fr',
-    gridTemplateAreas: '"rule" "data" "result" "error"',
-  },
-  compactRow: {
-    gridTemplateColumns: '1fr 1fr',
-    gridTemplateRows: '1fr 1fr 0fr',
-    gridTemplateAreas: '"rule rule" "data result" "error error"',
-  },
-  column: {
-    gridTemplateColumns: '1fr 1fr 1fr',
-    gridTemplateRows: '4fr 0fr',
-    gridTemplateAreas: '"rule data result" "error error error"',
-  },
-  jsonFormBuilder: {
-    gridTemplateColumns: '2fr 2fr',
-    gridTemplateRows: '1fr 1fr 0fr',
-    gridTemplateAreas: '"meta form" "meta values" "error error"',
-  },
- */
