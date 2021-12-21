bd80fe2dc30409f41fcef5483674795584fa3928
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2021-12-21 16:55:12 +0530

    --fix

--- src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/FlowGroup.jsx
+++ src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/FlowGroup.jsx
@@ -1,19 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import { selectors } from '../../../../reducers';
 import LoadResources from '../../../../components/LoadResources';
 import useSelectorMemo from '../../../../hooks/selectors/useSelectorMemo';
 import { getFlowGroup } from '../../../../utils/flows';
 
 export default function FlowGroupCrumb({ integrationId, sectionId }) {
-  const isIntegrationApp = useSelector(state => selectors.isIntegrationApp(state, integrationId));
   const flowGroupings = useSelectorMemo(selectors.mkFlowGroupingsTiedToIntegrations, integrationId);
 
-  const flowGroupName = isIntegrationApp ? sectionId : getFlowGroup(flowGroupings, '', sectionId)?.name;
+  const flowGroupName = flowGroupings.length ? getFlowGroup(flowGroupings, '', sectionId)?.name : sectionId;
 
   return (
     <LoadResources resources="integrations">
-      {(isIntegrationApp || flowGroupings?.length) ? flowGroupName : 'Flow group'}
+      {flowGroupName || 'Flow group'}
     </LoadResources>
   );
 }
