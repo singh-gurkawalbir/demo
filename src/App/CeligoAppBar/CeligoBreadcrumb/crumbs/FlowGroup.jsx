1873f69050561fdcadfb5e968785b6a689c6f4f1
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2021-12-22 11:02:19 +0530

    --required change

--- src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/FlowGroup.jsx
+++ src/App/CeligoAppBar/CeligoBreadcrumb/crumbs/FlowGroup.jsx
@@ -4,10 +4,11 @@ import LoadResources from '../../../../components/LoadResources';
 import useSelectorMemo from '../../../../hooks/selectors/useSelectorMemo';
 import { getFlowGroup } from '../../../../utils/flows';
 
-export default function FlowGroupCrumb({ integrationId, sectionId }) {
+export default function FlowGroupCrumb({ integrationId, childId, sectionId }) {
   const flowGroupings = useSelectorMemo(selectors.mkFlowGroupingsTiedToIntegrations, integrationId);
+  const flowSections = useSelectorMemo(selectors.mkIntegrationAppFlowSections, integrationId, childId);
 
-  const flowGroupName = flowGroupings.length ? getFlowGroup(flowGroupings, '', sectionId)?.name : sectionId;
+  const flowGroupName = flowGroupings.length ? getFlowGroup(flowGroupings, '', sectionId)?.name : flowSections.find(s => s.titleId === sectionId)?.title;
 
   return (
     <LoadResources resources="integrations">
