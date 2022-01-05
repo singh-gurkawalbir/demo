d4f9f6e655cdeed917b87f2e474e53a3e6fbaad5
Akhil Alur <akhil24code@gmail.com>
2022-01-05 15:16:50 +0530

    IO-24033 Fixed async helper clocked fields

--- src/forms/fieldDefinitions/resources/asyncHelpers.js
+++ src/forms/fieldDefinitions/resources/asyncHelpers.js
@@ -1,5 +1,4 @@
 import { isNewId } from '../../../utils/resource';
-import { isIntegrationApp } from '../../../utils/flows';
 
 export default {
   'http.status._exportId': {
@@ -14,7 +13,6 @@ export default {
     allowNew: true,
     allowEdit: true,
     required: true,
-    defaultDisabled: r => isIntegrationApp(r),
   },
   name: {
     isLoggable: true,
@@ -32,46 +30,39 @@ export default {
     isLoggable: true,
     type: 'text',
     label: 'Initial wait time',
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.pollWaitTime': {
     isLoggable: true,
     type: 'text',
     label: 'Poll wait time',
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.statusPath': {
     isLoggable: true,
     type: 'text',
     label: 'Status path',
     required: true,
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.inProgressValues': {
     isLoggable: true,
     type: 'textlist',
     label: 'In progress values',
     required: true,
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.doneValues': {
     isLoggable: true,
     type: 'textlist',
     label: 'Done values',
     required: true,
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.doneWithoutDataValues': {
     isLoggable: true,
     type: 'textlist',
     label: 'Done without data values',
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.status.errorValues': {
     isLoggable: true,
     type: 'textlist',
     label: 'Error values',
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.submit.resourcePath': {
     isLoggable: true,
@@ -96,7 +87,6 @@ export default {
     filter: r => ({ _connectionId: r._connectionId }),
     required: true,
     label: 'Result export',
-    defaultDisabled: r => isIntegrationApp(r),
   },
   'http.submit.sameAsStatus': {
     isLoggable: true,
