b649ca6a559bdfbdf28207fe818229e6dd170dfa
ashu-gupta <gupta.ashu1602@gmail.com>
2022-01-10 18:01:44 +0530

    IO-24044 test cases

--- src/utils/string.test.js
+++ src/utils/string.test.js
@@ -1,5 +1,5 @@
 /* global describe, test, expect */
-import { getTextAfterCount } from './string';
+import { getTextAfterCount, camelCase, isHTML } from './string';
 
 describe('getTextAfterCount util test cases', () => {
   test('should return correct string when count is zero', () => {
@@ -12,5 +12,31 @@ describe('getTextAfterCount util test cases', () => {
     expect(getTextAfterCount('value', 2)).toEqual('2 values');
     expect(getTextAfterCount('value', 3)).toEqual('3 values');
     expect(getTextAfterCount('value', 4)).toEqual('4 values');
+  });
+});
+
+describe('camelCase util test cases', () => {
+  test('should not throw exception if invalid or empty args are supplied', () => {
+    expect(camelCase()).toEqual('');
+    expect(camelCase('')).toEqual('');
+    expect(camelCase(null)).toEqual(null);
+  });
+  test('should return camel cases string', () => {
+    expect(camelCase('Magento store')).toEqual('magento store');
+    expect(camelCase('Shopify')).toEqual('shopify');
+  });
+});
+
+describe('isHTML util test cases', () => {
+  test('should not throw exception if invalid or empty args are supplied', () => {
+    expect(isHTML()).toEqual(false);
+    expect(isHTML(null)).toEqual(false);
+  });
+  test('should return false if invalid html is provided', () => {
+    expect(isHTML('test string')).toEqual(false);
+    expect(isHTML('<xml')).toEqual(false);
+  });
+  test('should return true if passed string is html', () => {
+    expect(isHTML('<br></br>')).toEqual(true);
   });
 });
