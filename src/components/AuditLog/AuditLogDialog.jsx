bed55881847fd5774cdb625b55f41d85cfc11e1a
Karthik Pillai <karthikpillaigithub@gmail.com>
2022-01-19 14:27:47 +0530

    fixed

--- src/components/AuditLog/AuditLogDialog.jsx
+++ src/components/AuditLog/AuditLogDialog.jsx
@@ -18,8 +18,7 @@ export default function AuditLogDialog({ resourceType, resourceId, onClose }) {
   const name = resource ? `: ${resource.name}` : '';
 
   return (
-    <ModalDialog
-      onClose={onClose} show maxWidth="xl" className={classes.auditLogModalContainer}>
+    <ModalDialog onClose={onClose} show maxWidth="xl" className={classes.auditLogModalContainer}>
       <div>Audit log{name}</div>
       <AuditLog
         onClick={onClose}
