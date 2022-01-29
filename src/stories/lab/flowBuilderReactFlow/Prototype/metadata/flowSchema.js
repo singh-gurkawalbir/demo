6d6cd7f5f7387e65fad2995783e86cfd7d33123f
driedl <dave@riedl.ca>
2022-01-29 10:05:33 -0800

    added more nodes

--- src/stories/lab/flowBuilderReactFlow/Prototype/metadata/flowSchema.js
+++ src/stories/lab/flowBuilderReactFlow/Prototype/metadata/flowSchema.js
@@ -4,24 +4,32 @@ export default [
     type: 'input', // input node
     sourcePosition: 'right',
     data: { label: 'Input Node' },
-    // position: { x: 100, y: 150 },
   },
   // default node
   {
     id: '2',
     type: 'step',
-    // you can also pass a React component as a label
     data: { label: 'Custom step node' },
-    // position: { x: 400, y: 125 },
+  },
+  {
+    id: '2a',
+    type: 'step',
+    data: { label: 'Custom step node' },
+  },
+  {
+    id: '2b',
+    type: 'step',
+    data: { label: 'Custom step node' },
   },
   {
     id: '3',
     type: 'output', // output node
     targetPosition: 'left',
     data: { label: 'Output Node' },
-    // position: { x: 700, y: 150 },
   },
 
   { id: 'e1-2', source: '1', target: '2', type: 'step' }, // , animated: true
-  { id: 'e2-3', source: '2', target: '3', type: 'linked' },
+  { id: 'e2-2a', source: '2', target: '2a', type: 'step' }, // , animated: true
+  { id: 'e2-2b', source: '2', target: '2b', type: 'step' }, // , animated: true
+  { id: 'e2b-3', source: '2b', target: '3', type: 'linked' },
 ];
