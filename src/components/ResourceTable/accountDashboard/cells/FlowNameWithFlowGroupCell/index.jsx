b0280e68fa3340ac3810ae0e00f7e744bd14bb29
ashu-gupta <gupta.ashu1602@gmail.com>
2022-02-02 15:22:22 +0530

    IO-24910

--- src/components/ResourceTable/accountDashboard/cells/FlowNameWithFlowGroupCell/index.jsx
+++ src/components/ResourceTable/accountDashboard/cells/FlowNameWithFlowGroupCell/index.jsx
@@ -6,25 +6,30 @@ import NameCell from '../../../commonCells/Name';
 import { useGetTableContext } from '../../../../CeligoTable/TableContext';
 import { getFlowGroup } from '../../../../../utils/flows';
 
-const FlowGroupName = ({ integrationId, flowId }) => {
-  const flow = useSelectorMemo(selectors.makeResourceSelector, 'flows', flowId);
-  const flowGroupings = useSelectorMemo(selectors.mkFlowGroupingsTiedToIntegrations, integrationId);
-
-  if (!flowGroupings?.length) return null;
-
-  const flowGroupName = getFlowGroup(flowGroupings, '', flow?._flowGroupingId)?.name;
-
-  return (
-    <Typography variant="body2" color="textSecondary">{flowGroupName}</Typography>
-  );
-};
 export default function FlowNameWithFlowGroupCell({ flowId, integrationId }) {
   const tableContext = useGetTableContext();
+  const flow = useSelectorMemo(selectors.makeResourceSelector, 'flows', flowId);
+  const flowGroupings = useSelectorMemo(selectors.mkFlowGroupingsTiedToIntegrations, integrationId);
+  const flowSectionsForIA1 = useSelectorMemo(selectors.mkIntegrationAppFlowSections, integrationId);
+  let sectionName;
+  let sectionId;
+
+  if (flow?._connectorId) {
+    const section = flowSectionsForIA1.find(flowSection => flowSection.flows?.some(flow => flow._id === flowId));
+
+    sectionName = section?.title;
+    sectionId = section?.titleId;
+  } else {
+    const section = getFlowGroup(flowGroupings, '', flow?._flowGroupingId);
+
+    sectionName = section?.name;
+    sectionId = section?._id;
+  }
 
   return (
     <>
-      <NameCell al={{resourceType: 'flow', _resourceId: flowId}} actionProps={tableContext} />
-      <FlowGroupName integrationId={integrationId} flowId={flowId} />
+      <NameCell al={{resourceType: 'flow', _resourceId: flowId, sectionId}} actionProps={tableContext} />
+      {sectionName && <Typography variant="body2" color="textSecondary">{sectionName}</Typography>}
     </>
   );
 }
