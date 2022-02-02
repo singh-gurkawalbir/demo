33c5d2036ed83c392c87e757e8c6ca7eddf105cb
Akhil Alur <akhil24code@gmail.com>
2022-02-02 15:09:27 +0530

    Memory optimizations for results

--- src/stories/lab/globalSearch/Prototype/hooks/useResults.js
+++ src/stories/lab/globalSearch/Prototype/hooks/useResults.js
@@ -1,16 +1,21 @@
+import { memoize } from 'lodash';
 import { useMemo } from 'react';
+import { createSelector } from 'reselect';
 import { useGlobalSearchContext } from '../GlobalSearchContext';
 import { getTabResults } from '../utils';
 import { useGlobalSearchState } from './useGlobalSearchState';
 
-const resultsSelector = getResults => state => {
-  const {keyword, filters} = state;
-  const searchTokens = keyword?.includes(':') ? keyword?.split(':') : keyword;
-  const searchValue = (Array.isArray(searchTokens) ? searchTokens[searchTokens?.length - 1] : searchTokens)?.trim?.();
-  const results = searchValue?.trim().length > 1 ? getResults(searchValue, filters) : {};
+const resultsSelector = memoize(getResults => createSelector(
+  state => state.keyword,
+  state => state.filters,
+  (keyword, filters) => {
+    const searchTokens = keyword?.includes(':') ? keyword?.split(':') : keyword;
+    const searchValue = (Array.isArray(searchTokens) ? searchTokens[searchTokens?.length - 1] : searchTokens)?.trim?.();
+    const results = searchValue?.trim().length > 1 ? getResults(searchValue, filters) : {};
 
-  return results;
-};
+    return results;
+  }));
+
 export default function useResults() {
   const { getResults } = useGlobalSearchContext();
   const results = useGlobalSearchState(resultsSelector(getResults));
