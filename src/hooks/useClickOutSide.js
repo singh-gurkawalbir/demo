17b2994f40c5ef87d9092327bc2441a0e3037130
Akhil Alur <akhil24code@gmail.com>
2022-02-08 11:46:34 +0530

    Optimized useOnClickOutside

--- src/hooks/useClickOutSide.js
+++ src/hooks/useClickOutSide.js
@@ -1,17 +1,25 @@
 import {useEffect} from 'react';
 
-export default function useClickOutSide(ref, callback) {
-  const handleClick = e => {
-    if (ref.current && !ref.current.contains(e.target)) {
-      callback();
-    }
-  };
+export default function useOnClickOutside(ref, handler) {
+  useEffect(
+    () => {
+      const listener = event => {
+        // Do nothing if clicking ref's element or descendent elements
+        if (!ref.current || ref.current.contains(event.target)) {
+          return;
+        }
+        handler(event);
+      };
 
-  useEffect(() => {
-    document.addEventListener('click', handleClick);
+      document.addEventListener('mousedown', listener);
+      document.addEventListener('touchstart', listener);
 
-    return () => {
-      document.removeEventListener('click', handleClick);
-    };
-  });
+      return () => {
+        document.removeEventListener('mousedown', listener);
+        document.removeEventListener('touchstart', listener);
+      };
+    },
+    // Add ref and handler to effect dependencies
+    [ref, handler]
+  );
 }
