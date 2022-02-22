f31b364a162e916da8e1d171e7929980962a6d4a
ashu-gupta <gupta.ashu1602@gmail.com>
2022-02-22 14:17:07 +0530

    added tree in dropdown

--- src/stories/lab/mapper2.0/Prototype/util.js
+++ src/stories/lab/mapper2.0/Prototype/util.js
@@ -132,11 +132,15 @@ export function generateTreeFromMappings(input) {
   return iterate(mappings, treeData);
 }
 
-export function allowDrop({ dragNode, dropNode }) {
+export function allowDrop({ dragNode, dropNode, dropPosition }) {
   const {parentKey: dragNodeParentKey, isTabNode: dragNodeIsTab} = dragNode;
-  const {parentKey: dropNodeParentKey, isTabNode: dropNodeIsTab} = dropNode;
+  const {key: dropNodeKey, parentKey: dropNodeParentKey, isTabNode: dropNodeIsTab} = dropNode;
 
   if (dragNodeIsTab || dropNodeIsTab) return false;
+
+  // dropping a child node at the 0th position in the children list
+  if (dropPosition === 0 && dragNodeParentKey === dropNodeKey) return true;
+
   // nodes can only be dropped at same level
   if ((dragNodeParentKey && !dropNodeParentKey) ||
     (!dragNodeParentKey && dropNodeParentKey)) {
@@ -161,4 +165,31 @@ export const findNode = (data, key, callback) => {
       findNode(item.children, key, callback);
     }
   });
+};
+
+export const sampleData = {
+  fName: 'John',
+  lName: 'Doe',
+  age: 20,
+  married: true,
+  hobbies: ['yoga', 'sports'],
+  daysBusy: [1, 4],
+  father: {
+    firstName: 'Dad',
+  },
+  mother: {
+    fName: 'Mom',
+    lName: 'Doe',
+  },
+  siblings: [
+    {
+      fName: 'Bro',
+      lName: '1',
+    },
+    {
+      fName: 'Bro',
+      lName: '2',
+      age: 34,
+    },
+  ],
 };
