bd7a26cd75d5255a0183bcbc36e191745becf613
Akhil Alur <akhil24code@gmail.com>
2022-02-28 13:18:22 +0530

    fixed test infrastructure

--- jest/setup.js
+++ jest/setup.js
@@ -1,10 +1,7 @@
 import 'regenerator-runtime/runtime';
 import '@testing-library/jest-dom';
-import 'whatwg-fetch';
 
 require('dotenv').config();
 
 Object.defineProperty(window, 'open', { value() {}, writable: true });
-
-global.fetch = window.fetch;
 
--- src/test/api/server.js
+++ src/test/api/server.js
@@ -2,6 +2,7 @@ import {setupServer} from 'msw/node';
 import path from 'path';
 import { cleanup } from '@testing-library/react';
 import { getAllDefaultEports } from './utils';
+import 'whatwg-fetch';
 
 const routesPath = path.join(__dirname, 'routes');
 const handlers = Object.values(getAllDefaultEports(routesPath));
@@ -11,12 +12,20 @@ const server = setupServer(
 );
 
 export function runServer() {
-  beforeAll(() => server.listen({onUnhandledRequest: 'bypass'}));
+  const originalFetch = global.fetch;
+
+  beforeAll(() => {
+    global.fetch = window.fetch;
+    server.listen({onUnhandledRequest: 'bypass'});
+  });
   afterEach(() => {
     server.resetHandlers();
     jest.setTimeout(5000);
     cleanup();
   });
-  afterAll(() => server.close());
+  afterAll(() => {
+    global.fetch = originalFetch;
+    server.close();
+  });
 }
 export default server;
