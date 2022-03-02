21e4293735db1cbc69d2a0e73ec06333539b371c
Mohammed Azhar Uddin <azharweb90@gmail.com>
2022-03-02 11:38:53 +0530

    fix

--- src/stories/lab/MenuStatusWithName/index.js
+++ src/stories/lab/MenuStatusWithName/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { IconButton } from '@material-ui/core';
+import AddIcon from '../../../components/icons/AddIcon';
 
-export default function MenuStatusWithName({name, status, isDragbar}) {
+export default function MenuStatusWithName({name, status, isDragbar, onClick}) {
   return (
     <li>
-      {isDragbar && <span> </span>} <span>{name}</span> <span>{status}</span>
+      {isDragbar && <span> <IconButton onClick={onClick}><AddIcon /></IconButton></span>} <span>{name}</span> <span>{status}</span>
     </li>
   );
 }
