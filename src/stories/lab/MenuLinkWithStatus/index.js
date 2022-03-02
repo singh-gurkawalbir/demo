572b30192f0ce0c4b6c4cd77eb478bb464f58b16
Mohammed Azhar Uddin <azharweb90@gmail.com>
2022-03-02 18:15:12 +0530

    fix

--- src/stories/lab/MenuLinkWithStatus/index.js
+++ src/stories/lab/MenuLinkWithStatus/index.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { makeStyles, Typography } from '@material-ui/core';
 import SortableHandle from '../../../components/Sortable/SortableHandle';
 
+const useStyles = makeStyles(theme => ({
+  wrapper: {
+    display: 'flex',
+    alignItems: 'center',
+    cursor: 'pointer',
+    '&:hover': {
+      '& >*  svg': {
+        color: isGripperVisible => isGripperVisible ? theme.palette.primary.main : '',
+      },
+    },
+  },
+  name: {
+    paddingRight: theme.spacing(1),
+  },
+}));
 export default function MenuLinkWithStatus({name, status, isGripperVisible = false, onMouseEnter, onMouseLeave}) {
+  const classes = useStyles(isGripperVisible);
+
   return (
-    <li>
+    <li className={classes.wrapper} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
       {isGripperVisible && (
-      <span onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}>
         <SortableHandle isVisible={isGripperVisible} />
-      </span>
       )}
-      <span>{name}</span>
-      <span>{status}</span>
+      <Typography className={classes.name}>{name}</Typography>
+      <span className={classes.status}>{status}</span>
     </li>
   );
 }
--- src/stories/lab/MenuLinkWithStatus/index.stories.jsx
+++ src/stories/lab/MenuLinkWithStatus/index.stories.jsx
@@ -1,19 +1,40 @@
+/* eslint-disable no-console */
 import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import {action} from '@storybook/addon-actions';
 import MenuLinkWithStaus from './index';
+import Status from '../../../components/Buttons/Status';
 
 export default {
   title: 'Lab / MenuLinkWithStatus',
   component: MenuLinkWithStaus,
-  parameters: {
-    layout: 'fullscreen',
-  },
 };
+
+const handleMouseEnter = action('Mouse Entered');
+const handleMouseLeave = action('Mouse Leave');
 
 const Template = args => <MenuLinkWithStaus {...args} />;
 
-export const onlyNameWithStatus = Template.bind();
+export const NameWithStatus = Template.bind();
+export const NameWithoutStatus = Template.bind();
+export const NameWithStatusAndGrippper = Template.bind();
+export const NameWithoutStatusAndGrippper = Template.bind();
 
-onlyNameWithStatus.args = {
-  name: 'IT is a very long name',
+NameWithStatus.args = {
+  name: 'It is a very long name',
+  status: <Status variant="success" />,
+};
+NameWithoutStatus.args = {
+  name: 'Welcometotheworld',
   isGripperVisible: true,
+};
+NameWithStatusAndGrippper.args = {
+  name: 'welcometotheworld',
+  isGripperVisible: true,
+  status: <Status variant="success" />,
+  onMouseEnter: handleMouseEnter,
+  onMouseLeave: handleMouseLeave,
+};
+NameWithoutStatusAndGrippper.args = {
+  name: 'welcometotheworld',
 };
