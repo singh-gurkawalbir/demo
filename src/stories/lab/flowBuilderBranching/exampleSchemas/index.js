99e4ac4bc748dcba342617b638d5fea0bb9f57ad
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-03-20 23:13:35 +0530

    overlapping edges story

--- src/stories/lab/flowBuilderBranching/exampleSchemas/index.js
+++ src/stories/lab/flowBuilderBranching/exampleSchemas/index.js
@@ -1,9 +1,11 @@
 import emptyFlow from './emptyFlow';
 import complexFlow from './complexFlow';
 import multipleSources from './multipleSources';
+import overlappingEdges from './overlappingEdges';
 
 export default {
   emptyFlow,
   complexFlow,
   multipleSources,
+  overlappingEdges,
 };
