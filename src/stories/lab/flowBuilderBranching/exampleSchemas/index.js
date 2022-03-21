116ae7512c2941b53c0045c827f1fa3ed50ae5fa
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-03-21 23:15:55 +0530

    Terminal edges

--- src/stories/lab/flowBuilderBranching/exampleSchemas/index.js
+++ src/stories/lab/flowBuilderBranching/exampleSchemas/index.js
@@ -2,10 +2,12 @@ import emptyFlow from './emptyFlow';
 import complexFlow from './complexFlow';
 import multipleSources from './multipleSources';
 import overlappingEdges from './overlappingEdges';
+import simpleTerminalEdges from './simpleTerminalEdges';
 
 export default {
   emptyFlow,
   complexFlow,
   multipleSources,
   overlappingEdges,
+  simpleTerminalEdges,
 };
