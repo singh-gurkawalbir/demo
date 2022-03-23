03bedd6bbd2821becd13b019d957854f106a3c9b
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-03-23 18:43:20 +0530

    Add destination steps

--- src/stories/lab/flowBuilderBranching/Prototype/titles/DestinationTitle.js
+++ src/stories/lab/flowBuilderBranching/Prototype/titles/DestinationTitle.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core';
+import { List, ListItem, ListItemText, makeStyles } from '@material-ui/core';
 import { useStoreState } from 'react-flow-renderer';
 import { FB_SOURCE_COLUMN_WIDTH } from '../constants';
 import Title from './Title';
+import ArrowPopper from '../../../../../components/ArrowPopper';
+import { useFlowContext } from '../Context';
+import { generateId, getAllPPNodes } from '../lib';
+import actions from '../reducer/actions';
+import { getSomeImport, getSomePpImport } from '../nodeGeneration';
 
 const useStyles = makeStyles(theme => ({
   sourceTitle: {
@@ -11,7 +16,53 @@ const useStyles = makeStyles(theme => ({
   },
 }));
 
-export default function DestinationTitle({onClick}) {
+const AddNodeMenuPopper = ({ anchorEl, handleClose }) => {
+  const classes = useStyles();
+  const open = Boolean(anchorEl);
+  const { flow, elements, setState } = useFlowContext();
+
+  const allPPSteps = getAllPPNodes(flow, elements);
+  const id = `new-${generateId()}`;
+  const flowNode = getSomePpImport(id);
+  const resourceNode = getSomeImport(id);
+
+  const handleCallback = stepId => () => {
+    const step = allPPSteps.find(s => s.id === stepId);
+
+    setState({
+      type: actions.ADD_NEW_STEP,
+      resourceType: 'imports',
+      path: step.path,
+      flowNode,
+      resourceNode,
+      flowId: flow._id,
+    });
+    handleClose();
+  };
+
+  return (
+    <ArrowPopper
+      open={open}
+      anchorEl={anchorEl}
+      placement="bottom-end"
+      onClose={handleClose}
+    >
+      <List
+        dense className={classes.listWrapper}>
+        {allPPSteps.map(({name, id}) => (
+          <ListItem
+            button
+            onClick={handleCallback(id)}
+            key={name}>
+            <ListItemText >{name}</ListItemText>
+          </ListItem>
+        ))}
+      </List>
+    </ArrowPopper>
+  );
+};
+
+export default function DestinationTitle() {
   // we dont care about the y axis since we always want 100% y axis coverage,
   // regardless of pan or zoom settings.
   const [x,, scale] = useStoreState(s => s.transform);
@@ -19,10 +70,25 @@ export default function DestinationTitle({onClick}) {
   const xOffset = columnWidth + 20;
 
   const classes = useStyles({xOffset});
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOpenMenu = event => {
+    event && setAnchorEl(event.currentTarget);
+  };
 
   return (
-    <Title onClick={onClick} className={classes.sourceTitle}>
-      DESTINATIONS & LOOKUPS
-    </Title>
+    <>
+      <Title onClick={handleOpenMenu} className={classes.sourceTitle}>
+        DESTINATIONS & LOOKUPS
+      </Title>
+      <AddNodeMenuPopper
+        anchorEl={anchorEl}
+        handleClose={handleCloseMenu}
+      />
+    </>
   );
 }
