4a33596c66ae1827f86649139c1cca4249c9ee92
Karthik Pillai <karthikpillaigithub@gmail.com>
2022-03-29 03:10:20 +0530

    IO-24753 fixed

--- src/components/DynaForm/fields/reactSelectStyles/styles.js
+++ src/components/DynaForm/fields/reactSelectStyles/styles.js
@@ -98,6 +98,7 @@ export function CustomReactSelectStyles() {
       backgroundColor: state.isDisabled ? theme.palette.background.paper2 : theme.palette.background.paper,
       boxShadow: 'none',
       borderRadius: 2,
+      height: 24,
       '&:hover': {
         borderColor: theme.palette.primary.main,
       },
@@ -114,13 +115,16 @@ export function CustomReactSelectStyles() {
     input: () => ({
       color: theme.palette.secondary.main,
       width: '100%',
+      padding: '0 7px',
       '& > div': {
         width: '100%',
       },
       '& * > input': {
         width: '100% !important',
         display: 'block !important',
-        fontFamily: 'source sans pro !important',
+        fontFamily: 'source sans pro',
+        fontSize: '15px !important',
+        letterSpacing: 'inherit',
       },
     }),
     placeholder: () => ({
