c38ea143f14eb6e32329a8e69979bbc8eafd938c
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2022-04-11 19:31:53 +0530

    removed unnecessary todos

--- src/components/DynaForm/fields/DynaScriptContent.jsx
+++ src/components/DynaForm/fields/DynaScriptContent.jsx
@@ -58,7 +58,6 @@ export default function DynaScriptContent(props) {
       options.scriptFunctionStub &&
       scriptHookStubs[options.scriptFunctionStub]
     ) {
-      // TODO @Raghu: Revisit updating script content
       const updatedScriptContent =
         value + scriptHookStubs[options.scriptFunctionStub];
 
