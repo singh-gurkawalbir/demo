bcb331268f2e9c66f5ecf635c9161c355452ea14
driedl <dave@riedl.ca>
2022-04-22 13:30:33 -0700

    added confirm dialog to delete menu option

--- src/components/AFE/Editor/panels/Router/MoreActionsButton/index.jsx
+++ src/components/AFE/Editor/panels/Router/MoreActionsButton/index.jsx
@@ -1,10 +1,12 @@
 /* eslint-disable no-param-reassign */
+import React, { useState, useCallback } from 'react';
 import { ClickAwayListener, IconButton, List, ListItem, ListItemIcon, ListItemText, makeStyles } from '@material-ui/core';
-import React from 'react';
 import ArrowPopper from '../../../../../ArrowPopper';
 import EllipsisHorizontalIcon from '../../../../../icons/EllipsisHorizontalIcon';
 import EditIcon from '../../../../../icons/EditIcon';
 import TrashIcon from '../../../../../icons/TrashIcon';
+import useConfirmDialog from '../../../../../ConfirmDialog';
+import RawHtml from '../../../../../RawHtml';
 
 const useStyles = makeStyles({
   root: { minWidth: 36 },
@@ -24,7 +26,8 @@ const BranchListItem = ({onClick, text, children}) => {
 };
 
 export default function MoreActionsButton() {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const { confirmDialog } = useConfirmDialog();
   const open = Boolean(anchorEl);
 
   const handleCloseMenu = event => {
@@ -35,6 +38,38 @@ export default function MoreActionsButton() {
     event.stopPropagation();
     setAnchorEl(event.currentTarget);
   };
+
+  const handleDeleteBranch = useCallback(
+    event => {
+      handleCloseMenu(event);
+
+      /* TODO: fetch configured/unconfigured steps and replace below */
+      /* If it is too hard to get both counts, we can replace with total step count */
+      const configuredCount = 3;
+      const unconfiguredCount = 2;
+      const message = `<p>Are you sure you want to delete this branch?</p>
+      <p>This will also remove all steps/branchings inside this branch 
+      (${configuredCount} configured steps, ${unconfiguredCount} unconfigured steps).</p>`;
+
+      confirmDialog({
+        title: 'Confirm delete',
+        message: <RawHtml html={message} />,
+        buttons: [
+          {
+            label: 'Confirm',
+            onClick: () => {
+              /* TODO: dispatch action to delete branch */
+            },
+          },
+          {
+            label: 'Cancel',
+            variant: 'text',
+          },
+        ],
+      });
+    },
+    [confirmDialog]
+  );
 
   return (
     <>
@@ -56,7 +91,7 @@ export default function MoreActionsButton() {
             <EditIcon />
           </BranchListItem>
 
-          <BranchListItem text="Delete branch" onClick={handleCloseMenu}>
+          <BranchListItem text="Delete branch" onClick={handleDeleteBranch}>
             <TrashIcon />
           </BranchListItem>
         </List>
