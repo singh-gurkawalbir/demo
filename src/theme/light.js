bc195ff1b2fde0f9f15f89cd9bb0e79fd9c90de7
Mohammed Azhar Uddin <azharweb90@gmail.com>
2022-04-27 13:59:52 +0530

    fix

--- src/theme/light.js
+++ src/theme/light.js
@@ -153,9 +153,9 @@ export default {
         borderColor: colors.celigoNeutral3,
         color: colors.celigoNeutral6,
         '&:hover': {
-          color: colors.celigoNeutral6,
+          color: colors.celigoAccent3,
           backgroundColor: colors.celigoWhite,
-          borderColor: colors.celigoNeutral3,
+          borderColor: colors.celigoAccent3,
         },
         '&:disabled': {
           color: colors.celigoNeutral4,
