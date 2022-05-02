b2cef26546b2b3d1eb245bb58d1aefe9f7c0ced7
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-05-02 21:50:06 +0530

    Open menu popper only when branching present

--- src/views/FlowBuilder/FlowBuilderBody/titles/DestinationTitle/index.js
+++ src/views/FlowBuilder/FlowBuilderBody/titles/DestinationTitle/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { useStoreState } from 'react-flow-renderer';
+import { useDispatch } from 'react-redux';
 import Title from '../Title';
 import AddNodeMenuPopper from './BranchMenuPopper';
 import { FB_SOURCE_COLUMN_WIDTH } from '../../../../../constants';
 import useMenuDrawerWidth from '../../../../../hooks/useMenuDrawerWidth';
+import { useSelectorMemo } from '../../../../../hooks';
+import { selectors } from '../../../../../reducers';
+import { useFlowContext } from '../../Context';
+import actions from '../../../../../actions';
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -17,11 +22,14 @@ const useStyles = makeStyles(theme => ({
 const DestinationTitle = () => {
   const menuWidth = useMenuDrawerWidth();
 
+  const {flow} = useFlowContext();
+  const flowOriginal = useSelectorMemo(selectors.makeResourceDataSelector, 'flows', flow?._id)?.merged;
+
   // we dont care about the y axis since we always want 100% y axis coverage,
   // regardless of pan or zoom settings.
+  const dispatch = useDispatch();
   const [x,, scale] = useStoreState(s => s.transform);
   const columnWidth = Math.max(0, FB_SOURCE_COLUMN_WIDTH * scale + x + menuWidth);
-
   const xOffset = columnWidth;
   const classes = useStyles({xOffset, columnWidth});
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -31,7 +39,11 @@ const DestinationTitle = () => {
   };
 
   const handleOpenMenu = event => {
-    event && setAnchorEl(event.currentTarget);
+    if (flowOriginal?.pageProcessors?.length) {
+      dispatch(actions.flow.addNewPPStep(flow._id));
+    } else {
+      event && setAnchorEl(event.currentTarget);
+    }
   };
 
   return (
