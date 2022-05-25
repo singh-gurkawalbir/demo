4b5fed7fd3147944d517af09e62218f836d04783
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-05-25 19:32:47 +0530

    test cases fix

--- src/utils/flows/flowbuilder.test.js
+++ src/utils/flows/flowbuilder.test.js
@@ -82,6 +82,53 @@ const flow1 = {
     ],
   }],
 };
+const flow2 = {
+  routers: [{
+    id: 'router1',
+    branches: [
+      {
+        name: 'branch1',
+        nextRouterId: 'router2',
+      },
+      {
+        name: 'branch1',
+        nextRouterId: 'router3',
+      },
+    ],
+  }, {
+    id: 'router2',
+    branches: [
+      {
+        name: 'branch1',
+        nextRouterId: 'router4',
+      },
+      {
+        name: 'branch1',
+        nextRouterId: 'router3',
+      },
+    ],
+  }, {
+    id: 'router3',
+    branches: [
+      {
+        name: 'branch6',
+      },
+      {
+        name: 'branch1',
+      },
+    ],
+  }, {
+    id: 'router4',
+    branches: [
+      {
+        name: 'branch6',
+      },
+      {
+        name: 'branch1',
+      },
+    ],
+  }],
+};
 
 describe('getAllRouterPaths function', () => {
   test('get all Router paths function test', () => {
@@ -96,10 +143,19 @@ describe('getAllRouterPaths function', () => {
       ['router1', 'router3'],
     ]);
   });
+  test('get all Router paths function test2', () => {
+    const paths = getAllRouterPaths(flow2);
+
+    expect(paths).toEqual([
+      ['router1', 'router2', 'router4'],
+      ['router1', 'router2', 'router3'],
+      ['router1', 'router3'],
+    ]);
+  });
 });
 
 describe('getPreceedingRoutersMap function', () => {
-  test('get all Router paths function test', () => {
+  test('getPreceedingRoutersMap function test', () => {
     const paths = getPreceedingRoutersMap(flow1);
 
     expect(paths).toEqual({
@@ -109,6 +165,17 @@ describe('getPreceedingRoutersMap function', () => {
       router4: ['router1', 'router2'],
       router5: [],
       router6: ['router1', 'router2', 'router3'],
+    });
+  });
+
+  test('getPreceedingRoutersMap function test2', () => {
+    const paths = getPreceedingRoutersMap(flow2);
+
+    expect(paths).toEqual({
+      router1: [],
+      router2: ['router1'],
+      router3: ['router1', 'router2'],
+      router4: ['router1', 'router2'],
     });
   });
 });
