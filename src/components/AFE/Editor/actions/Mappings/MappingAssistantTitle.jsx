e84597ebbb2b1f7ef82db601813dd6f6ada216a7
SiddharthMMore <SiddharthMMore@outlook.com>
2022-05-26 20:19:15 +0530

    removed comments

--- src/components/AFE/Editor/actions/Mappings/MappingAssistantTitle.jsx
+++ src/components/AFE/Editor/actions/Mappings/MappingAssistantTitle.jsx
@@ -1,12 +1,10 @@
 import React, { useCallback } from 'react';
-// import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { shallowEqual, useDispatch, useSelector } from 'react-redux';
 import { IconButton } from '@material-ui/core';
 import Help from '../../../../Help';
 import { selectors } from '../../../../../reducers';
 import actions from '../../../../../actions';
-// import ActionGroup from '../../../../ActionGroup';
 import RefreshIcon from '../../../../icons/RefreshIcon';
 
 const useStyles = makeStyles({
