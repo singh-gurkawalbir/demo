4549a6cad4d0932a296a6acf792f153267cc9494
Mounica Grandhi <mounicagrandhi@Mounicas-MacBook-Pro.local>
2022-05-31 15:45:32 +0530

    Update to IO-26427

--- src/forms/definitions/connections/custom/http/ebayXml.js
+++ src/forms/definitions/connections/custom/http/ebayXml.js
@@ -25,6 +25,7 @@ export default {
       '<WarningLevel>High</WarningLevel>\n' +
     '</GetTokenStatusRequest>',
     '/http/ping/method': 'POST',
+    '/http/headers': [{ name: 'X-EBAY-API-CALL-NAME', value: 'GetTokenStatus' }],
   }),
   fieldMap: {
     name: { fieldId: 'name' },
