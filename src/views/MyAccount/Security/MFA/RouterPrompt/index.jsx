9b88453f6cc3e0c1524af87cad286a12d2bf1f75
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2022-06-30 11:09:51 +0530

    Initial draft for router prompt

---
+++ src/views/MyAccount/Security/MFA/RouterPrompt/index.jsx
@@ -0,0 +1,76 @@
+
+import { useCallback, useEffect, useState } from 'react';
+import { useHistory, useRouteMatch } from 'react-router-dom';
+import useConfirmDialog from '../../../../../components/ConfirmDialog';
+
+export default function RouterPrompt({ when, onOK, onCancel}) {
+  const { confirmDialog } = useConfirmDialog();
+  const match = useRouteMatch();
+  const history = useHistory();
+  const [showPrompt, setShowPrompt] = useState(false);
+  const [currentPath, setCurrentPath] = useState('');
+
+  const handleOK = useCallback(async () => {
+    if (onOK) {
+      const canRoute = await Promise.resolve(onOK());
+
+      if (canRoute) {
+        history.block(() => {});
+        history.push(currentPath);
+      }
+    }
+  }, [currentPath, history, onOK]);
+
+  const handleCancel = useCallback(async () => {
+    if (onCancel) {
+      const canRoute = await Promise.resolve(onCancel());
+
+      if (canRoute) {
+        history.block(() => {});
+        history.push(currentPath);
+      }
+    }
+    setShowPrompt(false);
+  }, [currentPath, history, onCancel]);
+
+  useEffect(() => {
+    if (when) {
+      history.block(prompt => {
+        setCurrentPath(prompt.pathname);
+        setShowPrompt(true);
+
+        return 'true';
+      });
+    } else {
+      history.block(() => {});
+    }
+
+    return () => {
+      history.block(() => {});
+    };
+  }, [history, when, match.url]);
+
+  useEffect(() => {
+    if (showPrompt) {
+      confirmDialog({
+        title: 'Cancel MFA setup?',
+        message: 'Are you sure you want to leave? Your MFA settings will be disabled unless you connect your device successfully.',
+        buttons: [
+          {
+            label: 'Cancel',
+            variant: 'filled',
+            onClick: handleOK,
+
+          },
+          {
+            label: 'Continue setup',
+            variant: 'outlined',
+            onClick: handleCancel,
+          },
+        ],
+      });
+    }
+  }, [showPrompt, confirmDialog, handleCancel, handleOK]);
+
+  return null;
+}
--- src/views/MyAccount/Security/MFA/index.jsx
+++ src/views/MyAccount/Security/MFA/index.jsx
@@ -10,6 +10,7 @@ import CollapsableContainer from '../../../../components/CollapsableContainer';
 import { selectors } from '../../../../reducers';
 import actions from '../../../../actions';
 import MFASetup from './Setup';
+// import RouterPrompt from './RouterPrompt';
 import useNotifySetupSuccess from './useNotifySetupSuccess';
 import EditMFAConfiguration from './EditConfiguration';
 // import AccountSettings from './AccountSettings';
@@ -145,8 +146,16 @@ export default function MFA() {
   // Deals with showing notification whenever mfa setup is updated
   useNotifySetupSuccess();
 
+  // const onOK = useCallback(() => true, []);
+  // const onCancel = useCallback(() => false, []);
+
   return (
     <div className={classes.root}>
+      {/* <RouterPrompt
+        when
+        onOK={onOK}
+        onCancel={onCancel}
+       /> */}
       <PanelHeader title="Multifactor authentication (MFA)" infoText={infoTextMFA} />
       <MFADetails />
     </div>
