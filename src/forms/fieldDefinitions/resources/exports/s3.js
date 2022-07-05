8ba3ddcc15e4604168941d4a6059165faf276867
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2022-07-05 19:21:19 +0530

    IO-27219

--- src/forms/fieldDefinitions/resources/exports/s3.js
+++ src/forms/fieldDefinitions/resources/exports/s3.js
@@ -6,6 +6,7 @@ export default {
     type: 'select',
     label: 'Region',
     required: true,
+    defaultValue: r => r?.s3?.region || 'us-east-1',
     options: [
       {
         items: AWS_REGIONS_LIST,
