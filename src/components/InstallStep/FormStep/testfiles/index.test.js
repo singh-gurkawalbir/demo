b8f14e0bd2a643127e61561fdf749a33dfbdd617
PrashanthNesa <prashanthkumar.nesa@gmail.com>
2022-07-06 11:36:52 +0530

    IO-24750

---
+++ src/components/InstallStep/FormStep/testfiles/index.test.js
@@ -0,0 +1,245 @@
+/* global describe, test, expect, jest, */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { MemoryRouter, Route} from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../../../test/test-utils';
+import FormStepDrawer from '../index';
+import {formProps} from './formProps';
+import {getCreatedStore} from '../../../../store';
+
+function initFormDrawer(props = {}) {
+  const initialStore = getCreatedStore();
+
+  initialStore.getState().data.resources.integrations = [{
+    _id: '62beb29aa0f5f2144816f80c',
+    lastModified: '2022-07-01T08:39:31.036Z',
+    name: 'Data Warehouse Automation for Snowflake',
+    install: [],
+    mode: 'install',
+    version: '1.0.0',
+    sandbox: false,
+    _registeredConnectionIds: [
+      '62beb2c2a0f5f2144816f818',
+    ],
+    _templateId: '611f3bc1e3488c6cb37b8bc0',
+    settingsForm: {
+      form: {
+        layout: {
+          fields: [],
+        },
+      },
+      init: {
+        _scriptId: '62beb29aa0f5f2144816f809',
+        function: 'getIntegrationSettings',
+      },
+    },
+    preSave: {
+      function: 'processSnowflakeIntegrationSave',
+      _scriptId: '62beb29aa0f5f2144816f809',
+    },
+    installSteps: [
+      {
+        name: 'Setup Snowflake Connection',
+        completed: true,
+        type: 'connection',
+        sourceConnection: {
+          type: 'rdbms',
+          rdbms: {
+            type: 'snowflake',
+          },
+          name: 'Snowflake connection',
+        },
+        _connectionId: '62beb2c2a0f5f2144816f818',
+      },
+      {
+        name: 'Choose data sources',
+        description: 'Choose your data sources. You can always choose more data sources later',
+        completed: false,
+        type: 'form',
+        isCurrentStep: true,
+        isTriggered: true,
+        form: {
+          layout: {
+            fields: [],
+          },
+        },
+        formMeta: {
+          fieldMap: {
+            selectSourceApplications: {
+              id: 'selectSourceApplications',
+              name: 'selectSourceApplications',
+              type: 'multiselect',
+              helpText: 'Choose your data sources. You can always choose more data sources later.',
+              label: 'Choose data sources',
+              options: [
+                {
+                  items: [
+                    {
+                      label: 'Magento 2',
+                      value: 'magento',
+                    },
+                    {
+                      label: 'Zendesk Support',
+                      value: 'zendesk',
+                    },
+                    {
+                      label: 'Shopify',
+                      value: 'shopify',
+                    },
+                    {
+                      label: 'Mailchimp',
+                      value: 'mailchimp',
+                    },
+                    {
+                      label: 'HubSpot',
+                      value: 'hubspot',
+                    },
+                    {
+                      label: 'BigCommerce',
+                      value: 'bigcommerce',
+                    },
+                    {
+                      label: 'Returnly',
+                      value: 'returnly',
+                    },
+                    {
+                      label: 'Loop Returns',
+                      value: 'loopreturns',
+                    },
+                  ],
+                },
+              ],
+            },
+          },
+          layout: {
+            fields: [
+              'selectSourceApplications',
+            ],
+          },
+        },
+        function: 'addAdditionalInstallSteps',
+        initFormFunction: 'getSourceApplications',
+        _scriptId: '62beb29aa0f5f2144816f809',
+      },
+    ],
+    uninstallSteps: [],
+    flowGroupings: [],
+    createdAt: '2022-07-01T08:38:50.705Z',
+    _sourceId: '611f3e5482edfc2c354dd721',
+  }];
+  initialStore.getState().session.form = {'connections-new-w1g0iscy_': {}};
+  initialStore.getState().session.form['connections-new-w1g0iscy_'] = formProps;
+  const ui = (
+    <MemoryRouter
+      initialEntries={[{pathname: '/integrations/62beb29aa0f5f2144816f80c/setup/ui-drawer/form/install'}]}
+    >
+      <Route
+        path="/integrations/62beb29aa0f5f2144816f80c/setup"
+        params={{}}
+        >
+        <FormStepDrawer {...props} />
+      </Route>
+    </MemoryRouter>
+  );
+
+  return renderWithProviders(ui, {initialStore});
+}
+
+describe('FormStepDrawer UI tests', () => {
+  const mockSubmitHandler = jest.fn();
+  const mockCloseHandler = jest.fn();
+
+  test('should render the form correctly', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formSubmitHandler: mockSubmitHandler,
+      formCloseHandler: mockCloseHandler,
+    };
+
+    initFormDrawer(props);
+    expect(screen.queryAllByText(/Choose Data Sources/i)).toHaveLength(2);
+    expect(screen.getByText(/Please select/i)).toBeInTheDocument();
+    expect(screen.getByText(/Submit/i)).toBeInTheDocument();
+  });
+  test('should display the dropdown on Clicking Please select', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formSubmitHandler: mockSubmitHandler,
+      formCloseHandler: mockCloseHandler,
+    };
+
+    initFormDrawer(props);
+    userEvent.click(screen.getByText(/Please select/i));
+    expect(screen.getAllByRole('option')).toHaveLength(8);  // 8 options were passed in the formMeta of the integration,hence the value 8//
+  });
+  test('should run the mockSubmitHandler on clicking submit', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formSubmitHandler: mockSubmitHandler,
+      formCloseHandler: mockCloseHandler,
+    };
+
+    initFormDrawer(props);
+    userEvent.click(screen.getByText(/Submit/i));
+    expect(mockSubmitHandler).toBeCalled();
+  });
+  test('should run the mockCloseHandler on clicking ', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formSubmitHandler: mockSubmitHandler,
+      formCloseHandler: mockCloseHandler,
+    };
+
+    initFormDrawer(props);
+    const element = document.querySelector('[aria-label="Close"]');
+
+    userEvent.click(element);
+    expect(mockCloseHandler).toBeCalled();
+  });
+  test('should run the predefined handleclose function when formCloseHandler is not passed', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formSubmitHandler: jest.fn(),
+    };
+
+    initFormDrawer(props);
+    screen.debug();
+    const element = document.querySelector('[aria-label="Close"]');
+
+    userEvent.click(element);
+  });
+  test('should run the predefined handleSubmit function when formSubmitHandler is not passed,on clicking submit', () => {
+    const props = {
+      integrationId: '62beb29aa0f5f2144816f80c',
+      formCloseHandler: jest.fn(),
+    };
+
+    initFormDrawer(props);
+    expect(screen.getByText(/Submit/i)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/Submit/i));
+  });
+  test('Should render empty DOM when no integrationId is passed', () => {
+    const props = {
+      integrationId: null,
+      formCloseHandler: jest.fn(),
+      formSubmitHandler: jest.fn(),
+    };
+    const ui = (
+      <MemoryRouter
+        initialEntries={[{pathname: '/integrations/62beb29aa0f5f2144816f80c/setup/ui-drawer/form/install'}]}
+    >
+        <Route
+          path="/integrations/62beb29aa0f5f2144816f80c/setup"
+          params={{}}
+        >
+          <FormStepDrawer {...props} />
+        </Route>
+      </MemoryRouter>
+    );
+    const {utils} = renderWithProviders(ui);
+
+    expect(utils.container).toBeEmptyDOMElement();
+  });
+});
+
---
+++ src/components/InstallStep/index.test.js
@@ -0,0 +1,290 @@
+/* global describe, test, expect, jest, beforeEach, afterEach */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { MemoryRouter} from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as reactRedux from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders, reduxStore } from '../../test/test-utils';
+import InstallationStep from '.';
+import actions from '../../actions';
+
+const initialStore = reduxStore;
+
+function initInstallation(props = {}) {
+  initialStore.getState().data.resources.integrations = [{
+    _id: '62bd4ab37b94d20de64e9eaa',
+    lastModified: '2022-06-30T07:03:15.558Z',
+    name: 'Clone - demoint',
+    description: 'demo integration',
+    install: [],
+    mode: 'install',
+    sandbox: false,
+    _connectionid: '62bd43c87b94d20de64e9ab3',
+    _registeredConnectionIds: [],
+    installSteps: [
+      {
+        name: 'demo',
+        completed: false,
+        type: 'connection',
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+      },
+      {
+        name: 'demo sales',
+        completed: false,
+        type: 'connection',
+        sourceConnection: {
+          _id: '62bd452420ecb90e02f2a6f0',
+          type: 'salesforce',
+          name: 'demo sales',
+        },
+      },
+      {
+        name: 'Copy resources now from template zip',
+        completed: false,
+        type: 'template_zip',
+        templateZip: true,
+        isClone: true,
+      },
+    ],
+    uninstallSteps: [],
+    flowGroupings: [],
+    createdAt: '2022-06-30T07:03:15.558Z',
+    _sourceId: '6253af74cddb8a1ba550a010',
+  }];
+  initialStore.getState().data.resources.connections = [{
+    _id: '62bd43c87b94d20de64e9ab3',
+    createdAt: '2022-06-30T06:33:44.780Z',
+    lastModified: '2022-06-30T06:33:44.870Z',
+    type: 'http',
+    name: 'demo',
+    sandbox: false,
+    http: {
+      formType: 'rest',
+      mediaType: 'json',
+      baseURI: 'https://3jno0syp47.execute-api.us-west-2.amazonaws.com/test/orders',
+      unencrypted: {
+        field: 'value',
+      },
+      encrypted: '******',
+      auth: {
+        type: 'basic',
+        basic: {
+          username: 'demo',
+          password: '******',
+        },
+      },
+    },
+  }];
+
+  const ui = (<MemoryRouter><InstallationStep {...props} /></MemoryRouter>);
+
+  return renderWithProviders(ui, {initialStore});
+}
+
+describe('InstallationStep UI tests', () => {
+  let mockDispatchFn;
+  let useDispatchSpy;
+
+  beforeEach(() => {
+    useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
+    mockDispatchFn = jest.fn(action => {
+      switch (action.type) {
+        default:
+      }
+    });
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+  });
+
+  afterEach(() => {
+    useDispatchSpy.mockClear();
+  });
+  test('Should render all the options in the ui correctly', () => {
+    let type = 'connection';
+    const props = {
+      index: 1,
+      step: {
+        name: 'demo',
+        completed: false,
+        type: {type},
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+        isCurrentStep: true,
+      },
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    initInstallation(props);
+    expect(screen.getByText(/1/i)).toBeInTheDocument();
+    expect(screen.getByText(/demo/i)).toBeInTheDocument();
+    expect(screen.getByText(/configure/i)).toBeInTheDocument();
+
+    type = 'merge';
+  });
+  test('should run the handleStepclick function on clicking of configure button', () => {
+    const mockClick = jest.fn();
+    const props = {
+      index: 1,
+      handleStepClick: mockClick,
+      step: {
+        name: 'demo',
+        completed: false,
+        type: 'connection',
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+        isCurrentStep: true,
+      },
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    initInstallation(props);
+    userEvent.click(screen.getByText(/configure/i));
+    screen.debug();
+    expect(mockClick).toBeCalled();
+  });
+  test('should make the respective dispatch calls when step.type equals "install package"', () => {
+    const mockClick = jest.fn();
+    const props = {
+      index: 1,
+      handleStepClick: mockClick,
+      step: {
+        name: 'demo',
+        completed: false,
+        type: 'installPackage',
+        options: {_connectionId: '62bd43c87b94d20de64e9ab3' },
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+        isCurrentStep: true,
+      },
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    initInstallation(props);
+    expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.template.updateStep(
+      { ...props.step, status: 'verifying' },
+      undefined
+    ));
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.template.verifyBundleOrPackageInstall(
+      props.step,
+      initialStore.getState().data.resources.connections[0],
+      undefined
+    ));
+  });
+  test('should make the respective dispatch calls when revisionId,step.isCurrentStep,step.url,step.connectionId are defined', () => {
+    const mockClick = jest.fn();
+    const props = {
+      revisionId: '123456789abcdefgh',
+      index: 1,
+      handleStepClick: mockClick,
+      step: {
+        name: 'demo',
+        completed: false,
+        url: 'http://demourlforTests',
+        type: 'installPackage',
+        connectionId: '62bd43c87b94d20de64e9ab3',
+        options: {_connectionId: '62bd43c87b94d20de64e9ab3' },
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+        isCurrentStep: true,
+      },
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    initInstallation(props);
+    expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.integrationLCM.installSteps.updateStep('123456789abcdefgh', 'verify'));
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.integrationLCM.installSteps.verifyBundleOrPackageInstall({
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      connectionId: '62bd43c87b94d20de64e9ab3',
+      revisionId: '123456789abcdefgh',
+    }));
+  });
+  test('should make the respective dispatch calls when currentStep,connectionId,installURL, are defined and it is not an integration App', () => {
+    const mockClick = jest.fn();
+    const props = {
+      revisionId: '123456789abcdefgh',
+      index: 1,
+      handleStepClick: mockClick,
+      step: {
+        name: 'demo',
+        completed: false,
+        url: 'http://demourlforTests',
+        _connId: '987654321abcdefgh',
+        type: 'connection',
+        options: {_connectionId: '62bd43c87b94d20de64e9ab3' },
+        sourceConnection: {
+          _id: '62bd43c87b94d20de64e9ab3',
+          type: 'http',
+          name: 'demo',
+          http: {
+            formType: 'rest',
+          },
+        },
+        isCurrentStep: true,
+      },
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    initInstallation(props);
+    expect(mockDispatchFn).toHaveBeenCalledTimes(2);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.integrationApp.installer.updateStep(
+      '62bd4ab37b94d20de64e9eaa',
+      undefined,
+      'verify'
+    ));
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.integrationApp.templates.installer.verifyBundleOrPackageInstall(
+      '62bd4ab37b94d20de64e9eaa',
+      '987654321abcdefgh',
+      undefined,
+      3
+    ));
+  });
+  test('Should render empty DOM when impropper props are provided', () => {
+    const props = {
+      index: 1,
+      integrationId: '62bd4ab37b94d20de64e9eaa',
+      isFrameWork2: 3,
+    };
+
+    const {utils} = renderWithProviders(<MemoryRouter><InstallationStep {...props} /></MemoryRouter>);
+
+    expect(utils.container).toBeEmptyDOMElement();
+  });
+});
