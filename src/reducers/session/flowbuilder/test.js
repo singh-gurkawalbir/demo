54876025dfed1465f377b1f44964168bf5a73164
SiddharthMMore <SiddharthMMore@outlook.com>
2022-07-15 13:19:53 +0530

    Added selector test cases for flow branching

--- src/reducers/session/flowbuilder/test.js
+++ src/reducers/session/flowbuilder/test.js
@@ -240,9 +240,160 @@ describe('Reducers test cases', () => {
 });
 
 describe('Selectors test cases', () => {
-  test('fbRouterStepsInfo must return correct configured and unconfigured steps', () => {
-    expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'firstRouter')).toEqual({configuredCount: 10, unconfiguredCount: 5});
-    expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'fourthRouter')).toEqual({configuredCount: 0, unconfiguredCount: 0});
-    expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'secondRouter')).toEqual({configuredCount: 4, unconfiguredCount: 2});
+  describe('fbGraphElements test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbGraphElements()).toEqual([]);
+      expect(selectors.fbGraphElements({})).toEqual([]);
+      expect(selectors.fbGraphElements({}, '123')).toEqual([]);
+      expect(selectors.fbGraphElements('123')).toEqual([]);
+      expect(selectors.fbGraphElements({}, '')).toEqual([]);
+    });
+    test('should return flow elements for given flowId', () => {
+      const state = {
+        123: {elements: ['123']},
+      };
+
+      expect(selectors.fbGraphElements(state, '123')).toEqual(['123']);
+    });
+  });
+  describe('fbGraphElementsMap test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbGraphElementsMap()).toEqual({});
+      expect(selectors.fbGraphElementsMap({})).toEqual({});
+      expect(selectors.fbGraphElementsMap({}, '123')).toEqual({});
+      expect(selectors.fbGraphElementsMap('123')).toEqual({});
+      expect(selectors.fbGraphElementsMap({}, '')).toEqual({});
+    });
+    test('should return flow elements map for given flowId', () => {
+      const elementsMap = {123: ['123']};
+      const state = {
+        123: {elementsMap},
+      };
+
+      expect(selectors.fbGraphElementsMap(state, '123')).toEqual(elementsMap);
+    });
+  });
+  describe('fbFlow test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbFlow()).toBeUndefined();
+      expect(selectors.fbFlow({})).toBeUndefined();
+      expect(selectors.fbFlow({}, '123')).toBeUndefined();
+      expect(selectors.fbFlow('123')).toBeUndefined();
+      expect(selectors.fbFlow({}, '')).toBeUndefined();
+    });
+    test('should return the flow for given flowId', () => {
+      const flow = {pageProcessors: [], pageGenerators: []};
+      const state = {
+        123: {flow},
+      };
+
+      expect(selectors.fbFlow(state, '123')).toEqual(flow);
+    });
+  });
+  describe('fbDragStepId test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbDragStepId()).toBeUndefined();
+      expect(selectors.fbDragStepId({})).toBeUndefined();
+      expect(selectors.fbDragStepId({}, '123')).toBeUndefined();
+      expect(selectors.fbDragStepId('123')).toBeUndefined();
+      expect(selectors.fbDragStepId({}, '')).toBeUndefined();
+    });
+    test('should return drag step Id for given flowId', () => {
+      const dragStepId = '123';
+      const state = {
+        123: {dragStepId},
+      };
+
+      expect(selectors.fbDragStepId(state, '123')).toEqual(dragStepId);
+    });
+  });
+  describe('fbMergeTargetType test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbMergeTargetType()).toBeUndefined();
+      expect(selectors.fbMergeTargetType({})).toBeUndefined();
+      expect(selectors.fbMergeTargetType({}, '123')).toBeUndefined();
+      expect(selectors.fbMergeTargetType('123')).toBeUndefined();
+      expect(selectors.fbMergeTargetType({}, '')).toBeUndefined();
+    });
+    test('should return merge target type for given flowId', () => {
+      const mergeTargetType = 'branch';
+      const state = {
+        123: {mergeTargetType},
+      };
+
+      expect(selectors.fbMergeTargetType(state, '123')).toEqual(mergeTargetType);
+    });
+  });
+  describe('fbMergeTargetId test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbMergeTargetId()).toBeUndefined();
+      expect(selectors.fbMergeTargetId({})).toBeUndefined();
+      expect(selectors.fbMergeTargetId({}, '123')).toBeUndefined();
+      expect(selectors.fbMergeTargetId('123')).toBeUndefined();
+      expect(selectors.fbMergeTargetId({}, '')).toBeUndefined();
+    });
+    test('should return flow elements for given flowId', () => {
+      const mergeTargetId = '123';
+      const state = {
+        123: {mergeTargetId},
+      };
+
+      expect(selectors.fbMergeTargetId(state, '123')).toEqual(mergeTargetId);
+    });
+  });
+  describe('fbDragStepIdInProgress test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbDragStepIdInProgress()).toBeUndefined();
+      expect(selectors.fbDragStepIdInProgress({})).toBeUndefined();
+      expect(selectors.fbDragStepIdInProgress({}, '123')).toBeUndefined();
+      expect(selectors.fbDragStepIdInProgress('123')).toBeUndefined();
+      expect(selectors.fbDragStepIdInProgress({}, '')).toBeUndefined();
+    });
+    test('should return "drag step id in progress" for given flowId', () => {
+      const dragStepIdInProgress = true;
+      const state = {
+        123: {dragStepIdInProgress},
+      };
+
+      expect(selectors.fbDragStepIdInProgress(state, '123')).toEqual(dragStepIdInProgress);
+    });
+  });
+  describe('isFlowSaveInProgress test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.isFlowSaveInProgress()).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress({})).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress({}, '123')).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress('123')).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress({}, '')).toBeFalsy();
+    });
+    test('should return false if status is not "saving"', () => {
+      const state = {
+        123: {status: 'failed'},
+        124: {status: 'error'},
+        125: {status: 'received'},
+      };
+
+      expect(selectors.isFlowSaveInProgress(state, '123')).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress(state, '123')).toBeFalsy();
+      expect(selectors.isFlowSaveInProgress(state, '123')).toBeFalsy();
+    });
+    test('should return true if status is "saving"', () => {
+      const state = {
+        123: {status: 'saving'},
+      };
+
+      expect(selectors.isFlowSaveInProgress(state, '123')).toBeTruthy();
+    });
+  });
+  describe('fbRouterStepsInfo test cases', () => {
+    test('should not throw exception for invalid arguments', () => {
+      expect(selectors.fbRouterStepsInfo()).toEqual({configuredCount: 0, unconfiguredCount: 0});
+      expect(selectors.fbRouterStepsInfo({}, 'flow1', 'firstRouter')).toEqual({configuredCount: 0, unconfiguredCount: 0});
+    });
+    test('fbRouterStepsInfo must return correct configured and unconfigured steps', () => {
+      expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'firstRouter')).toEqual({configuredCount: 10, unconfiguredCount: 5});
+      expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'fourthRouter')).toEqual({configuredCount: 0, unconfiguredCount: 0});
+      expect(selectors.fbRouterStepsInfo(state1, 'flow1', 'secondRouter')).toEqual({configuredCount: 4, unconfiguredCount: 2});
+    });
   });
 });
