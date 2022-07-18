b90e36ce7f9c1b154c6327e0b49d200a45981afe
Dileep krishna sai <dileepkrishnasai53@gmail.com>
2022-07-18 14:54:41 +0530

    removing coverage

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,6 @@ module.exports = {
     'src/reducers/**/*.{js,jsx}',
     'src/sagas/**/*.{js,jsx}',
     'src/utils/**/*.{js,jsx}',
-    'src/views/AccessTokenList/**/*.{js,jsx}',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest/setup.js', 'jest-date-mock', 'core-js'],
   coverageThreshold: {
