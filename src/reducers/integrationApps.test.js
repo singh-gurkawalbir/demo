df8ea65cc7a0a8dbc315e555c0ef000a2a9b21f4
ashu-gupta <gupta.ashu1602@gmail.com>
2022-07-19 14:37:39 +0530

    IO-27177, minor change

--- src/reducers/integrationApps.test.js
+++ src/reducers/integrationApps.test.js
@@ -3118,7 +3118,7 @@ describe('integrationApps selector testcases', () => {
       });
     });
 
-    test('should retun license details for the integration app for owner user for non-expired', () => {
+    test('should return license details for the integration app for owner user for non-expired', () => {
       const state = reducer(
         {
           user: {
@@ -3131,7 +3131,7 @@ describe('integrationApps selector testcases', () => {
                       {
                         _id: 'l1',
                         _integrationId: 'i1',
-                        expires: '2022-08-18T06:00:43.721Z',
+                        expires: '2030-08-18T06:00:43.721Z',
                         created: '2018-07-10T10:03:02.169Z',
                       },
                     ],
@@ -3152,8 +3152,8 @@ describe('integrationApps selector testcases', () => {
         _integrationId: 'i1',
         created: '2018-07-10T10:03:02.169Z',
         createdText: 'Started on Jul 10th, 2018',
-        expires: '2022-08-18T06:00:43.721Z',
-        expiresText: 'Expires on Aug 18th, 2022',
+        expires: '2030-08-18T06:00:43.721Z',
+        expiresText: 'Expires on Aug 18th, 2030',
         showLicenseExpiringWarning: false,
         upgradeRequested: false,
         upgradeText: '',
@@ -3247,7 +3247,7 @@ describe('integrationApps selector testcases', () => {
       });
     });
 
-    test('should retun license details for the integration app for non owner user for non-expired', () => {
+    test('should return license details for the integration app for non owner user for non-expired', () => {
       const state = reducer(
         {
           user: {
@@ -3260,7 +3260,7 @@ describe('integrationApps selector testcases', () => {
                       {
                         _id: 'l1',
                         _integrationId: 'i1',
-                        expires: '2022-08-18T06:00:43.721Z',
+                        expires: '2030-08-18T06:00:43.721Z',
                         created: '2018-07-10T10:03:02.169Z',
                       },
                     ],
@@ -3281,8 +3281,8 @@ describe('integrationApps selector testcases', () => {
         _integrationId: 'i1',
         created: '2018-07-10T10:03:02.169Z',
         createdText: 'Started on Jul 10th, 2018',
-        expires: '2022-08-18T06:00:43.721Z',
-        expiresText: 'Expires on Aug 18th, 2022',
+        expires: '2030-08-18T06:00:43.721Z',
+        expiresText: 'Expires on Aug 18th, 2030',
         showLicenseExpiringWarning: false,
         upgradeRequested: false,
         upgradeText: '',
