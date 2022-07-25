efa85c2a8bc43cb535e0d41e863180bed4672fda
Dileep krishna sai <dileepkrishnasai53@gmail.com>
2022-07-25 12:10:39 +0530

    IO-24442

---
+++ src/views/Marketplace/ApplicationsList.test.js
@@ -0,0 +1,118 @@
+/* global describe, test, expect */
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import ApplicationsList from './ApplicationsList';
+import { runServer } from '../../test/api/server';
+import { renderWithProviders, reduxStore } from '../../test/test-utils';
+
+async function initApplicationsList({
+  props = {},
+} = {}) {
+  const initialStore = reduxStore;
+
+  initialStore.getState().user.preferences = {
+    environment: 'production',
+    defaultAShareId: 'own',
+  };
+  initialStore.getState().user.org = {
+    accounts: [
+      {
+        accessLevel: 'owner',
+        _id: 'own',
+        ownerUser: {
+          licenses: [{
+            type: 'endpoint',
+          }],
+        },
+      },
+    ],
+  };
+  initialStore.getState().data.resources = {
+    integrations: [
+      {
+        _id: 'id_1',
+        name: 'name 1',
+      },
+    ],
+  };
+  initialStore.getState().data.marketplace = {
+    templates: [
+      {
+        _id: 'id_1',
+        name: 'name 1',
+        applications: ['salesforce'],
+      },
+    ],
+    connectors: [
+      {
+        _id: 'id_3',
+        name: 'oame 3',
+        applications: ['salesforce'],
+        framework: 'twoDotZero',
+      },
+      {
+        _id: 'id_1',
+        name: 'name 1',
+        applications: ['netsuite'],
+        framework: 'twoDotZero',
+      },
+      {
+        _id: 'id_2',
+        name: 'mame 2',
+        applications: ['netsuite'],
+        framework: 'twoDotZero',
+      },
+      {
+        _id: 'id_2',
+        name: 'mame 2',
+        applications: ['dummyname'],
+        framework: 'twoDotZero',
+      },
+    ],
+  };
+  const ui = (
+    <MemoryRouter>
+      <ApplicationsList {...props} />
+    </MemoryRouter>
+  );
+  const { store, utils } = await renderWithProviders(ui, { initialStore });
+
+  return {
+    store,
+    utils,
+  };
+}
+
+describe('ApplicationsList test cases', () => {
+  runServer();
+
+  test('should pass the initial render with default value', async () => {
+    await initApplicationsList();
+    expect(screen.queryByText(/NetSuite/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Salesforce/i)).toBeInTheDocument();
+  });
+
+  test('should pass the initial render with search key as netsuite', async () => {
+    await initApplicationsList({
+      props: {
+        filter: {
+          keyword: 'netsuite',
+        },
+      },
+    });
+    expect(screen.queryByText(/NetSuite/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Salesforce/i)).not.toBeInTheDocument();
+  });
+
+  test('should pass the initial render with search key as novalue', async () => {
+    await initApplicationsList({
+      props: {
+        filter: {
+          keyword: 'novalue',
+        },
+      },
+    });
+    expect(screen.queryByText(/Your search didn’t return any matching results. Try expanding your search criteria./i)).toBeInTheDocument();
+  });
+});
