d896c714b5ff95cee398817bb2a39d99ede50807
chaitanyareddy-mula <chaitanyareddy.mule@gmail.com>
2022-07-27 11:49:13 +0530

    IO-24893

---
+++ src/components/DragContainer/index.test.jsx
@@ -0,0 +1,107 @@
+/* global describe, test, expect, jest */
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { screen } from '@testing-library/react';
+import { renderWithProviders } from '../../test/test-utils';
+import DragContainer from '.';
+
+const props = {
+  onDrop: '',
+  disabled: false,
+  SortableItemComponent: jest.fn().mockReturnValue('mock1'),
+  LastRowSortableItemComponent: jest.fn().mockReturnValue('mock2'),
+  onSortEnd: jest.fn(),
+};
+
+describe('Drag Container', () => {
+  test('Should able to test the Drag Container with the list of items which have keys', async () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <DragContainer
+          {...{...props,
+            items: [
+              {
+                key: 'bPV_fPAkA',
+                extract: 'ResourceList',
+                generate: 'test',
+              },
+              {
+                key: 'TiiIbfTd-r',
+                extract: '_links',
+                generate: 'test1',
+              },
+            ] }} />
+      </MemoryRouter>
+    );
+    const mock1 = await screen.findAllByText('mock1');
+
+    expect(mock1).toHaveLength(2);
+    const mock2 = screen.getByText('mock2');
+
+    expect(mock2).toBeInTheDocument();
+  });
+  test('Should able to test the Drag Container with the empty list of items', () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <DragContainer {...props} />
+      </MemoryRouter>
+    );
+    const mock2 = screen.getByText('mock2');
+
+    expect(mock2).toBeInTheDocument();
+  });
+  test('Should able to test the Drag Container with the list of items which has section id', async () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <DragContainer
+          {...{...props,
+            items: [
+              {
+                sectionId: 'bPV_fPAkA',
+                extract: 'ResourceList',
+                generate: 'test',
+              },
+              {
+                sectionId: 'TiiIbfTd-r',
+                extract: '_links',
+                generate: 'test1',
+              },
+            ] }} />
+      </MemoryRouter>
+    );
+    const mock1 = await screen.findAllByText('mock1');
+
+    expect(mock1).toHaveLength(2);
+    const mock2 = screen.getByText('mock2');
+
+    expect(mock2).toBeInTheDocument();
+  });
+  test('Should able to test the Drag Container with the list of items which has section id and disabled as true', async () => {
+    renderWithProviders(
+      <MemoryRouter>
+        <DragContainer
+          {...{...props,
+            items: [
+              {
+                sectionId: 'bPV_fPAkA',
+                extract: 'ResourceList',
+                generate: 'test',
+              },
+              {
+                sectionId: 'TiiIbfTd-r',
+                extract: '_links',
+                generate: 'test1',
+              },
+            ],
+            disabled: true }} />
+      </MemoryRouter>
+    );
+    const mock1 = await screen.findAllByText('mock1');
+
+    expect(mock1).toHaveLength(2);
+    const mock2 = screen.getByText('mock2');
+
+    expect(mock2).toBeInTheDocument();
+  });
+});
---
+++ src/components/StackShare/SharedUserList/SharedUserRow.test.jsx
@@ -0,0 +1,153 @@
+/* global describe, test, expect, afterEach, jest */
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { cleanup, fireEvent, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter, Route} from 'react-router-dom';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import userEvent from '@testing-library/user-event';
+import SharedUserRow from './SharedUserRow';
+import { renderWithProviders, mockPutRequestOnce } from '../../../test/test-utils';
+import { ConfirmDialogProvider } from '../../ConfirmDialog';
+import { runServer } from '../../../test/api/server';
+
+const props = {
+  user: {
+    _id: '62d4100770b1915aa17b6614',
+    _stackId: '62d2ef8aa7777017e5a8081a',
+    sharedWithUser: {
+      _id: '6040b91267059b24eb522db6',
+      email: 'testuser+1@celigo.com',
+      authTypeSSO: null,
+    },
+  },
+};
+
+describe('Shared User Row', () => {
+  runServer();
+  afterEach(cleanup);
+  test('Should able to list the accepted disabled Shared User List and enable the user', async () => {
+    renderWithProviders(
+      <ConfirmDialogProvider>
+        <MemoryRouter>
+          <Route>
+            <SharedUserRow {...{...props, user: {...props.user, accepted: true, disabled: true}}} />
+          </Route>
+        </MemoryRouter>
+      </ConfirmDialogProvider>
+    );
+    const emailText = screen.getByText('testuser+1@celigo.com');
+
+    expect(emailText).toBeInTheDocument();
+    const acceptedText = screen.getByText('Accepted');
+
+    expect(acceptedText).toBeInTheDocument();
+    const checkBox = screen.getByRole('checkbox');
+
+    expect(checkBox).toBeInTheDocument();
+    fireEvent.click(checkBox);
+    const confirmEnable = screen.getByText('Confirm enable');
+
+    expect(confirmEnable).toBeInTheDocument();
+    const confirmMessage = screen.getByText('Are you sure you want to enable sharing with this user?');
+
+    expect(confirmMessage).toBeInTheDocument();
+    const enableButton = screen.getByText('Enable');
+
+    expect(enableButton).toBeInTheDocument();
+    userEvent.click(enableButton);
+    expect(confirmEnable).not.toBeInTheDocument();
+  });
+  test('Should able to list the pending shared user and delete the user', async () => {
+    renderWithProviders(
+      <ConfirmDialogProvider>
+        <MemoryRouter>
+          <Route>
+            <SharedUserRow {...{...props, user: {...props.user, accepted: false}}} />
+          </Route>
+        </MemoryRouter>
+      </ConfirmDialogProvider>
+    );
+    const emailText = screen.getByText('testuser+1@celigo.com');
+
+    expect(emailText).toBeInTheDocument();
+    const pendingText = screen.getByText('Pending');
+
+    expect(pendingText).toBeInTheDocument();
+    const deleteMessage = screen.getByRole('button');
+
+    expect(deleteMessage).toBeInTheDocument();
+    userEvent.click(deleteMessage);
+    const confirmRemoveText = screen.getByText('Confirm remove');
+
+    expect(confirmRemoveText).toBeInTheDocument();
+    const deleteMessageText = screen.getByText('Are you sure you want to remove?');
+
+    expect(deleteMessageText).toBeInTheDocument();
+    const removeText = screen.getByText('Remove');
+
+    expect(removeText).toBeInTheDocument();
+    userEvent.click(removeText);
+    expect(confirmRemoveText).not.toBeInTheDocument();
+  });
+  test('Should able to list the enabled accepted shared users', async () => {
+    renderWithProviders(
+      <ConfirmDialogProvider>
+        <MemoryRouter>
+          <Route>
+            <SharedUserRow {...{...props, user: {...props.user, accepted: true, disabled: false}}} />
+          </Route>
+        </MemoryRouter>
+      </ConfirmDialogProvider>
+    );
+    const emailText = screen.getByText('testuser+1@celigo.com');
+
+    expect(emailText).toBeInTheDocument();
+    const acceptedText = screen.getByText('Accepted');
+
+    expect(acceptedText).toBeInTheDocument();
+    const checkBox = screen.getByRole('checkbox');
+
+    expect(checkBox).toBeInTheDocument();
+    fireEvent.click(checkBox);
+    const confirmDisable = screen.getByText('Confirm disable');
+
+    expect(confirmDisable).toBeInTheDocument();
+    const confirmMessage = screen.getByText('Are you sure you want to disable sharing with this user?');
+
+    expect(confirmMessage).toBeInTheDocument();
+    const disableButton = screen.getByText('Disable');
+
+    expect(disableButton).toBeInTheDocument();
+    userEvent.click(disableButton);
+    expect(confirmDisable).not.toBeInTheDocument();
+  });
+  test('Should able to list the dismissed shared users list and reinvite the user', async () => {
+    const mockPutResolverFunction = jest.fn();
+
+    mockPutRequestOnce('/api/sshares/62d4100770b1915aa17b6614', (req, res, ctx) => {
+      mockPutResolverFunction();
+
+      return res(ctx.json([]));
+    });
+    renderWithProviders(
+      <ConfirmDialogProvider>
+        <MemoryRouter>
+          <Route>
+            <SharedUserRow {...{...props, user: {...props.user, dismissed: true}}} />
+          </Route>
+        </MemoryRouter>
+      </ConfirmDialogProvider>
+    );
+    const emailText = screen.getByText('testuser+1@celigo.com');
+
+    expect(emailText).toBeInTheDocument();
+    const dismissedText = screen.getByText('Dismissed');
+
+    expect(dismissedText).toBeInTheDocument();
+    const button = screen.queryAllByRole('button');
+
+    expect(button[0]).toBeInTheDocument();
+    userEvent.click(button[0]);
+    await waitFor(() => expect(mockPutResolverFunction).toHaveBeenCalledTimes(1));
+  });
+});
