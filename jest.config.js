1750b8052a5bc73fc9b9a33fc58da2dacc40ed06
Yashveer Singh <yashveersinghvns2@gmail.com>
2022-08-02 01:20:28 +0530

    IO-24883

--- jest.config.js
+++ jest.config.js
@@ -20,10 +20,6 @@ module.exports = {
     'src/reducers/**/*.{js,jsx}',
     'src/sagas/**/*.{js,jsx}',
     'src/utils/**/*.{js,jsx}',
-    'src/components/IsLoggableContextProvider/*.{js,jsx}',
-    'src/components/KeyColumnsDeprecationNotification/*.{js,jsx}',
-    'src/components/LogoStrip/*.{js,jsx}',
-    'src/components/Spinner/*.{js,jsx}',
   ],
   setupFilesAfterEnv: ['<rootDir>/jest/setup.js', 'jest-date-mock', 'core-js'],
   coverageThreshold: {
