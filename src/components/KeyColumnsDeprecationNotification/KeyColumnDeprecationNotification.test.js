25d932d17a8437acf851bdbee1dfed9c747bd9a0
Yashveer Singh <yashveersinghvns2@gmail.com>
2022-08-02 10:35:17 +0530

    IO-24883

--- src/components/KeyColumnsDeprecationNotification/KeyColumnDeprecationNotification.test.js
+++ src/components/KeyColumnsDeprecationNotification/KeyColumnDeprecationNotification.test.js
@@ -1,15 +1,14 @@
 /* global describe, test, expect */
 import React from 'react';
-import { screen, waitFor } from '@testing-library/react';
-import {renderWithProviders, mockGetRequestOnce} from '../../test/test-utils';
-import { runServer } from '../../test/api/server';
-import actions from '../../actions';
+import { screen } from '@testing-library/react';
+import {renderWithProviders, reduxStore} from '../../test/test-utils';
 import KeyColumnsDeprecationNotification from '.';
 
 describe('KeyColumnsDeprecationNotification UI testing', () => {
-  runServer();
   async function renderWithStore(file, adaptorType, mediaType) {
-    mockGetRequestOnce('/api/exports', [{
+    const initialStore = reduxStore;
+
+    initialStore.getState().data.resources.exports = [{
       _id: '5e5f495a3a9b335b1a007b43',
       createdAt: '2020-03-04T06:23:22.828Z',
       lastModified: '2020-03-04T06:23:22.896Z',
@@ -33,22 +32,14 @@ describe('KeyColumnsDeprecationNotification UI testing', () => {
         },
       },
       adaptorType,
-    }]);
-    mockGetRequestOnce('/api/connections', [{
+    }];
+    initialStore.getState().data.resources.connections = [{
       _id: '5d8339d00f4f5a567f0b6188',
       type: 'rest',
       rest: {mediaType},
-    }]);
+    }];
 
-    const {store, utils} = renderWithProviders(<KeyColumnsDeprecationNotification resourceId="5e5f495a3a9b335b1a007b43" />);
-
-    store.dispatch(actions.resource.requestCollection('connections'));
-
-    await waitFor(() => expect(store?.getState()?.data?.resources?.connections).toBeDefined());
-
-    store.dispatch(actions.resource.requestCollection('exports'));
-
-    await waitFor(() => expect(store?.getState()?.data?.resources?.exports).toBeDefined());
+    const {utils} = renderWithProviders(<KeyColumnsDeprecationNotification resourceId="5e5f495a3a9b335b1a007b43" />, {initialStore});
 
     return utils.container;
   }
@@ -57,7 +48,8 @@ describe('KeyColumnsDeprecationNotification UI testing', () => {
     const message = screen.getByText('Learn more');
 
     expect(message).toHaveAttribute(
-      'href', 'https://integrator.io/zendesk/sso?return_to=https://docs.celigo.com/hc/en-us/articles/4405373029019-Sort-and-group-content-for-all-file-providers'
+      'href',
+      'https://integrator.io/zendesk/sso?return_to=https://docs.celigo.com/hc/en-us/articles/4405373029019-Sort-and-group-content-for-all-file-providers'
     );
   });
   test('Should not show notification because of adaptor type', async () => {
