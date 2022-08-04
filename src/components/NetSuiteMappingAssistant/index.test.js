eb452a44e12cc2f8875021206d3409cecd03635d
Neetin Kumar <kneetin5@gmail.com>
2022-08-05 00:39:46 +0530

    SalesforceMappingAssistant test cases

---
+++ src/components/NetSuiteMappingAssistant/index.test.js
@@ -0,0 +1,274 @@
+/* global describe, expect, jest, test, beforeEach, afterEach */
+import React from 'react';
+import * as reactRedux from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { screen, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderWithProviders } from '../../test/test-utils';
+import { getCreatedStore } from '../../store';
+import messageStore from '../../utils/messageStore';
+import NetSuiteMappingAssistant from '.';
+import actions from '../../actions';
+
+jest.mock('../Spinner', () => ({
+  __esModule: true,
+  ...jest.requireActual('../Spinner'),
+  default: () => (<div>Spinner</div>),
+}));
+
+jest.mock('../../utils/mapping/application/netsuite', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../utils/mapping/application/netsuite'),
+  default: {
+    isNSMappingAssistantSupported: jest.fn().mockReturnValueOnce(false).mockReturnValue(true),
+  },
+}));
+
+async function initNetSuiteMappingAssistant(props = {}, initialStore) {
+  const dispatch = reactRedux.useDispatch();
+  const ui = (
+    <MemoryRouter>
+      <button className="hello" type="button" onClick={() => { dispatch(actions.mapping.setNSAssistantFormLoaded(true)); }}>
+        BTNN
+      </button>
+      <NetSuiteMappingAssistant {...props} />
+    </MemoryRouter>
+  );
+
+  return renderWithProviders(ui, {initialStore});
+}
+
+describe('test suite for NetSuiteMappingAssistant', () => {
+  let useDispatchSpy;
+  let mockDispatchFn;
+
+  beforeEach(() => {
+    useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
+    mockDispatchFn = jest.fn(action => {
+      switch (action.type) {
+        default:
+      }
+    });
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+  });
+
+  afterEach(() => {
+    useDispatchSpy.mockClear();
+    mockDispatchFn.mockClear();
+  });
+
+  test('should show error details when connection is not configured', async () => {
+    await initNetSuiteMappingAssistant();
+    expect(screen.getByText('Missing connection configuration.')).toBeInTheDocument();
+  });
+
+  test('should display spinner if netSuiteRecordMetadata is not found', async () => {
+    const initialStore = getCreatedStore();
+    const netSuiteConnectionId = '173uww';
+
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    await initNetSuiteMappingAssistant({netSuiteConnectionId}, initialStore);
+    expect(screen.getByText('Spinner')).toBeInTheDocument();
+  });
+
+  test('should display the steps to login and launch NetSuite Mapping Assistant', async () => {
+    const netSuiteConnectionId = '173uww';
+    const netSuiteRecordType = 'componentinventorydetail';
+    const path = `netsuite/metadata/suitescript/connections/${netSuiteConnectionId}/recordTypes`;
+    const initialStore = getCreatedStore();
+
+    initialStore.getState().session.metadata.application[netSuiteConnectionId] = {};
+    initialStore.getState().session.metadata.application[netSuiteConnectionId][path] = {
+      status: 'requested',
+      data: [{
+        name: 'asd',
+        scriptId: 'inventorydetail',
+        url: 'https:://sampleURL.com',
+      }],
+    };
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    const connection = initialStore.getState().data.resources.connections[0].netsuite;
+
+    await initNetSuiteMappingAssistant({netSuiteConnectionId, netSuiteRecordType}, initialStore);
+    const launchButton = screen.getByRole('button', {name: 'Launch NetSuite assistant'});
+
+    expect(launchButton).toBeEnabled();
+
+    const list = screen.getByRole('list');
+    const { getAllByRole } = within(list);
+    const items = getAllByRole('listitem');
+
+    expect(items.length).toBe(3);
+    const launchSteps = items.map(item => item.textContent);
+
+    expect(launchSteps).toEqual([
+      'Please make sure that you have "Celigo integrator.io" bundle (ID: 20038) version 1.7.4.5 or higher.',
+      `Please click here to login to your NetSuite account ${connection.account}.`,
+      'After login, please click the "Launch NetSuite assistant" button.',
+    ]);
+
+    const loginLink = screen.getByRole('link', {name: 'here'});
+
+    expect(loginLink).toHaveAttribute('href', `${connection.dataCenterURLs.systemDomain}/app/login/secure/enterpriselogin.nl?c=${connection.account}&whence=`);
+    expect(loginLink).toHaveAttribute('target', 'blank');
+  });
+
+  test('should enqueue error snackbar if unable to launch assistant', async () => {
+    const initialStore = getCreatedStore();
+    const netSuiteConnectionId = '173uww';
+    const netSuiteRecordType = 'manualrecordtype';
+    const path = `netsuite/metadata/suitescript/connections/${netSuiteConnectionId}/recordTypes`;
+
+    initialStore.getState().session.metadata.application[netSuiteConnectionId] = {};
+    initialStore.getState().session.metadata.application[netSuiteConnectionId][path] = {
+      status: 'requested',
+      data: [{
+        name: 'asd',
+        scriptId: netSuiteRecordType,
+        url: 'https:://sampleURL.com',
+      }],
+    };
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    await initNetSuiteMappingAssistant({netSuiteConnectionId, netSuiteRecordType}, initialStore);
+    const launchButton = screen.getByRole('button', {name: 'Launch NetSuite assistant'});
+
+    expect(launchButton).toBeEnabled();
+    userEvent.click(launchButton);
+    const snackBar = screen.getByRole('alert');
+
+    expect(snackBar.textContent).toEqual(messageStore('NETSUITE_ASSISTANT_LAUNCH_ERROR'));
+  });
+
+  test('should be able to successfully launch assistant', async () => {
+    const initialStore = getCreatedStore();
+    const netSuiteConnectionId = '173uww';
+    const netSuiteRecordType = 'manualrecordtype';
+    const path = `netsuite/metadata/suitescript/connections/${netSuiteConnectionId}/recordTypes`;
+
+    initialStore.getState().session.metadata.application[netSuiteConnectionId] = {};
+    initialStore.getState().session.metadata.application[netSuiteConnectionId][path] = {
+      status: 'requested',
+      data: [{
+        name: 'asd',
+        scriptId: netSuiteRecordType,
+        url: 'https:://sampleURL.com',
+      }],
+    };
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    await initNetSuiteMappingAssistant({netSuiteConnectionId, netSuiteRecordType}, initialStore);
+
+    const launchButton = screen.getByRole('button', {name: 'Launch NetSuite assistant'});
+
+    expect(launchButton).toBeEnabled();
+    userEvent.click(launchButton);
+    const nsMapAsst = screen.getByTitle('NetSuite Mapping Assistant');
+
+    expect(nsMapAsst).toBeInTheDocument();
+  });
+
+  test('should unset isNSAssistantFormLoaded on unmounting', async () => {
+    const initialStore = getCreatedStore();
+    const netSuiteConnectionId = '173uww';
+    const netSuiteRecordType = 'manualrecordtype';
+    const path = `netsuite/metadata/suitescript/connections/${netSuiteConnectionId}/recordTypes`;
+
+    initialStore.getState().session.metadata.application[netSuiteConnectionId] = {};
+    initialStore.getState().session.metadata.application[netSuiteConnectionId][path] = {
+      status: 'requested',
+      data: [{
+        name: 'asd',
+        scriptId: netSuiteRecordType,
+        url: 'https:://sampleURL.com',
+      }],
+    };
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    initialStore.getState().session.mapping.mapping = { isNSAssistantFormLoaded: true };
+    const { utils: {unmount}} = await initNetSuiteMappingAssistant({netSuiteConnectionId, netSuiteRecordType}, initialStore);
+
+    unmount();
+    expect(mockDispatchFn).toBeCalledWith(actions.mapping.setNSAssistantFormLoaded(false));
+  });
+
+  test('should throw error if data is provided but isNSAssistantFormLoaded is true', async () => {
+    const data = {
+      data: {
+        returnedObjects: {
+          jsObjects: {
+            data: [{
+              data: 'manualData',
+            }],
+          },
+        },
+      },
+    };
+    const initialStore = getCreatedStore();
+    const netSuiteConnectionId = '173uww';
+    const netSuiteRecordType = 'manualrecordtype';
+    const path = `netsuite/metadata/suitescript/connections/${netSuiteConnectionId}/recordTypes`;
+
+    initialStore.getState().session.metadata.application[netSuiteConnectionId] = {};
+    initialStore.getState().session.metadata.application[netSuiteConnectionId][path] = {
+      status: 'requested',
+      data: [{
+        name: 'asd',
+        scriptId: netSuiteRecordType,
+        url: 'https:://sampleURL.com',
+      }],
+    };
+    initialStore.getState().data.resources.connections = [{
+      _id: netSuiteConnectionId,
+      netsuite: {
+        account: 'ASDFGH123',
+        dataCenterURLs: {
+          systemDomain: 'https://asdfgh123.app.netsuite.com',
+        },
+      },
+    }];
+    initialStore.getState().session.mapping.mapping = { isNSAssistantFormLoaded: true };
+    try {
+      await initNetSuiteMappingAssistant({netSuiteConnectionId, netSuiteRecordType, data}, initialStore);
+    } catch (e) {
+      expect(e).toEqual(expect.any(TypeError));
+    }
+  });
+});
