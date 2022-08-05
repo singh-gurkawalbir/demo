4a642dec0b2889cd75edfd38bdaf8a3f0d7aa777
vamsi547 <chandrabhatla.raghuvamsi@gmail.com>
2022-08-05 10:07:18 +0530

    info text

--- src/views/MyAccount/infoText.js
+++ src/views/MyAccount/infoText.js
@@ -4,7 +4,7 @@ export default {
   Transfers: 'Transfer individual integrations between integrator.io accounts. Send integrations by specifying the email of the owner of the integrator.io account you want to send the integration to. The receiving account owner needs to accept the transfer. Once accepted, the integration will be removed from your account and instead reside in the receiver’s account. Note: the receiver needs to be an account owner and cannot be part of the same organization as the sender.',
   Subscription: 'See details of your product subscription and request an upgrade when needed. An endpoint refers to each application or technology service that you create a connection to in integrator.io. Unlimited connections to each application are allowed and only count as one endpoint.',
   Users: 'Invite users to either Administer your account, or Manage or Monitor your integrations. A user who has been invited to Administer an account will have all the same permissions as the account Owner, including inviting users and changing their access rights. A user who has been invited to Manage an integration will have permissions to edit existing flows or create flows within an integration tile. A user who has Monitor permissions will only be allowed to view the integration, they do not have permissions to make any changes. They are however allowed to run the flows within the integration. The user will see only those integrations that you have invited them to.',
-  MFA: 'MFA info',
+  MFA: 'Configure multifactor authentication in this section',
   SSO: 'Configure single sign-on settings in this section',
 };
 
