e360a8eb2d2249ddf99a804d9781df83d51a8b3f
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-09-14 16:40:10 +0530

    IO-28366

--- src/reducers/session/editors/processorLogic/router/index.js
+++ src/reducers/session/editors/processorLogic/router/index.js
@@ -217,14 +217,13 @@ export default {
     const data = {};
     const parsedData = safeParse(sampleData);
 
-    delete parsedData.settings;
-    data.filter = JSON.stringify(parsedData, null, 2);
+    data.filter = JSON.stringify(parsedData, null, 2) || '';
     // for JS panel, 'rows' is also represented as 'record'
     if (parsedData?.rows) {
       parsedData.record = parsedData.rows;
       delete parsedData.rows;
     }
-    data.javascript = JSON.stringify(parsedData, null, 2);
+    data.javascript = JSON.stringify(parsedData, null, 2) || '';
 
     return data;
   },
