093e4a11533b41514be942890f222e10ca6aef82
GrandhiMounica <mounicagrandhi96@gmail.com>
2022-09-21 08:35:24 +0530

    IO-21761

--- src/forms/definitions/connections/custom/http/shopify.js
+++ src/forms/definitions/connections/custom/http/shopify.js
@@ -175,14 +175,14 @@ export default {
       required: true,
       defaultValue: r =>
         (r && r.http && r.http.unencrypted && r.http.unencrypted.version) ||
-        '2022-10',
+        '2022-07',
       visible: r => !(r?._connectorId),
       options: [
         {
           items: [
+            { label: '2022-10', value: '2022-01' },
             { label: '2022-04', value: '2022-04' },
             { label: '2022-07', value: '2022-07' },
-            { label: '2022-10', value: '2022-10' },
           ],
         },
       ],
