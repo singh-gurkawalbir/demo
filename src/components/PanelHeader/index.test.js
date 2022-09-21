5ee98429b922b94456c75aa37a325cd7343832a6
chaitanyareddy-mula <chaitanyareddy.mule@gmail.com>
2022-09-22 03:25:28 +0530

    IO-28629

---
+++ src/components/PanelHeader/index.test.js
@@ -0,0 +1,51 @@
+/* global describe, test, expect */
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import PanelHeader from '.';
+import { renderWithProviders } from '../../test/test-utils';
+
+async function initPanelheader({children = '', title = '', infoText = '', placement = ''} = {}) {
+  const ui = (
+    <MemoryRouter>
+      <PanelHeader
+        title={title}
+        infoText={infoText}
+        placement={placement}
+      >
+        {children}
+      </PanelHeader>
+    </MemoryRouter>
+  );
+
+  return renderWithProviders(ui);
+}
+
+describe('Testsuite for Panel Header', () => {
+  test('should test the panel header with empty children, title, infoText and placement', async () => {
+    const { utils } = await initPanelheader();
+
+    expect(utils.container.firstChild.lastChild).toBeEmptyDOMElement();
+  });
+  test('should test the panel header when children has a value of type number, title, infoText and placement', async () => {
+    await initPanelheader({children: 123, title: 'Testing Title', infoText: 'Testing InfoText', placement: 'right'});
+    expect(screen.getByRole('heading', {name: /Testing Title/i})).toBeInTheDocument();
+    const buttonNode = screen.getAllByRole('button').find(eachOption => eachOption.getAttribute('data-test') === 'openPageInfo');
+
+    expect(buttonNode).toBeInTheDocument();
+    userEvent.click(buttonNode);
+    expect(screen.getByText(/Testing InfoText/i)).toBeInTheDocument();
+    expect(screen.getByText(/123/i)).toBeInTheDocument();
+  });
+  test('should test the panel header when children has a value of type string, title, infoText and placement', async () => {
+    await initPanelheader({children: 'Testing Children', title: 'Testing Title', infoText: 'Testing InfoText', placement: 'right'});
+    expect(screen.getByRole('heading', {name: /Testing Title/i})).toBeInTheDocument();
+    const buttonNode = screen.getAllByRole('button').find(eachOption => eachOption.getAttribute('data-test') === 'openPageInfo');
+
+    expect(buttonNode).toBeInTheDocument();
+    userEvent.click(buttonNode);
+    expect(screen.getByText(/Testing InfoText/i)).toBeInTheDocument();
+    expect(screen.getByText(/Testing Children/i)).toBeInTheDocument();
+  });
+});
