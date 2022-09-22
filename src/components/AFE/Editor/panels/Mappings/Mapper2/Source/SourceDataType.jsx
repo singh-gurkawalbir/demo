dd83a5755704364998393124f2ee932dcc60d3c5
kiran chandrasekharan <kiranchandrasekharan@Kirans-MacBook-Pro.local>
2022-09-22 20:25:05 +0530

    added default string

--- src/components/AFE/Editor/panels/Mappings/Mapper2/Source/SourceDataType.jsx
+++ src/components/AFE/Editor/panels/Mappings/Mapper2/Source/SourceDataType.jsx
@@ -181,7 +181,7 @@ export default function SourceDataType({
             endIcon={sourceDataTypes && sourceDataTypes.length > 1 ? '' : <ArrowDownFilledIcon />}
             className={classes.dataType} >
             <CeligoTruncate placement="bottom" disableHoverListener lines={1}>
-              {selectedDataTypeLabels.join('')|| 'string'}
+              {selectedDataTypeLabels.join()}
             </CeligoTruncate>
           </TextButton>
         </span>
