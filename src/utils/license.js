f3b5f3ac85721280ff1aa402675bccf6c34bd45c
kiran chandrasekharan <kiranchandrasekharan@Kirans-MacBook-Pro.local>
2022-10-03 17:00:41 +0530

    add id changes

--- src/utils/license.js
+++ src/utils/license.js
@@ -130,6 +130,7 @@ export function platformLicenseActionDetails(license) {
       licenseActionDetails = {
         action: 'startTrial',
         label: 'Get unlimited flows',
+        id: 'unlimited-flows-button',
       };
     }
   } else if (license.tier === 'free') {
@@ -137,18 +138,21 @@ export function platformLicenseActionDetails(license) {
       licenseActionDetails = {
         action: 'upgrade',
         label: 'Request upgrade now',
+        id: 'request-upgrade-buttton',
       };
     } else if (license.status === 'IN_TRIAL') {
       if (license.expiresInDays < 1) {
         licenseActionDetails = {
           action: 'upgrade',
           label: 'Request upgrade now',
+          id: 'request-upgrade-buttton',
         };
       } else {
         licenseActionDetails = {
           action: 'upgrade',
           label: 'Request upgrade now -',
           daysLeft: `${license.expiresInDays} days left`,
+          id: 'request-upgrade-buttton',
         };
         licenseActionDetails.expiresSoon = license.expiresInDays < 10;
       }
@@ -156,11 +160,13 @@ export function platformLicenseActionDetails(license) {
       licenseActionDetails = {
         action: 'startTrial',
         label: 'Get unlimited flows',
+        id: 'unlimited-flows-button',
       };
     } else if (!license.trialEndDate && !license.expires) {
       licenseActionDetails = {
         action: 'startTrial',
         label: 'Get unlimited flows',
+        id: 'unlimited-flows-button',
       };
     }
   }
