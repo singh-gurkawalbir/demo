e72d305d8f7cc83b2c9945d3754422cc5bfa1509
Dileep krishna sai <dileepkrishnasai53@gmail.com>
2022-10-06 17:38:07 +0530

    IO-24582

--- src/views/FlowBuilder/drawers/Hooks/index.test.js
+++ src/views/FlowBuilder/drawers/Hooks/index.test.js
@@ -1,26 +1,29 @@
-/* global describe, test, expect, beforeEach, afterEach, jest */
+/* global describe, test, expect */
 import React from 'react';
-import * as reactRedux from 'react-redux';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { screen } from '@testing-library/react';
 import HooksDrawer from '.';
 import { runServer } from '../../../../test/api/server';
-import { renderWithProviders, reduxStore } from '../../../../test/test-utils';
+import { renderWithProviders } from '../../../../test/test-utils';
 
 async function initHooksDrawer({
   props = {
     flowId: 'flow_id',
     integrationId: 'integration_id',
   },
+  pathname = '/flowBuilder/flow_id/hooks/connections/resource_id',
 } = {}) {
-  const initialStore = reduxStore;
-
-  const ui = (
-    <MemoryRouter>
-      <HooksDrawer {...props} />
+  const { store, utils } = await renderWithProviders((
+    <MemoryRouter
+      initialEntries={[{pathname}]}
+    >
+      <Route
+        path="/flowBuilder/flow_id"
+      >
+        <HooksDrawer {...props} />
+      </Route>
     </MemoryRouter>
-  );
-
-  const { store, utils } = await renderWithProviders(ui, { initialStore });
+  ));
 
   return {
     store,
@@ -30,27 +33,18 @@ async function initHooksDrawer({
 
 describe('HooksDrawer test cases', () => {
   runServer();
-  let mockDispatchFn;
-  let useDispatchSpy;
-
-  beforeEach(() => {
-    useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
-    mockDispatchFn = jest.fn(action => {
-      switch (action.type) {
-        default:
-      }
-    });
-    useDispatchSpy.mockReturnValue(mockDispatchFn);
-  });
-
-  afterEach(() => {
-    useDispatchSpy.mockClear();
-    mockDispatchFn.mockClear();
-  });
 
   test('should pass the initial render with default value', async () => {
-    const { utils } = await initHooksDrawer();
+    await initHooksDrawer();
 
-    expect(utils.container).toBeEmptyDOMElement();
+    expect(screen.queryByText('Hooks')).toBeInTheDocument();
+  });
+
+  test('should pass the initial render with different pathname value', async () => {
+    await initHooksDrawer({
+      pathname: '/flowBuilder/flow_id',
+    });
+
+    expect(screen.queryByText('Hooks')).not.toBeInTheDocument();
   });
 });
