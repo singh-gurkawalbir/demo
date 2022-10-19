d436a29355d3f517a722617d9a7337d6562b082b
Lalit Agrawal <lalitagrawal24seven@gmail.com>
2022-10-19 23:16:23 +0530

    drawer/flowStepDebugLogs

---
+++ src/components/drawer/FlowStepDebugLogs/LogsDrawerActions.test.js
@@ -0,0 +1,192 @@
+/* global describe, test, expect,beforeEach,afterEach, jest */
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as reactRedux from 'react-redux';
+import { renderWithProviders, reduxStore } from '../../../test/test-utils';
+import LogsDrawerActions from './LogsDrawerActions';
+import actions from '../../../actions';
+
+const props = {
+  flowId: 'random_mock_flowId',
+  resourceType: 'imports',
+  resourceId: 'random_resource_id_mock',
+};
+
+async function initLogsDrawerActions({props,
+  logsStatus = 'recieved',
+  logsSummary = [{
+    key: 'randomActiveLogKey',
+    stage: 'import',
+  },
+  {
+    key: 'randomActiveLogKey',
+    stage: 'import',
+  },
+  {
+    key: 'randomActiveLogKey',
+    stage: 'import',
+  },
+  {
+    key: 'randomActiveLogKey',
+    stage: 'import',
+  },
+  {
+    key: 'randomActiveLogKey',
+    stage: 'import',
+  }],
+  hasNewLogs = false}) {
+  const initialStore = reduxStore;
+
+  initialStore.getState().session.logs.flowStep = {
+    random_resource_id_mock: {
+      logsStatus,
+      logsSummary,
+      loadMoreStatus: 'received',
+      hasNewLogs,
+      fetchStatus: 'completed',
+      activeLogKey: 'randomActiveLogKey',
+      nextPageURL: '/v1(api)/flows/:_flowId',
+    },
+  };
+
+  initialStore.getState().session.filters = {
+    flowStepLogs: { paging: {currPage: 1},
+    },
+  };
+
+  initialStore.getState().user = {
+    preferences: {
+      defaultAShareId: '_Id',
+      accounts: { shareMockId: {}},
+    },
+    org: {accounts: [
+      { _id: '_Id',
+        accepted: true,
+        accessLevel: 'manage',
+        integrationAccessLevel: [],
+      }],
+    }};
+
+  const ui = (
+    <MemoryRouter>
+      <LogsDrawerActions {...props} />
+    </MemoryRouter>
+  );
+
+  const { store, utils } = renderWithProviders(ui, { initialStore });
+
+  return {
+    store,
+    utils,
+  };
+}
+jest.mock('../../../utils/flowStepLogs', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../utils/flowStepLogs'),
+  DEFAULT_ROWS_PER_PAGE: 2,
+}));
+
+jest.mock('../../FetchProgressIndicator', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../FetchProgressIndicator'),
+  default: props => {
+    const {pauseHandler: onPause, resumeHandler: onResume } = props;
+
+    return (
+      <>
+        <button type="button" onClick={onPause}>Pause</button>
+        <button type="button" onClick={onResume}>Resume</button>
+      </>
+    );
+  },
+}));
+
+jest.mock('../../StartDebugEnhanced', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../StartDebugEnhanced'),
+  default: props => {
+    const {startDebugHandler: onClick, stopDebugHandler: onCancel } = props;
+
+    return (
+      <>
+        <button type="button" onClick={() => onClick(15)}>Start debug</button>
+        <button type="button" onClick={onCancel}>Stop debug</button>
+      </>
+    );
+  },
+}));
+
+describe('LogsDrawerActions tests', () => {
+  let mockDispatchFn;
+  let useDispatchSpy;
+
+  beforeEach(() => {
+    useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
+    mockDispatchFn = jest.fn();
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+  });
+
+  afterEach(() => {
+    useDispatchSpy.mockClear();
+    mockDispatchFn.mockClear();
+  });
+
+  test('Should able to pass initial render with default values having logs > 0 and navigate to next and previous Page', async () => {
+    await initLogsDrawerActions({props});
+    expect(screen.queryByText(/3 - 4 of 5+/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Start debug/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Refresh logs/i)).toBeInTheDocument();
+    const buttons = screen.queryAllByRole('button');
+    const nextPage = buttons.find(button => button.getAttribute('data-testid') === 'nextPage');
+
+    expect(nextPage).toBeInTheDocument();
+    userEvent.click(nextPage);
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(1, actions.logs.flowStep.request({flowId: props.flowId, resourceId: props.resourceId, loadMore: true}));
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(2, actions.patchFilter('flowStepLogs', {paging: {currPage: 2}}));
+  });
+  test('Should able to pass initial render with logsCount = 0 And start & stop debug', async () => {
+    await initLogsDrawerActions({props,
+      logsSummary: []});
+    expect(screen.queryByText(/3 - 4 of 5+/i)).not.toBeInTheDocument();
+    const startDebug = screen.queryByText(/Start debug/i);
+    const stopDebug = screen.queryByText(/Stop debug/i);
+
+    expect(startDebug).toBeInTheDocument();
+    expect(stopDebug).toBeInTheDocument();
+    userEvent.click(startDebug);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.logs.flowStep.startDebug(props.flowId, props.resourceId, props.resourceType, 15));
+    userEvent.click(stopDebug);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.logs.flowStep.stopDebug(props.flowId, props.resourceId, props.resourceType));
+  });
+
+  test('Should able to pass render with logsCount = 0 And pause & Resume debug logs', async () => {
+    await initLogsDrawerActions({props,
+      logsSummary: []});
+    const pauseFetch = screen.queryByText(/Pause/i);
+    const resumeFetch = screen.queryByText(/Resume/i);
+
+    expect(pauseFetch).toBeInTheDocument();
+    expect(resumeFetch).toBeInTheDocument();
+    userEvent.click(pauseFetch);
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(1, actions.logs.flowStep.setFetchStatus(props.resourceId, 'paused'));
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(2, actions.logs.flowStep.pauseFetch(props.flowId, props.resourceId));
+    userEvent.click(resumeFetch);
+    expect(mockDispatchFn).toHaveBeenCalledWith(actions.logs.flowStep.request({flowId: props.flowId, resourceId: props.resourceId, loadMore: true}));
+  });
+
+  test('Should able to pass render with logsCount > 0, status requested and hasNewLogs', async () => {
+    await initLogsDrawerActions({props,
+      logsStatus: 'requested',
+      hasNewLogs: true,
+    });
+    expect(screen.queryByText(/3 - 4 of 5+/i)).not.toBeInTheDocument();
+    const refreshLogs = screen.getByText(/Refresh logs/i);
+
+    expect(refreshLogs).toBeInTheDocument();
+    userEvent.click(refreshLogs);
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(1, actions.clearFilter('flowStepLogs'));
+    expect(mockDispatchFn).toHaveBeenNthCalledWith(2, actions.logs.flowStep.request({flowId: props.flowId, resourceId: props.resourceId, loadMore: undefined}));
+  });
+});
