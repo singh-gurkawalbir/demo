a7b834abc8f0c8156ef9e96e2160f55efee8115d
Lalit Agrawal <lalitagrawal24seven@gmail.com>
2022-10-21 00:32:38 +0530

    configConnectionDebugger

--- src/components/drawer/FlowStepDebugLogs/LogsDrawerActions.test.js
+++ src/components/drawer/FlowStepDebugLogs/LogsDrawerActions.test.js
@@ -4,9 +4,16 @@ import { MemoryRouter } from 'react-router-dom';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import * as reactRedux from 'react-redux';
+import moment from 'moment';
 import { renderWithProviders, reduxStore } from '../../../test/test-utils';
 import LogsDrawerActions from './LogsDrawerActions';
 import actions from '../../../actions';
+
+const mockDate = new Date('2022-10-20');
+const mockTime = {
+  startDate: moment(new Date('2022-10-20')).startOf('day').toDate(),
+  endDate: moment(new Date('2022-10-20')).endOf('day').toDate(),
+};
 
 const props = {
   flowId: 'random_mock_flowId',
@@ -36,7 +43,8 @@ async function initLogsDrawerActions({props,
     key: 'randomActiveLogKey',
     stage: 'import',
   }],
-  hasNewLogs = false}) {
+  hasNewLogs = false,
+  time = {}}) {
   const initialStore = reduxStore;
 
   initialStore.getState().session.logs.flowStep = {
@@ -46,6 +54,7 @@ async function initLogsDrawerActions({props,
       loadMoreStatus: 'received',
       hasNewLogs,
       fetchStatus: 'completed',
+      currQueryTime: mockDate.getTime(),
       activeLogKey: 'randomActiveLogKey',
       nextPageURL: '/v1(api)/flows/:_flowId',
     },
@@ -53,6 +62,10 @@ async function initLogsDrawerActions({props,
 
   initialStore.getState().session.filters = {
     flowStepLogs: { paging: {currPage: 1},
+      codes: [
+        'not_all',
+      ],
+      time,
     },
   };
 
@@ -121,8 +134,12 @@ jest.mock('../../StartDebugEnhanced', () => ({
 describe('LogsDrawerActions tests', () => {
   let mockDispatchFn;
   let useDispatchSpy;
+  let dateSpy;
 
   beforeEach(() => {
+    jest.useFakeTimers();
+    dateSpy = jest.spyOn(global.Date, 'now').mockImplementation(() => mockDate);
+
     useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
     mockDispatchFn = jest.fn();
     useDispatchSpy.mockReturnValue(mockDispatchFn);
@@ -131,6 +148,7 @@ describe('LogsDrawerActions tests', () => {
   afterEach(() => {
     useDispatchSpy.mockClear();
     mockDispatchFn.mockClear();
+    dateSpy.mockRestore();
   });
 
   test('Should able to pass initial render with default values having logs > 0 and navigate to next and previous Page', async () => {
@@ -163,7 +181,8 @@ describe('LogsDrawerActions tests', () => {
 
   test('Should able to pass render with logsCount = 0 And pause & Resume debug logs', async () => {
     await initLogsDrawerActions({props,
-      logsSummary: []});
+      logsSummary: [],
+      time: mockTime});
     const pauseFetch = screen.queryByText(/Pause/i);
     const resumeFetch = screen.queryByText(/Resume/i);
 
