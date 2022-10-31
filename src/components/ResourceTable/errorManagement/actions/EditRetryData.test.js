05d9430b3d8ad87a42262145dfae67297298a0ce
Yashveer Singh <yashveersinghvns2@gmail.com>
2022-10-31 13:02:40 +0530

    IO-29620

---
+++ src/components/ResourceTable/errorManagement/actions/EditRetryData.test.js
@@ -0,0 +1,79 @@
+/* global test, expect, describe, jest */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { renderWithProviders, reduxStore } from '../../../../test/test-utils';
+import metadata from '../resolvedErrors/metadata';
+import CeligoTable from '../../../CeligoTable';
+
+const mockDispatch = jest.fn();
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+jest.mock('react-redux', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+
+const initialStore = reduxStore;
+
+initialStore.getState().data.resources.exports = [{
+  _id: 'resourceId',
+  adaptorType: 'NetSuiteExport',
+}];
+
+function renderFuntion(actionProps, data, errorType) {
+  renderWithProviders(
+    <MemoryRouter initialEntries={[`/${errorType}`]}>
+      <Route path="/:errorType">
+        <CeligoTable
+          actionProps={actionProps}
+          {...metadata}
+          data={
+                       [
+                         data,
+                       ]
+                      }
+              />
+      </Route>
+    </MemoryRouter>,
+    {initialStore}
+  );
+  userEvent.click(screen.getByRole('button', {name: /more/i}));
+}
+
+describe('Error Management Retry UI tests ', () => {
+  test('should redirect to edit Retry page when errot type is close', () => {
+    renderFuntion({resourceId: 'resourceId'}, {retryDataKey: 'somereqAndResKey', errorId: 'someerrorId'}, 'close');
+    const editRetry = screen.getByText('Edit retry data');
+
+    expect(editRetry).toBeInTheDocument();
+    userEvent.click(editRetry);
+    expect(mockHistoryPush).toHaveBeenCalledWith('/close/details/someerrorId/editRetry');
+  });
+  test('should redirect to edit Retry page and make dispatch call when error type is open', () => {
+    renderFuntion({resourceId: 'resourceId'}, {retryDataKey: 'somereqAndResKey', errorId: 'someerrorId'}, 'open');
+    const editRetry = screen.getByText('Edit retry data');
+
+    expect(editRetry).toBeInTheDocument();
+    userEvent.click(editRetry);
+    expect(mockHistoryPush).toHaveBeenCalledWith('/open/details/someerrorId/editRetry');
+    expect(mockDispatch).toHaveBeenCalledWith(
+      {filter: {activeErrorId: 'someerrorId'}, name: 'openErrors', type: 'PATCH_FILTER'}
+    );
+  });
+  test('should have enable title when flow is disabled', () => {
+    renderFuntion({resourceId: 'resourceId', isFlowDisabled: true}, {retryDataKey: 'somereqAndResKey', errorId: 'someerrorId'}, 'open');
+    const editRetry = screen.getByTitle('Enable the flow to edit retry data');
+
+    expect(editRetry).toBeInTheDocument();
+    expect(editRetry.textContent).toBe('Edit retry data');
+  });
+});
---
+++ src/components/ResourceTable/errorManagement/actions/Retry.test.js
@@ -0,0 +1,97 @@
+/* global test, expect, describe, jest */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import { renderWithProviders } from '../../../../test/test-utils';
+// import obj from './metadata';
+import metadata from '../resolvedErrors/metadata';
+import CeligoTable from '../../../CeligoTable';
+import actions from '../../../../actions';
+import { ConfirmDialogProvider } from '../../../ConfirmDialog';
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+
+function renderFuntion(actionProps, data) {
+  renderWithProviders(
+    <ConfirmDialogProvider>
+      <MemoryRouter>
+        <CeligoTable
+          actionProps={actionProps}
+          {...metadata}
+          data={
+                       [
+                         data,
+                       ]
+                      }
+              />
+      </MemoryRouter>
+    </ConfirmDialogProvider>
+  );
+  userEvent.click(screen.getByRole('button', {name: /more/i}));
+}
+
+describe('Error Management Retry UI tests ', () => {
+  test('should show "Enable the flow to retry" message when flow is disabled', () => {
+    renderFuntion({isFlowDisabled: true}, {retryDataKey: 'someKey'});
+    userEvent.click(screen.getByRole('button', {name: /more/i}));
+    const retry = screen.getByTitle('Enable the flow to retry');
+
+    expect(retry).toBeInTheDocument();
+    expect(retry.textContent).toBe('Retry');
+  });
+  test('should show make dispatch call for retry', () => {
+    renderFuntion({
+      isFlowDisabled: false,
+      isResolved: false,
+      flowId: 'someflowId',
+      resourceId: 'someresourceId',
+    }, {retryDataKey: 'someKey'});
+    userEvent.click(screen.getByRole('button', {name: /more/i}));
+    const retry = screen.getByText('Retry');
+
+    expect(retry).toBeInTheDocument();
+    userEvent.click(retry);
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.errorManager.flowErrorDetails.retry(
+        {
+          flowId: 'someflowId',
+          resourceId: 'someresourceId',
+          retryIds: ['someKey'],
+          isResolved: false,
+        }
+      )
+    );
+  });
+  test('should show "Enable the flow to retry" mergessage when flow is disabled', () => {
+    renderFuntion({
+      isFlowDisabled: false,
+      isResolved: true,
+      flowId: 'someflowId',
+      resourceId: 'someresourceId',
+    }, {retryDataKey: 'someKey'});
+    userEvent.click(screen.getByRole('button', {name: /more/i}));
+    const retry = screen.getByText('Retry');
+
+    expect(retry).toBeInTheDocument();
+    userEvent.click(retry);
+    expect(screen.getByText('Confirm retry')).toBeInTheDocument();
+    userEvent.click(screen.getByText('Retry'));
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.errorManager.flowErrorDetails.retry(
+        {
+          flowId: 'someflowId',
+          resourceId: 'someresourceId',
+          retryIds: ['someKey'],
+          isResolved: true,
+        }
+      )
+    );
+  });
+});
