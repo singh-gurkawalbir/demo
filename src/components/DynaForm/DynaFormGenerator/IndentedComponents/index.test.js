5c766bbde48f4a34b4ac242a70b6f2cea310e9ec
PrashanthNesa <prashanthkumar.nesa@gmail.com>
2022-11-03 10:17:31 +0530

    29761

---
+++ src/components/DynaForm/DynaFormGenerator/IndentedComponents/index.test.js
@@ -0,0 +1,159 @@
+/* global describe, test, expect */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import IndentedComponent from '.';
+import { renderWithProviders } from '../../../../test/test-utils';
+import { getCreatedStore } from '../../../../store';
+
+const initialStore = getCreatedStore();
+
+function initIndentedComponent(props = {}) {
+  initialStore.getState().data.resources = {scripts: [{
+    _id: '5ff687fa4f59bb348d41b332',
+    lastModified: '2022-10-06T16:26:46.934Z',
+    createdAt: '2021-01-07T04:03:06.229Z',
+    name: 'test',
+  }]};
+  initialStore.getState().session.form = {'scripts-5ff687fa4f59bb348d41b332': {fields: props.formfields, validationOnSaveIdentifier: true}};
+
+  return renderWithProviders(<IndentedComponent {...props} />, {initialStore});
+}
+
+const props = {
+  classes: {
+    container: 'class-containers',
+    child: 'class-child',
+  },
+  containers: [
+    {
+      collapsed: false,
+      label: 'General',
+      header: 'header1',
+      helpKey: 'help content1',
+      fields: [
+        'name',
+        'description',
+      ],
+    },
+    {
+      collapsed: false,
+      label: 'Script content',
+      header: 'header2',
+      helpKey: 'help content2',
+      fields: [
+        'insertFunction',
+        'content',
+      ],
+    },
+  ],
+  fieldMap: {
+    name: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'text',
+      label: 'Name',
+      required: true,
+      fieldId: 'name',
+      id: 'name',
+      defaultValue: 'test',
+    },
+    description: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'text',
+      label: 'Description',
+      fieldId: 'description',
+      id: 'description',
+      defaultValue: '',
+    },
+    insertFunction: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'select',
+      label: 'Insert function stub',
+      options: [
+        {
+          items: [
+            {
+              label: 'Pre save page',
+              value: 'preSavePage',
+            },
+            {
+              label: 'Pre map',
+              value: 'preMap',
+            },
+          ],
+        },
+      ],
+      fieldId: 'insertFunction',
+      id: 'insertFunction',
+      defaultValue: '',
+    },
+    content: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      defaultValue: {
+        _scriptId: '5ff687fa4f59bb348d41b332',
+        function: 'main',
+      },
+      type: 'scriptcontent',
+      label: 'Edit content',
+      fieldId: 'content',
+      refreshOptionsOnChangesTo: [
+        'insertFunction',
+      ],
+      id: 'content',
+    },
+  },
+  layout: {
+    type: 'collapse',
+    fields: [
+      'name',
+      'description',
+    ],
+    containers: [
+      {
+        collapsed: false,
+        label: 'General',
+        fields: [
+          'name',
+          'description',
+        ],
+      },
+      {
+        collapsed: false,
+        label: 'Script content',
+        fields: [
+          'insertFunction',
+          'content',
+        ],
+      },
+    ],
+  },
+  formKey: 'scripts-5ff687fa4f59bb348d41b332',
+  resourceType: 'scripts',
+  resourceId: '5ff687fa4f59bb348d41b332',
+};
+
+describe('Collapsed Components UI tests', () => {
+  test('should pass the initial render', () => {
+    props.formfields = [{visible: true, required: true, id: 'name', isValid: false}, {visible: 'true', required: true, id: 'description', isValid: false}, {visible: 'true', required: true, id: 'content', isValid: false}, {visible: 'true', required: true, id: 'insertFunction', isValid: false}];
+    initIndentedComponent(props);
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    expect(screen.getByText('header1')).toBeInTheDocument();
+    expect(screen.getByText('header2')).toBeInTheDocument();
+  });
+  test('should render empty html div when no containers are passed in the props', () => {
+    props.containers = [];
+    const {utils} = initIndentedComponent(props);
+
+    screen.debug();
+
+    expect(utils.container).toContainHTML('<div></div>');
+  });
+});
---
+++ src/components/DynaForm/DynaFormGenerator/TabComponent/index.test.js
@@ -0,0 +1,192 @@
+/* global describe, test, expect, */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import * as Components from '.';
+import { renderWithProviders } from '../../../../test/test-utils';
+
+const props = {
+  containers: [
+    {
+      type: 'collapsed',
+      collapsed: false,
+      label: 'General',
+      fields: [
+        'name',
+        'description',
+      ],
+    },
+    {
+      type: 'collapsed',
+      collapsed: false,
+      label: 'Script content',
+      fields: [
+        'insertFunction',
+        'content',
+      ],
+    },
+  ],
+  fieldMap: {
+    name: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'text',
+      label: 'Name',
+      required: true,
+      fieldId: 'name',
+      id: 'name',
+      defaultValue: 'test',
+    },
+    description: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'text',
+      label: 'Description',
+      fieldId: 'description',
+      id: 'description',
+      defaultValue: '',
+    },
+    insertFunction: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      type: 'select',
+      label: 'Insert function stub',
+      options: [
+        {
+          items: [
+            {
+              label: 'Pre save page',
+              value: 'preSavePage',
+            },
+            {
+              label: 'Pre map',
+              value: 'preMap',
+            },
+          ],
+        },
+      ],
+      fieldId: 'insertFunction',
+      id: 'insertFunction',
+      defaultValue: '',
+    },
+    content: {
+      resourceId: '5ff687fa4f59bb348d41b332',
+      resourceType: 'scripts',
+      isLoggable: true,
+      defaultValue: {
+        _scriptId: '5ff687fa4f59bb348d41b332',
+        function: 'main',
+      },
+      type: 'scriptcontent',
+      label: 'Edit content',
+      fieldId: 'content',
+      refreshOptionsOnChangesTo: [
+        'insertFunction',
+      ],
+      id: 'content',
+    },
+  },
+  layout: {
+    type: 'collapse',
+    containers: [
+      {
+        collapsed: false,
+        label: 'General',
+        fields: [
+          'name',
+          'description',
+        ],
+      },
+      {
+        collapsed: false,
+        label: 'Script content',
+        fields: [
+          'insertFunction',
+          'content',
+        ],
+      },
+    ],
+  },
+  formKey: 'scripts-5ff687fa4f59bb348d41b332',
+  resourceType: 'scripts',
+  resourceId: '5ff687fa4f59bb348d41b332',
+};
+
+// jest.mock('..', () => ({
+//   __esModule: true,
+//   ...jest.requireActual('..'),
+//   default: () => <div>Form Fragment</div>,
+// }));
+
+describe('BoxComponents UI tests', () => {
+  test('should pass the initial render for simple Tab component', () => {
+    const Component = Components.TabComponentSimple;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  TabIAComponent', () => {
+    const Component = Components.TabIAComponent;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    expect(screen.getByText('Save')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  TabIAComponent', () => {
+    const Component = Components.TabIAComponent;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    expect(screen.getByText('Save')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  SuiteScriptTabIACompleteSave Component', () => {
+    const Component = Components.SuiteScriptTabIACompleteSave;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    expect(screen.getByText('Save')).toBeInTheDocument();
+    userEvent.click(screen.getByText('General'));
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  TabComponentWithoutSave Component', () => {
+    const Component = Components.TabComponentWithoutSave;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  TabComponentWithoutSave Component', () => {
+    const Component = Components.TabComponentWithoutSave;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+  test('should pass the initial render for  TabComponentWithoutSaveVertical Component', () => {
+    const Component = Components.TabComponentWithoutSaveVertical;
+
+    renderWithProviders(<Component {...props} />);
+
+    expect(screen.getByText('General')).toBeInTheDocument();
+    expect(screen.getByText('Script content')).toBeInTheDocument();
+    screen.debug(null, Infinity);
+  });
+});
