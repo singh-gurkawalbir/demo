483de6e3a583d4b7ec23dc988a1c477c687434ae
Lalit Agrawal <lalitagrawal24seven@gmail.com>
2022-11-03 17:55:23 +0530

    added hooks tests for revisions

---
+++ src/components/drawer/Revisions/hooks/useHandleInvalidNewRevision.test.js
@@ -0,0 +1,45 @@
+/* global describe, test, expect,afterEach jest */
+import React from 'react';
+import { renderWithProviders, reduxStore } from '../../../../test/test-utils';
+import useHandleInvalidNewRevision from './useHandleInvalidNewRevision';
+
+const mockHistoryReplace = jest.fn();
+const props = { integrationId: '_integrationId', revisionId: '_revId', parentUrl: '/parentUrl' };
+
+const MockComponent = props => {
+  useHandleInvalidNewRevision(props);
+
+  return <></>;
+};
+
+async function inituseHandleInvalidNewRevision(props = {}) {
+  const initialStore = reduxStore;
+
+  initialStore.getState().session.lifeCycleManagement.revision = {
+    _integrationId: props.revisionId ? {
+      _revId: {},
+    } : undefined,
+  };
+
+  return renderWithProviders(<MockComponent {...props} />, {initialStore});
+}
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    replace: mockHistoryReplace,
+  }),
+}));
+describe('useHandleInvalidNewRevision tests', () => {
+  afterEach(() => {
+    mockHistoryReplace.mockClear();
+  });
+  test('Should able to test the hook with valid new revision', async () => {
+    await inituseHandleInvalidNewRevision(props);
+    expect(mockHistoryReplace).not.toHaveBeenCalledWith('/parentUrl');
+  });
+  test('Should able to test the hook with invalid new revision', async () => {
+    await inituseHandleInvalidNewRevision({...props, revisionId: undefined});
+    expect(mockHistoryReplace).toHaveBeenCalledWith('/parentUrl');
+  });
+});
