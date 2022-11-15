8a58f485084f06a2be4e26f9372da986c38bf966
Pooja Reddy Kadari <poojareddykadari0207@gmail.com>
2022-11-15 14:14:25 +0530

    fixes

--- src/components/ResourceTable/auditLog/utils.test.jsx
+++ src/components/ResourceTable/auditLog/utils.test.jsx
@@ -52,14 +52,14 @@ describe('UI test cases for utils', () => {
 
     expect(result).toBe(false);
   });
-  test('should return false provided old value stringified object length is less than minimum stringified object length to show diff link, new value is not provided', () => {
+  test('should return true provided old value stringified object length is less than minimum stringified object length to show diff link, new value is not provided', () => {
     oldValue = dataobj;
     expect(JSON.stringify(oldValue).length).toBeGreaterThan(10);
     const result = hasLongLength(oldValue);
 
     expect(result).toBe(true);
   });
-  test('should return true provided old value stringified object length is less than minimum stringified object length to show diff link, new value stringified object length is greater than minimum stringified object length', () => {
+  test('should return true provided old value stringified object length is greater than minimum stringified object length to show diff link, new value stringified object length is greater than minimum stringified object length', () => {
     oldValue = dataobj;
     newValue = dataobj;
     expect(JSON.stringify(oldValue).length).toBeGreaterThan(10);
@@ -68,11 +68,30 @@ describe('UI test cases for utils', () => {
 
     expect(result).toBe(true);
   });
-  test('should return true provided old value string length is less than minimum string length to show diff link,', () => {
+  test('should return false provided old value string length is less than minimum string length to show diff link,', () => {
     oldValue = 'Click to view';
     expect(oldValue.length).toBeLessThan(300);
     const result = hasLongLength(oldValue);
 
     expect(result).toBe(false);
+  });
+  test('should return false provided old value stringified object length is less than minimum stringified object length to show diff link, new value stringified object length is less than minimum stringified object length', () => {
+    oldValue = {id: 1};
+    newValue = {id: 2};
+    expect(JSON.stringify(oldValue).length).toBeLessThan(10);
+    expect(JSON.stringify(newValue).length).toBeLessThan(10);
+    const result = hasLongLength(oldValue, oldValue);
+
+    expect(result).toBe(false);
+  });
+
+  test('should return true provided old value string length is greater than minimum string length to show diff link, new value object length is greater than minimum stringified object length', () => {
+    oldValue = 'We believe that by automating processes across applications, we are freeing up your time to do more in your organization. We believe that reducing manual processes empowers you to focus on growing your business. We are passionate about solving integration challenges and believe that integration should be easy and simple for both technical and business users.';
+    newValue = dataobj;
+    expect(oldValue.length).toBeGreaterThan(300);
+    expect(JSON.stringify(newValue).length).toBeGreaterThan(10);
+    const result = hasLongLength(oldValue, newValue);
+
+    expect(result).toBe(true);
   });
 });
