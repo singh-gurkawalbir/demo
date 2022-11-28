673e4bde1dedf9740ca85147ed46ce7af236af6e
Sravan Dandra <sravandandra@users.noreply.github.com>
2022-11-28 21:15:03 +0530

    IO-30179 (#6176)
    * IO-30179
    
    * IO=30306
    
    * Revert "IO=30306"
    
    This reverts commit feef2d3dab75a583e30503c63c0d6ae0b9d3fbcd.

--- src/components/AFE/Editor/panels/Router/BranchFilter/index.jsx
+++ src/components/AFE/Editor/panels/Router/BranchFilter/index.jsx
@@ -860,7 +860,7 @@ export default function BranchFilter({ editorId, position }) {
         // eslint-disable-next-line camelcase
         const isSingleInputOperator = !state.rule?.operator?.nb_inputs;
 
-        if (state.data.rhs.value || isSingleInputOperator) return;
+        if (state.data.rhs.type !== 'value' || state.data.rhs.value || isSingleInputOperator) return;
 
         const $emptyRule = state.rule;
 
