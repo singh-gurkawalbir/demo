3f821a4a60fc9d506ca92122e1aacc5b4490fd9d
Yashveer Singh <101522932+YashveerSinghgit@users.noreply.github.com>
2022-12-01 10:40:39 +0530

    IO-30184 (#6177)

---
+++ src/components/DynaForm/fields/DynaUserEmail.test.js
@@ -0,0 +1,58 @@
+/* global describe, test, expect, jest, afterEach */
+import React from 'react';
+import {screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import DynaUserEmail from './DynaUserEmail';
+import { renderWithProviders} from '../../../test/test-utils';
+import actions from '../../../actions';
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+
+function initDynaUserEmail(props = {}) {
+  const ui = (
+    <DynaUserEmail
+      {...props}
+      />
+  );
+
+  return renderWithProviders(ui);
+}
+
+describe('DynaUserEmail UI test cases', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test('should show the modal dialog when clicked on action button', () => {
+    initDynaUserEmail({label: 'PropsLabel', value: 'PropsValue', readOnly: false});
+    expect(screen.getByText('PropsLabel'));
+    expect(screen.getByRole('textbox')).toBeDisabled();
+    userEvent.click(screen.getByRole('button'));
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.api.clearComms()
+    );
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+    userEvent.click(screen.getByTestId('closeModalDialog'));
+    expect(screen.queryByRole('dialog')).toBeInTheDocument();
+  });
+  test('should not have action button when read only mode is avaiable', () => {
+    initDynaUserEmail({label: 'PropsLabel', value: 'PropsValue', readOnly: true});
+
+    expect(screen.getByText('PropsLabel'));
+    expect(screen.getByRole('textbox')).toBeDisabled();
+    expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+  test('should show help text', () => {
+    initDynaUserEmail({label: 'PropsLabel', value: 'PropsValue', readOnly: true, helpText: 'Provided help text'});
+
+    expect(screen.getByText('PropsLabel'));
+    expect(screen.getByRole('textbox')).toBeDisabled();
+    userEvent.click(screen.getByRole('button'));
+    expect(screen.getByText('Provided help text')).toBeInTheDocument();
+  });
+});
