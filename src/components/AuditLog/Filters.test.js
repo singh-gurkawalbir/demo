ad2f0e5054266d5eb5c96fbae2cb54815d7d2da8
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2022-12-13 18:53:24 +0530

    UX test cases

--- src/components/AuditLog/Filters.test.js
+++ src/components/AuditLog/Filters.test.js
@@ -35,7 +35,7 @@ initialStore.getState().data.audit = {
   },
 };
 initialStore.getState().session.filters = {
-  auditLogs: {
+  'integrations-6253af74cddb8a1ba550a010-auditLogs': {
     paging: {
       rowsPerPage: 50,
       currPage: 0,
