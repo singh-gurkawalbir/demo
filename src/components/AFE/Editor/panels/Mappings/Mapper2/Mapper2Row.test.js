76dfa3ee155e9416ea000a1a8da7d2594282f979
Yashveer Singh <101522932+YashveerSinghgit@users.noreply.github.com>
2022-12-23 14:12:39 +0530

    IO-30749 (#6353)
    * IO-30749
    
    * IO-30749
    
    * review comments

---
+++ src/components/AFE/Editor/panels/Mappings/Mapper2/Mapper2Row.test.js
@@ -0,0 +1,231 @@
+/* global describe, test, expect, jest */
+import React from 'react';
+import { screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import {renderWithProviders, reduxStore} from '../../../../../../test/test-utils';
+import actions from '../../../../../../actions';
+import Mapper2Row from './Mapper2Row';
+import { ConfirmDialogProvider } from '../../../../../ConfirmDialog';
+
+const mockDispatch = jest.fn();
+
+jest.mock('react-redux', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-redux'),
+  useDispatch: () => mockDispatch,
+}));
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+    location: {pathname: 'intialURL'},
+  }),
+}));
+
+jest.mock('../../../../../icons/StaticLookupIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/StaticLookupIcon'),
+  default: () => <div>StaticLookupIcon</div>,
+}));
+
+jest.mock('../../../../../icons/DynamicLookupIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/DynamicLookupIcon'),
+  default: () => <div>DynamicLookupIcon</div>,
+}));
+
+jest.mock('../../../../../icons/HardCodedIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/HardCodedIcon'),
+  default: () => <div>HardCodedIcon</div>,
+}));
+
+jest.mock('../../../../../icons/HandlebarsExpressionIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/HandlebarsExpressionIcon'),
+  default: () => <div>HandlebarsExpressionIcon</div>,
+}));
+
+jest.mock('../../../../../icons/TrashIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/TrashIcon'),
+  default: () => <div>TrashIcon</div>,
+}));
+
+jest.mock('../../../../../icons/AddIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/AddIcon'),
+  default: () => <div>AddIcon</div>,
+}));
+
+jest.mock('../../../../../icons/SettingsIcon', () => ({
+  __esModule: true,
+  ...jest.requireActual('../../../../../icons/SettingsIcon'),
+  default: () => <div>SettingsIcon</div>,
+}));
+
+jest.mock('./TabbedRow', () => ({
+  __esModule: true,
+  ...jest.requireActual('./TabbedRow'),
+  default: () => <div>Mock Tabbed Row</div>,
+}));
+
+jest.mock('./Destination/Mapper2Generates', () => ({
+  __esModule: true,
+  ...jest.requireActual('./Destination/Mapper2Generates'),
+  default: props => <input disabled={props.disabled} onChange={e => props.onBlur(e.target.value)} data-testid="fieldMappingGenerate" type="text" />,
+}));
+
+jest.mock('./Source/Mapper2ExtractsTypeableSelect', () => ({
+  __esModule: true,
+  ...jest.requireActual('./Source/Mapper2ExtractsTypeableSelect'),
+  default: props => <input disabled={props.disabled} onChange={e => props.onBlur(e.target.value, 'somejsonpath')} data-testid="mapper2ExtractsTypeableSelect" type="text" />,
+}));
+
+const initialStore = reduxStore;
+
+function initMapper2Row(props = {}, initialStore) {
+  const ui = (
+    <ConfirmDialogProvider>
+      <Mapper2Row
+        {...props}
+      />
+    </ConfirmDialogProvider>
+  );
+
+  return renderWithProviders(ui, initialStore);
+}
+
+describe('Mapper2Row UI test cases ', () => {
+  test('should not show mapping row when hidden props is provided', () => {
+    const {utils} = initMapper2Row({hidden: true});
+
+    expect(utils.container).toBeEmptyDOMElement();
+  });
+  test('should show tabs rows when isTabNode prop is provided', () => {
+    initMapper2Row({isTabNode: true});
+
+    expect(screen.getByText('Mock Tabbed Row')).toBeInTheDocument();
+  });
+  test('should make dispatch call when generate field is changed', () => {
+    initMapper2Row();
+
+    const generateInput = screen.getByTestId('fieldMappingGenerate');
+
+    userEvent.type(generateInput, 'sometext');
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.patchField('generate', undefined, 'sometext')
+    );
+  });
+  test('should make dispatch call when extract field is changed', () => {
+    initMapper2Row({});
+
+    const typableExtractInput = screen.getByTestId('mapper2ExtractsTypeableSelect');
+
+    userEvent.type(typableExtractInput, 'sometext');
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.patchField('extract', undefined, 'sometext', undefined, 'somejsonpath')
+    );
+  });
+  test('should make dispatch call to add new row when add button is clicked', () => {
+    const nodeKey = 'somenodeey';
+
+    initMapper2Row({nodeKey});
+
+    const addButton = screen.getByText('AddIcon');
+
+    userEvent.click(addButton);
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.addRow(nodeKey)
+    );
+  });
+  test('should click on trash icon and delete row for rows having no child', () => {
+    const nodeKey = 'somenodekey';
+
+    initMapper2Row({nodeKey});
+
+    const deleteButton = screen.getByText('TrashIcon');
+
+    userEvent.click(deleteButton);
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.deleteRow(nodeKey)
+    );
+  });
+  test('should show dialog box on clicking trashicon and make dispatch call for deleting node', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    const deleteButton = screen.getByText('TrashIcon');
+
+    userEvent.click(deleteButton);
+
+    expect(screen.getByRole('dialog')).toBeInTheDocument();
+    userEvent.click(screen.getByText('Delete'));
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.deleteRow(nodeKey)
+    );
+  });
+  test('should redirect to setting page when setting icon is clicked', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row generate={[]} nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    const settingsIcon = screen.getByText('SettingsIcon');
+
+    userEvent.click(settingsIcon);
+    expect(mockHistoryPush).toHaveBeenCalledWith('intialURL/settings/v2/somenodekey/');
+    expect(mockDispatch).toHaveBeenCalledWith(
+      actions.mapping.v2.updateActiveKey(nodeKey)
+    );
+  });
+  test('should show static Look UP icon when mapping is dynamic look up ', () => {
+    const nodeKey = 'somenodekey';
+
+    initialStore.getState().session.mapping = {mapping: {importId: 'someimportID', lookups: [{name: 'someLookUpName', map: []}]}};
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row lookupName="someLookUpName" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>, {initialStore});
+
+    expect(screen.getByText('StaticLookupIcon')).toBeInTheDocument();
+  });
+  test('should show Dynamic Look UP icon when mapping is dynamic look up ', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row lookupName="someLookUpName" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    expect(screen.getByText('DynamicLookupIcon')).toBeInTheDocument();
+  });
+  test('should icon of hardcode when mapping is hardcoded ', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row hardCodedValue="somevalue" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    expect(screen.getByText('HardCodedIcon')).toBeInTheDocument();
+  });
+  test('should icon of isHandlebarExp when mapping is hardcoded ', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row extract="/" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    expect(screen.getByText('HandlebarsExpressionIcon')).toBeInTheDocument();
+  });
+  test('should show the title of required messahe when mapping is mandatory', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row isRequired extract="/" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    expect(screen.getByTitle('This field is required by the application you are importing into')).toBeInTheDocument();
+  });
+  test('should not show extract field when dataypye is of object and copysource is no', () => {
+    const nodeKey = 'somenodekey';
+
+    renderWithProviders(<ConfirmDialogProvider ><MemoryRouter><Mapper2Row dataType="object" nodeKey={nodeKey} >somechildren </Mapper2Row></MemoryRouter></ConfirmDialogProvider>);
+
+    expect(screen.queryByTestId('mapper2ExtractsTypeableSelect')).not.toBeInTheDocument();
+  });
+});
---
+++ src/components/DynaForm/fields/DynaNetSuiteQualifier_afe.test.js
@@ -0,0 +1,152 @@
+/* global describe, test, expect, jest, beforeEach, afterEach */
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter, Route } from 'react-router-dom';
+import * as reactRedux from 'react-redux';
+import DynaNetSuiteQualifieafe from './DynaNetSuiteQualifier_afe';
+import { renderWithProviders, reduxStore} from '../../../test/test-utils';
+import actions from '../../../actions';
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  __esModule: true,
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+const initialStore = reduxStore;
+
+initialStore.getState().session.metadata = {application: {someconnectionId: {somePath: {
+  data: [{name: 'someName', scriptId: 'once', doesNotSupportCreate: true}],
+}}}};
+
+const mockOnFieldChange = jest.fn();
+
+function initDynaNetSuiteQualifieafe(options = {}, value = 'providedValue') {
+  renderWithProviders(
+    <MemoryRouter initialEntries={['/integrations/none/flowBuilder/new-DOsWPJry5/edit/exports/new-NNW5LX']}>
+      <Route path="/integrations/none/flowBuilder/new-DOsWPJry5/edit/exports/new-NNW5LX">
+        <DynaNetSuiteQualifieafe
+          value={value}
+          defaultValue="someDefaultValue"
+          label="ProvidedLabel"
+          id="someID"
+          onFieldChange={mockOnFieldChange}
+          connectionId="someconnectionId"
+          selectOptions={[{}]}
+          options={options}
+          filterKey="suitescript-recordTypes"
+          errorMessages="someErrorMessage"
+          formKey="someFormKey"
+          flowId="someflowId"
+          resourceId="SomeresourceId"
+          resourceType="resourceType"
+          placeholder="someplaceholder"
+      />
+      </Route>
+    </MemoryRouter>);
+}
+
+describe('DynaNetSuiteQualifieafe test cases', () => {
+  let mockDispatchFn;
+  let useDispatchSpy;
+  let saveFunction;
+
+  beforeEach(() => {
+    useDispatchSpy = jest.spyOn(reactRedux, 'useDispatch');
+    mockDispatchFn = jest.fn(action => {
+      saveFunction = action.options.onSave;
+    });
+    useDispatchSpy.mockReturnValue(mockDispatchFn);
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  test('should call on field change function with json stringify value', async () => {
+    initDynaNetSuiteQualifieafe();
+
+    expect(screen.getByText('ProvidedLabel'));
+    const input = screen.getByPlaceholderText('someplaceholder');
+    const value = input.getAttribute('value');
+    const classOfInput = input.getAttribute('class');
+
+    expect(classOfInput.indexOf('Mui-disabled')).toBeGreaterThan(-1);
+    expect(value).toBe('providedValue');
+    expect(screen.getByText('someErrorMessage'));
+
+    const actionButton = screen.getByRole('button');
+
+    userEvent.click(actionButton);
+    expect(mockDispatchFn).toHaveBeenCalledWith(
+      actions.editor.init('someID', 'netsuiteQualificationCriteria', {
+        formKey: 'someFormKey',
+        flowId: 'someflowId',
+        resourceId: 'SomeresourceId',
+        resourceType: 'resourceType',
+        fieldId: 'someID',
+        data: 'dummy data',
+        onSave: saveFunction,
+        customOptions: {},
+      })
+    );
+    saveFunction({rule: 'somestring'});
+    expect(mockOnFieldChange).toHaveBeenCalledWith('someID', 'somestring');
+    expect(mockHistoryPush).toHaveBeenCalledWith('/integrations/none/flowBuilder/new-DOsWPJry5/edit/exports/new-NNW5LX/editor/someID');
+  });
+  test('should call on field change function with id and empty string when savefunction is called with empty array rule', async () => {
+    initDynaNetSuiteQualifieafe();
+    const actionButton = screen.getByRole('button');
+
+    userEvent.click(actionButton);
+    saveFunction({rule: ['arraydata']});
+    expect(mockOnFieldChange).toHaveBeenCalledWith('someID', JSON.stringify(['arraydata']));
+  });
+  test('should call on field change function with id and resetValue from useEffect when reset value is provided in options', () => {
+    initDynaNetSuiteQualifieafe({commMetaPath: 'somepath', resetValue: 'someresetvalue'});
+    expect(mockOnFieldChange).toHaveBeenCalledWith('someID', [], true);
+    const actionButton = screen.getByRole('button');
+
+    userEvent.click(actionButton);
+    saveFunction({rule: ['arraydata']});
+    expect(mockOnFieldChange).toHaveBeenCalledWith('someID', JSON.stringify(['arraydata']));
+  });
+  test('should call on field change function with id and default value from useEffect when reset value is not provided in options', () => {
+    initDynaNetSuiteQualifieafe({commMetaPath: 'somepath'});
+    expect(mockOnFieldChange).toHaveBeenCalledWith('someID', 'someDefaultValue', true);
+  });
+  test('should show stringified value when value is provided as an array', () => {
+    const array = [];
+
+    initDynaNetSuiteQualifieafe({commMetaPath: 'somepath'}, []);
+    const textbox = screen.getByPlaceholderText('someplaceholder');
+    const value = textbox.getAttribute('value');
+
+    expect(value).toEqual(JSON.stringify(array));
+  });
+  test('should show placeholder as value when value is null', () => {
+    initDynaNetSuiteQualifieafe({commMetaPath: 'somepath'}, null);
+    const textbox = screen.getByPlaceholderText('someplaceholder');
+    const value = textbox.getAttribute('value');
+
+    expect(value).toEqual('someplaceholder');
+  });
+  test('should show placeholder as "Define criteria" as a default placeholder', () => {
+    renderWithProviders(
+      <MemoryRouter initialEntries={['/integrations/none/flowBuilder/new-DOsWPJry5/edit/exports/new-NNW5LX']}>
+        <Route path="/integrations/none/flowBuilder/new-DOsWPJry5/edit/exports/new-NNW5LX">
+          <DynaNetSuiteQualifieafe
+            onFieldChange={mockOnFieldChange}
+            selectOptions={[{}]}
+            options={{}}
+        />
+        </Route>
+      </MemoryRouter>);
+    const textbox = screen.getByPlaceholderText('Define criteria');
+
+    expect(textbox).toBeInTheDocument();
+  });
+});
