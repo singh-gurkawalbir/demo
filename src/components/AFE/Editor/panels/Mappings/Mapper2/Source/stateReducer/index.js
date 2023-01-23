84e085298cf627b1c155a8f86baeb9bdba8d7694
Harsh Kumar <>
2023-01-23 13:29:43 +0530

    added immer

--- src/components/AFE/Editor/panels/Mappings/Mapper2/Source/stateReducer/index.js
+++ src/components/AFE/Editor/panels/Mappings/Mapper2/Source/stateReducer/index.js
@@ -1,28 +1,27 @@
-import { cloneDeep } from 'lodash';
+import produce from 'immer';
 
 export default function reducer(state, action) {
   const { type, value } = action;
-  const draft = cloneDeep(state);
 
-  switch (type) {
-    case 'onCursorChange':
-      draft.cursorPosition = value;
-      break;
-    case 'onTruncate':
-      draft.isTruncated = value;
-      break;
-    case 'setAnchorEL':
-      draft.anchorEl = value;
-      break;
-    case 'onFocused':
-      draft.isFocused = value;
-      break;
-    case 'onDataTypeSelector':
-      draft.dataTypeSelector = value;
-      break;
-    default:
-      break;
-  }
-
-  return draft;
+  return produce(state, draft => {
+    switch (type) {
+      case 'onCursorChange':
+        draft.cursorPosition = value;
+        break;
+      case 'onTruncate':
+        draft.isTruncated = value;
+        break;
+      case 'setAnchorEL':
+        draft.anchorEl = value;
+        break;
+      case 'onFocused':
+        draft.isFocused = value;
+        break;
+      case 'onDataTypeSelector':
+        draft.dataTypeSelector = value;
+        break;
+      default:
+        break;
+    }
+  });
 }
