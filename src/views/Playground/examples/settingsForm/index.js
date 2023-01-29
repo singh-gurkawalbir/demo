4f74b488d9a19babb3f7711617b1169ef9375023
ashok10m006@gmail.com <ashok10m006@gmail.com>
2023-01-29 06:59:45 +0530

    Added form conditions to dev play ground

--- src/views/Playground/examples/settingsForm/index.js
+++ src/views/Playground/examples/settingsForm/index.js
@@ -2,5 +2,6 @@ import simple from './simple';
 import column from './column';
 import fieldContainers from './fieldContainers';
 import fieldDictionary from './fieldDictionary';
+import conditions from './conditions';
 
-export default [simple, column, fieldContainers, fieldDictionary];
+export default [simple, column, fieldContainers, fieldDictionary, conditions];
