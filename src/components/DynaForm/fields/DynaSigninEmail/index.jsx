3b2fd6658289835a22b8150acb03164c05149006
Mohammed Azhar Uddin <azharweb90@gmail.com>
2023-02-26 03:05:44 +0530

    fixed the required messages

--- src/components/DynaForm/fields/DynaSigninEmail/index.jsx
+++ src/components/DynaForm/fields/DynaSigninEmail/index.jsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import actions from '../../../../actions';
 import { EMAIL_REGEX } from '../../../../constants';
-import messageStore, { message } from '../../../../utils/messageStore';
 import DynaText from '../DynaText';
 
 export default function DynaSigninEmail(props) {
@@ -11,26 +10,17 @@ export default function DynaSigninEmail(props) {
   const isValidEmail = EMAIL_REGEX.test(value);
 
   useEffect(() => {
-    if (!value) {
-      dispatch(actions.form.forceFieldState(formKey)(id, {isValid: false, errorMessages: errorMessage}));
-    }
     if (value) {
       if (isValidEmail) {
         dispatch(actions.form.forceFieldState(formKey)(id, {isValid: true}));
-      } else {
-        dispatch(actions.form.forceFieldState(formKey)(id, {
-          isValid: false,
-          errorMessages: message.USER_SIGN_IN.INVALID_EMAIL,
-        }));
+
+        return;
       }
-
-      return;
     }
-
-    dispatch(actions.form.forceFieldState(formKey)(id, {
-      isValid: false,
-      errorMessages: messageStore('SIGNIN_REQUIRED', {label: 'Email'}),
-    }));
+    dispatch(actions.form.forceFieldState(formKey)(id,
+      {isValid: false,
+        errorMessages: errorMessage,
+      }));
   }, [dispatch, errorMessage, formKey, id, isValidEmail, value]);
 
   // suspend force field state computation once the component turns invisible
