14d92961375854937dcb8a34aaa281645bb9c146
chaitanyareddy-mula <chaitanyareddy.mule@gmail.com>
2023-02-28 22:22:34 +0530

    review comments

--- src/utils/domPurify.js
+++ src/utils/domPurify.js
@@ -80,7 +80,7 @@ export default function getDomPurify(options = {}) {
     }
 
     // check all anchor tags which doesn't has href attribute and converting them to text
-    if (options.allowedTags && options.allowedTags.includes('a') && node.tagName === 'A' && !node.hasAttribute('href')) {
+    if (options?.allowedTags?.includes('a') && node.tagName === 'A' && !node.hasAttribute('href')) {
       DOMPurify.addHook('afterSanitizeElements', node => {
         if (node.tagName === 'A') {
           const text = document.createTextNode(node.textContent);
