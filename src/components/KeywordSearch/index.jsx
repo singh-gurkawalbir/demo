99416cea83c45c96f30329e43c4ec47610d01d5a
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2023-03-01 17:18:05 +0530

    Integrating SearchInput from CL

--- src/components/KeywordSearch/index.jsx
+++ src/components/KeywordSearch/index.jsx
@@ -1,47 +1,61 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useCallback, useEffect } from 'react';
+import {SearchInput} from '@celigo/fuse-ui';
+import { makeStyles } from '@material-ui/core/styles';
 import actions from '../../actions';
-import SearchInput from '../SearchInput';
 import { selectors } from '../../reducers';
-import HomeSearchInput from '../SearchInput/HomeSearchInput';
-import useDebouncedValue from '../../hooks/useDebouncedInput';
+
+const useStyles = makeStyles(theme => ({
+  homeSearchInput: {
+    marginRight: theme.spacing(1),
+    height: 38,
+  },
+  searchInput: {
+    width: 'auto',
+    '& .MuiInputBase-input': {
+      width: 70,
+      transition: theme.transitions.create('width'),
+    },
+    '& .MuiInputBase-input:focus': {
+      width: 200,
+    },
+  },
+}));
 
 export default function KeywordSearch({ filterKey, isHomeSearch, onFocus}) {
   const dispatch = useDispatch();
-  const filter =
-    useSelector(state => selectors.filter(state, filterKey));
+  const filter = useSelector(state => selectors.filter(state, filterKey));
+  const classes = useStyles();
 
-  const [text, setText] = useDebouncedValue(filter?.keyword || '', value => {
+  // when using the same filterkey patch to filter states and setText to text field will follow faithfully
+  // but if you change the filterkey we should pull in the new filterState value into the text field
+  useEffect(() => {
+    dispatch(
+      actions.patchFilter(filterKey, {
+        keyword: filter?.keyword || '',
+      })
+    );
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [filterKey]);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const handleKeywordChange = useCallback(value => {
     dispatch(
       actions.patchFilter(filterKey, {
         keyword: value,
       })
     );
-  });
+  }, [dispatch, filterKey]);
 
-  // when using the same filterkey patch to filter states and setText to text field will follow faithfully
-  // but if you change the filterkey we should pull in the new filterState value into the text field
-  useEffect(() => {
-    setText(filter?.keyword || '');
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [filterKey]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const handleKeywordChange = useCallback(e => {
-    setText(e.target.value);
-  }, [setText]);
-
-  return isHomeSearch ? (
-    <HomeSearchInput
-      value={text}
+  return (
+    <SearchInput
+      value={filter?.keyword || ''}
       onChange={handleKeywordChange}
-  />
-  )
-    : (
-      <SearchInput
-        value={text}
-        onChange={handleKeywordChange}
-        onFocus={onFocus}
+      onFocus={onFocus}
+      placeholder={isHomeSearch ? 'Search integrations & flows' : 'Search…'}
+      debounceDuration={300}
+      size={isHomeSearch ? 'large' : 'small'}
+      className={isHomeSearch ? classes.homeSearchInput : classes.searchInput}
     />
-    );
+  );
 }
