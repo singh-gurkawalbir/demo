e0b55499616d2b5b37e445d971b3a9a3149dc2fb
driedl <dave@riedl.ca>
2023-04-07 08:29:46 -0700

    cleaned up dube code, moved fieldMeta to seperate file

--- src/reducers/session/editors/processorLogic/handlebars/index.js
+++ src/reducers/session/editors/processorLogic/handlebars/index.js
@@ -2,6 +2,7 @@ import util from '../../../../../utils/json';
 import { isOldRestAdaptor, inferResourceType } from '../../../../../utils/resource';
 import { PAGING_FIELD_IDS } from '../../../../../utils/editor';
 import { GRAPHQL_JSON_FIELDS } from '../../../../../utils/graphql';
+import handlebarHelpersList from '../handlebarHelpersList';
 
 /* this util is used to read field label and generate editor title from it
 * eg, label = 'Build HTTP request body', editor title would be same i.e. 'Build HTTP request body'
@@ -275,57 +276,7 @@ export default {
         {
           role: 'user',
           content: `Restrict all handlebar helpers to the following list:
-abs: {{abs field}}
-add: {{add field field}}
-and: {{#and field field}} expr {{else}} expr {{/and}}
-avg: {{avg field field}}
-base64Encode: {{base64Encode field}}
-base64Decode: {{base64Decode field decodeFormat}}
-capitalize: {{capitalize field}}
-capitalizeAll: {{capitalizeAll field}}
-ceil: {{ceil field}}
-compare: {{#compare field operator field}} expr {{else}} expr {{/compare}}
-contains: {{#contains collection field}} expr {{else}} expr {{/contains}}
-dateAdd: {{dateAdd dateField offsetField timeZoneField}}
-dateFormat: {{dateFormat o/pformat date i/pformat timezone}}
-decodeURI: {{decodeURI field}}
-divide: {{divide field field}}
-each: {{#each field}}{{this}}{{/each}}
-encodeURI: {{encodeURI field}}
-floor: {{floor field}}
-getValue: {{getValue field "defaultValue"}}
-hash: {{hash algorithm encoding field}}
-hmac: {{hmac algorithm key encoding field keyEncoding}}
-if_else: {{#if field}} expr {{else}} expr {{/if}}
-ifEven: {{#ifEven field}} expr {{else}} expr {{/ifEven}}
-join: {{join delimiterField field1 field2}}
-jsonEncode: {{jsonEncode field}}
-jsonSerialize: {{jsonSerialize field}}
-lookup: {{lookup lookupName contextPath}}
-lowercase: {{lowercase field}}
-multiply: {{multiply field field}}
-neither: {{#neither field field}} expr {{else}} expr {{/neither}}
-or: {{#or field field}} expr {{else}} expr {{/or}}
-random: {{random “CRYPTO”/“UUID” length}}
-regexMatch: {{regexMatch field regex index options}}
-regexReplace: {{regexReplace field1 field2 regex options}}
-regexSearch: {{regexSearch field regex options}}
-replace: {{replace field string string}}
-round: {{round field}}
-sortnumbers: {{sort field number="true"}}
-sortstrings: {{sort field}}
-split: {{split field delimiter index}}
-substring: {{substring stringField startIndex endIndex}}
-subtract: {{subtract field field}}
-sum: {{sum <array>}}
-timestamp: {{timestamp format timezone}}
-toExponential: {{toExponential field fractionDigits}}
-toFixed: {{toFixed field digits}}
-toPrecision: {{toPrecision field precision}}
-trim: {{trim field}}
-unless: {{#unless field}} expr {{else}} expr {{/unless}}
-uppercase: {{uppercase field}}
-with: {{#with field}} {{field1}} {{field2}} {{/with}}`,
+${handlebarHelpersList}`,
         },
         {
           role: 'user',
