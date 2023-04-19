4c0e85f62aca26cd6c1bd74ee2a8f00999adafc6
PrashanthNesa <prashanthkumar.nesa@gmail.com>
2023-04-19 14:57:27 +0530

    Revert "IO-34492 patch pointed"
    This reverts commit a7d216e93aab338df5c98b1da37b291121c109e4.

--- src/components/CeligoTable/ActionMenu/MultipleAction/index.jsx
+++ src/components/CeligoTable/ActionMenu/MultipleAction/index.jsx
@@ -1,17 +1,8 @@
 import { MenuItem, Tooltip } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 import React from 'react';
-import clsx from 'clsx';
 import { useGetAllActionProps } from '../hooks';
 
-const useStyles = makeStyles(theme => ({
-  deleteWrapper: {
-    color: theme.palette.error.dark,
-  },
-}));
-
 const MultipleAction = ({rowData, handleMenuClose, setSelectedComponent, meta}) => {
-  const classes = useStyles();
   const {key} = meta;
   const {
     handleActionClick,
@@ -31,7 +22,8 @@ const MultipleAction = ({rowData, handleMenuClose, setSelectedComponent, meta}) 
       <Tooltip title={disabledActionTitle} placement="bottom" >
         <div>
           <MenuItem data-test={key} disabled>
-            {actionIcon}{label}
+            {actionIcon}
+            {label}
           </MenuItem>
         </div>
       </Tooltip>
@@ -39,8 +31,9 @@ const MultipleAction = ({rowData, handleMenuClose, setSelectedComponent, meta}) 
   }
 
   return (
-    <MenuItem className={clsx({[classes.deleteWrapper]: key === 'delete'})} data-test={key} onClick={handleActionClick} disabled={disabled}>
-      {actionIcon}{label}
+    <MenuItem data-test={key} onClick={handleActionClick} disabled={disabled}>
+      {actionIcon}
+      {label}
     </MenuItem>
   );
 };
