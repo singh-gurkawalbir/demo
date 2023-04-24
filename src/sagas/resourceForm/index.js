86fdb9e6418e62bd354e7024f0b61a1a549aee5b
Snigdha-R-Panigrahy <snigdharani.panigrahy712@gmail.com>
2023-04-24 13:13:23 +0530

    update

--- src/sagas/resourceForm/index.js
+++ src/sagas/resourceForm/index.js
@@ -21,7 +21,7 @@ import {
 import { _fetchRawDataForFileAdaptors } from '../sampleData/rawDataUpdates/fileAdaptorUpdates';
 import { fileTypeToApplicationTypeMap } from '../../utils/file';
 import { uploadRawData } from '../uploadFile';
-import { UI_FIELD_VALUES, FORM_SAVE_STATUS, emptyObject, CONNECTORS_TO_IGNORE, EMPTY_RAW_DATA, PageProcessorRegex } from '../../constants';
+import { UI_FIELD_VALUES, FORM_SAVE_STATUS, emptyObject, EMPTY_RAW_DATA, PageProcessorRegex } from '../../constants';
 import { isIntegrationApp, isFlowUpdatedWithPgOrPP, shouldUpdateLastModified, flowLastModifiedPatch } from '../../utils/flows';
 import getResourceFormAssets from '../../forms/formFactory/getResourceFromAssets';
 import getFieldsWithDefaults from '../../forms/formFactory/getFieldsWithDefaults';
@@ -890,7 +890,7 @@ export function* initFormValues({
   } else if (resourceType === 'exports') {
     httpPublishedConnector = getHttpConnector(resource?._httpConnectorId || resource?.webhook?._httpConnectorId);
   } else if (resourceType === 'iClients') {
-    const applications = applicationsList().filter(app => !CONNECTORS_TO_IGNORE.includes(app.id));
+    const applications = applicationsList().filter(app => app._httpConnectorId);
     let app;
 
     if (resource?.application) {
