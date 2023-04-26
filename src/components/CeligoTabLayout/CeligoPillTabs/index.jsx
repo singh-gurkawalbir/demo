d1e14451c4589bc993a54497a543484b1e4ab4fe
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2023-04-27 00:36:05 +0530

    PillTabGroup integration

--- src/components/CeligoTabLayout/CeligoPillTabs/index.jsx
+++ src/components/CeligoTabLayout/CeligoPillTabs/index.jsx
@@ -1,44 +1,7 @@
 import React, { useEffect } from 'react';
 import { Box } from '@mui/material';
-import TextToggle from '../../TextToggle';
+import { PillTabGroup } from '@celigo/fuse-ui';
 import { useTabContext } from '../CeligoTabWrapper';
-
-const toggleButtonCss = {
-  backgroundColor: theme => theme.palette.background.paper,
-  border: '1px solid',
-  padding: 0,
-  borderColor: theme => theme.palette.secondary.lightest,
-  maxWidth: '100%',
-  '& > button': {
-    height: 30,
-    whiteSpace: 'nowrap',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    padding: theme => theme.spacing(0, 5),
-    '&:first-child': {
-      borderRadius: props => props.tabs?.length > 1 ? [[24, 0, 0, 24]] : 'none',
-    },
-    '&:last-child': {
-      height: 30,
-      padding: theme => theme.spacing(0, 5),
-      borderTopRightRadius: '24px !important',
-      borderBottomRightRadius: '24px !important',
-    },
-    '&.Mui-selected': {
-      backgroundColor: theme => theme.palette.primary.main,
-      '&:hover': {
-        backgroundColor: theme => theme.palette.primary.light,
-      },
-    },
-    '&:not(:first-child)': {
-      borderLeft: theme => `1px solid ${theme.palette.secondary.lightest}`,
-      borderRadius: 0,
-    },
-    '&:hover': {
-      background: theme => theme.palette.background.paper2,
-    },
-  },
-};
 
 export default function CeligoPillTabs(props) {
   const { tabs, defaultTab } = props;
@@ -54,9 +17,8 @@ export default function CeligoPillTabs(props) {
 
   return (
     <Box textAlign="center" position="relative">
-      <TextToggle
+      <PillTabGroup
         value={activeTab}
-        className={toggleButtonCss}
         onChange={setActiveTab}
         exclusive
         options={tabs}
