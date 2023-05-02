dba5042032f438a676663d3113259c354ead4300
PrashanthNesa <prashanthkumar.nesa@gmail.com>
2023-05-02 10:58:45 +0530

    adornment added

--- src/components/DynaForm/fields/DynaEditable/index.jsx
+++ src/components/DynaForm/fields/DynaEditable/index.jsx
@@ -1,11 +1,13 @@
-import { TextField, makeStyles, Typography, IconButton, Paper } from '@material-ui/core';
+import { TextField, InputAdornment, Button, FormControl, FormLabel, makeStyles, Typography, IconButton, Paper } from '@material-ui/core';
+// import FormControl from '@material-ui/core/FormControl';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import React, { useContext, useState } from 'react';
 import isLoggableAttr from '../../../../utils/isLoggableAttr';
-import TextButton from '../../../Buttons/TextButton';
 import AddIcon from '../../../icons/AddIcon';
 import EditIcon from '../../../icons/EditIcon';
 import SearchIcon from '../../../icons/SearchIcon';
+import FieldHelp from '../../FieldHelp';
+import FieldMessage from '../FieldMessage';
 
 const useStyles = makeStyles(theme => ({
   fieldWrapper: {
@@ -51,26 +53,32 @@ const Option = option => {
   return (<><Typography>{option?.label}</Typography><IconButton onClick={() => onEditClick(option.value)}><EditIcon /></IconButton></>);
 };
 
-const Component = props => {
-  const {classes} = props;
+const PaperComponentCustom = options => {
+  const classes = useStyles();
+  const { containerProps, children } = options;
   const data = useContext(DropdownContext);
   const {onCreateClick} = data;
 
   return (
-    <Paper>
-      {props.children}
-      <TextButton
-        startIcon={<AddIcon />}
-        className={classes?.createButton}
-        onClick={onCreateClick}>
-        Create connection
-      </TextButton>
+    <Paper className={classes.paper} {...containerProps}>
+      {children}
+      {(
+        <Button
+          fullWidth
+          color="primary"
+          className={classes.buttonBackground}
+          onMouseDown={event => { event.preventDefault(); onCreateClick(); }}
+        >
+          <AddIcon />
+          <Typography >Create connection</Typography>
+        </Button>
+      )}
     </Paper>
   );
 };
 
 export default function DynaEditable(props) {
-  const {options, id, onFieldChange, value, onCreateClick, onEditClick} = props;
+  const {options, id, onFieldChange, value, required, isValid, label, disabled, removeHelperText, onCreateClick, onEditClick} = props;
   const classes = useStyles();
   const selectedValue = options.find(option => option.value === value)?.label;
   const [inputValue, setInputValue] = useState(selectedValue);
@@ -80,31 +88,60 @@ export default function DynaEditable(props) {
     allOptions: options, onEditClick, onCreateClick, classes,
   };
 
-  const handleInputChange = (evt, newVal) => setInputValue(newVal);
+  const handleInputChange = (evt, newVal) => { setIsMenuOpen('true'); setInputValue(newVal); };
 
   return (
     <div>
-      <DropdownContext.Provider value={dropdownProps}>
-        <Autocomplete
-          disablePortal
-          id="combo-box-demo"
-          options={options}
-          getOptionLabel={option => option?.label}
-          renderOption={Option}
-          value={renderValue}
-          popupIcon={<SearchIcon />}
-          open={isMenuOpen}
-          inputValue={inputValue}
-          onInputChange={handleInputChange}
-          onFocus={() => setIsMenuOpen(true)}
-          onClose={() => setIsMenuOpen(false)}
-          onBlur={() => { setIsMenuOpen(false); setInputValue(selectedValue); }}
-          onChange={(event, newValue) => { setIsMenuOpen(false); setRenderValue(newValue?.label); onFieldChange(id, newValue?.value); }}
-          className={classes.inputTextField}
-          PaperComponent={Component}
-          {...isLoggableAttr(true)}
-          renderInput={params => <TextField {...params} variant="filled" className={classes.textareaInput} />} />
-      </DropdownContext.Provider>
+      <div>
+        <FormLabel htmlFor={id} required={required} error={!isValid}>
+          {label}
+        </FormLabel>
+        <FieldHelp {...props} />
+      </div>
+      <FormControl
+        key={id}
+        disabled={disabled}
+        required={required}
+        fullWidth>
+        <DropdownContext.Provider value={dropdownProps}>
+          <Autocomplete
+            disablePortal
+            id="combo-box-demo"
+            options={options}
+            getOptionLabel={option => option?.label}
+            renderOption={Option}
+            value={renderValue}
+            disableClearable
+            forcePopupIcon={false}
+            open={isMenuOpen}
+            inputValue={inputValue}
+            onInputChange={handleInputChange}
+            onFocus={() => setIsMenuOpen(true)}
+            onClose={() => setIsMenuOpen(false)}
+            onBlur={() => { setIsMenuOpen(false); setInputValue(selectedValue); }}
+            onChange={(event, newValue) => { setIsMenuOpen(false); setRenderValue(newValue?.label); onFieldChange(id, newValue?.value); }}
+            className={classes.inputTextField}
+            PaperComponent={PaperComponentCustom}
+            {...isLoggableAttr(true)}
+            renderInput={params => (
+              <TextField
+                {...params}
+                variant="filled"
+                className={classes.textareaInput}
+                fullWidth
+                InputProps={{
+                  ...params.InputProps,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <SearchIcon />
+                    </InputAdornment>
+                  ),
+                }}
+             />
+            )} />
+        </DropdownContext.Provider>
+        {!removeHelperText && <FieldMessage {...props} />}
+      </FormControl>
     </div>
   );
 }
