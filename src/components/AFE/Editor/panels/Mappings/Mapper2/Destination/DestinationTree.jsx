a3c39fc6cefa206bd988e5d54fcf402c4837d8bd
Mohammed Azhar Uddin <azharweb90@gmail.com>
2023-05-03 21:06:32 +0530

    fixed the spacing left from the dropdown destination field

--- src/components/AFE/Editor/panels/Mappings/Mapper2/Destination/DestinationTree.jsx
+++ src/components/AFE/Editor/panels/Mappings/Mapper2/Destination/DestinationTree.jsx
@@ -34,7 +34,7 @@ const useStyles = makeStyles(theme => ({
       display: 'flex',
     },
     '& .childTree-indent-unit': {
-      width: theme.spacing(3),
+      width: theme.spacing(1.6),
       position: 'relative',
       height: '100%',
     },
