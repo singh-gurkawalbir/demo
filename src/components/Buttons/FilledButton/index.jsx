a7f5217a6eb19471277df636b7a70e09a05fea49
Kaushik Nandan Uyyala <kaushiknandan32@gmail.com>
2023-05-22 16:42:13 +0530

    Remove integrated buttons

--- src/components/Buttons/FilledButton/index.jsx
+++ src/components/Buttons/FilledButton/index.jsx
@@ -1,36 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
-// eslint-disable-next-line import/no-extraneous-dependencies
-import Button from '@mui/material/Button';
-import makeStyles from '@mui/styles/makeStyles';
-import PropTypes from 'prop-types';
 import { FilledButton as FuseUiFilledButton } from '@celigo/fuse-ui';
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    fontFamily: props => props.bold ? 'Roboto500' : 'Roboto400',
-  },
-  error: {
-    background: theme.palette.error.main,
-    borderColor: theme.palette.error.main,
-    color: theme.palette.background.paper,
-    '&:hover': {
-      background: theme.palette.error.dark,
-      borderColor: theme.palette.error.main,
-    },
-    '&:focus': {
-      background: theme.palette.error.dark,
-      borderColor: theme.palette.error.main,
-    },
-  },
-  submitBtn: {
-    width: '100%',
-    borderRadius: 4,
-    height: 38,
-    fontSize: theme.spacing(2),
-    marginTop: theme.spacing(1),
-  },
-}));
 
 export function SubmitButton({sx, ...rest}) {
   return (
@@ -49,31 +18,3 @@ export function SubmitButton({sx, ...rest}) {
     />
   );
 }
-
-export default function FilledButton(props) {
-  const classes = useStyles(props);
-  const { error, bold, className, submit, ...rest} = props;
-
-  return (
-    <Button
-      variant="contained"
-      className={clsx(classes.root, {[classes.error]: error}, {[classes.submitBtn]: submit}, className)}
-      {...rest} />
-  );
-}
-
-FilledButton.propTypes = {
-  disabled: PropTypes.bool,
-  children: PropTypes.node.isRequired,
-  size: PropTypes.oneOf(['small', 'medium', 'large']),
-  error: PropTypes.bool,
-  color: PropTypes.oneOf(['primary', 'secondary']),
-  bold: PropTypes.bool,
-};
-
-FilledButton.defaultProps = {
-  color: 'primary',
-  size: 'medium',
-  disabled: false,
-  error: false,
-};
